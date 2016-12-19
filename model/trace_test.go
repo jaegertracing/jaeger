3aed44642d47b0c3c19d602e62dd871885967c19
Yuri Shkuro <ys@uber.com>
2016-12-19 15:46:56 -0500

    go fmt

--- model/trace_test.go
+++ model/trace_test.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package model_test
 
 import (
@@ -11,9 +31,9 @@ import (
 func TestTraceFindSpanByID(t *testing.T) {
 	trace := &model.Trace{
 		Spans: []*model.Span{
-			&model.Span{SpanID: model.SpanID(1), OperationName: "x"},
-			&model.Span{SpanID: model.SpanID(2), OperationName: "y"},
-			&model.Span{SpanID: model.SpanID(1), OperationName: "z"}, // same span ID
+			{SpanID: model.SpanID(1), OperationName: "x"},
+			{SpanID: model.SpanID(2), OperationName: "y"},
+			{SpanID: model.SpanID(1), OperationName: "z"}, // same span ID
 		},
 	}
 	s1 := trace.FindSpanByID(model.SpanID(1))
