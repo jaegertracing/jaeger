e9080097c610e13799a30ed12a3cfc290ab7c3df
Won Jun Jang <wjang@uber.com>
2017-04-10 17:30:11 -0400

    address comments

--- travis/build-all-in-one-image.sh
+++ travis/build-all-in-one-image.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Only build docker container for master branch
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+set -ex
 
 export REPO=jaegertracing/all-in-one
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo "${BRANCH///}"; fi`
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, PR=$PR, BRANCH=$BRANCH, TAG=$TAG"
 
-make build-all-in-one-linux || { echo 'failed build-all-in-one-linux' ; exit 0; }
+make build-all-in-one-linux
+
+# Only build push the docker container to Docker Hub for master branch
+if [[ "$TRAVIS_PULL_REQUEST" == "false"  || "$BRANCH" == "master" ]]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f cmd/standalone/Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
