899c1b86d70f89282bf03b345d9ebdde99a0fe47
Won Jun Jang <wonjun.jangobe@gmail.com>
2017-04-11 13:34:02 -0400

    Only upload to docker hub on master branch (#94)

--- travis/build-all-in-one-image.sh
+++ travis/build-all-in-one-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 export REPO=jaegertracing/all-in-one
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
@@ -10,9 +10,12 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, PR=$PR, BRANCH=$BRANCH, TAG=$TAG
 make build-all-in-one-linux
 
 # Only push the docker container to Docker Hub for master branch
-if [[ "$TRAVIS_PULL_REQUEST" == "false"  || "$BRANCH" == "master" ]]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+set -x
+
 docker build -f cmd/standalone/Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
