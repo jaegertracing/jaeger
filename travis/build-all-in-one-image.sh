e2f80c4a447dbef5ee16bafaacad57ccb785d71a
Won Jun Jang <wjang@uber.com>
2017-04-11 14:18:29 -0400

    update jaeger-ui version

--- travis/build-all-in-one-image.sh
+++ travis/build-all-in-one-image.sh
@@ -10,7 +10,7 @@ echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, PR=$PR, BRANCH=$BRANCH, TAG=$TAG
 make build-all-in-one-linux
 
 # Only push the docker container to Docker Hub for master branch
-if [[ "$TRAVIS_PULL_REQUEST" == "false"  || "$BRANCH" == "master" ]]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+#if [[ "$TRAVIS_PULL_REQUEST" == "false"  || "$BRANCH" == "master" ]]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f cmd/standalone/Dockerfile -t $REPO:$COMMIT .
