a1e295ebd44b11b6440c693429a47aaf77d3d1a1
Won Jun Jang <wjang@uber.com>
2017-04-14 10:15:44 -0400

    fixes

--- travis/install-ui-deps.sh
+++ travis/install-ui-deps.sh
@@ -10,6 +10,8 @@ sudo apt-key adv --keyserver pgp.mit.edu --recv D101F7899D41F3C3
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -qq
 sudo apt-get install -y -qq yarn
+
+source ~/.nvm/nvm.sh
 nvm install 6
 
 yarn --version
