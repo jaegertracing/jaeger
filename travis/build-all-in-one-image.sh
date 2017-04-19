db1a4bc5b9833ca6a7c29013e443fbb5bd9aaed6
Won Jun Jang <wjang@uber.com>
2017-04-19 11:44:35 -0400

    test upload to docker

--- travis/build-all-in-one-image.sh
+++ travis/build-all-in-one-image.sh
@@ -10,6 +10,8 @@ export REPO=jaegertracing/all-in-one
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 # Only push the docker container to Docker Hub for master branch
-if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+#if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+
+docker build -f cmd/standalone/Dockerfile -t $REPO:$COMMIT .
 
 ./travis/upload-to-docker.sh
--- travis/build-crossdock.sh
+++ travis/build-crossdock.sh
@@ -8,7 +8,7 @@ export REPO=jaegertracing/test-driver
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 # Only push the docker container to Docker Hub for master branch
-if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
+#if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
 
 docker build -f crossdock/Dockerfile -t $REPO:$COMMIT .
 
