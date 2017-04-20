8d736af8cc3a8cc365f5033acef36c8cfbdeb7cf
Won Jun Jang <wonjun.jangobe@gmail.com>
2017-04-20 11:44:45 -0400

    Upload test driver (#120)

--- travis/build-crossdock.sh
+++ travis/build-crossdock.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-source ~/.nvm/nvm.sh
-nvm use 6
-make build-all-in-one-linux
+make build-crossdock
 
-export REPO=jaegertracing/all-in-one
+export REPO=jaegertracing/test-driver
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 # Only push the docker container to Docker Hub for master branch
 if [ "$BRANCH" == "master" ]; then echo 'upload to Docker Hub'; else echo 'skip docker upload for PR'; exit 0; fi
 
-docker build -f cmd/standalone/Dockerfile -t $REPO:$COMMIT .
+docker build -f crossdock/Dockerfile -t $REPO:$COMMIT .
 
 bash ./travis/upload-to-docker.sh
