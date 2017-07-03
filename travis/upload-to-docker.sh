5909ff225ccbd1a7ef8a00d9aff1b48538be9df6
Yuri Shkuro <ys@uber.com>
2017-07-03 15:34:59 -0400

    Push to Docker Hub on release tags
    * Allow Docker Hub push if there is a release tag in the v#.#.# format
    * Add alias tags `major` and `major.minor`

--- travis/upload-to-docker.sh
+++ travis/upload-to-docker.sh
@@ -2,13 +2,39 @@
 
 set -e
 
-export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo "${BRANCH///}"; fi`
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, PR=$PR, BRANCH=$BRANCH, TAG=$TAG"
+BRANCH=${BRANCH:?'missing BRANCH env var'}
+IMAGE="${REPO:?'missing REPO env var'}:${COMMIT:?'missing COMMIT env var'}"
 
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+unset major minor patch
+if [[ "$BRANCH" == "master" ]]; then
+  TAG="latest"
+elif [[ $BRANCH =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  patch="${BASH_REMATCH[3]}"
+  TAG=${major}.${minor}.${patch}
+  echo "BRANCH is a release tag: major=$major, minor=$minor, patch=$patch"
+else
+  # TODO why do we do /// ?
+  TAG="${BRANCH///}"
+fi
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, BRANCH=$BRANCH, TAG=$TAG, IMAGE=$IMAGE"
 
+DOCKER=docker
+$DOCKER login -u $DOCKER_USER -p $DOCKER_PASS
+
+# Do not enable echo before the `docker login` command to avoid revealing the password.
 set -x
 
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
+$DOCKER tag $IMAGE $REPO:$TAG
+$DOCKER tag $IMAGE $REPO:travis-$TRAVIS_BUILD_NUMBER
+# add major and major.minor as aliases
+if [[ -n $major ]]; then
+  $DOCKER tag $IMAGE $REPO:$major
+  if [[ -n $minor ]]; then
+    $DOCKER tag $IMAGE $REPO:${major}.${minor}
+  fi
+fi
+
+# TOOO why are we pushing as $REPO instead of $IMAGE?
+$DOCKER push $REPO
