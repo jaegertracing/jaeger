06d2710557258d6201f979bb6d3a453ecfccf57b
Min Ho Park <minho.park@uber.com>
2017-07-12 15:04:20 -0400

    testing travis build

--- travis/es-integration-test.sh
+++ travis/es-integration-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 docker pull docker.elastic.co/elasticsearch/elasticsearch:5.4.0
 export CID=$(docker run -d -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:5.4.0)
 export ES_INTEGRATION_TEST=test
