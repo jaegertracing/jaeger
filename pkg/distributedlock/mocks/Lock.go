9275e34fe400436c367560c98232bc83c562463c
isaach1000 <isaachier@gmail.com>
2017-08-21 10:24:46 -0400

    Implement forfeit lock. (#345)

--- pkg/distributedlock/mocks/Lock.go
+++ pkg/distributedlock/mocks/Lock.go
@@ -49,3 +49,23 @@ func (_m *Lock) Acquire(resource string, ttl time.Duration) (bool, error) {
 
 	return r0, r1
 }
+
+func (_m *Lock) Forfeit(resource string) (bool, error) {
+	ret := _m.Called(resource)
+
+	var r0 bool
+	if rf, ok := ret.Get(0).(func(string) bool); ok {
+		r0 = rf(resource)
+	} else {
+		r0 = ret.Get(0).(bool)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(resource)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
