54151f880ef18d7aebf07215275092063c908df6
Pavol Loffay <p.loffay@gmail.com>
2017-10-09 18:51:47 +0200

    Remove unused flags (#463)
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- cmd/flags/flags.go
+++ cmd/flags/flags.go
@@ -31,10 +31,8 @@ const (
 	MemoryStorageType = "memory"
 	// ESStorageType is the storage type flag denoting an ElasticSearch backing store
 	ESStorageType                  = "elasticsearch"
-	runtimeMetricsFrequency        = "runtime-metrics-frequency"
 	spanStorageType                = "span-storage.type"
 	logLevel                       = "log-level"
-	dependencyStorageType          = "dependency-storage.type"
 	dependencyStorageDataFrequency = "dependency-storage.data-frequency"
 )
 
@@ -49,17 +47,14 @@ type SharedFlags struct {
 // InitFromViper initializes SharedFlags with properties from viper
 func (flags *SharedFlags) InitFromViper(v *viper.Viper) *SharedFlags {
 	flags.SpanStorage.Type = v.GetString(spanStorageType)
-	flags.DependencyStorage.Type = v.GetString(dependencyStorageType)
 	flags.DependencyStorage.DataFrequency = v.GetDuration(dependencyStorageDataFrequency)
 	return flags
 }
 
 // AddFlags adds flags for SharedFlags
 func AddFlags(flagSet *flag.FlagSet) {
-	flagSet.Duration(runtimeMetricsFrequency, 1*time.Second, "The frequency of reporting Go runtime metrics")
 	flagSet.String(spanStorageType, CassandraStorageType, fmt.Sprintf("The type of span storage backend to use, options are currently [%v,%v,%v]", CassandraStorageType, ESStorageType, MemoryStorageType))
 	flagSet.String(logLevel, "info", "Minimal allowed log level")
-	flagSet.String(dependencyStorageType, CassandraStorageType, fmt.Sprintf("The type of dependency storage backend to use, options are currently [%v,%v,%v]", CassandraStorageType, ESStorageType, MemoryStorageType))
 	flagSet.Duration(dependencyStorageDataFrequency, time.Hour*24, "Frequency of service dependency calculations")
 }
 
