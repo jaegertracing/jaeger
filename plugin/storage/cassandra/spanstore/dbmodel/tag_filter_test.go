d5757cd7767accc60fa8bafb557baef63fb2e125
Won Jun Jang <wonjun.jangobe@gmail.com>
2017-12-21 13:48:15 -0500

    Add ChainedTagFilter (#607)
    Signed-off-by: GRECO, FRANK <frankgreco@northwesternmutual.com>

--- plugin/storage/cassandra/spanstore/dbmodel/tag_filter_test.go
+++ plugin/storage/cassandra/spanstore/dbmodel/tag_filter_test.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/kr/pretty"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/jaegertracing/jaeger/model"
 )
 
 func TestDefaultTagFilter(t *testing.T) {
@@ -29,8 +31,47 @@ func TestDefaultTagFilter(t *testing.T) {
 	for _, log := range span.Logs {
 		filteredTags = append(filteredTags, DefaultTagFilter.FilterLogFields(log.Fields)...)
 	}
-	if !assert.EqualValues(t, expectedTags, filteredTags) {
-		for _, diff := range pretty.Diff(expectedTags, filteredTags) {
+	compareTags(t, expectedTags, filteredTags)
+}
+
+type onlyStringsFilter struct{}
+
+func (f onlyStringsFilter) filterStringTags(tags model.KeyValues) model.KeyValues {
+	var ret model.KeyValues
+	for _, tag := range tags {
+		if tag.VType == model.StringType {
+			ret = append(ret, tag)
+		}
+	}
+	return ret
+}
+
+func (f onlyStringsFilter) FilterProcessTags(processTags model.KeyValues) model.KeyValues {
+	return f.filterStringTags(processTags)
+}
+
+func (f onlyStringsFilter) FilterTags(tags model.KeyValues) model.KeyValues {
+	return f.filterStringTags(tags)
+}
+
+func (f onlyStringsFilter) FilterLogFields(logFields model.KeyValues) model.KeyValues {
+	return f.filterStringTags(logFields)
+}
+
+func TestChainedTagFilter(t *testing.T) {
+	expectedTags := model.KeyValues{model.String(someStringTagKey, someStringTagValue)}
+	filter := NewChainedTagFilter(DefaultTagFilter, onlyStringsFilter{})
+	filteredTags := filter.FilterProcessTags(someTags)
+	compareTags(t, expectedTags, filteredTags)
+	filteredTags = filter.FilterTags(someTags)
+	compareTags(t, expectedTags, filteredTags)
+	filteredTags = filter.FilterLogFields(someTags)
+	compareTags(t, expectedTags, filteredTags)
+}
+
+func compareTags(t *testing.T, expected, actual model.KeyValues) {
+	if !assert.EqualValues(t, expected, actual) {
+		for _, diff := range pretty.Diff(expected, actual) {
 			t.Log(diff)
 		}
 	}
