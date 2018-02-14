d5250a66d4249e308fe6b03ceb36321dd0a8eb5d
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2018-02-14 16:30:35 -0200

    Copy only Hotrod binary to Dockerfile
    - exposing more ports on Dockerfile
    and copy only binary to Dockerfile
    
    Signed-off-by: Guilherme Baufaker Rêgo <gbaufake@redhat.com>

--- examples/hotrod/create-image.sh
+++ examples/hotrod/create-image.sh
@@ -1,5 +1,5 @@
 cd $GOPATH/src/github.com/jaegertracing/jaeger
 make install
 cd $GOPATH/src/github.com/jaegertracing/jaeger/examples/hotrod
-CGO_ENABLED=0 GOOS=linux installsuffix=cgo go build -o collector-linux main.go
+CGO_ENABLED=0 GOOS=linux installsuffix=cgo go build -o hotrod-linux main.go
 docker build -t jaegertracing/hotrod -f Dockerfile .
