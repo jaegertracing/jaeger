fd5dddda15006a797863a829ee4ba75f41791c70
Won Jun Jang <wonjun.jangobe@gmail.com>
2018-06-20 15:56:16 -0400

    Add cassandra index filter (#876)

--- plugin/storage/cassandra/spanstore/writer_test.go
+++ plugin/storage/cassandra/spanstore/writer_test.go
@@ -314,6 +314,30 @@ func TestStorageMode_IndexOnly(t *testing.T) {
 	}, StoreIndexesOnly())
 }
 
+var filterEverything = func(*dbmodel.Span, int) bool {
+	return false
+}
+
+func TestStorageMode_IndexOnly_WithFilter(t *testing.T) {
+	withSpanWriter(0, func(w *spanWriterTest) {
+		w.writer.indexFilter = filterEverything
+		w.writer.serviceNamesWriter = func(serviceName string) error { return nil }
+		w.writer.operationNamesWriter = func(serviceName, operationName string) error { return nil }
+		span := &model.Span{
+			TraceID: model.NewTraceID(0, 1),
+			Process: &model.Process{
+				ServiceName: "service-a",
+			},
+		}
+		err := w.writer.WriteSpan(span)
+		assert.NoError(t, err)
+		w.session.AssertExpectations(t)
+		w.session.AssertNotCalled(t, "Query", stringMatcher(serviceOperationIndex))
+		w.session.AssertNotCalled(t, "Query", stringMatcher(serviceNameIndex))
+		w.session.AssertNotCalled(t, "Query", stringMatcher(durationIndex))
+	}, StoreIndexesOnly())
+}
+
 func TestStorageMode_StoreWithoutIndexing(t *testing.T) {
 	withSpanWriter(0, func(w *spanWriterTest) {
 
