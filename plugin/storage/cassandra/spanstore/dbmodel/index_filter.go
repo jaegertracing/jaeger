e30ca8a95aad5f3dcf28565aac9287f727e526ee
Won Jun Jang <wjang@uber.com>
2018-06-20 13:27:05 -0400

    stupid comments are hard
    Signed-off-by: Won Jun Jang <wjang@uber.com>

--- plugin/storage/cassandra/spanstore/dbmodel/index_filter.go
+++ plugin/storage/cassandra/spanstore/dbmodel/index_filter.go
@@ -15,20 +15,20 @@
 package dbmodel
 
 const (
-	// DurationIndex represents the flag for indexing by duration
+	// DurationIndex represents the flag for indexing by duration.
 	DurationIndex = iota
 
-	// ServiceIndex represents the flag for indexing by service
+	// ServiceIndex represents the flag for indexing by service.
 	ServiceIndex
 
-	// OperationIndex represents the flag for indexing by service-operation
+	// OperationIndex represents the flag for indexing by service-operation.
 	OperationIndex
 )
 
-// IndexFilter filters out any spans that should not be indexed.
+// IndexFilter filters out any spans that should not be indexed depending on the index specified.
 type IndexFilter func(span *Span, index int) bool
 
-// DefaultIndexFilter returns a filter that indexes everything.
+// DefaultIndexFilter is a filter that indexes everything.
 var DefaultIndexFilter = func(span *Span, index int) bool {
 	return true
 }
