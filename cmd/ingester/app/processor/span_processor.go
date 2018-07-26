8f7e497b70f90f4d20975cb3945618fb5c716d04
David Yeghshatyan <6davidy@gmail.com>
2018-07-26 14:21:21 -0400

    Add ingester processor to unmarshal and write spans  (#944)
    Unmarshal consumed messages into spans and write them

--- cmd/ingester/app/processor/span_processor.go
+++ cmd/ingester/app/processor/span_processor.go
@@ -16,9 +16,14 @@ package processor
 
 import (
 	"io"
+
+	"github.com/pkg/errors"
+
+	"github.com/jaegertracing/jaeger/plugin/storage/kafka"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
 )
 
-//go:generate mockery -name=SpanProcessor
+//go:generate mockery -name=KafkaSpanProcessor
 
 // SpanProcessor processes kafka spans
 type SpanProcessor interface {
@@ -29,4 +34,34 @@ type SpanProcessor interface {
 // Message contains the fields of the kafka message that the span processor uses
 type Message interface {
 	Value() []byte
+}
+
+// SpanProcessorParams stores the necessary parameters for a SpanProcessor
+type SpanProcessorParams struct {
+	Writer       spanstore.Writer
+	Unmarshaller kafka.Unmarshaller
+}
+
+// KafkaSpanProcessor implements SpanProcessor for Kafka messages
+type KafkaSpanProcessor struct {
+	unmarshaller kafka.Unmarshaller
+	writer       spanstore.Writer
+	io.Closer
+}
+
+// NewSpanProcessor creates a new KafkaSpanProcessor
+func NewSpanProcessor(params SpanProcessorParams) *KafkaSpanProcessor {
+	return &KafkaSpanProcessor{
+		unmarshaller: params.Unmarshaller,
+		writer:       params.Writer,
+	}
+}
+
+// Process unmarshals and writes a single kafka message
+func (s KafkaSpanProcessor) Process(message Message) error {
+	mSpan, err := s.unmarshaller.Unmarshal(message.Value())
+	if err != nil {
+		return errors.Wrap(err, "cannot unmarshall byte array into span")
+	}
+	return s.writer.WriteSpan(mSpan)
 }
