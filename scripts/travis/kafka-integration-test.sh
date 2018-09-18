43f458ebf4526c176647d80a99c4088905094655
Pavol Loffay <ploffay@redhat.com>
2018-09-18 17:38:16 +0200

    Fix kafka integration test
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- scripts/travis/kafka-integration-test.sh
+++ scripts/travis/kafka-integration-test.sh
@@ -2,16 +2,20 @@
 
 set -e
 
-docker pull spotify/kafka
-CID=$(docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --rm spotify/kafka)
+docker pull wurstmeister/kafka
+docker pull zookeeper
+CID_ZOOKEEPER=$(docker run -d --rm --name zookeeper zookeeper)
+CID_KAFKA=$(docker run -d --rm --link zookeeper --name kafka -p 9092:9092 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper -e KAFKA_LISTENERS=PLAINTEXT://:9092 wurstmeister/kafka)
 
 # Guarantees no matter what happens, docker will remove the instance at the end.
-trap 'docker rm -f $CID 2>/dev/null' EXIT INT TERM
+trap 'docker rm -f $CID_KAFKA 2>/dev/null' EXIT INT TERM
+trap 'docker rm -f $CID_ZOOKEEPER 2>/dev/null' EXIT INT TERM
 
-export STORAGE=kafka
-while true; do
-    if nc -z localhost 9092; then
-        break
-    fi
+echo 'executing wait'
+until docker exec -it kafka /bin/bash opt/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server=localhost:9092
+do
+  echo 'Kafka is not ready yet'
+  sleep 1
 done
-make storage-integration-test
+
+STORAGE=kafka make storage-integration-test
