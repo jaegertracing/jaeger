c473bd0ff576f0d170f1964b73169310b5cf1f22
Isaac Hier <ihier@uber.com>
2018-11-22 12:09:28 -0500

    Fix Cassandra timeout by testing port and increasing timeout
    Signed-off-by: Isaac Hier <ihier@uber.com>

--- plugin/storage/cassandra/schema/docker.sh
+++ plugin/storage/cassandra/schema/docker.sh
@@ -6,28 +6,38 @@
 CQLSH=${CQLSH:-"/usr/bin/cqlsh"}
 CQLSH_HOST=${CQLSH_HOST:-"cassandra"}
 CQLSH_SSL=${CQLSH_SSL:-""}
-CASSANDRA_WAIT_TIMEOUT=${CASSANDRA_WAIT_TIMEOUT:-"60"}
+CASSANDRA_WAIT_TIMEOUT=${CASSANDRA_WAIT_TIMEOUT:-"180"}
 DATACENTER=${DATACENTER:-"dc1"}
 KEYSPACE=${KEYSPACE:-"jaeger_v1_${DATACENTER}"}
 MODE=${MODE:-"test"}
 
 total_wait=0
-while true
+
+while (( total_wait < CASSANDRA_WAIT_TIMEOUT ))
 do
-  ${CQLSH} ${CQLSH_SSL} -e "describe keyspaces"
-  if (( $? == 0 )); then
-    break
-  else
-    if (( total_wait >= ${CASSANDRA_WAIT_TIMEOUT} )); then
-      echo "Timed out waiting for Cassandra."
-      exit 1
-    fi
-    echo "Cassandra is still not up at ${CQLSH_HOST}. Waiting 1 second."
-    sleep 1s
-    ((total_wait++))
-  fi
+	if nc -z "${CQLSH_HOST}" 9042; then
+		break
+	fi
+	echo "Cassandra is still not up at ${CQLSH_HOST}. Waiting 1 second."
+	sleep 1s
+	((total_wait++))
 done
+
+while (( total_wait < CASSANDRA_WAIT_TIMEOUT ))
+do
+  if ${CQLSH} "${CQLSH_SSL}" -e "describe keyspaces"; then
+    break
+	fi
+	echo "Cassandra is still not up at ${CQLSH_HOST}. Waiting 1 second."
+	sleep 1s
+	((total_wait++))
+done
+
+if (( total_wait >= CASSANDRA_WAIT_TIMEOUT )); then
+	echo "Timed out waiting for Cassandra."
+	exit 1
+fi
 
 echo "Generating the schema for the keyspace ${KEYSPACE} and datacenter ${DATACENTER}"
 
-MODE="${MODE}" DATACENTER="${DATACENTER}" KEYSPACE="${KEYSPACE}" /cassandra-schema/create.sh | ${CQLSH} ${CQLSH_SSL}
+MODE="${MODE}" DATACENTER="${DATACENTER}" KEYSPACE="${KEYSPACE}" /cassandra-schema/create.sh | ${CQLSH} "${CQLSH_SSL}"
