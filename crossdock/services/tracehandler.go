6e34c9103e5a02b9c87a07f021050b1fb3301660
Isaac Hier <isaachier@gmail.com>
2018-11-25 22:26:19 -0500

    Increase query attempts in crossdock test driver
    Signed-off-by: Isaac Hier <isaachier@gmail.com>

--- crossdock/services/tracehandler.go
+++ crossdock/services/tracehandler.go
@@ -219,9 +219,10 @@ func (h *TraceHandler) createAndRetrieveTraces(service string, request *traceReq
 }
 
 func (h *TraceHandler) getTraces(service, operation string, tags map[string]string) []*ui.Trace {
+	const maxAttempts = 60
 	// Retry multiple time since SASI indexing takes a couple of seconds
-	for i := 0; i < 10; i++ {
-		h.logger.Info(fmt.Sprintf("Waiting for traces, iteration %d out of 10", i+1))
+	for i := 0; i < maxAttempts; i++ {
+		h.logger.Info(fmt.Sprintf("Waiting for traces, iteration %d out of %d", i+1, maxAttempts))
 		traces, err := h.query.GetTraces(getTracerServiceName(service), operation, tags)
 		if err == nil && len(traces) > 0 {
 			return traces
