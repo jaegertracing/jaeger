da336af7d097a31899766d6dbb61ab710d3a9734
Yuri Shkuro <ys@uber.com>
2018-12-12 13:11:49 -0500

    make fmt
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- cmd/tracegen/main.go
+++ cmd/tracegen/main.go
@@ -1,15 +1,30 @@
+// Copyright (c) 2018 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"flag"
 	"time"
 
-	"github.com/jaegertracing/jaeger/internal/tracegen"
 	"github.com/opentracing/opentracing-go"
 	jaegerConfig "github.com/uber/jaeger-client-go/config"
 	jaegerZap "github.com/uber/jaeger-client-go/log/zap"
 	"github.com/uber/jaeger-lib/metrics/prometheus"
 	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/internal/tracegen"
 )
 
 var logger, _ = zap.NewDevelopment()
