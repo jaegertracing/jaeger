ab4d7284ce4e19105dec182e47a5b7b9c0f335b0
Annanay <annanay.a@media.net>
2019-01-30 00:36:11 +0530

    Initial commit, grpc for query-service
    Signed-off-by: Annanay <annanay.a@media.net>

---
+++ cmd/query/app/grpc_handler.go
@@ -0,0 +1,88 @@
+// Copyright (c) 2019 Jaegertracing Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package app
+
+import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+
+	"github.com/jaegertracing/jaeger/proto-gen/api_v2"
+	"github.com/jaegertracing/jaeger/storage/dependencystore"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
+)
+
+// GRPCHandler implements the GRPC endpoint of the query service.
+type GRPCHandler struct {
+	spanReader        spanstore.Reader
+	archiveSpanReader spanstore.Reader
+	dependencyReader  dependencystore.Reader
+	logger            *zap.Logger
+	queryParser       queryParser
+	tracer            opentracing.Tracer
+}
+
+// NewGRPCHandler returns a GRPCHandler
+func NewGRPCHandler(spanReader spanstore.Reader, dependencyReader dependencystore.Reader, options ...HandlerOption) *GRPCHandler {
+	gH := &GRPCHandler{
+		spanReader:       spanReader,
+		dependencyReader: dependencyReader,
+		queryParser: queryParser{
+			traceQueryLookbackDuration: defaultTraceQueryLookbackDuration,
+			timeNow:                    time.Now,
+		},
+	}
+
+	if gH.logger == nil {
+		gH.logger = zap.NewNop()
+	}
+	if gH.tracer == nil {
+		gH.tracer = opentracing.NoopTracer{}
+	}
+
+	return gH
+}
+
+// NewCombinedHandler returns a handler where GRPC and HTTP are multiplexed.
+func NewCombinedHandler(grpcServer *grpc.Server, recoveryHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			recoveryHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+// GetTrace is the GRPC handler to fetch traces.
+func (g *GRPCHandler) GetTrace(ctx context.Context, r *api_v2.GetTraceRequest) (*api_v2.GetTraceResponse, error) {
+	ID := r.GetId()
+
+	trace, err := g.spanReader.GetTrace(ctx, ID)
+	if err == spanstore.ErrTraceNotFound {
+		if g.archiveSpanReader == nil {
+			return &api_v2.GetTraceResponse{}, err
+		}
+
+		trace, err = g.archiveSpanReader.GetTrace(ctx, traceID)
+	}
+
+	return &api_v2.GetTraceResponse{Trace: trace}, nil
+}
--- cmd/query/main.go
+++ cmd/query/main.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -29,6 +30,8 @@ import (
 	jaegerClientZapLog "github.com/uber/jaeger-client-go/log/zap"
 	"github.com/uber/jaeger-lib/metrics"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 
 	"github.com/jaegertracing/jaeger/cmd/env"
 	"github.com/jaegertracing/jaeger/cmd/flags"
@@ -120,12 +123,37 @@ func main() {
 			compressHandler := handlers.CompressHandler(r)
 			recoveryHandler := recoveryhandler.NewRecoveryHandler(logger, true)
 
+			// Create GRPC Server.
+			grpcServer := grpc.NewServer()
+
+			// Add logging.
+			grpclog.SetLoggerV2(grpclog.NewLoggerV2(ioutil.Discard, os.Stderr, os.Stderr))
+
+			// Register GRPC Handler.
+			spanReaderGRPC, err := storageFactory.CreateSpanReader()
+			if err != nil {
+				logger.Fatal("Failed to create span reader", zap.Error(err))
+			}
+			spanReaderGRPC = storageMetrics.NewReadMetricsDecorator(spanReaderGRPC, baseFactory.Namespace(metrics.NSOptions{Name: "query-grpc", Tags: nil}))
+			dependencyReaderGRPC, err := storageFactory.CreateDependencyReader()
+			if err != nil {
+				logger.Fatal("Failed to create dependency reader", zap.Error(err))
+			}
+
+			grpcHandler := NewGRPCHandler(spanReaderGRPC, dependencyReaderGRPC, apiHandlerOptions)
+			api_v2.RegisterQueryServiceHandler(grpcServer, grpcHandler)
+
 			go func() {
-				logger.Info("Starting HTTP server", zap.Int("port", queryOpts.Port))
-				if err := http.ListenAndServe(portStr, recoveryHandler(compressHandler)); err != nil {
+				logger.Info("Starting HTTP+GRPC server", zap.Int("port", queryOpts.Port))
+				conn, err := net.Listen("tcp", fmt.Sprintf(":%d", queryOpts.Port))
+				if err != nil {
 					logger.Fatal("Could not launch service", zap.Error(err))
 				}
-				svc.HC().Set(healthcheck.Unavailable)
+
+				// Multiplexed server.
+				// Use CombinedHandler here, which will "reverse-proxy" between HTTP and GRPC backends.
+				srv.Serve(conn, NewCombinedHandler(grpcServer, recoveryHandler(compressHandler)))
+				hc.Set(healthcheck.Unavailable)
 			}()
 
 			svc.RunAndThen(nil)
