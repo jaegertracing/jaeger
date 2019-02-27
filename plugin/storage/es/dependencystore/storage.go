0172aedf0c07730dcdae9b1391b20c50387c850f
Pavol Loffay <ploffay@redhat.com>
2019-02-27 13:14:40 +0100

    Deprecate colon in index prefix in ES dependency store
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- plugin/storage/es/dependencystore/storage.go
+++ plugin/storage/es/dependencystore/storage.go
@@ -39,18 +39,23 @@ type DependencyStore struct {
 	client                es.Client
 	logger                *zap.Logger
 	dependencyIndexPrefix string
+	dependencyIndexPrefixDeprecated string
 }
 
 // NewDependencyStore returns a DependencyStore
 func NewDependencyStore(client es.Client, logger *zap.Logger, indexPrefix string) *DependencyStore {
+	var prefix string
+	var prefixDeprecated string
 	if indexPrefix != "" {
-		indexPrefix += ":"
+		prefix = indexPrefix + "-"
+		prefixDeprecated = indexPrefix + ":"
 	}
 	return &DependencyStore{
 		ctx:                   context.Background(),
 		client:                client,
 		logger:                logger,
-		dependencyIndexPrefix: indexPrefix + dependencyIndex,
+		dependencyIndexPrefix: prefix + dependencyIndex,
+		dependencyIndexPrefixDeprecated: prefixDeprecated + dependencyIndex,
 	}
 }
 
@@ -81,7 +86,11 @@ func (s *DependencyStore) writeDependencies(indexName string, ts time.Time, depe
 
 // GetDependencies returns all interservice dependencies
 func (s *DependencyStore) GetDependencies(endTs time.Time, lookback time.Duration) ([]model.DependencyLink, error) {
-	searchResult, err := s.client.Search(getIndices(s.dependencyIndexPrefix, endTs, lookback)...).
+	indices := getIndices(s.dependencyIndexPrefix, endTs, lookback)
+	if s.dependencyIndexPrefix != s.dependencyIndexPrefixDeprecated {
+		indices = append(indices, getIndices(s.dependencyIndexPrefixDeprecated, endTs, lookback)...)
+	}
+	searchResult, err := s.client.Search(indices...).
 		Type(dependencyType).
 		Size(10000). // the default elasticsearch allowed limit
 		Query(buildTSQuery(endTs, lookback)).
