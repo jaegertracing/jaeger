db7790a8d8042c0d235dd7f0d824ce8e73f8c48d
Charles Dixon <chvckd@gmail.com>
2019-03-20 10:16:33 +0000

    Add marshal/unmarshal tests for DependencyLink
    Signed-off-by: Charles Dixon <chvckd@gmail.com>

--- model/dependencies_test.go
+++ model/dependencies_test.go
@@ -15,9 +15,15 @@
 package model
 
 import (
+	"bytes"
 	"testing"
 
+	"github.com/jaegertracing/jaeger/model/prototest"
+
+	"github.com/gogo/protobuf/jsonpb"
+	proto "github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDependencyLinkApplyDefaults(t *testing.T) {
@@ -27,4 +33,53 @@ func TestDependencyLinkApplyDefaults(t *testing.T) {
 	networkSource := DependencyLinkSource("network")
 	dl = DependencyLink{Source: networkSource}.ApplyDefaults()
 	assert.Equal(t, networkSource, dl.Source)
+}
+
+func TestDependencyLinkSourceMarshalProto(t *testing.T) {
+	testCases := []struct {
+		name      string
+		marshal   func(proto.Message) ([]byte, error)
+		unmarshal func([]byte, proto.Message) error
+		expected  string
+	}{
+		{
+			name:      "protobuf",
+			marshal:   proto.Marshal,
+			unmarshal: proto.Unmarshal,
+		},
+		{
+			name: "JSON",
+			marshal: func(m proto.Message) ([]byte, error) {
+				out := new(bytes.Buffer)
+				err := new(jsonpb.Marshaler).Marshal(out, m)
+				if err != nil {
+					return nil, err
+				}
+				return out.Bytes(), nil
+			},
+			unmarshal: func(in []byte, m proto.Message) error {
+				return jsonpb.Unmarshal(bytes.NewReader(in), m)
+			},
+			expected: `{"parent":"parent","child":"child","callCount":"1","source":"jaeger"}`,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			link1 := DependencyLink{CallCount: 1, Child: "child", Parent: "parent", Source: JaegerDependencyLinkSource}
+			link2 := prototest.DependencyLink{CallCount: 1, Child: "child", Parent: "parent", Source: "jaeger"}
+			d1, err := testCase.marshal(&link1)
+			require.NoError(t, err)
+			d2, err := testCase.marshal(&link2)
+			require.NoError(t, err)
+			assert.Equal(t, d2, d1)
+			if testCase.expected != "" {
+				assert.Equal(t, testCase.expected, string(d1))
+			}
+			// test unmarshal
+			var link1u DependencyLink
+			err = testCase.unmarshal(d2, &link1u)
+			require.NoError(t, err)
+			assert.Equal(t, link1, link1u)
+		})
+	}
 }
