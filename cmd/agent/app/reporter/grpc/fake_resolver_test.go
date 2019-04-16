743421c3fd37aa4e407d26ecc0cbc754ec22e92d
Jude Wang <judew@uber.com>
2019-04-16 09:52:26 -0400

    add one more test to test fakeResolver with roundrobin balancer
    Signed-off-by: Jude Wang <judew@uber.com>

--- cmd/agent/app/reporter/grpc/fake_resolver_test.go
+++ cmd/agent/app/reporter/grpc/fake_resolver_test.go
@@ -15,8 +15,10 @@
 package grpc
 
 import (
+	"context"
 	"testing"
 
+	"google.golang.org/grpc"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/naming"
 )
@@ -47,4 +49,26 @@ func TestFakeResolver_Resolve(t *testing.T) {
 
 func mockDNSUpdate(updateCh chan []string, updates []string) {
 	updateCh <- updates
+}
+
+func TestFakeResolver_RoundRobin(t *testing.T) {
+	updateCh := make(chan []string)
+	r := newFakeResolver(updateCh)
+	numberGets := 1000
+	address1 := "111.222.333.444:8888"
+	address2 := "111.222.333.445:8888"
+	b := grpc.RoundRobin(r)
+	err := b.Start("dns://random", grpc.BalancerConfig{})
+	assert.NoError(t, err)
+	mockDNSUpdate(updateCh, []string{address1, address2})
+	count := 0
+	for i := 0; i < numberGets; i++ {
+		address, _, _ := b.Get(context.Background(), grpc.BalancerGetOptions{
+			BlockingWait: false,
+		})
+		if address.Addr == address1 {
+			count += 1
+		}
+	}
+	assert.Equal(t, 0.5, float64(count)/float64(numberGets), "Roundrobin balancer should return each address exactly half of the times")
 }
