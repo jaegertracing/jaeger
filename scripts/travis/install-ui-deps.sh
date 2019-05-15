9864d4e6120f6e84b572106e12f252f2fac0a582
Pavol Loffay <ploffay@redhat.com>
2019-05-15 16:53:35 +0200

    Move to script
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- scripts/travis/install-ui-deps.sh
+++ scripts/travis/install-ui-deps.sh
@@ -6,7 +6,7 @@ set -e
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
-
+PATH=$HOME/.yarn/bin:$PATH
 
 source ~/.nvm/nvm.sh
 nvm install 8
