387049f02c103274f40fac427c69888202d9f5a5
Abhilash Gnan <abhilashgnan@gmail.com>
2019-07-25 22:58:12 +0200

    update deps
    Signed-off-by: Abhilash Gnan <abhilashgnan@gmail.com>

--- cmd/collector/app/zipkin/protov2.go
+++ cmd/collector/app/zipkin/protov2.go
@@ -6,4 +6,6 @@ import "github.com/openzipkin/zipkin-go/model"
 // Converts Zipkin Protobuf spans to Thrift model
 func protoSpansV2ToThrift(zss []*model.SpanModel) ([]*zipkincore.Span, error) {
 	// TODO
+
+	return nil, nil
 }
