c23b18bc8e897052f38c9483c39417ac31e21285
Greg Franklin <gregoryfranklin@rentalcars.com>
2019-08-05 18:34:17 +0100

    Run es-integration-test.sh against ES5, ES6 and ES7
    Signed-off-by: Greg Franklin <gregoryfranklin@rentalcars.com>

--- scripts/travis/es-integration-test.sh
+++ scripts/travis/es-integration-test.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-# Default to elasticsearch 5.6.12 if ES_VERSION is not set
-ES_VERSION=${ES_VERSION:-5.6.12}
+run_integration_test() {
+  ES_VERSION=$1
+  docker pull docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
+  CID=$(docker run -d -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "xpack.security.enabled=false" -e "xpack.monitoring.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION})
+  STORAGE=elasticsearch make storage-integration-test
+  make index-cleaner-integration-test
+  docker kill $CID
+}
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
-CID=$(docker run -d -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "xpack.security.enabled=false" -e "xpack.monitoring.enabled=false" docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION})
-STORAGE=elasticsearch make storage-integration-test
-make index-cleaner-integration-test
-docker kill $CID
+run_integration_test "5.6.12"
+run_integration_test "6.8.1"
+run_integration_test "7.2.0"
 
 echo "Executing token propatagion test"
 
