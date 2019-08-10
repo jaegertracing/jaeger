2724a5807759401f78377cd24a3834115ea3c29e
guanw <judew@uber.com>
2019-08-10 16:17:51 -0400

    Show Docker logs if integration test fails (#1587)
    * dump log if integration test fails
    
    Signed-off-by: Jude Wang <judew@uber.com>
    
    * update the condition to check exit code
    
    Signed-off-by: Jude Wang <judew@uber.com>

--- scripts/travis/build-all-in-one-image.sh
+++ scripts/travis/build-all-in-one-image.sh
@@ -13,6 +13,14 @@ export REPO=jaegertracing/all-in-one
 docker build -f cmd/all-in-one/Dockerfile -t $REPO:latest cmd/all-in-one
 export CID=$(docker run -d -p 16686:16686 -p 5778:5778 $REPO:latest)
 make integration-test
+
+if [ $? -ne 0 ] ; then
+    echo "---- integration test failed unexpectedly ----"
+    echo "--- check the docker log below for details ---"
+    docker logs $CID
+    exit 1
+fi
+
 docker kill $CID
 
 # Only push the docker container to Docker Hub for master branch
