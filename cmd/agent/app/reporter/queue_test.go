9e35cc26452ea38b90ba18af334b6c232d5de077
Michael Burman <yak@iki.fi>
2019-08-26 15:27:11 +0300

    Odd missing fmts..
    Signed-off-by: Michael Burman <yak@iki.fi>

--- cmd/agent/app/reporter/queue_test.go
+++ cmd/agent/app/reporter/queue_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2019 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package reporter
 
 import (
@@ -7,12 +21,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/jaegertracing/jaeger/cmd/agent/app/reporter/queue"
-	"github.com/jaegertracing/jaeger/thrift-gen/jaeger"
-	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
 	"github.com/stretchr/testify/assert"
 	"github.com/uber/jaeger-lib/metrics/metricstest"
 	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/cmd/agent/app/reporter/queue"
+	"github.com/jaegertracing/jaeger/thrift-gen/jaeger"
+	"github.com/jaegertracing/jaeger/thrift-gen/zipkincore"
 )
 
 var _ Queue = &queue.NonQueue{}
