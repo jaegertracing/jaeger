404ce68ad033ffaccda063014a60978349f015c8
Prithvi Raj <vprithvi@users.noreply.github.com>
2019-09-25 11:15:41 -0400

    [refactor] pass saramaMessageWrapper by value (#1818)
    Signed-off-by: Prithvi Raj <p.r@uber.com>

--- cmd/ingester/app/consumer/consumer.go
+++ cmd/ingester/app/consumer/consumer.go
@@ -149,7 +149,7 @@ func (c *Consumer) handleMessages(pc sc.PartitionConsumer) {
 				defer msgProcessor.Close()
 			}
 
-			msgProcessor.Process(&saramaMessageWrapper{msg})
+			msgProcessor.Process(saramaMessageWrapper{msg})
 
 		case <-deadlockDetector.closePartitionChannel():
 			c.logger.Info("Closing partition due to inactivity", zap.Int32("partition", pc.Partition()))
