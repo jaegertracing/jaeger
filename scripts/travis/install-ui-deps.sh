f22f81c92846a37c318c9906515df0be6211dcbb
Yuri Shkuro <ys@uber.com>
2019-10-13 16:58:47 -0400

    Bump Node to 10
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- scripts/travis/install-ui-deps.sh
+++ scripts/travis/install-ui-deps.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Repo for newer Node.js versions
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 source ~/.nvm/nvm.sh
-nvm install 8
+nvm install 10
 
 yarn --version
