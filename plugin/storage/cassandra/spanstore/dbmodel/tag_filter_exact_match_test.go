eca658afb14cba98f5cbb28fe21542f9d046d29d
Joe Elliott <number101010@gmail.com>
2019-11-08 08:56:09 -0500

    Added exact match tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ plugin/storage/cassandra/spanstore/dbmodel/tag_filter_exact_match_test.go
@@ -0,0 +1,121 @@
+// Copyright (c) 2019 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dbmodel
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jaegertracing/jaeger/model"
+)
+
+func TestBlacklistFilter(t *testing.T) {
+	// expected
+	tt := [][][]string{
+		{
+			{"a", "b", "c"}, // input
+			{"a"},           // filter
+			{"b", "c"},      // expected
+		},
+		{
+			{"a", "b", "c"},
+			{"A"},
+			{"a", "b", "c"},
+		},
+	}
+
+	for _, test := range tt {
+		input := test[0]
+		filter := test[1]
+		expected := test[2]
+
+		var inputKVs model.KeyValues
+		for _, i := range input {
+			inputKVs = append(inputKVs, model.String(i, ""))
+		}
+		var expectedKVs model.KeyValues
+		for _, e := range expected {
+			expectedKVs = append(expectedKVs, model.String(e, ""))
+		}
+		expectedKVs.Sort()
+
+		tf := NewExactMatchTagFilter(filter, true)
+		actualKVs := tf.filter(inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterLogFields(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterProcessTags(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterTags(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+	}
+}
+
+func TestWhitelistFilter(t *testing.T) {
+	// expected
+	tt := [][][]string{
+		{
+			{"a", "b", "c"}, // input
+			{"a"},           // filter
+			{"a"},           // expected
+		},
+		{
+			{"a", "b", "c"},
+			{"A"},
+			{},
+		},
+	}
+
+	for _, test := range tt {
+		input := test[0]
+		filter := test[1]
+		expected := test[2]
+
+		var inputKVs model.KeyValues
+		for _, i := range input {
+			inputKVs = append(inputKVs, model.String(i, ""))
+		}
+		var expectedKVs model.KeyValues
+		for _, e := range expected {
+			expectedKVs = append(expectedKVs, model.String(e, ""))
+		}
+		expectedKVs.Sort()
+
+		tf := NewExactMatchTagFilter(filter, false)
+		actualKVs := tf.filter(inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterLogFields(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterProcessTags(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+
+		actualKVs = tf.FilterTags(nil, inputKVs)
+		actualKVs.Sort()
+		assert.Equal(t, actualKVs, expectedKVs)
+	}
+}
