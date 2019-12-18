43e8f5d33d2982c82d2471485f4f9513958723c7
chandresh-pancholi <chandreshpancholi007@gmail.com>
2019-12-19 02:46:11 +0530

    Configure Jager Collector to work with Kafka With Basic Authentication #1966
    Signed-off-by: chandresh-pancholi <chandreshpancholi007@gmail.com>

---
+++ pkg/kafka/auth/plaintext.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2019 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"github.com/Shopify/sarama"
+)
+
+// PlainTextConfig describes the configuration properties needed for SASL/PLAIN with kafka
+type PlainTextConfig struct {
+	UserName string
+	Password string
+}
+
+func setPlainTextConfiguration(config *PlainTextConfig, saramaConfig *sarama.Config) {
+	saramaConfig.Net.SASL.Enable = true
+	saramaConfig.Net.SASL.User = config.UserName
+	saramaConfig.Net.SASL.Password = config.Password
+}
