74f53b248ce15a2ff60b4dac4fe3c5d9de5ff654
Chadi El Masri <celmasri@murex.com>
2020-01-08 10:28:41 +0100

    Added unit test for admin.
    Signed-off-by: Chadi El Masri <celmasri@murex.com>

---
+++ cmd/flags/admin_test.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flags
+
+import (
+	"testing"
+
+	"github.com/jaegertracing/jaeger/pkg/config"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+func TestAdminServerHandlesPortZero(t *testing.T) {
+	adminServer := NewAdminServer(0)
+
+	v, _ := config.Viperize(adminServer.AddFlags)
+
+	zapCore, logs := observer.New(zap.InfoLevel)
+	logger := zap.New(zapCore)
+
+	adminServer.initFromViper(v, logger)
+
+	assert.NoError(t, adminServer.Serve())
+	defer adminServer.Close()
+
+	message := logs.FilterMessage("Admin server started")
+	assert.Equal(t, 1, message.Len(), "Expected Admin server started log message.")
+
+	onlyEntry := message.All()[0]
+	port := onlyEntry.ContextMap()["http-port"].(int64)
+	assert.Greater(t, port, int64(0))
+}
