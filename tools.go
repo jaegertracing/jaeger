aec0cf5f6f170307415ea3402c044327cc6f2153
Pavol Loffay <ploffay@redhat.com>
2020-02-26 17:38:38 +0100

    Move to go modules, not working tools, vendor in makefile
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

---
+++ tools.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2020 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build tools
+
+package jaeger
+
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+import (
+	_ "honnef.co/go/tools/cmd/staticcheck"
+
+	_ "github.com/gogo/protobuf/protoc-gen-gogo"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/mjibson/esc"
+	_ "github.com/sectioneight/md-to-godoc"
+	_ "github.com/securego/gosec/cmd/gosec"
+	_ "github.com/vektra/mockery/cmd/mockery"
+	_ "github.com/wadey/gocovmerge"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/cover"
+)
