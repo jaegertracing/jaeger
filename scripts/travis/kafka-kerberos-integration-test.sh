b7f6f4bde4da26dc94b828534eaa29bf38cd9835
Ruben Vargas <ruben.vp8510@gmail.com>
2020-05-27 23:15:46 -0500

    Execute kerberos E2E test on travis CI
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- scripts/travis/kafka-kerberos-integration-test.sh
+++ scripts/travis/kafka-kerberos-integration-test.sh
@@ -2,9 +2,34 @@
 
 set -e
 
+sudo apt-get update
+sudo apt-get -y install krb5-user
+
 docker pull rubensvp/kafka-kerberos
+
 temp_dir=$(mktemp -d)
 
+export KAFKA_KERBEROS_CONFIG=${temp_dir}/krb5.conf
+export KAFKA_KERBEROS_COLLECTOR_KEYTAB=${temp_dir}/collector.keytab
+export KAFKA_KERBEROS_INGESTER_KEYTAB=${temp_dir}/ingester.keytab
+
+cat <<EOF > ${KAFKA_KERBEROS_CONFIG}
+[libdefaults]
+ dns_lookup_realm = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ rdns = false
+ default_realm = EXAMPLE.COM
+
+[realms]
+EXAMPLE.COM = {
+    kdc = 127.0.0.1
+    admin_server = 127.0.0.1
+ }
+EOF
+
+export KRB5_CONFIG=${KAFKA_KERBEROS_CONFIG}
 
 CID=$(docker run -d -e REALM=EXAMPLE.COM -e KADMIN_PASSWORD=qwerty -p 88:88/udp -p 9092:9092 -p 464:464 -p 749:749 --rm rubensvp/kafka-kerberos)
 # Guarantees no matter what happens, docker will remove the instance at the end.
@@ -25,10 +50,6 @@ function kadminCommand {
     kadmin -p $KADMIN_PRINCIPAL_FULL -w $KADMIN_PASSWORD -q "$1"
 }
 
-export KAFKA_KERBEROS_CONFIG=${temp_dir}/krb5.conf
-export KAFKA_KERBEROS_COLLECTOR_KEYTAB=${temp_dir}/collector.keytab
-export KAFKA_KERBEROS_INGESTER_KEYTAB=${temp_dir}/ingester.keytab
-
 echo "Add ingester user"
 kadminCommand "addprinc -pw secret ingester/localhost@EXAMPLE.COM"
 echo "Create ingester keytab"
@@ -38,22 +59,6 @@ echo "Add collector user"
 kadminCommand "addprinc -pw secret collector/localhost@EXAMPLE.COM"
 echo "Create collector keytab"
 kadminCommand "xst -k ${KAFKA_KERBEROS_COLLECTOR_KEYTAB} collector/localhost"
-
-cat <<EOF > ${KAFKA_KERBEROS_CONFIG}
-[libdefaults]
- dns_lookup_realm = false
- ticket_lifetime = 24h
- renew_lifetime = 7d
- forwardable = true
- rdns = false
- default_realm = EXAMPLE.COM
-
-[realms]
-EXAMPLE.COM = {
-    kdc = 127.0.0.1
-    admin_server = 127.0.0.1
- }
-EOF
 
 sleep 30
 
