3174303fb195d0df3deee9261ce6d4353f82f80d
Pavol Loffay <ploffay@redhat.com>
2020-06-04 11:27:51 +0200

    Add godoc for OTEL component packages
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- cmd/opentelemetry-collector/app/exporter/badgerexporter/doc.go
+++ cmd/opentelemetry-collector/app/exporter/badgerexporter/doc.go
@@ -12,5 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package kafkaexporter implements Jaeger Kafka ingester as OpenTelemetry exporter.
-package kafkaexporter
+
+// Package badgerexporter implements Jaeger Badger storage as OpenTelemetry exporter.
+package badgerexporter
+
