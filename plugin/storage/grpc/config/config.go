b78bb2f3cc7acc8194a76903a5eaf7755d846dbd
Andrew Putilov <to.merge@gmail.com>
2020-07-21 10:52:15 +0300

    Introduce PluginServices
    Signed-off-by: Andrew Putilov <to.merge@gmail.com>

--- plugin/storage/grpc/config/config.go
+++ plugin/storage/grpc/config/config.go
@@ -38,7 +38,7 @@ type PluginBuilder interface {
 }
 
 // Build instantiates a StoragePlugin
-func (c *Configuration) Build() (shared.StoragePlugin, shared.ArchiveStoragePlugin, shared.PluginCapabilities, error) {
+func (c *Configuration) Build() (*PluginServices, error) {
 	// #nosec G204
 	cmd := exec.Command(c.PluginBinary, "--config", c.PluginConfigurationFile)
 
@@ -60,26 +60,37 @@ func (c *Configuration) Build() (shared.StoragePlugin, shared.ArchiveStoragePlug
 
 	rpcClient, err := client.Client()
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("error attempting to connect to plugin rpc client: %s", err)
+		return nil, fmt.Errorf("error attempting to connect to plugin rpc client: %s", err)
 	}
 
 	raw, err := rpcClient.Dispense(shared.StoragePluginIdentifier)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("unable to retrieve storage plugin instance: %s", err)
+		return nil, fmt.Errorf("unable to retrieve storage plugin instance: %s", err)
 	}
 
 	storagePlugin, ok := raw.(shared.StoragePlugin)
 	if !ok {
-		return nil, nil, nil, fmt.Errorf("unexpected type for plugin \"%s\"", shared.StoragePluginIdentifier)
+		return nil, fmt.Errorf("unexpected type for plugin \"%s\"", shared.StoragePluginIdentifier)
 	}
 
 	archiveStoragePlugin := raw.(shared.ArchiveStoragePlugin)
 	capabilities := raw.(shared.PluginCapabilities)
 
-	return storagePlugin, archiveStoragePlugin, capabilities, nil
+	return &PluginServices{
+		Store:        storagePlugin,
+		ArchiveStore: archiveStoragePlugin,
+		Capabilities: capabilities,
+	}, nil
+}
+
+// PluginServices defines services plugin can expose
+type PluginServices struct {
+	Store        shared.StoragePlugin
+	ArchiveStore shared.ArchiveStoragePlugin
+	Capabilities shared.PluginCapabilities
 }
 
 // PluginBuilder is used to create storage plugins
 type PluginBuilder interface {
-	Build() (shared.StoragePlugin, shared.ArchiveStoragePlugin, shared.PluginCapabilities, error)
+	Build() (*PluginServices, error)
 }
