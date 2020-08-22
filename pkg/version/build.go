22392e08201ced7b11b181737a764d2c81cfd243
Alan Pinkert <alan.pinkert@gmail.com>
2020-08-22 10:12:34 -0400

    Fixed documentation for build information
    Signed-off-by: Alan Pinkert <alan.pinkert@gmail.com>

--- pkg/version/build.go
+++ pkg/version/build.go
@@ -15,10 +15,10 @@
 package version
 
 var (
-	// commitFromGit is a constant representing the source version that
+	// commitSHA is a constant representing the source version that
 	// generated this build. It should be set during build via -ldflags.
 	commitSHA string
-	// versionFromGit is a constant representing the version tag that
+	// latestVersion is a constant representing the version tag that
 	// generated this build. It should be set during build via -ldflags.
 	latestVersion string
 	// build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
