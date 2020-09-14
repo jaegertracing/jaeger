9ceb37f98f1dab51a5abd72f9aee19da67f43e68
Andrew Putilov <to.merge@gmail.com>
2020-09-14 11:50:26 +0300

    Rebase-related updates
    Signed-off-by: Andrew Putilov <to.merge@gmail.com>

--- plugin/storage/grpc/factory_test.go
+++ plugin/storage/grpc/factory_test.go
@@ -20,7 +20,6 @@ import (
 
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/uber/jaeger-lib/metrics"
 	"go.uber.org/zap"
