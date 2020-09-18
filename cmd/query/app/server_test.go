51eb5dfc1ce5d4a7890792a98e4fc37539b16523
albertteoh <albert.teoh@logz.io>
2020-09-18 15:37:21 +1000

    Add require dependency
    Signed-off-by: albertteoh <albert.teoh@logz.io>

--- cmd/query/app/server_test.go
+++ cmd/query/app/server_test.go
@@ -24,6 +24,7 @@ import (
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest/observer"
 
