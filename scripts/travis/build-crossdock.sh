76ce1086497bf3cf1bf20ce26f2b901fafd1f2a3
Ashmita <ashmita.bohara152@gmail.com>
2020-12-07 02:43:47 +0800

    Add gh action for crossdock and crossdock otel ci jobs (#2681)
    * Add gh action for crossdock and crossdock otel ci jobs
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Separate out BRANCH logic to separate action
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Separate out BRANCH logic to separate action
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Separate out BRANCH logic to separate action
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Separate out BRANCH logic to separate action
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>

--- scripts/travis/build-crossdock.sh
+++ scripts/travis/build-crossdock.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-set -e
+set -euxf -o pipefail
 
 BRANCH=${BRANCH:?'missing BRANCH env var'}
+DOCKERHUB_LOGIN=${DOCKERHUB_LOGIN:-false}
+COMMIT=${GITHUB_SHA::8}
 
 make build-and-run-crossdock
 
-# Only push the docker container to Docker Hub for master branch
-if [[ "$BRANCH" == "master" && "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
+# Only push the docker container to Docker Hub for master branch and when dockerhub login is done
+if [[ "$BRANCH" == "master" && "$DOCKERHUB_LOGIN" == "true" ]]; then
   echo 'upload to Docker Hub'
 else
   echo 'skip docker upload for PR'
   exit 0
 fi
-
-set -x
 
 # docker image has been build when running the crossdock
 export REPO=jaegertracing/test-driver
