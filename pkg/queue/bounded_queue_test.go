26a349e6b3278fa0ff194ad846d99a8ceb18f961
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-01-06 14:03:31 +0100

    Refactor bounded queue tests to extract common parts The common part with the assertions was moved to a new `checkQueue` function that is used by `TestBoundedQueue` and `TestBoundedQueueWithFactory`.
    Signed-off-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- pkg/queue/bounded_queue_test.go
+++ pkg/queue/bounded_queue_test.go
@@ -30,33 +30,14 @@ import (
 	uatomic "go.uber.org/atomic"
 )
 
-// In this test we run a queue with capacity 1 and a single consumer.
-// We want to test the overflow behavior, so we block the consumer
-// by holding a startLock before submitting items to the queue.
-func TestBoundedQueue(t *testing.T) {
-	mFact := metricstest.NewFactory(0)
-	counter := mFact.Counter(metrics.Options{Name: "dropped", Tags: nil})
+func checkQueue(
+	t *testing.T,
+	q *BoundedQueue,
+	startLock *sync.Mutex,
+	consumerState *consumerState,
+	mFact *metricstest.Factory,
+) {
 	gauge := mFact.Gauge(metrics.Options{Name: "size", Tags: nil})
-
-	q := NewBoundedQueue(1, func(item interface{}) {
-		counter.Inc(1)
-	})
-	assert.Equal(t, 1, q.Capacity())
-
-	var startLock sync.Mutex
-
-	startLock.Lock() // block consumers
-	consumerState := newConsumerState(t)
-
-	q.StartConsumers(1, func(item interface{}) {
-		consumerState.record(item.(string))
-
-		// block further processing until startLock is released
-		startLock.Lock()
-		//lint:ignore SA2001 empty section is ok
-		startLock.Unlock()
-	})
-
 	assert.True(t, q.Produce("a"))
 
 	// at this point "a" may or may not have been received by the consumer go-routine
@@ -112,10 +93,40 @@ func TestBoundedQueue(t *testing.T) {
 	assert.False(t, q.Produce("x"), "cannot push to closed queue")
 }
 
+// In this test we run a queue with capacity 1 and a single consumer.
+// We want to test the overflow behavior, so we block the consumer
+// by holding a startLock before submitting items to the queue.
+func TestBoundedQueue(t *testing.T) {
+	mFact := metricstest.NewFactory(0)
+	counter := mFact.Counter(metrics.Options{Name: "dropped", Tags: nil})
+
+	q := NewBoundedQueue(1, func(item interface{}) {
+		counter.Inc(1)
+	})
+	assert.Equal(t, 1, q.Capacity())
+
+	var startLock sync.Mutex
+
+	startLock.Lock() // block consumers
+	consumerState := newConsumerState(t)
+
+	q.StartConsumers(1, func(item interface{}) {
+		consumerState.record(item.(string))
+
+		// block further processing until startLock is released
+		startLock.Lock()
+		//lint:ignore SA2001 empty section is ok
+		startLock.Unlock()
+	})
+
+	checkQueue(t, q, &startLock, consumerState, mFact)
+}
+
+// This test is identical to the previous one but we start the
+// queue using a consumer factory instead of a callback.
 func TestBoundedQueueWithFactory(t *testing.T) {
 	mFact := metricstest.NewFactory(0)
 	counter := mFact.Counter(metrics.Options{Name: "dropped", Tags: nil})
-	gauge := mFact.Gauge(metrics.Options{Name: "size", Tags: nil})
 
 	q := NewBoundedQueue(1, func(item interface{}) {
 		counter.Inc(1)
@@ -131,59 +142,7 @@ func TestBoundedQueueWithFactory(t *testing.T) {
 		return newStatefulConsumer(&startLock, consumerState)
 	})
 
-	assert.True(t, q.Produce("a"))
-
-	// at this point "a" may or may not have been received by the consumer go-routine
-	// so let's make sure it has been
-	consumerState.waitToConsumeOnce()
-
-	// at this point the item must have been read off the queue, but the consumer is blocked
-	assert.Equal(t, 0, q.Size())
-	consumerState.assertConsumed(map[string]bool{
-		"a": true,
-	})
-
-	// produce two more items. The first one should be accepted, but not consumed.
-	assert.True(t, q.Produce("b"))
-	assert.Equal(t, 1, q.Size())
-	// the second should be rejected since the queue is full
-	assert.False(t, q.Produce("c"))
-	assert.Equal(t, 1, q.Size())
-
-	q.StartLengthReporting(time.Millisecond, gauge)
-	for i := 0; i < 1000; i++ {
-		_, g := mFact.Snapshot()
-		if g["size"] == 0 {
-			time.Sleep(time.Millisecond)
-		} else {
-			break
-		}
-	}
-
-	c, g := mFact.Snapshot()
-	assert.EqualValues(t, 1, c["dropped"])
-	assert.EqualValues(t, 1, g["size"])
-
-	startLock.Unlock() // unblock consumer
-
-	consumerState.assertConsumed(map[string]bool{
-		"a": true,
-		"b": true,
-	})
-
-	// now that consumers are unblocked, we can add more items
-	expected := map[string]bool{
-		"a": true,
-		"b": true,
-	}
-	for _, item := range []string{"d", "e", "f"} {
-		assert.True(t, q.Produce(item))
-		expected[item] = true
-		consumerState.assertConsumed(expected)
-	}
-
-	q.Stop()
-	assert.False(t, q.Produce("x"), "cannot push to closed queue")
+	checkQueue(t, q, &startLock, consumerState, mFact)
 }
 
 type consumerState struct {
