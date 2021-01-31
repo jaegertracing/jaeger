92f670be622f8795cafed32c0cb49edd321c4c08
Ashmita <ashmita.bohara152@gmail.com>
2021-02-01 04:28:37 +1100

    Github action for jaeger packages and docker image (#2740)
    * - Build docker images for jaeger components
    - Build tar file for jaeger components
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Add action for release
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Set DEPLOY as true for package binary script
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Fix BRANCH env variable for release event
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Feedbacks
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Fix BRANCH for release
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>
    
    * Feedbacks
    
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>

--- scripts/travis/upload-all-docker-images.sh
+++ scripts/travis/upload-all-docker-images.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# this script should only be run after build-docker-images.sh
+# this script expects all docker images to be already built, it only uploads them to Docker Hub
 
-set -e
+set -euxf -o pipefail
 
 BRANCH=${BRANCH:?'missing BRANCH env var'}
+DOCKERHUB_LOGIN=${DOCKERHUB_LOGIN:-false}
 
-if [[ "$TRAVIS_SECURE_ENV_VARS" == "false" ]]; then
-  echo "skip docker upload, TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS"
-  exit 0
-fi
-
-# Only push images to Docker Hub for master branch or for release tags vM.N.P
-if [[ "$BRANCH" == "master" || $BRANCH =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+# Only push images to Docker Hub for master branch or for release tags vM.N.P and when dockerhub login is done
+if [[ ("$BRANCH" == "master" || $BRANCH =~ ^v[0-9]+\.[0-9]+\.[0-9]+$) && "$DOCKERHUB_LOGIN" == "true" ]]; then
   echo "upload to Docker Hub, BRANCH=$BRANCH"
 else
   echo 'skip Docker upload, only allowed for tagged releases or master (latest tag)'
