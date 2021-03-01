e18692e92ed82277edfaf8a1a3a64ecfc1ef9ae6
Joe Elliott <number101010@gmail.com>
2021-03-01 08:57:01 -0500

    create a simple shim codec around gogoproto
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ proto-gen/api_v2/codec.go
@@ -0,0 +1,34 @@
+package api_v2
+
+import (
+	proto "github.com/gogo/protobuf/proto"
+	"google.golang.org/grpc/encoding"
+)
+
+func init() {
+	encoding.RegisterCodec(newCodec())
+}
+
+// gogoCodec forces the use of gogo proto marshalling/unmarshalling for
+// proto over grpc
+type gogoCodec struct {
+}
+
+func newCodec() *gogoCodec {
+	return &gogoCodec{}
+}
+
+// Name implements encoding.Codec
+func (c *gogoCodec) Name() string {
+	return "proto"
+}
+
+// Marshal implements encoding.Codec
+func (c *gogoCodec) Marshal(v interface{}) ([]byte, error) {
+	return proto.Marshal(v.(proto.Message))
+}
+
+// Unmarshal implements encoding.Codec
+func (c *gogoCodec) Unmarshal(data []byte, v interface{}) error {
+	return proto.Unmarshal(data, v.(proto.Message))
+}
