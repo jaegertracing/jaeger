b76bce4bbd0d954dd30c200b5c88e0c2361c228a
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-03-17 12:14:27 +0100

    First review comments
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- v2/storage/memory/factory.go
+++ v2/storage/memory/factory.go
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright The Jaeger Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package memory
 
 import (
@@ -60,10 +46,5 @@ func createDefaultConfig() configmodels.Exporter {
 
 func createTraceExporter(_ context.Context, params component.ExporterCreateParams, config configmodels.Exporter) (component.TracesExporter, error) {
 	expCfg := config.(*Config)
-	exp, err := newExporter(expCfg, params.Logger)
-	if err != nil {
-		return nil, err
-	}
-
-	return exp, nil
+	return newExporter(expCfg, params.Logger)
 }
