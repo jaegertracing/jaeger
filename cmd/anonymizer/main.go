051860f1189cc7305233d607ac0bfb8143ba1d68
wangchenglong01 <wangchl01@inspur.com>
2021-03-25 19:54:02 +0800

    change variable name replacing golang's keyword "error"
    Signed-off-by: wangchenglong <luckymrwang@163.com>

--- cmd/anonymizer/main.go
+++ cmd/anonymizer/main.go
@@ -89,8 +89,8 @@ func main() {
 
 	command.AddCommand(version.Command())
 
-	if error := command.Execute(); error != nil {
-		fmt.Println(error.Error())
+	if err := command.Execute(); err != nil {
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 }
