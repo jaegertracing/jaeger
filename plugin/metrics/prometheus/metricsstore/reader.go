009ae422d248d73628116ee608f2af8c26e5d7bd
albertteoh <albert.teoh@logz.io>
2021-05-16 21:11:09 +1000

    Address review comments
    Signed-off-by: albertteoh <albert.teoh@logz.io>

--- plugin/metrics/prometheus/metricsstore/reader.go
+++ plugin/metrics/prometheus/metricsstore/reader.go
@@ -16,10 +16,12 @@ package metricsstore
 
 import (
 	"context"
+	"net"
+	"net/http"
 	"time"
 
 	"github.com/prometheus/client_golang/api"
-	"github.com/prometheus/client_golang/api/prometheus/v1"
+	promapi "github.com/prometheus/client_golang/api/prometheus/v1"
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger/proto-gen/api_v2/metrics"
@@ -28,21 +30,32 @@ import (
 
 // MetricsReader is a Prometheus metrics reader.
 type MetricsReader struct {
-	client v1.API
+	client promapi.API
 	logger *zap.Logger
 }
 
-// NewMetricsReader returns a new MetricsReader, assigning the first reachable host:port from the provided list.
-// This host:port forms part of the URL to call when making queries to the underlying metrics store.
-func NewMetricsReader(logger *zap.Logger, hostPort string) (*MetricsReader, error) {
+// NewMetricsReader returns a new MetricsReader.
+func NewMetricsReader(logger *zap.Logger, hostPort string, connectTimeout time.Duration) (*MetricsReader, error) {
+	// KeepAlive and TLSHandshake timeouts are kept to existing Prometheus client's
+	// DefaultRoundTripper to simplify user configuration and may be made configurable when required.
+	roundTripper := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   connectTimeout,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout: 10 * time.Second,
+	}
+
 	client, err := api.NewClient(api.Config{
-		Address: "http://" + hostPort,
+		Address:      "http://" + hostPort,
+		RoundTripper: roundTripper,
 	})
 	if err != nil {
 		return nil, err
 	}
 	mr := &MetricsReader{
-		client: v1.NewAPI(client),
+		client: promapi.NewAPI(client),
 		logger: logger,
 	}
 	logger.Info("Prometheus reader initialized", zap.String("addr", hostPort))
@@ -50,21 +63,21 @@ func NewMetricsReader(logger *zap.Logger, hostPort string) (*MetricsReader, erro
 }
 
 // GetLatencies gets the latency metrics for the given set of latency query parameters.
-func (m *MetricsReader) GetLatencies(ctx context.Context, params *metricsstore.LatenciesQueryParameters) (mf metrics.MetricFamily, err error) {
+func (m *MetricsReader) GetLatencies(ctx context.Context, params *metricsstore.LatenciesQueryParameters) (metrics.MetricFamily, error) {
 	// TODO: Implement me
-	return
+	return metrics.MetricFamily{}, nil
 }
 
 // GetCallRates gets the call rate metrics for the given set of call rate query parameters.
-func (m *MetricsReader) GetCallRates(ctx context.Context, params *metricsstore.CallRateQueryParameters) (mf metrics.MetricFamily, err error) {
+func (m *MetricsReader) GetCallRates(ctx context.Context, params *metricsstore.CallRateQueryParameters) (metrics.MetricFamily, error) {
 	// TODO: Implement me
-	return
+	return metrics.MetricFamily{}, nil
 }
 
 // GetErrorRates gets the error rate metrics for the given set of error rate query parameters.
-func (m *MetricsReader) GetErrorRates(ctx context.Context, params *metricsstore.ErrorRateQueryParameters) (mf metrics.MetricFamily, err error) {
+func (m *MetricsReader) GetErrorRates(ctx context.Context, params *metricsstore.ErrorRateQueryParameters) (metrics.MetricFamily, error) {
 	// TODO: Implement me
-	return
+	return metrics.MetricFamily{}, nil
 }
 
 // GetMinStepDuration gets the minimum step duration (the smallest possible duration between two data points in a time series) supported.
