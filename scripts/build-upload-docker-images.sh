e5395d9f729a428b12bbbeda3b82cf1fef06be8c
GaruGaru <garuglieritommaso.dev@gmail.com>
2021-06-30 20:47:22 +0200

    build images for linux/arm64 Signed-off-by: Tommaso Garuglieri <garuglieritommaso.dev@gmail.com>
    Signed-off-by: GaruGaru <garuglieritommaso.dev@gmail.com>

--- scripts/build-upload-docker-images.sh
+++ scripts/build-upload-docker-images.sh
@@ -7,9 +7,10 @@ make create-baseimg-debugimg
 # build multi-arch binaries
 make build-binaries-linux
 make build-binaries-s390x
+make build-binaries-arm64
 
 # build multi-arch docker images
-platforms="linux/amd64,linux/s390x"
+platforms="linux/amd64,linux/s390x,linux/arm64"
 
 # build/upload images for release version of Jaeger backend components
 for component in agent collector query ingester
