f5e2b21086639c8376ead6b2362e61093ad0c399
Ashmita Bohara <ashmita.bohara152@gmail.com>
2021-08-19 21:36:27 +1000

    Ran make fmt
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>

--- plugin/storage/integration/token_propagation_test.go
+++ plugin/storage/integration/token_propagation_test.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build token_propagation
 // +build token_propagation
 
 package integration
