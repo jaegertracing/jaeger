13ccfa561f4195539ce30c9a8523dbc2ba201f99
Ashmita <ashmita.bohara152@gmail.com>
2021-08-20 21:51:34 +0800

    Upgrade to Go v1.17 (#3220)

--- plugin/storage/integration/es_index_cleaner_test.go
+++ plugin/storage/integration/es_index_cleaner_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build index_cleaner
+//go:build index_cleaner
 
 package integration
 
