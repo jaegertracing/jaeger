4d93c07b55e18fd3f49f083eadbaa676bfd105ca
Ashmita Bohara <ashmita.bohara152@gmail.com>
2021-08-24 22:41:19 +1000

    Feedbacks
    Signed-off-by: Ashmita Bohara <ashmita.bohara152@gmail.com>

--- scripts/cassandra-integration-test.sh
+++ scripts/cassandra-integration-test.sh
@@ -6,8 +6,6 @@ set -ex
 # requires this current build to succeed before this test can use it; chicken and egg problem.
 docker build -t jaeger-cassandra-schema-integration-test plugin/storage/cassandra/
 docker run -e CQLSH_HOST=localhost -e CQLSH_PORT=9042 -e TEMPLATE=/cassandra-schema/$1.cql.tmpl --network=host jaeger-cassandra-schema-integration-test
-docker run -e CQLSH_HOST=localhost -e CQLSH_PORT=9043 -e TEMPLATE=/cassandra-schema/$2.cql.tmpl --network=host jaeger-cassandra-schema-integration-test
-docker run -e CQLSH_HOST=localhost -e CQLSH_PORT=9044 -e TEMPLATE=/cassandra-schema/$3.cql.tmpl --network=host jaeger-cassandra-schema-integration-test
 
 # Run the test.
 export STORAGE=cassandra
