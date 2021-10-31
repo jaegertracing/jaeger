996d1e0d0163f95ea5839ccf63c0cc6e5dfb23d7
Rodrigo Broggi <ro_broggi@hotmail.com>
2021-10-31 18:10:21 +0100

    reestablish valid comment
    Signed-off-by: rbroggi <ro_broggi@hotmail.com>

--- pkg/es/wrapper/wrapper_nolint.go
+++ pkg/es/wrapper/wrapper_nolint.go
@@ -16,6 +16,9 @@ package eswrapper
 
 import "github.com/jaegertracing/jaeger/pkg/es"
 
+// Some of the functions of elastic.BulkIndexRequest violate golint rules,
+// e.g. Id() should be ID() and BodyJson() should be BodyJSON().
+
 // Id calls this function to internal service.
 func (i IndexServiceWrapper) Id(id string) es.IndexService {
 	return WrapESIndexService(i.bulkIndexReq.Id(id), i.bulkService, i.esVersion)
