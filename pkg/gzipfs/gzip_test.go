30971118066d968d0c7f2e097058ccf83631ed2f
Ashmita Bohara <ashmita.bohara@logz.io>
2021-11-21 22:22:25 +1100

    Feedbacks
    Signed-off-by: Ashmita Bohara <ashmita.bohara@logz.io>

--- pkg/gzipfs/gzip_test.go
+++ pkg/gzipfs/gzip_test.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2021 The Jaeger Authors.
+// Copyright 2021 The Prometheus Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
