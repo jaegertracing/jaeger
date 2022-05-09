256f016b97c7283e0c2db10cac0a6cbc37614a26
Albert Teoh <see.kwang.teoh@gmail.com>
2022-05-09 20:34:53 +1000

    Add license
    Signed-off-by: Albert Teoh <see.kwang.teoh@gmail.com>

--- storage/metricsstore/metrics/decorator.go
+++ storage/metricsstore/metrics/decorator.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2022 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package metrics
 
 import (
@@ -10,7 +24,7 @@ import (
 	"github.com/jaegertracing/jaeger/storage/metricsstore"
 )
 
-// ReadMetricsDecorator wraps a spanstore.Reader and collects metrics around each read operation.
+// ReadMetricsDecorator wraps a metricsstore.Reader and collects metrics around each read operation.
 type ReadMetricsDecorator struct {
 	reader                    metricsstore.Reader
 	getLatenciesMetrics       *queryMetrics
