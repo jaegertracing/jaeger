2f7b5bab59a402de10ee5b0de24b66fad95c63bc
Yuri Shkuro <github@ysh.us>
2022-05-20 15:45:52 -0400

    imports
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/flags/service_test.go
+++ cmd/flags/service_test.go
@@ -19,8 +19,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/crossdock/crossdock-go/assert"
-	"github.com/crossdock/crossdock-go/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
 
 	"github.com/jaegertracing/jaeger/pkg/config"
