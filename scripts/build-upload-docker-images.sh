a026fdff7ab3353dfc8862d343af7ca9abdb9e23
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2022-08-02 12:23:52 -0400

    Build remote-storage (#3841)

--- scripts/build-upload-docker-images.sh
+++ scripts/build-upload-docker-images.sh
@@ -13,30 +13,19 @@ make build-binaries-arm64
 # build multi-arch docker images
 platforms="linux/amd64,linux/s390x,linux/ppc64le,linux/arm64"
 
-# build/upload images for release version of Jaeger backend components
-for component in agent collector query ingester
+# build/upload raw and debug images of Jaeger backend components
+for component in agent collector query ingester remote-storage
 do
 	bash scripts/build-upload-a-docker-image.sh -b -c "jaeger-${component}" -d "cmd/${component}" -p "${platforms}" -t release
+	bash scripts/build-upload-a-docker-image.sh -b -c "jaeger-${component}-debug" -d "cmd/${component}" -t debug
 done
 
-# build/upload images for jaeger-es-index-cleaner and jaeger-es-rollover
 bash scripts/build-upload-a-docker-image.sh -b -c jaeger-es-index-cleaner -d cmd/es-index-cleaner -p "${platforms}" -t release
 bash scripts/build-upload-a-docker-image.sh -b -c jaeger-es-rollover -d cmd/es-rollover  -p "${platforms}" -t release
+bash scripts/build-upload-a-docker-image.sh -c jaeger-cassandra-schema -d plugin/storage/cassandra/
 
 # build/upload images for jaeger-tracegen and jaeger-anonymizer
 for component in tracegen anonymizer
 do
 	bash scripts/build-upload-a-docker-image.sh -c "jaeger-${component}" -d "cmd/${component}" -p "${platforms}"
-done 
-
-
-# build amd64 docker images
-
-# build/upload images for debug version of Jaeger backend components
-for component in agent collector query ingester
-do
-	bash scripts/build-upload-a-docker-image.sh -b -c "jaeger-${component}-debug" -d "cmd/${component}" -t debug
 done
-
-# build/upload images for jaeger-cassandra-schema
-bash scripts/build-upload-a-docker-image.sh -c jaeger-cassandra-schema -d plugin/storage/cassandra/
