551f463dc74dc3468bfff3105c45cc6dedf232c8
Yuri Shkuro <github@ysh.us>
2023-01-15 21:40:19 -0500

    move
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/query/app/token_propagation_test.go
+++ cmd/query/app/token_propagation_test.go
@@ -16,20 +16,30 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/olivere/elastic"
+	"github.com/opentracing/opentracing-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+
+	"github.com/jaegertracing/jaeger/cmd/flags"
+	"github.com/jaegertracing/jaeger/cmd/query/app/querysvc"
+	"github.com/jaegertracing/jaeger/pkg/bearertoken"
+	"github.com/jaegertracing/jaeger/pkg/config"
+	"github.com/jaegertracing/jaeger/pkg/metrics"
+	"github.com/jaegertracing/jaeger/pkg/tenancy"
+	"github.com/jaegertracing/jaeger/plugin/storage/es"
+	"github.com/jaegertracing/jaeger/ports"
 )
 
 const (
 	bearerToken  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI"
 	bearerHeader = "Bearer " + bearerToken
-	queryUrlFmt     = "http://127.0.0.1:16686/api/services"
 )
 
 type elasticsearchHandlerMock struct {
@@ -37,49 +47,60 @@ type elasticsearchHandlerMock struct {
 }
 
 func (h *elasticsearchHandlerMock) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	authValue := r.Header.Get("Authorization")
-	if authValue != "" {
-		headerValue := strings.Split(authValue, " ")
-		token := ""
-		if len(headerValue) == 2 {
-			if headerValue[0] == "Bearer" {
-				token = headerValue[1]
-			}
-		} else if len(headerValue) == 1 {
-			token = authValue
-		}
-		assert.Equal(h.test, bearerToken, token)
-		if token == bearerToken {
-			// Return empty results, we don't care about the result here.
-			// we just need to make sure the token was propagated to the storage and the query-service returns 200
-			ret := new(elastic.SearchResult)
-			json_ret, _ := json.Marshal(ret)
-			w.Header().Add("Content-Type", "application/json; charset=UTF-8")
-			w.Write(json_ret)
-			return
-		}
+	if token, ok := bearertoken.GetBearerToken(r.Context()); ok && token == bearerToken {
+		// Return empty results, we don't care about the result here.
+		// we just need to make sure the token was propagated to the storage and the query-service returns 200
+		ret := new(elastic.SearchResult)
+		json_ret, _ := json.Marshal(ret)
+		w.Header().Add("Content-Type", "application/json; charset=UTF-8")
+		w.Write(json_ret)
+		return
 	}
+
 	// No token, return error!
 	http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 }
-
-// func createElasticsearchMock(srv *http.Server, t *testing.T) {
-// 	srv.Handler = &elasticsearchHandlerMock{
-// 		test: t,
-// 	}
-// 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-// 		assert.Error(t, err)
-// 	}
-// }
 
 func runMockElasticsearchServer(t *testing.T) *httptest.Server {
 	handler := &elasticsearchHandlerMock{
 		test: t,
 	}
-	return httptest.NewServer(handler)
+	return httptest.NewServer(
+		bearertoken.PropagationHandler(zaptest.NewLogger(t), handler),
+	)
 }
 
-func TestBearTokenPropagation(t *testing.T) {
+func runQueryService(t *testing.T, esURL string) *Server {
+	flagsSvc := flags.NewService(ports.QueryAdminHTTP)
+	flagsSvc.Logger = zaptest.NewLogger(t)
+
+	f := es.NewFactory()
+	v, command := config.Viperize(f.AddFlags)
+	require.NoError(t, command.ParseFlags([]string{
+		"--es.tls.enabled=false",
+		"--es.version=7",
+		"--es.server-urls=" + esURL,
+	}))
+	f.InitFromViper(v, flagsSvc.Logger)
+	// set AllowTokenFromContext manually because we don't register the respective CLI flag from query svc
+	f.Options.Primary.AllowTokenFromContext = true
+	require.NoError(t, f.Initialize(metrics.NullFactory, flagsSvc.Logger))
+
+	spanReader, err := f.CreateSpanReader()
+	require.NoError(t, err)
+
+	querySvc := querysvc.NewQueryService(spanReader, nil, querysvc.QueryServiceOptions{})
+	server, err := NewServer(flagsSvc.Logger, querySvc, nil,
+		&QueryOptions{GRPCHostPort: ":0", HTTPHostPort: ":0", BearerTokenPropagation: true},
+		tenancy.NewManager(&tenancy.Options{}),
+		opentracing.NoopTracer{},
+	)
+	require.NoError(t, err)
+	require.NoError(t, server.Start())
+	return server
+}
+
+func TestBearerTokenPropagation(t *testing.T) {
 	testCases := []struct {
 		name        string
 		headerValue string
@@ -90,23 +111,28 @@ func TestBearTokenPropagation(t *testing.T) {
 		{name: "X-Forwarded-Access-Token", headerName: "X-Forwarded-Access-Token", headerValue: bearerHeader},
 	}
 
-	srv := runMockElasticsearchServer(t)
-	defer srv.Close()
+	esSrv := runMockElasticsearchServer(t)
+	defer esSrv.Close()
+	t.Logf("mock ES server started on %s", esSrv.URL)
 
-	query :=
+	querySrv := runQueryService(t, esSrv.URL)
+	defer querySrv.Close()
+	queryAddr := querySrv.httpConn.Addr().String()
+	// Will try to load service names, this should return 200.
+	url := fmt.Sprintf("http://%s/api/services", queryAddr)
 
-	// Test cases.
 	for _, testCase := range testCases {
-		// Ask for services query, this should return 200
-		req, err := http.NewRequest("GET", queryUrl, nil)
-		require.NoError(t, err)
-		req.Header.Add(testCase.headerName, testCase.headerValue)
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		assert.NoError(t, err)
-		assert.NotNil(t, resp)
-		if err == nil && resp != nil {
+		t.Run(testCase.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", url, nil)
+			require.NoError(t, err)
+			req.Header.Add(testCase.headerName, testCase.headerValue)
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+
 			assert.Equal(t, resp.StatusCode, http.StatusOK)
-		}
+		})
 	}
 }
