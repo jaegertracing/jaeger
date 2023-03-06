2503333b3ccd77b7b983ec960953bfd65676b599
shubbham1215 <sawaikershubham@gmail.com>
2023-03-07 03:14:07 +0530

    4163 | Speed-up CI by using published UI artifacts
    Signed-off-by: shubbham1215 <sawaikershubham@gmail.com>

--- scripts/rebuild-ui.sh
+++ scripts/rebuild-ui.sh
@@ -9,13 +9,10 @@ if [[ "$tag" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]];  then
     temp_file=$(mktemp)
     trap "rm -f ${temp_file}" EXIT
     release_url="https://github.com/jaegertracing/jaeger-ui/releases/download/${tag}/assets.tar.gz"
-
-     if curl -O "$release_url"; then
-        curl --location --output "$temp_file" "$release_url"
-        mkdir -p packages/jaeger-ui/build/
-        tar -zxvf "$temp_file" packages/jaeger-ui/build/
-        exit 0
-    fi
+    curl --location --output "$temp_file" "$release_url"
+    mkdir -p packages/jaeger-ui/build/
+    tar -zxvf "$temp_file" packages/jaeger-ui/build/
+    exit 0
 fi
 
 # do a regular full build
