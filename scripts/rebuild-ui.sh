af13a21789c115a22b64193307e9eebeef902f61
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2023-03-06 22:41:47 -0500

    Check for curl exit status
    Signed-off-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>

--- scripts/rebuild-ui.sh
+++ scripts/rebuild-ui.sh
@@ -9,10 +9,11 @@ if [[ "$tag" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]];  then
     temp_file=$(mktemp)
     trap "rm -f ${temp_file}" EXIT
     release_url="https://github.com/jaegertracing/jaeger-ui/releases/download/${tag}/assets.tar.gz"
-    curl --location --output "$temp_file" "$release_url"
-    mkdir -p packages/jaeger-ui/build/
-    tar -zxvf "$temp_file" packages/jaeger-ui/build/
-    exit 0
+    if curl --silent --fail --location --output "$temp_file" "$release_url"; then
+        mkdir -p packages/jaeger-ui/build/
+        tar -zxvf "$temp_file" packages/jaeger-ui/build/
+        exit 0
+    fi
 fi
 
 # do a regular full build
