bc8e1d16524dcbdac80683ebc007e22cac8406c7
Rakshit Parashar <34675136+ChillOrb@users.noreply.github.com>
2023-03-27 18:39:34 -0700

    Race condition detected in tests #4340 (#4344)
    ## Which problem is this PR solving?
    - Resolves #4340 
    
    ## Short description of the changes
    - Server in test case was not closed
    
    ---------
    
    Signed-off-by: ChillOrb <rakshitparashar1@gmail.com>

--- cmd/collector/app/server/http_test.go
+++ cmd/collector/app/server/http_test.go
@@ -275,6 +275,7 @@ func TestStartHTTPServerParams(t *testing.T) {
 
 	server, err := StartHTTPServer(params)
 	require.NoError(t, err)
+	defer server.Close()
 	assert.Equal(t, 5*time.Minute, server.IdleTimeout)
 	assert.Equal(t, 6*time.Minute, server.ReadTimeout)
 	assert.Equal(t, 7*time.Second, server.ReadHeaderTimeout)
