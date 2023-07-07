29a75e962f8ea33b896c13a9bbe77e7d26f8596d
Afzal Ansari <afzal442@gmail.com>
2023-07-07 19:40:58 +0000

    adds OTLP to jtracer
    Signed-off-by: Afzal Ansari <afzal442@gmail.com>

--- pkg/jtracer/jtracer.go
+++ pkg/jtracer/jtracer.go
@@ -14,16 +14,57 @@
 
 package jtracer
 
-import "github.com/opentracing/opentracing-go"
+import (
+	"context"
+	"fmt"
+
+	"github.com/opentracing/opentracing-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+)
 
 type JTracer struct {
-	OT opentracing.Tracer
+	OT   opentracing.Tracer
+	OTEL trace.TracerProvider
 }
 
 func OT(t opentracing.Tracer) JTracer {
 	return JTracer{OT: t}
 }
 
+func OTEL(tp trace.TracerProvider) JTracer {
+	return JTracer{OTEL: tp}
+}
+
 func NoOp() JTracer {
 	return JTracer{OT: opentracing.NoopTracer{}}
+}
+
+func NoOpOtel() JTracer {
+	return JTracer{OTEL: trace.NewNoopTracerProvider()}
+}
+
+func NewOTELProvider() (trace.TracerProvider, error) {
+	opts := []otlptracehttp.Option{otlptracehttp.WithInsecure()}
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracehttp.NewClient(opts...),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create exporter: %w", err)
+	}
+
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	return tracerProvider, nil
 }
