58e2bdb087a5d58f2ba2f986105a3dabeddf9727
bugslayer-332 <ayashwanth9503@gmail.com>
2023-07-15 12:51:47 +0530

    added a new pkg,corscfg to enable cors on OTLP and Zipkin
    Signed-off-by: bugslayer-332 <ayashwanth9503@gmail.com>

---
+++ pkg/config/corscfg/flags.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2019 The Jaeger Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package corscfg
+
+import (
+	"flag"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	corsPrefix         = ".cors"
+	corsAllowedHeaders = corsPrefix + ".allowed-headers"
+	corsAllowedOrigins = corsPrefix + ".allowed-origins"
+)
+
+type CorsFlagsConfig struct {
+	Prefix string
+}
+
+func (c CorsFlagsConfig) AddFlags(flags *flag.FlagSet) {
+	flags.String(c.Prefix+corsAllowedHeaders, "content-type", "Allowed headers for the HTTP port , default content-type")
+	flags.String(c.Prefix+corsAllowedOrigins, "*", "Allowed origins for the HTTP port , default accepts all")
+}
+
+func (c CorsFlagsConfig) InitFromViper(v *viper.Viper) (Settings, error) {
+	var p Settings
+	p.AllowedHeaders = v.GetStringSlice(c.Prefix + corsAllowedHeaders)
+	p.AllowedOrigins = v.GetStringSlice(c.Prefix + corsAllowedOrigins)
+	return p, nil
+}
