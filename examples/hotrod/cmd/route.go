96f7fdcbbfe0308a8f679dafe1058486c1ecb4b2
Afzal Ansari <afzal442@gmail.com>
2023-07-16 05:19:38 +0530

    [hotrod] Upgrade HotROD to use OpenTelemetry instrumentation (#4548)
    ## Which problem is this PR solving?
    * Part of #3380
    * This PR tries to instrument the hotROD app with otel downgrading
    opentracing. Related
    https://github.com/jaegertracing/jaeger/pull/4533#discussion_r1234363250
    
    ## Short description of the changes
    - Upgrades hotROD application to support `otel tracer`
    
    ---------
    
    Signed-off-by: Afzal Ansari <afzal442@gmail.com>
    Signed-off-by: afzal442 <afzal442@gmail.com>
    Signed-off-by: Afzal <94980910+afzalbin64@users.noreply.github.com>
    Co-authored-by: Afzal <94980910+afzalbin64@users.noreply.github.com>

--- examples/hotrod/cmd/route.go
+++ examples/hotrod/cmd/route.go
@@ -37,7 +37,7 @@ var routeCmd = &cobra.Command{
 		logger := log.NewFactory(zapLogger)
 		server := route.NewServer(
 			net.JoinHostPort("0.0.0.0", strconv.Itoa(routePort)),
-			tracing.Init("route", otelExporter, metricsFactory, logger),
+			tracing.InitOTEL("route", otelExporter, metricsFactory, logger),
 			logger,
 		)
 		return logError(zapLogger, server.Run())
--- examples/hotrod/pkg/tracing/http.go
+++ examples/hotrod/pkg/tracing/http.go
@@ -22,26 +22,35 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/opentracing-contrib/go-stdlib/nethttp"
-	"github.com/opentracing/opentracing-go"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // HTTPClient wraps an http.Client with tracing instrumentation.
 type HTTPClient struct {
-	Tracer opentracing.Tracer
-	Client *http.Client
+	TracerProvider trace.TracerProvider
+	Client         *http.Client
+}
+
+func NewHTTPClient(tp trace.TracerProvider) *HTTPClient {
+	return &HTTPClient{
+		TracerProvider: tp,
+		Client: &http.Client{
+			Transport: otelhttp.NewTransport(
+				http.DefaultTransport,
+				otelhttp.WithTracerProvider(tp),
+			),
+		},
+	}
 }
 
 // GetJSON executes HTTP GET against specified url and tried to parse
 // the response into out object.
 func (c *HTTPClient) GetJSON(ctx context.Context, endpoint string, url string, out interface{}) error {
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return err
 	}
-	req = req.WithContext(ctx)
-	req, ht := nethttp.TraceRequest(c.Tracer, req, nethttp.OperationName("HTTP GET: "+endpoint))
-	defer ht.Finish()
 
 	res, err := c.Client.Do(req)
 	if err != nil {
@@ -57,6 +66,7 @@ func (c *HTTPClient) GetJSON(ctx context.Context, endpoint string, url string, o
 		}
 		return errors.New(string(body))
 	}
+
 	decoder := json.NewDecoder(res.Body)
 	return decoder.Decode(out)
 }
