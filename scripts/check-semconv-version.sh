35b30cf79e9992f9e5ef305cc24b0aec85f8ab10
Harish Shan <140232061+perhapsmaple@users.noreply.github.com>
2023-08-13 02:36:45 -0400

    Refactor shell scripts to address review
    - Introduced variables for package name and regex expression
    - Replaced unnecessary grep to extract version number with bash parameter expansion
    - Moved print "Run <script>" to the end of the output and addressed alignment
    
    Signed-off-by: Harish Shan <140232061+perhapsmaple@users.noreply.github.com>

--- scripts/check-semconv-version.sh
+++ scripts/check-semconv-version.sh
@@ -2,20 +2,22 @@
 
 declare -A semconv_map
 
+package_name="go.opentelemetry.io/otel/semconv"
+version_regex="v[0-9]\.[0-9]\+\.[0-9]\+"
+
 while IFS=: read -r file_name package_string; do
-    version_number=$(echo "$package_string" | grep -o -E "v1\.[0-9]+\.[0-9]+")
-    semconv_map["$file_name"]=$version_number
-done < <(find . -type f -name "*.go" -exec grep -o -H "go.opentelemetry.io/otel/semconv/v1\.[0-9]\+\.[0-9]\+" {} +)
+    semconv_map["$file_name"]="${package_string##*/}"
+done < <(find . -type f -name "*.go" -exec grep -o -H "$package_name/$version_regex" {} +)
 
 semconv_versions=($(printf "%s\n" "${semconv_map[@]}" | sort -u))
 
 if [ ${#semconv_versions[@]} -gt 1 ]; then
-    echo "Error: semconv version mismatch detected. Run ./scripts/update-semconv-version.sh to update semconv to latest version."
+    echo "Error: semconv version mismatch detected."
     {
         for key in "${!semconv_map[@]}"; do
-            printf "Source File: %-30s | Semconv Version: %s\n" "$key" "${semconv_map[$key]}"
+            printf "Source File: %-50s | Semconv Version: %s\n" "$key" "${semconv_map[$key]}"
         done
     } | column -t -s '|'
-
+    echo "Run ./scripts/update-semconv-version.sh to update semconv to latest version."
     exit 1
 fi
--- scripts/update-semconv-version.sh
+++ scripts/update-semconv-version.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+package_name="go.opentelemetry.io/otel/semconv"
+version_regex="v[0-9]\.[0-9]\+\.[0-9]\+"
+
 latest_semconv_version=$(
-    curl -s https://pkg.go.dev/go.opentelemetry.io/otel/semconv \
+    curl -s https://pkg.go.dev/$package_name \
     | grep -oP 'data-id="v\d+\.\d+\.\d+"' \
-    | sed -E 's/data-id="v([0-9]+\.[0-9]+\.[0-9]+)"/v\1/' \
+    | sed -E 's/\"($version_regex)\"/v\1/' \
     | sort -Vr \
-    | head -n 1
+    | head -n 1 \
+    | awk -F'"' '{print $2}'
 )
 
-latest_package_string="go.opentelemetry.io/otel/semconv/$latest_semconv_version"
+latest_package_string="$package_name/$latest_semconv_version"
 
 while IFS=: read -r file_name package_string; do
-    version_number=$(echo "$package_string" | grep -o -E "v1\.[0-9]+\.[0-9]+")
+    version_number=${package_string##*/}
 
     if [ "$version_number" != "$latest_semconv_version" ]; then
-	sed -i "s#go\.opentelemetry\.io\/otel\/semconv\/v[0-9]\+\.[0-9]\+\.[0-9]\+#$latest_package_string#g" "$file_name"
+	sed -i "s#$package_name/$version_regex#$latest_package_string#g" "$file_name"
 	{
             printf "Source File: %-60s | Previous Semconv Version: %s | Updated Semconv Version: %s\n" "$file_name" "$version_number" "$latest_semconv_version"
         } | column -t -s '|'
     fi
-done < <(find . -type f -name "*.go" -exec grep -o -H "go.opentelemetry.io/otel/semconv/v1\.[0-9]\+\.[0-9]\+" {} +)
+done < <(find . -type f -name "*.go" -exec grep -o -H "$package_name/$version_regex" {} +)
