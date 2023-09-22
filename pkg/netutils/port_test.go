af70f261831a8c415f460cdc8071e932793a1be7
Pavol Loffay <p.loffay@gmail.com>
2023-09-22 17:17:47 +0200

    Support HTTP proxy env variables (#4769)
    Support HTTP proxy variables (some docs
    https://superuser.com/questions/944958/are-http-proxy-https-proxy-and-no-proxy-environment-variables-standard).
    
    The codebase already supports it, but some places were missed. There
    were also two problems:
    1. query grpc gateway was connecting to :16686 which is not recognized
    by NO_PROXY
    2. grpc reporter in agent when used with all-in-one was connecting to
    `:14250` also not recognized by NO_PROXY
    
    
    Example of proxy vars set on OpenShift
    ```
     - name: HTTP_PROXY
                  value: >-
                    http://proxy-user1:foo@ec2-32-138-32-212.us-east-2.compute.amazonaws.com:3128
                - name: http_proxy
                  value: >-
                    http://proxy-user1:foo@ec2-32-138-32-212.us-east-2.compute.amazonaws.com:3128
                - name: HTTPS_PROXY
                  value: >-
                    http://proxy-user1:foo@ec2-32-138-32-221.us-east-2.compute.amazonaws.com:3128
                - name: https_proxy
                  value: >-
                    http://proxy-user1:foo@ec2-32-138-32-212.us-east-2.compute.amazonaws.com:3128
                - name: NO_PROXY
                  value: >-
                    .cluster.local,.svc,.us-east-2.compute.internal,10.0.0.0/16,10.128.0.0/14,127.0.0.1,169.254.169.254,172.30.0.0/16,api-int.rererer.qe.devcluster.openshift.com,localhost,test.no-proxy.com
                - name: no_proxy
                  value: >-
                    .cluster.local,.svc,.us-east-2.compute.internal,10.0.0.0/16,10.128.0.0/14,127.0.0.1,169.254.169.254,172.30.0.0/16,api-int.rerererer.qe.devcluster.openshift.com,localhost,test.no-proxy.com
    ```
    
    PR is related to
    https://github.com/jaegertracing/jaeger-operator/pull/2330
    ```
    make build-binaries-linux
    DOCKER_NAMESPACE=pavolloffay DOCKER_TAG=proxy-3 make docker-images-jaeger-backend
    ```
    
    ---------
    
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/netutils/port_test.go
+++ pkg/netutils/port_test.go
@@ -66,3 +66,9 @@ func TestGetPort(t *testing.T) {
 		}
 	}
 }
+
+func TestFixLocalhost(t *testing.T) {
+	endpoints := []string{"collector:1111", ":2222"}
+	fixed := FixLocalhost(endpoints)
+	assert.Equal(t, []string{"collector:1111", "localhost:2222"}, fixed)
+}
