555610d2e242b4936a3b5363463d647ec41a618d
Yuri Shkuro <github@ysh.us>
2023-10-18 19:42:42 -0400

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- scripts/build-upload-a-docker-image.sh
+++ scripts/build-upload-a-docker-image.sh
@@ -64,11 +64,9 @@ fi
 # Some of the variables can be blank and should not produce extra arguments,
 # so we need to disable the linter checks for quoting.
 # TODO: collect arguments into an array and add optional once conditionally
-# shellcheck disable=SC208
-docker buildx build --output "${PUSHTAG}" \
+# shellcheck disable=SC2086
+docker buildx build --output "${PUSHTAG}" ${target_arg} ${base_debug_img_arg} \
 	--progress=plain \
-	${target_arg} \
-	${base_debug_img_arg} \
 	--platform="${platforms}" \
 	--file "${docker_file_arg}" \
 	"${IMAGE_TAGS[@]}" \
