f3fa1c0515eda36f986a8e742d335d18901ffe89
Yuri Shkuro <github@ysh.us>
2023-10-18 18:38:41 -0400

    treat IMAGE_TAGS as array
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- scripts/build-crossdock.sh
+++ scripts/build-crossdock.sh
@@ -11,7 +11,7 @@ make build-and-run-crossdock
 if [[ "$BRANCH" == "main" ]]; then
   echo 'upload images to dockerhub/quay.io'
   REPO=jaegertracing/test-driver
-  IMAGE_TAGS=$(bash scripts/compute-tags.sh $REPO)
+  IFS=" " read -r -a IMAGE_TAGS <<< "$(bash scripts/compute-tags.sh '$REPO')"
   IMAGE_TAGS+=("--tag" "docker.io/${REPO}:${COMMIT}" "--tag" "quay.io/${REPO}:${COMMIT}")
   bash scripts/docker-login.sh
   docker buildx build --push \
