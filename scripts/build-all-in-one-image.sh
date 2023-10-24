e8a0a0451611a412350e5f82d7523d347ff16e33
Yuri Shkuro <github@ysh.us>
2023-10-23 20:11:34 -0400

    ci: Test jaeger-v2 as all-in-one in CI
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- scripts/build-all-in-one-image.sh
+++ scripts/build-all-in-one-image.sh
@@ -2,12 +2,20 @@
 
 set -exu
 
+if [[ "$1" == "pr-only" ]]; then
+  pull_request="1"
+else
+  pull_request=""
+fi
+
+# alternative can be jaeger-v2
+BINARY=${BINARY:-'all-in-one'}
+
 # Set default GOARCH variable to the host GOARCH, the target architecture can
 # be overrided by passing architecture value to the script:
 # `GOARCH=<target arch> ./scripts/build-all-in-one-image.sh`.
 GOARCH=${GOARCH:-$(go env GOARCH)}
-mode=${1-main}
-repo=jaegertracing/all-in-one
+repo="jaegertracing/${BINARY}"
 
 # verify Node.js version
 expected_version="v$(cat jaeger-ui/.nvmrc)"
@@ -35,29 +43,34 @@ run_integration_test() {
   docker kill "$CID"
 }
 
-if [ "$mode" = "pr-only" ]; then
+if [ -n $pull_request ]; then
   make create-baseimg
-  # build architecture for linux/amd64 only for pull requests
-  platforms="linux/amd64"
-  make build-all-in-one GOOS=linux GOARCH=amd64
+  # build current architecture only for pull requests
+  platforms="linux/${GOARCH}"
+  make "build-${BINARY}" GOOS=linux "GOARCH=${GOARCH}"
 else
   make create-baseimg-debugimg
   platforms="linux/amd64,linux/s390x,linux/ppc64le,linux/arm64"
-  make build-all-in-one GOOS=linux GOARCH=amd64
-  make build-all-in-one GOOS=linux GOARCH=s390x
-  make build-all-in-one GOOS=linux GOARCH=ppc64le
-  make build-all-in-one GOOS=linux GOARCH=arm64
+  make "build-${BINARY}" GOOS=linux GOARCH=amd64
+  make "build-${BINARY}" GOOS=linux GOARCH=s390x
+  make "build-${BINARY}" GOOS=linux GOARCH=ppc64le
+  make "build-${BINARY}" GOOS=linux GOARCH=arm64
 fi
 
 # build all-in-one image locally for integration test
-bash scripts/build-upload-a-docker-image.sh -l -b -c all-in-one -d cmd/all-in-one -p "${platforms}" -t release
+bash scripts/build-upload-a-docker-image.sh -l -b -c "${BINARY}" -d "cmd/${BINARY}" -p "${platforms}" -t release
 run_integration_test localhost:5000/$repo
+
+# skip building and uploading real Docker images if it's not all-in-one
+if [[ "${BINARY}" != "all-in-one" ]]; then
+  exit
+fi
 
 # build all-in-one image and upload to dockerhub/quay.io
 bash scripts/build-upload-a-docker-image.sh -b -c all-in-one -d cmd/all-in-one -p "${platforms}" -t release
 
 # build debug image if not on a pull request
-if [ "$mode" != "pr-only" ]; then
+if [ -z $pull_request ]; then
   make build-all-in-one GOOS=linux GOARCH="$GOARCH" DEBUG_BINARY=1
   repo=${repo}-debug
 
