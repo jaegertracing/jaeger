107c91b1c874af1e9c48cc110b41f13b603dc2e9
James Ryans <46216691+james-ryans@users.noreply.github.com>
2023-10-28 00:13:49 +0700

    Add reload interval to OTel server certificates (#4898)
    ## Which problem is this PR solving?
    - Resolves #4554 
    
    ## Description of the changes
    - Enable reload interval functionality to OTel server certificates by
    adding `otlp.http.tls.reload-interval` and
    `otlp.grpc.tls.reload-interval` flags (0s means disabled)
    
    ## How was this change tested?
    - Tested manually in local
    - Added `*.tls.reload-interval` flag unit tests to `flags.go` and
    `otel_receiver.go`
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `yarn lint` and `yarn test`
    
    ---------
    
    Signed-off-by: James Ryans <james.ryans2012@gmail.com>

--- pkg/config/tlscfg/flags.go
+++ pkg/config/tlscfg/flags.go
@@ -35,6 +35,7 @@ const (
 	tlsCipherSuites   = tlsPrefix + ".cipher-suites"
 	tlsMinVersion     = tlsPrefix + ".min-version"
 	tlsMaxVersion     = tlsPrefix + ".max-version"
+	tlsReloadInterval = tlsPrefix + ".reload-interval"
 )
 
 // ClientFlagsConfig describes which CLI flags for TLS client should be generated.
@@ -44,7 +45,8 @@ type ClientFlagsConfig struct {
 
 // ServerFlagsConfig describes which CLI flags for TLS server should be generated.
 type ServerFlagsConfig struct {
-	Prefix string
+	Prefix                   string
+	EnableCertReloadInterval bool
 }
 
 // AddFlags adds flags for TLS to the FlagSet.
@@ -66,6 +68,9 @@ func (c ServerFlagsConfig) AddFlags(flags *flag.FlagSet) {
 	flags.String(c.Prefix+tlsCipherSuites, "", "Comma-separated list of cipher suites for the server, values are from tls package constants (https://golang.org/pkg/crypto/tls/#pkg-constants).")
 	flags.String(c.Prefix+tlsMinVersion, "", "Minimum TLS version supported (Possible values: 1.0, 1.1, 1.2, 1.3)")
 	flags.String(c.Prefix+tlsMaxVersion, "", "Maximum TLS version supported (Possible values: 1.0, 1.1, 1.2, 1.3)")
+	if c.EnableCertReloadInterval {
+		flags.Duration(c.Prefix+tlsReloadInterval, 0, "The duration after which the certificate will be reloaded (0s means will not be reloaded)")
+	}
 }
 
 // InitFromViper creates tls.Config populated with values retrieved from Viper.
@@ -100,6 +105,7 @@ func (c ServerFlagsConfig) InitFromViper(v *viper.Viper) (Options, error) {
 	}
 	p.MinVersion = v.GetString(c.Prefix + tlsMinVersion)
 	p.MaxVersion = v.GetString(c.Prefix + tlsMaxVersion)
+	p.ReloadInterval = v.GetDuration(c.Prefix + tlsReloadInterval)
 
 	if !p.Enabled {
 		var empty Options
