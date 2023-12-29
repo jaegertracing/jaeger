ffb5a580321650f1b111d279db2d3d7900e3c3e4
Albert Teoh <albert@packsmith.io>
2023-12-30 09:32:46 +1100

    Fail test
    Signed-off-by: Albert Teoh <albert@packsmith.io>

--- empty_test.go
+++ empty_test.go
@@ -15,6 +15,7 @@
 package jaeger
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"go.uber.org/goleak"
@@ -23,6 +24,7 @@ import (
 func TestDummy(t *testing.T) {
 	// This is a dummy test in the root package.
 	// Without it `go test -v .` prints "testing: warning: no tests to run".
+	assert.FailNow(t, "failing to test out summary output")
 }
 
 func TestMain(m *testing.M) {
