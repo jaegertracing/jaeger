5c6348e171a2c7a42949e0495675b2bbb02bc6f5
Harshvir Potpose <122517264+akagami-harsh@users.noreply.github.com>
2023-12-31 02:27:23 +0530

    Fix Goroutine leaks in several packages (#5066)
    ## Which problem is this PR solving?
    - fix gorourine leak in some packages 
    
    ## Description of the changes
    - part of #5006 
    
    ## How was this change tested?
    - go test
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
    
    ---------
    
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- plugin/storage/package_test.go
+++ plugin/storage/package_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The Jaeger Authors.
+// Copyright (c) 2023 The Jaeger Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,27 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package env
+package storage
 
 import (
-	"bytes"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"go.uber.org/goleak"
 
 	"github.com/jaegertracing/jaeger/pkg/testutils"
 )
-
-func TestCommand(t *testing.T) {
-	cmd := Command()
-	buf := new(bytes.Buffer)
-	cmd.SetOut(buf)
-	cmd.Run(cmd, nil)
-	assert.True(t, strings.Contains(buf.String(), "METRICS_BACKEND"))
-	assert.True(t, strings.Contains(buf.String(), "SPAN_STORAGE"))
-}
 
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m, testutils.IgnoreGlogFlushDaemonLeak())
