a2693ee42c9a80bea135e51b7e5756410b507f96
Harshvir Potpose <hpotpose62@gmail.com>
2024-01-03 12:57:33 +0530

    add goleak check and run make fmt
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- cmd/jaeger/internal/package_test.go
+++ cmd/jaeger/internal/package_test.go
@@ -17,19 +17,11 @@ package internal
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
+
+	"github.com/jaegertracing/jaeger/pkg/testutils"
 )
 
-func TestCommand(t *testing.T) {
-	cmd := Command()
-
-	assert.NotNil(t, cmd, "Command() should return a non-nil *cobra.Command instance")
-
-	expectedShortDescription := "Jaeger backend v2"
-	assert.Equal(t, expectedShortDescription, cmd.Short, "Command short description should be '%s'", expectedShortDescription)
-
-	expectedLongDescription := "Jaeger backend v2"
-	assert.Equal(t, expectedLongDescription, cmd.Long, "Command long description should be '%s'", expectedLongDescription)
-
-	assert.NotNil(t, cmd.RunE, "Command should have RunE function set")
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m, testutils.IgnoreOpenCensusWorkerLeak())
 }
