ca69919ba24abfb5630bf710c6a94791e22e5db3
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-01-04 22:38:55 +0530

    fix
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- examples/hotrod/services/config/empty_test.go
+++ examples/hotrod/services/config/empty_test.go
@@ -20,11 +20,6 @@ import (
 	"go.uber.org/goleak"
 )
 
-func TestDummy(t *testing.T) {
-	// This is a dummy test in the root package.
-	// Without it `go test -v .` prints "testing: warning: no tests to run".
-}
-
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m)
 }
