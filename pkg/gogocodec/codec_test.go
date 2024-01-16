0a89afa6c15555d0a1d821fb52681fef54f16894
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-01-16 11:41:10 +0530

    fix
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- pkg/gogocodec/codec_test.go
+++ pkg/gogocodec/codec_test.go
@@ -33,6 +33,7 @@ func TestCodecMarshallAndUnmarshall_jaeger_type(t *testing.T) {
 	data, err := c.Marshal(s1)
 	require.NoError(t, err)
 	s2 := &model.Span{}
+
 	err = c.Unmarshal(data, s2)
 	require.NoError(t, err)
 	assert.Equal(t, s1, s2)
@@ -43,6 +44,7 @@ func TestCodecMarshallAndUnmarshall_no_jaeger_type(t *testing.T) {
 	goprotoMessage1 := &emptypb.Empty{}
 	data, err := c.Marshal(goprotoMessage1)
 	require.NoError(t, err)
+
 	goprotoMessage2 := &emptypb.Empty{}
 	err = c.Unmarshal(data, goprotoMessage2)
 	require.NoError(t, err)
@@ -54,12 +56,15 @@ func TestWireCompatibility(t *testing.T) {
 	s1 := &model.Span{OperationName: "foo", TraceID: model.NewTraceID(1, 2)}
 	data, err := c.Marshal(s1)
 	require.NoError(t, err)
+
 	var goprotoMessage emptypb.Empty
 	err = proto.Unmarshal(data, &goprotoMessage)
 	require.NoError(t, err)
+
 	data2, err := proto.Marshal(&goprotoMessage)
 	require.NoError(t, err)
 	s2 := &model.Span{}
+
 	err = c.Unmarshal(data2, s2)
 	require.NoError(t, err)
 	assert.Equal(t, s1, s2)
