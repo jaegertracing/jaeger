0ac22dcedd3bafd452e61ae82b7516adf8177d30
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-01-16 11:27:15 +0530

    lint fix
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- pkg/gogocodec/codec_test.go
+++ pkg/gogocodec/codec_test.go
@@ -36,6 +36,7 @@ func TestCodecMarshallAndUnmarshall_jaeger_type(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, s1, s2)
 }
+
 func TestCodecMarshallAndUnmarshall_no_jaeger_type(t *testing.T) {
 	c := newCodec()
 	goprotoMessage1 := &emptypb.Empty{}
@@ -46,6 +47,7 @@ func TestCodecMarshallAndUnmarshall_no_jaeger_type(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, goprotoMessage1, goprotoMessage2)
 }
+
 func TestWireCompatibility(t *testing.T) {
 	c := newCodec()
 	s1 := &model.Span{OperationName: "foo", TraceID: model.NewTraceID(1, 2)}
@@ -61,6 +63,7 @@ func TestWireCompatibility(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, s1, s2)
 }
+
 func TestUseGogo(t *testing.T) {
 	assert.False(t, useGogo(nil))
 
