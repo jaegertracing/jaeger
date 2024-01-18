43ec3c7b3cf901d27e799a3e6e6436c98077d16a
Harshvir Potpose <hpotpose62@gmail.com>
2024-01-18 23:57:52 +0530

    change name to badger_primary
    Signed-off-by: Harshvir Potpose <hpotpose62@gmail.com>

--- cmd/jaeger/internal/extension/jaegerstorage/config.go
+++ cmd/jaeger/internal/extension/jaegerstorage/config.go
@@ -11,7 +11,7 @@ import (
 // Config has the configuration for jaeger-query,
 type Config struct {
 	Memory map[string]memoryCfg.Configuration   `mapstructure:"memory"`
-	Badger map[string]badgerCfg.NamespaceConfig `mapstructure:"badger"`
+	Badger map[string]badgerCfg.NamespaceConfig `mapstructure:"badger_primary"`
 	// TODO add other storage types here
 	// TODO how will this work with 3rd party storage implementations?
 	//      Option: instead of looking for specific name, check interface.
