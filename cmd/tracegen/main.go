8c00cc14523f08d3ec8f729ce7f2f02fb3b9c193
Chirag Ghosh <75582834+chirag-ghosh@users.noreply.github.com>
2024-01-24 09:06:16 +0530

    Upgrade OpenTelemetry SDK to v1.22.0 (#5137)
    ## Which problem is this PR solving?
    - Fixes #5116 
    - Replaces #5115 
    
    ## Description of the changes
    - Upgrade OTel SDK to v1.22 (dependabot)
    - Upgrade semconv version to v1.24 ([Reference
    issue](https://github.com/open-telemetry/opentelemetry-go/issues/2341)).
    I tried using semconv v1.22 but it was still failing.
    - Replace deprecated function `semconv.HTTPStatusCodeKey` with
    `semconv.HTTPResponseStatusCodeKey`
    
    ## How was this change tested?
    - 
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `yarn lint` and `yarn test`
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- cmd/tracegen/main.go
+++ cmd/tracegen/main.go
@@ -29,7 +29,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
