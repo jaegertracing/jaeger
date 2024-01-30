bab80b1e53d3ec13e9e50ebcedcce6bd6123712b
Wise-Wizard <saransh.shankar@gmail.com>
2024-01-30 11:45:06 +0530

    Deleted Nocover file
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- pkg/cassandra/gocql/testutils/udt_test.go
+++ pkg/cassandra/gocql/testutils/udt_test.go
@@ -1,24 +1,10 @@
-// Copyright (c) 2019 The Jaeger Authors.
-// Copyright (c) 2017 Uber Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package testutils_test
 
 import (
 	"testing"
 
 	"github.com/gocql/gocql"
+
 	"github.com/jaegertracing/jaeger/pkg/cassandra/gocql/testutils"
 )
 
@@ -53,7 +39,6 @@ func (c *CustomUDT) UnmarshalUDT(name string, info gocql.TypeInfo, data []byte) 
 }
 
 func TestUDTTestCase(t *testing.T) {
-	// Create an example UDT instance
 	udtInstance := &CustomUDT{
 		Field1: 42,
 		Field2: "test",
@@ -73,7 +58,6 @@ func TestUDTTestCase(t *testing.T) {
 			ValIn: []byte("test"),
 			Err:   false,
 		},
-		// Add more UDTField entries as needed
 	}
 
 	// Create a UDTTestCase
@@ -84,6 +68,5 @@ func TestUDTTestCase(t *testing.T) {
 		Fields:  udtFields,
 	}
 
-	// Run the test case
 	testCase.Run(t)
 }
