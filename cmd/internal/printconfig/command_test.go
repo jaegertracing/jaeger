95d44785aac0d998b5373ee6f75e8d793bc66e7d
Guilherme Mafra da Costa <g.mafra.costa@gmail.com>
2024-02-17 12:45:49 -0300

    Adding new values and a new test for the all flag
    Signed-off-by: Guilherme Mafra da Costa <g.mafra.costa@gmail.com>

--- cmd/internal/printconfig/command_test.go
+++ cmd/internal/printconfig/command_test.go
@@ -16,45 +16,76 @@ package printconfig
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/jaegertracing/jaeger/pkg/testutils"
 )
 
-func TestCommand(t *testing.T) {
-	expected := `-------------------------------------------------
-| Configuration Option Name Value Source        |
--------------------------------------------------
-| test_metrics              1     user-assigned |
-| test_num_works            0     user-assigned |
-| test_reporter_log_spans   true  default       |
--------------------------------------------------
+func TestAllFlag(t *testing.T) {
+	expected := `-------------------------------------------------------
+| Configuration Option Name       Value Source        |
+-------------------------------------------------------
+| metrics_enabled_test                  user-assigned |
+| new_feature_test                      default       |
+| num_works_test                  5     user-assigned |
+| reporter_log_spans_enabled_test true  default       |
+| status.http.host_port_test      :8080 user-assigned |
+-------------------------------------------------------
 `
 
 	v := viper.New()
-
-	v.Set("TEST_METRICS", "1")
-	v.Set("TEST_NUM_WORKS", "0")
-	pflag.Bool("TEST_REPORTER_LOG_SPANS", true, "")
-	v.BindPFlags(pflag.CommandLine)
-
-	buf := new(bytes.Buffer)
-	printCmd := Command(v)
-	printCmd.SetOut(buf)
-	_, err := printCmd.ExecuteC()
-	if err != nil {
-		require.NoError(t, err, "printCmd.ExecuteC() returned the error %v", err)
-	}
-
-	actual := buf.String()
+	setConfig(v)
+	actual := runPrintConfigCommand(v, t, true)
 	assert.Equal(t, expected, actual)
 }
 
-func TestMain(m *testing.M) {
-	testutils.VerifyGoLeaks(m)
+func TestPrintConfigCommand(t *testing.T) {
+	expected := `-------------------------------------------------------
+| Configuration Option Name       Value Source        |
+-------------------------------------------------------
+| num_works_test                  5     user-assigned |
+| reporter_log_spans_enabled_test true  default       |
+| status.http.host_port_test      :8080 user-assigned |
+-------------------------------------------------------
+`
+	v := viper.New()
+	setConfig(v)
+	actual := runPrintConfigCommand(v, t, false)
+	assert.Equal(t, expected, actual)
+}
+
+func setConfig(v *viper.Viper) {
+
+	v.Set("STATUS.HTTP.HOST_PORT_TEST", ":8080")
+	v.Set("METRICS_ENABLED_TEST", "")
+	v.Set("NEW_FEATURE_TEST", nil)
+
+	if flag := pflag.Lookup("REPORTER_LOG_SPANS_ENABLED_TEST"); flag == nil {
+		pflag.Bool("REPORTER_LOG_SPANS_ENABLED_TEST", true, "")
+	}
+	v.BindPFlags(pflag.CommandLine)
+
+	os.Setenv("NUM_WORKS_TEST", "5")
+	v.BindEnv("NUM_WORKS_TEST")
+}
+
+func runPrintConfigCommand(v *viper.Viper, t *testing.T, allFlag bool) string {
+	buf := new(bytes.Buffer)
+	printCmd := Command(v)
+	printCmd.SetOut(buf)
+
+	if allFlag {
+		err := printCmd.Flags().Set("all", "true")
+		require.NoError(t, err, "printCmd.Flags() returned the error %v", err)
+	}
+
+	_, err := printCmd.ExecuteC()
+	require.NoError(t, err, "printCmd.ExecuteC() returned the error %v", err)
+
+	return buf.String()
+
 }
