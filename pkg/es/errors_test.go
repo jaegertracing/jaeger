864eb7064b0649438e349732842db7862b593240
Yuri Shkuro <github@ysh.us>
2024-02-18 17:23:28 -0500

    s/assert/require
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- pkg/es/errors_test.go
+++ pkg/es/errors_test.go
@@ -8,11 +8,11 @@ import (
 	"testing"
 
 	"github.com/olivere/elastic"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDetailedError(t *testing.T) {
-	assert.ErrorContains(t, fmt.Errorf("some err"), "some err", "no panic")
+	require.ErrorContains(t, fmt.Errorf("some err"), "some err", "no panic")
 
 	esErr := &elastic.Error{
 		Status: 400,
@@ -27,9 +27,9 @@ func TestDetailedError(t *testing.T) {
 			},
 		},
 	}
-	assert.ErrorContains(t, DetailedError(esErr), "actual reason")
+	require.ErrorContains(t, DetailedError(esErr), "actual reason")
 
 	esErr.Details.RootCause[0] = nil
-	assert.ErrorContains(t, DetailedError(esErr), "useless reason")
-	assert.NotContains(t, DetailedError(esErr).Error(), "actual reason")
+	require.ErrorContains(t, DetailedError(esErr), "useless reason")
+	require.NotContains(t, DetailedError(esErr).Error(), "actual reason")
 }
