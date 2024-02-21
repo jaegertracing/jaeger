5d5e85ba3a5a53d588e1844d0d405d73bed960d6
Sidhartha-Biswal <119291884+Sidhartha-Biswal@users.noreply.github.com>
2024-02-21 17:33:18 +0530

    Update static_handler.go
    Signed-off-by: Sidhartha-Biswal <119291884+Sidhartha-Biswal@users.noreply.github.com>

--- cmd/query/app/static_handler.go
+++ cmd/query/app/static_handler.go
@@ -147,11 +147,19 @@ func (sH *StaticAssetsHandler) loadAndEnrichIndexHTML(open func(string) (http.Fi
 	if sH.options.UIBasePath == "" {
 		sH.options.UIBasePath = "/ui" // default value
 	}
-	if sH.options.BasePath != "/" && sH.options.UIBasePath != "/ui" {
-		if !strings.HasPrefix(sH.options.BasePath, "/") || strings.HasSuffix(sH.options.BasePath, "/") {
-			return nil, fmt.Errorf("invalid base path '%s'. Must start but not end with a slash '/', e.g. '/jaeger/ui'", sH.options.BasePath)
+	if sH.options.BasePath != "/" {
+		if sH.options.UIBasePath != "/ui" {
+			if !strings.HasPrefix(sH.options.UIBasePath, "/") || strings.HasSuffix(sH.options.UIBasePath, "/") {
+				return nil, fmt.Errorf("invalid base path '%s'. Must start but not end with a slash '/', e.g. '/jaeger/ui'", sH.options.BasePath)
+			}
+			indexBytes = basePathPattern.ReplaceAll(indexBytes, []byte(fmt.Sprintf(`<base href="%s/"`, sH.options.UIBasePath)))
 		}
-		indexBytes = basePathPattern.ReplaceAll(indexBytes, []byte(fmt.Sprintf(`<base href="%s/"`, sH.options.BasePath)))
+		else {
+	        	if !strings.HasPrefix(sH.options.BasePath, "/") || strings.HasSuffix(sH.options.BasePath, "/") {
+				return nil, fmt.Errorf("invalid base path '%s'. Must start but not end with a slash '/', e.g. '/jaeger/ui'", sH.options.BasePath)
+			}
+			indexBytes = basePathPattern.ReplaceAll(indexBytes, []byte(fmt.Sprintf(`<base href="%s/"`, sH.options.BasePath)))
+		}
 	}
 
 	return indexBytes, nil
