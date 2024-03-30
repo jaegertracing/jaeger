34f6a251dc3b7915c2c2424efc343dc35fbd7c9c
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2024-03-30 18:09:00 -0400

    [storage] Simplify integration tests (#5312)
    ## Description of the changes
    - Remove usage of build tags, since they make it mode difficult to
    compile files in the IDE.
    - Make all tests use `STORAGE` env var
    - Pass `testing.T` to functions, simplify error handling
    
    ## How was this change tested?
    - CI
    
    ---------
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- plugin/storage/integration/memstore_test.go
+++ plugin/storage/integration/memstore_test.go
@@ -12,15 +12,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build memory_storage_integration
-// +build memory_storage_integration
 
 package integration
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger/pkg/testutils"
@@ -32,7 +29,7 @@ type MemStorageIntegrationTestSuite struct {
 	logger *zap.Logger
 }
 
-func (s *MemStorageIntegrationTestSuite) initialize() error {
+func (s *MemStorageIntegrationTestSuite) initialize(_ *testing.T) {
 	s.logger, _ = testutils.NewLogger()
 
 	store := memory.NewStore()
@@ -45,21 +42,13 @@ func (s *MemStorageIntegrationTestSuite) initialize() error {
 
 	// TODO DependencyWriter is not implemented in memory store
 
-	s.Refresh = s.refresh
-	s.CleanUp = s.cleanUp
-	return nil
-}
-
-func (s *MemStorageIntegrationTestSuite) refresh() error {
-	return nil
-}
-
-func (s *MemStorageIntegrationTestSuite) cleanUp() error {
-	return s.initialize()
+	s.Refresh = func(t *testing.T) {}
+	s.CleanUp = s.initialize
 }
 
 func TestMemoryStorage(t *testing.T) {
+	skipUnlessEnv(t, "memory")
 	s := &MemStorageIntegrationTestSuite{}
-	require.NoError(t, s.initialize())
-	s.IntegrationTestAll(t)
+	s.initialize(t)
+	s.RunAll(t)
 }
