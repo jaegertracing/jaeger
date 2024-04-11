d1958c01526bef73c8a4a98c1eba8b77d9866ad9
Vamshi Maskuri <gwcchintu@gmail.com>
2024-04-12 00:38:59 +0530

    updated workflow
    Signed-off-by: Vamshi Maskuri <gwcchintu@gmail.com>

--- scripts/build-crossdock.sh
+++ scripts/build-crossdock.sh
@@ -5,7 +5,22 @@ set -euxf -o pipefail
 BRANCH=${BRANCH:?'missing BRANCH env var'}
 COMMIT=${GITHUB_SHA::8}
 
+mode=${1-main}
+
 make build-and-run-crossdock
+
+if [[ "$mode" == "pr-only" ]]; then
+  make create-baseimg
+  # build artifacts for linux/amd64 only for pull requests
+  platforms="linux/amd64"
+else
+  make create-baseimg-debugimg
+  platforms="linux/amd64,linux/s390x,linux/ppc64le,linux/arm64"
+  # build multi-arch binaries
+  make build-binaries-s390x
+  make build-binaries-ppc64le
+  make build-binaries-arm64
+fi
 
 # Only push images to dockerhub/quay.io for the main branch
 if [[ "$BRANCH" == "main" ]]; then
