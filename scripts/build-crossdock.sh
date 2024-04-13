9d48a84f8017f5a8d4311eaef8d9d295678d699f
Wise-Wizard <saransh.shankar@gmail.com>
2024-04-13 19:20:40 +0530

    Changed .sh file
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- scripts/build-crossdock.sh
+++ scripts/build-crossdock.sh
@@ -11,14 +11,21 @@ make build-and-run-crossdock
 if [[ "$BRANCH" == "main" ]]; then
   echo 'upload images to dockerhub/quay.io'
   REPO=jaegertracing/test-driver
-  IFS=" " read -r -a IMAGE_TAGS <<< "$(bash scripts/compute-tags.sh ${REPO})"
-  IMAGE_TAGS+=("--tag" "docker.io/${REPO}:${COMMIT}" "--tag" "quay.io/${REPO}:${COMMIT}")
+
+  BASE_IMAGE_TAGS="$(bash scripts/compute-tags.sh ${REPO})"
+
+  TARGET_ARCH="linux/arm64"
+
+  IMAGE_TAGS=("--tag" "docker.io/${REPO}:${COMMIT}-${TARGET_ARCH}" "--tag" "quay.io/${REPO}:${COMMIT}-${TARGET_ARCH}")
+
+  # Build the image for the target architecture
   bash scripts/docker-login.sh
   docker buildx build --push \
     --progress=plain \
-    --platform=linux/amd64 \
+    --platform "${TARGET_ARCH}" \
     "${IMAGE_TAGS[@]}" \
     crossdock/
+
 else
   echo 'skip docker images upload for PR'
 fi
