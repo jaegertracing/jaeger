b1f11900ed8eb1fbe967a908e2f365ca5cdfca54
Yuri Shkuro <github@ysh.us>
2024-04-28 21:10:47 -0400

    timeout
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/query/app/server_test.go
+++ cmd/query/app/server_test.go
@@ -524,7 +524,7 @@ func TestServerGRPCTLS(t *testing.T) {
 				client = newGRPCClientWithTLS(t, ports.PortToHostPort(ports.QueryGRPC), nil)
 			}
 
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 			defer cancel()
 
 			{ // TODO remove me
