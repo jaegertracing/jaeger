6cbd5d86f4b009a182e4fcee17713117823a805a
James Ryans <james.ryans2012@gmail.com>
2024-05-01 23:30:08 +0700

    Change spanstore Reader & Writer func back to accepts struct
    Signed-off-by: James Ryans <james.ryans2012@gmail.com>

--- storage_v2/spanstore/reader.go
+++ storage_v2/spanstore/reader.go
@@ -21,7 +21,7 @@ type Reader interface {
 	// GetTrace retrieves the trace with a given id.
 	//
 	// If no spans are stored for this trace, it returns ErrTraceNotFound.
-	GetTrace(ctx context.Context, traceID pcommon.TraceID) (*ptrace.Traces, error)
+	GetTrace(ctx context.Context, traceID pcommon.TraceID) (ptrace.Traces, error)
 
 	// GetServices returns all service names known to the backend from spans
 	// within its retention period.
@@ -37,13 +37,13 @@ type Reader interface {
 	// by different spans.
 	//
 	// If no matching traces are found, the function returns (nil, nil).
-	FindTraces(ctx context.Context, query *TraceQueryParameters) ([]*ptrace.Traces, error)
+	FindTraces(ctx context.Context, query TraceQueryParameters) ([]ptrace.Traces, error)
 
 	// FindTraceIDs does the same search as FindTraces, but returns only the list
 	// of matching trace IDs.
 	//
 	// If no matching traces are found, the function returns (nil, nil).
-	FindTraceIDs(ctx context.Context, query *TraceQueryParameters) ([]pcommon.TraceID, error)
+	FindTraceIDs(ctx context.Context, query TraceQueryParameters) ([]pcommon.TraceID, error)
 }
 
 // TraceQueryParameters contains parameters of a trace query.
