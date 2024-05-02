a143aa3ca646d5de26566f168f866f96e2097485
Wise-Wizard <saransh.shankar@gmail.com>
2024-05-03 00:53:30 +0530

    Changed yml file
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- scripts/build-crossdock.sh
+++ scripts/build-crossdock.sh
@@ -11,19 +11,14 @@ make build-and-run-crossdock
 if [[ "$BRANCH" == "main" ]]; then
   echo 'upload images to dockerhub/quay.io'
   REPO=jaegertracing/test-driver
-
-  TARGET_ARCH="linux/arm64"
-
-  IMAGE_TAGS=("--tag" "docker.io/${REPO}:${COMMIT}-${TARGET_ARCH}" "--tag" "quay.io/${REPO}:${COMMIT}-${TARGET_ARCH}")
-
-  # Build the image for the target architecture
+  IFS=" " read -r -a IMAGE_TAGS <<< "$(bash scripts/compute-tags.sh ${REPO})"
+  IMAGE_TAGS+=("--tag" "docker.io/${REPO}:${COMMIT}" "--tag" "quay.io/${REPO}:${COMMIT}")
   bash scripts/docker-login.sh
   docker buildx build --push \
     --progress=plain \
-    --platform "${TARGET_ARCH}" \
+    --platform=linux/amd64 \
     "${IMAGE_TAGS[@]}" \
     crossdock/
-
 else
   echo 'skip docker images upload for PR'
 fi
