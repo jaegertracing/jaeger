c2fbf6ab6a6516727c7ec513fee3880dc5e40664
Wise-Wizard <saransh.shankar@gmail.com>
2024-05-06 13:01:32 +0530

    Added Whitespace
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- plugin/storage/cassandra/factory.go
+++ plugin/storage/cassandra/factory.go
@@ -153,6 +153,7 @@ func (f *Factory) Initialize(metricsFactory metrics.Factory, logger *zap.Logger)
 		return err
 	}
 	f.primarySession = primarySession
+	
 	if f.archiveConfig != nil {
 		if archiveSession, err := f.archiveConfig.NewSession(logger); err == nil {
 			f.archiveSession = archiveSession
