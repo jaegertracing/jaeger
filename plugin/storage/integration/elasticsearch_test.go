c3c678dceebcbec973bf663d7287e91820a65977
Vamshi Maskuri <gwcchintu@gmail.com>
2024-05-21 13:01:01 +0530

    fix
    Signed-off-by: Vamshi Maskuri <gwcchintu@gmail.com>

--- plugin/storage/integration/elasticsearch_test.go
+++ plugin/storage/integration/elasticsearch_test.go
@@ -99,8 +99,7 @@ func (s *ESStorageIntegration) initializeES(t *testing.T, allTagsAsFields bool) 
 
 	s.CleanUp = func(t *testing.T) {
 		s.esCleanUp(t)
-		s.factory.Close()
-	}
+\	}
 	s.esCleanUp(t)
 }
 
