50add85be0e77fff927e38402a4ef1d3ed5ae240
Your Name <you@example.com>
2024-05-30 11:39:21 +0530

    update ci
    Signed-off-by: Your Name <you@example.com>

--- scripts/kafka-integration-test.sh
+++ scripts/kafka-integration-test.sh
@@ -3,11 +3,11 @@
 set -e
 
 export STORAGE=kafka
-
+compose_file="docker compose -f docker-compose/kafka-integration-test/v3.yml"
 # Check if the -k parameter is provided or not
 if [ "$1" == "-k" ]; then
     echo "Starting Kafka using Docker Compose..."
-    docker-compose -f ./docker-compose/kafka-integration-test/v3.yml up -d kafka
+    $compose_file up -d kafka
 fi
 
 # Set the timeout in seconds
@@ -15,12 +15,17 @@ timeout=180
 # Set the interval between checks in seconds
 interval=5
 
+# Function to check if Kafka is ready
+is_kafka_ready() {
+    $compose_file exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092 >/dev/null 2>&1
+}
+
 # Calculate the end time
 end_time=$((SECONDS + timeout))
 
 while [ $SECONDS -lt $end_time ]; do
     # Check if Kafka is ready by attempting to describe a topic
-    if  docker-compose -f docker-compose/kafka-integration-test/v3.yml  exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092>/dev/null 2>&1; then
+    if is_kafka_ready; then
         break
     fi
     echo "Kafka broker not ready, waiting ${interval} seconds"
@@ -28,7 +33,7 @@ while [ $SECONDS -lt $end_time ]; do
 done
 
 # Check if Kafka is still not available after the timeout
-if ! docker-compose -f docker-compose/kafka-integration-test/v3.yml  exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092>/dev/null 2>&1; then
+if ! is_kafka_ready; then
     echo "Timed out waiting for Kafka to start"
     exit 1
 fi
