78299c4210df68e5ff4b862fc4de7ccaf6c044c3
Griffin <prakritimandal611@gmail.com>
2024-05-30 14:49:35 +0530

    fixing goleak checks
    Signed-off-by: Griffin <prakritimandal611@gmail.com>

--- internal/safeexpvar/cacheexpvar_test.go
+++ internal/safeexpvar/cacheexpvar_test.go
@@ -2,12 +2,15 @@ package safeexpvar
 
 import (
 	"expvar"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSetExpvarInt(t *testing.T) {
+	initialGoroutines := runtime.NumGoroutine()
 	// Test with a new variable
 	name := "metrics-test-1"
 	value := int64(42)
@@ -20,4 +23,11 @@ func TestSetExpvarInt(t *testing.T) {
 	expInt, ok := v.(*expvar.Int)
 	assert.True(t, ok, "expected variable %s to be of type *expvar.Int", name)
 	assert.Equal(t, value, expInt.Value(), "expected variable %s value to be %d", name, value)
+
+	time.Sleep(100 * time.Millisecond)
+
+	finalGoroutines := runtime.NumGoroutine()
+	goroutineDiff := finalGoroutines - initialGoroutines
+
+	assert.Equal(t, 0, goroutineDiff, "goroutine leak detected: initial goroutines %d, final goroutines %d", initialGoroutines, finalGoroutines)
 }
