jaegertracing/jaeger

commit ca50f53fe70d5fbefaef8b6310d8c3d2941140f3

Add Elasticsearch helper binaries to release (#5501)
## Which problem is this PR solving?
- Resolves #5499 
## Description of the changes
- Add the es helpers to scripts/package-deploy.sh

## How was this change tested?
- 

## Checklist
- [x] I have read
https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
- [x] I have signed all commits
- [ ] I have added unit tests for the new functionality
- [x] I have run lint and test steps successfully
  - for `jaeger`: `make lint test`
  - for `jaeger-ui`: `yarn lint` and `yarn test`

---------

Signed-off-by: FlamingSaint <raghuramkannan400@gmail.com>

scripts/package-deploy.sh
@@ -16,6 +16,18 @@ function stage-platform-files {
     cp "./cmd/ingester/ingester-${PLATFORM}"      "${PACKAGE_STAGING_DIR}/jaeger-ingester${FILE_EXTENSION}"
     cp "./examples/hotrod/hotrod-${PLATFORM}"     "${PACKAGE_STAGING_DIR}/example-hotrod${FILE_EXTENSION}"
 }
+# stage-tool-platform-files stages the different tool files in the platform ($1) into the package
+# staging dir ($2). If you pass in a file extension ($3) it will be used when
+# copying on the source
+function stage-tool-platform-files {
+    local -r PLATFORM=$1
+    local -r TOOLS_PACKAGE_STAGING_DIR=$2
+    local -r FILE_EXTENSION=${3:-}
+
+    cp "./cmd/es-index-cleaner/es-index-cleaner-${PLATFORM}"        "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-index-cleaner${FILE_EXTENSION}"
+    cp "./cmd/es-rollover/es-rollover-${PLATFORM}"                  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-rollover${FILE_EXTENSION}"
+    cp "./cmd/esmapping-generator/esmapping-generator-${PLATFORM}"  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-esmapping-generator${FILE_EXTENSION}"
+}
 
 # package pulls built files for the platform ($2) and compresses it using the compression ($1).
 # If you pass in a file extension ($3) it will be look for binaries with that extension.
@@ -25,28 +37,44 @@ function package {
     local -r FILE_EXTENSION=${3:-}
     local -r PACKAGE_NAME=jaeger-$VERSION-$PLATFORM
     local -r PACKAGE_STAGING_DIR=$PACKAGE_NAME
+    local -r TOOLS_PACKAGE_NAME=jaeger-tools-$VERSION-$PLATFORM
+    local -r TOOLS_PACKAGE_STAGING_DIR=$TOOLS_PACKAGE_NAME
 
     if [ -d "$PACKAGE_STAGING_DIR" ]
     then
         rm -vrf "$PACKAGE_STAGING_DIR"
     fi
+    if [ -d "$TOOLS_PACKAGE_STAGING_DIR" ]
+    then
+        rm -vrf "$TOOLS_PACKAGE_STAGING_DIR"
+    fi
     mkdir "$PACKAGE_STAGING_DIR"
+    mkdir "$TOOLS_PACKAGE_STAGING_DIR"
     stage-platform-files "$PLATFORM" "$PACKAGE_STAGING_DIR" "$FILE_EXTENSION"
+    stage-tool-platform-files "$PLATFORM" "$TOOLS_PACKAGE_STAGING_DIR" "$FILE_EXTENSION"
     # Create a checksum file for all the files being packaged in the archive. Sorted by filename.
     find "$PACKAGE_STAGING_DIR" -type f -exec shasum -b -a 256 {} \; | sort -k2 | tee "./deploy/$PACKAGE_NAME.sha256sum.txt"
+    find "$TOOLS_PACKAGE_STAGING_DIR" -type f -exec shasum -b -a 256 {} \; | sort -k2 | tee "./deploy/$TOOLS_PACKAGE_NAME.sha256sum.txt"
 
     if [ "$COMPRESSION" == "zip" ]
     then
         local -r ARCHIVE_NAME="$PACKAGE_NAME.zip"
         echo "Packaging into $ARCHIVE_NAME:"
         zip -r "./deploy/$ARCHIVE_NAME" "$PACKAGE_STAGING_DIR"
+        local -r TOOLS_ARCHIVE_NAME="$TOOLS_PACKAGE_NAME.zip"
+        echo "Packaging into $TOOLS_ARCHIVE_NAME:"
+        zip -r "./deploy/$TOOLS_ARCHIVE_NAME" "$TOOLS_PACKAGE_STAGING_DIR"
     else
         local -r ARCHIVE_NAME="$PACKAGE_NAME.tar.gz"
         echo "Packaging into $ARCHIVE_NAME:"
         tar --sort=name -czvf "./deploy/$ARCHIVE_NAME" "$PACKAGE_STAGING_DIR"
+        local -r TOOLS_ARCHIVE_NAME="$TOOLS_PACKAGE_NAME.tar.gz"
+        echo "Packaging into $TOOLS_ARCHIVE_NAME:"
+        tar --sort=name -czvf "./deploy/$TOOLS_ARCHIVE_NAME" "$TOOLS_PACKAGE_STAGING_DIR"
     fi
 
-    rm -rf "$PACKAGE_STAGING_DIR"
+    rm -rf "$PACKAGE_STAGING_DIR" 
+    rm -rf "$TOOLS_PACKAGE_STAGING_DIR" 
 }
 
 set -e

Makefile
@@ -95,7 +95,7 @@ test-and-lint: test fmt lint
 
 .PHONY: echo-version
 echo-version:
-	@echo "GIT_CLOSEST_TAG=$(GIT_CLOSEST_TAG)"
+	@echo "$(GIT_CLOSEST_TAG)"
 
 .PHONY: echo-all-pkgs
 echo-all-pkgs:

commit 03136eb972baaf9c09026d0a67f4f2a1d7e84568

Fix binary path in package-deploy.sh (#5561)
## Which problem is this PR solving?
- A bug was introduced in #5501 where the wrong binary path was used 

## Description of the changes
- Use the right path
- Allow overriding `tar` command since on MacOS the `--sort` option is
not supported (so I had to use `gtar` to test)

## How was this change tested?
These steps succeeded locally:
```
$ make build-all-platforms
$ TARCMD=gtar bash scripts/package-deploy.sh
```

Signed-off-by: Yuri Shkuro <github@ysh.us>

scripts/package-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxf -o pipefail
 
+TARCMD=${TARCMD:-tar}
+
 # stage-platform-files stages the different the platform ($1) into the package
 # staging dir ($2). If you pass in a file extension ($3) it will be used when
 # copying on the source
@@ -24,9 +26,9 @@ function stage-tool-platform-files {
     local -r TOOLS_PACKAGE_STAGING_DIR=$2
     local -r FILE_EXTENSION=${3:-}
 
-    cp "./cmd/es-index-cleaner/es-index-cleaner-${PLATFORM}"        "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-index-cleaner${FILE_EXTENSION}"
-    cp "./cmd/es-rollover/es-rollover-${PLATFORM}"                  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-rollover${FILE_EXTENSION}"
-    cp "./cmd/esmapping-generator/esmapping-generator-${PLATFORM}"  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-esmapping-generator${FILE_EXTENSION}"
+    cp "./cmd/es-index-cleaner/es-index-cleaner-${PLATFORM}"  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-index-cleaner${FILE_EXTENSION}"
+    cp "./cmd/es-rollover/es-rollover-${PLATFORM}"            "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-es-rollover${FILE_EXTENSION}"
+    cp "./plugin/storage/es/esmapping-generator-${PLATFORM}"  "${TOOLS_PACKAGE_STAGING_DIR}/jaeger-esmapping-generator${FILE_EXTENSION}"
 }
 
 # package pulls built files for the platform ($2) and compresses it using the compression ($1).
@@ -67,14 +69,14 @@ function package {
     else
         local -r ARCHIVE_NAME="$PACKAGE_NAME.tar.gz"
         echo "Packaging into $ARCHIVE_NAME:"
-        tar --sort=name -czvf "./deploy/$ARCHIVE_NAME" "$PACKAGE_STAGING_DIR"
+        ${TARCMD} --sort=name -czvf "./deploy/$ARCHIVE_NAME" "$PACKAGE_STAGING_DIR"
         local -r TOOLS_ARCHIVE_NAME="$TOOLS_PACKAGE_NAME.tar.gz"
         echo "Packaging into $TOOLS_ARCHIVE_NAME:"
-        tar --sort=name -czvf "./deploy/$TOOLS_ARCHIVE_NAME" "$TOOLS_PACKAGE_STAGING_DIR"
+        ${TARCMD} --sort=name -czvf "./deploy/$TOOLS_ARCHIVE_NAME" "$TOOLS_PACKAGE_STAGING_DIR"
     fi
 
-    rm -rf "$PACKAGE_STAGING_DIR" 
-    rm -rf "$TOOLS_PACKAGE_STAGING_DIR" 
+    rm -rf "$PACKAGE_STAGING_DIR"
+    rm -rf "$TOOLS_PACKAGE_STAGING_DIR"
 }
 
 set -e