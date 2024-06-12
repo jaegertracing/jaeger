53ae6bd5675631f620f9f187569be99bb02f171b
Vamshi Maskuri <gwcchintu@gmail.com>
2024-06-12 07:26:25 +0530

    rename directory
    Signed-off-by: Vamshi Maskuri <gwcchintu@gmail.com>

--- cmd/jaeger/internal/exporters/storageexporter/sanitizer/service_name_sanitizer.go
+++ cmd/jaeger/internal/exporters/storageexporter/sanitizer/service_name_sanitizer.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 The Jaeger Authors.
 // SPDX-License-Identifier: Apache-2.0
 
-package sanitizer_v2
+package sanitizer
 
 import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
