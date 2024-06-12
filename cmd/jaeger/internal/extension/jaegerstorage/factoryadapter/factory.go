6eefb4cd7542a0705e8313bac556ca42f4da4901
James Ryans <james.ryans2012@gmail.com>
2024-06-12 13:26:28 +0700

    un-implement the Initialize method
    Signed-off-by: James Ryans <james.ryans2012@gmail.com>

--- cmd/jaeger/internal/extension/jaegerstorage/factoryadapter/factory.go
+++ cmd/jaeger/internal/extension/jaegerstorage/factoryadapter/factory.go
@@ -9,26 +9,23 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/jaegertracing/jaeger/pkg/metrics"
 	storage_v1 "github.com/jaegertracing/jaeger/storage"
 	"github.com/jaegertracing/jaeger/storage_v2/spanstore"
 )
 
 type Factory struct {
-	logger *zap.Logger
-	ss     storage_v1.Factory
+	ss storage_v1.Factory
 }
 
 func NewFactory(logger *zap.Logger, ss storage_v1.Factory) spanstore.Factory {
 	return &Factory{
-		logger: logger,
-		ss:     ss,
+		ss: ss,
 	}
 }
 
 // Initialize implements spanstore.Factory.
 func (f *Factory) Initialize(_ context.Context) error {
-	return f.ss.Initialize(metrics.NullFactory, f.logger)
+	panic("not implemented")
 }
 
 // Close implements spanstore.Factory.
