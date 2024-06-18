a52182665580dadfa426476e0807ef04f00ac13f
James Ryans <james.ryans2012@gmail.com>
2024-06-19 01:07:18 +0700

    add unit tests for factoryadapter
    Signed-off-by: James Ryans <james.ryans2012@gmail.com>

---
+++ cmd/jaeger/internal/extension/jaegerstorage/factoryadapter/writer_test.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package factoryadapter
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/jaegertracing/jaeger/plugin/storage/memory"
+	spanstoreMocks "github.com/jaegertracing/jaeger/storage/spanstore/mocks"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+func TestWriteTraces(t *testing.T) {
+	memstore := memory.NewStore()
+	traceWriter := &TraceWriter{
+		spanWriter: memstore,
+	}
+
+	err := traceWriter.WriteTraces(context.Background(), makeTraces())
+	require.NoError(t, err)
+}
+
+func TestWriteTracesError(t *testing.T) {
+	mockstore := spanstoreMocks.NewWriter(t)
+	mockstore.On("WriteSpan", mock.AnythingOfType("context.backgroundCtx"), mock.AnythingOfType("*model.Span")).Return(errors.New("mocked error"))
+
+	traceWriter := &TraceWriter{
+		spanWriter: mockstore,
+	}
+
+	err := traceWriter.WriteTraces(context.Background(), makeTraces())
+	require.ErrorContains(t, err, "mocked error")
+}
+
+func makeTraces() ptrace.Traces {
+	traces := ptrace.NewTraces()
+	rSpans := traces.ResourceSpans().AppendEmpty()
+	sSpans := rSpans.ScopeSpans().AppendEmpty()
+	span := sSpans.Spans().AppendEmpty()
+
+	spanID := pcommon.NewSpanIDEmpty()
+	spanID[5] = 5 // 0000000000050000
+	span.SetSpanID(spanID)
+
+	traceID := pcommon.NewTraceIDEmpty()
+	traceID[15] = 1 // 00000000000000000000000000000001
+	span.SetTraceID(traceID)
+
+	return traces
+}
