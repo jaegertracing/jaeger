88ec66f4ca0a74d3fc12218c84f31e96ee795c81
Yuri Shkuro <github@ysh.us>
2024-06-17 23:11:54 -0400

    add tests
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/jaeger/internal/extension/remotesampling/extension_test.go
+++ cmd/jaeger/internal/extension/remotesampling/extension_test.go
@@ -8,26 +8,91 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/extension"
+	nooptrace "go.opentelemetry.io/otel/trace/noop"
+	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/cmd/jaeger/internal/extension/jaegerstorage"
+	memoryCfg "github.com/jaegertracing/jaeger/pkg/memory/config"
 )
 
-func TestNewExtension(t *testing.T) {
-	cfg := createDefaultConfig().(*Config)
-	cfg.File.Path = filepath.Join("..", "..", "..", "sampling-strategies.json")
-	e := newExtension(cfg, componenttest.NewNopTelemetrySettings())
-
-	assert.NotNil(t, e)
+type storageHost struct {
+	t                *testing.T
+	storageExtension component.Component
 }
 
-func TestStartAndShutdownLocalFile(t *testing.T) {
-	cfg := createDefaultConfig().(*Config)
+func (host storageHost) GetExtensions() map[component.ID]component.Component {
+	return map[component.ID]component.Component{
+		jaegerstorage.ID: host.storageExtension,
+	}
+}
+
+func (host storageHost) ReportFatalError(err error) {
+	host.t.Fatal(err)
+}
+
+func (storageHost) GetFactory(_ component.Kind, _ component.Type) component.Factory { return nil }
+func (storageHost) GetExporters() map[component.DataType]map[component.ID]component.Component {
+	return nil
+}
+
+func makeStorageExtension(t *testing.T, memstoreName string) storageHost {
+	extensionFactory := jaegerstorage.NewFactory()
+	storageExtension, err := extensionFactory.CreateExtension(
+		context.Background(),
+		extension.CreateSettings{
+			TelemetrySettings: component.TelemetrySettings{
+				Logger:         zap.L(),
+				TracerProvider: nooptrace.NewTracerProvider(),
+			},
+		},
+		&jaegerstorage.Config{Memory: map[string]memoryCfg.Configuration{
+			memstoreName: {MaxTraces: 10000},
+		}})
+	require.NoError(t, err)
+	host := storageHost{t: t, storageExtension: storageExtension}
+
+	err = storageExtension.Start(context.Background(), host)
+	require.NoError(t, err)
+	t.Cleanup(func() { require.NoError(t, storageExtension.Shutdown(context.Background())) })
+	return host
+}
+
+func TestStartFileBasedProvider(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
 	cfg.File.Path = filepath.Join("..", "..", "..", "sampling-strategies.json")
+	cfg.Adaptive = nil
+	cfg.HTTP = nil
+	cfg.GRPC = nil
+	require.NoError(t, cfg.Validate())
 
-	e := newExtension(cfg, componenttest.NewNopTelemetrySettings())
-	require.NotNil(t, e)
-	require.NoError(t, e.Start(context.Background(), componenttest.NewNopHost()))
+	ext, err := factory.CreateExtension(context.Background(), extension.CreateSettings{
+		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
+	}, cfg)
+	require.NoError(t, err)
+	host := makeStorageExtension(t, "foobar")
+	require.NoError(t, ext.Start(context.Background(), host))
+	require.NoError(t, ext.Shutdown(context.Background()))
+}
 
-	require.NoError(t, e.Shutdown(context.Background()))
+func TestStartAdaptiveProvider(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.File = nil
+	cfg.Adaptive.SamplingStore = "foobar"
+	cfg.HTTP = nil
+	cfg.GRPC = nil
+	require.NoError(t, cfg.Validate())
+
+	ext, err := factory.CreateExtension(context.Background(), extension.CreateSettings{
+		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
+	}, cfg)
+	require.NoError(t, err)
+	host := makeStorageExtension(t, "foobar")
+	require.NoError(t, ext.Start(context.Background(), host))
+	require.NoError(t, ext.Shutdown(context.Background()))
 }
