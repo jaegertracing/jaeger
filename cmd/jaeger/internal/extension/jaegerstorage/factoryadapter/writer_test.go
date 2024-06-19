eeefba6601a4f86ac0e323db9770f77480130dc3
James Ryans <james.ryans2012@gmail.com>
2024-06-19 10:03:28 +0700

    check if trace stored in factoryadapter writer unit test
    Signed-off-by: James Ryans <james.ryans2012@gmail.com>

--- cmd/jaeger/internal/extension/jaegerstorage/factoryadapter/writer_test.go
+++ cmd/jaeger/internal/extension/jaegerstorage/factoryadapter/writer_test.go
@@ -8,11 +8,13 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
+	"github.com/jaegertracing/jaeger/model"
 	"github.com/jaegertracing/jaeger/plugin/storage/memory"
 	spanstoreMocks "github.com/jaegertracing/jaeger/storage/spanstore/mocks"
 )
@@ -23,13 +25,26 @@ func TestWriteTraces(t *testing.T) {
 		spanWriter: memstore,
 	}
 
-	err := traceWriter.WriteTraces(context.Background(), makeTraces())
+	td := makeTraces()
+	err := traceWriter.WriteTraces(context.Background(), td)
 	require.NoError(t, err)
+
+	tdID := td.ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0).TraceID()
+	traceID, err := model.TraceIDFromBytes(tdID[:])
+	require.NoError(t, err)
+	trace, err := memstore.GetTrace(context.Background(), traceID)
+	require.NoError(t, err)
+	require.NotNil(t, trace)
+	assert.Len(t, trace.Spans, 1)
 }
 
 func TestWriteTracesError(t *testing.T) {
 	mockstore := spanstoreMocks.NewWriter(t)
-	mockstore.On("WriteSpan", mock.AnythingOfType("context.backgroundCtx"), mock.AnythingOfType("*model.Span")).Return(errors.New("mocked error"))
+	mockstore.On(
+		"WriteSpan",
+		mock.AnythingOfType("context.backgroundCtx"),
+		mock.AnythingOfType("*model.Span"),
+	).Return(errors.New("mocked error"))
 
 	traceWriter := &TraceWriter{
 		spanWriter: mockstore,
