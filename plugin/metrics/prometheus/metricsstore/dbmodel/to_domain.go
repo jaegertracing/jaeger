jaegertracing/jaeger

commit ada556991ab8743613b4f7f4452d01718c2a02c9

Remove support for OTEL spanmetrics processor (#5539)

plugin/metrics/prometheus/metricsstore/dbmodel/to_domain.go
@@ -31,8 +31,8 @@ type Translator struct {
 // New returns a new Translator.
 func New(spanNameLabel string) Translator {
 	return Translator{
-		// "operator" is the label name that Jaeger UI expects.
-		labelMap: map[string]string{spanNameLabel: "operation"},
+		// "span_name" is the label name that Jaeger UI expects.
+		labelMap: map[string]string{spanNameLabel: "span_name"},
 	}
 }
 

plugin/metrics/prometheus/metricsstore/reader.go
@@ -90,10 +90,7 @@ func NewMetricsReader(cfg config.Configuration, logger *zap.Logger, tracer trace
 		return nil, fmt.Errorf("failed to initialize prometheus client: %w", err)
 	}
 
-	operationLabel := "operation"
-	if cfg.SupportSpanmetricsConnector {
-		operationLabel = "span_name"
-	}
+	operationLabel := "span_name"
 
 	mr := &MetricsReader{
 		client: promapi.NewAPI(client),
@@ -134,11 +131,7 @@ func (m MetricsReader) GetLatencies(ctx context.Context, requestParams *metricss
 }
 
 func buildFullLatencyMetricName(cfg config.Configuration) string {
-	metricName := "latency"
-	if !cfg.SupportSpanmetricsConnector {
-		return metricName
-	}
-	metricName = "duration"
+	metricName := "duration"
 
 	if cfg.MetricNamespace != "" {
 		metricName = cfg.MetricNamespace + "_" + metricName
@@ -181,10 +174,6 @@ func (m MetricsReader) GetCallRates(ctx context.Context, requestParams *metricss
 
 func buildFullCallsMetricName(cfg config.Configuration) string {
 	metricName := "calls"
-	if !cfg.SupportSpanmetricsConnector {
-		return metricName
-	}
-
 	if cfg.MetricNamespace != "" {
 		metricName = cfg.MetricNamespace + "_" + metricName
 	}

plugin/metrics/prometheus/metricsstore/dbmodel/to_domain_test.go
@@ -48,7 +48,7 @@ func TestToDomainMetricsFamily(t *testing.T) {
 
 	wantMetricLabels := map[string]string{
 		"label_key": "label_value",
-		"operation": "span_name_value", // assert the name is translated to a Jaeger-friendly label.
+		"span_name": "span_name_value", // assert the name is translated to a Jaeger-friendly label.
 	}
 	assert.Len(t, mf.Metrics, 1)
 	for _, ml := range mf.Metrics[0].Labels {

plugin/metrics/prometheus/metricsstore/reader_test.go
@@ -16,6 +16,7 @@ package metricsstore
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -61,9 +62,8 @@ const defaultTimeout = 30 * time.Second
 
 // defaultConfig should consist of the default values for the prometheus.query.* command line options.
 var defaultConfig = config.Configuration{
-	SupportSpanmetricsConnector: false,
-	MetricNamespace:             "",
-	LatencyUnit:                 "ms",
+	MetricNamespace: "",
+	LatencyUnit:     "ms",
 }
 
 func tracerProvider(t *testing.T) (trace.TracerProvider, *tracetest.InMemoryExporter, func()) {
@@ -172,7 +172,7 @@ func TestGetLatencies(t *testing.T) {
 			wantLabels: map[string]string{
 				"service_name": "emailservice",
 			},
-			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(latency_bucket{service_name =~ "emailservice", ` +
+			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_bucket{service_name =~ "emailservice", ` +
 				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,le))`,
 		},
 		{
@@ -183,11 +183,11 @@ func TestGetLatencies(t *testing.T) {
 			wantName:         "service_operation_latencies",
 			wantDescription:  "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
-				"operation":    "/OrderResult",
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
-			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(latency_bucket{service_name =~ "emailservice", ` +
-				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,operation,le))`,
+			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_bucket{service_name =~ "emailservice", ` +
+				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,span_name,le))`,
 		},
 		{
 			name:             "two services and span kinds result in regex 'or' symbol in query",
@@ -199,16 +199,15 @@ func TestGetLatencies(t *testing.T) {
 			wantLabels: map[string]string{
 				"service_name": "emailservice",
 			},
-			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(latency_bucket{service_name =~ "frontend|emailservice", ` +
+			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_bucket{service_name =~ "frontend|emailservice", ` +
 				`span_kind =~ "SPAN_KIND_SERVER|SPAN_KIND_CLIENT"}[10m])) by (service_name,le))`,
 		},
 		{
-			name:             "enable support for spanmetrics connector with normalized metric name",
+			name:             "enable support for spanmetrics connector with a namespace",
 			serviceNames:     []string{"emailservice"},
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.MetricNamespace = "span_metrics"
 				cfg.LatencyUnit = "s"
 				return cfg
@@ -216,6 +215,7 @@ func TestGetLatencies(t *testing.T) {
 			wantName:        "service_operation_latencies",
 			wantDescription: "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(span_metrics_duration_bucket{service_name =~ "emailservice", ` +
@@ -227,7 +227,6 @@ func TestGetLatencies(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.NormalizeDuration = true
 				cfg.LatencyUnit = "s"
 				return cfg
@@ -235,6 +234,7 @@ func TestGetLatencies(t *testing.T) {
 			wantName:        "service_operation_latencies",
 			wantDescription: "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_seconds_bucket{service_name =~ "emailservice", ` +
@@ -257,7 +257,7 @@ func TestGetLatencies(t *testing.T) {
 
 			m, err := reader.GetLatencies(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -286,11 +286,11 @@ func TestGetCallRates(t *testing.T) {
 			wantName:         "service_operation_call_rate",
 			wantDescription:  "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"operation":    "/OrderResult",
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls{service_name =~ "emailservice", ` +
-				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,operation)`,
+				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,span_name)`,
 		},
 		{
 			name:             "two services and span kinds result in regex 'or' symbol in query",
@@ -311,13 +311,13 @@ func TestGetCallRates(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.MetricNamespace = "span_metrics"
 				return cfg
 			},
 			wantName:        "service_operation_call_rate",
 			wantDescription: "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(span_metrics_calls{service_name =~ "emailservice", ` +
@@ -329,13 +329,13 @@ func TestGetCallRates(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.NormalizeCalls = true
 				return cfg
 			},
 			wantName:        "service_operation_call_rate",
 			wantDescription: "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls_total{service_name =~ "emailservice", ` +
@@ -357,7 +357,7 @@ func TestGetCallRates(t *testing.T) {
 
 			m, err := reader.GetCallRates(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -387,12 +387,12 @@ func TestGetErrorRates(t *testing.T) {
 			wantName:         "service_operation_error_rate",
 			wantDescription:  "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"operation":    "/OrderResult",
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
-				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,operation) / ` +
-				`sum(rate(calls{service_name =~ "emailservice", span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,operation)`,
+				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,span_name) / ` +
+				`sum(rate(calls{service_name =~ "emailservice", span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name,span_name)`,
 		},
 		{
 			name:             "two services and span kinds result in regex 'or' symbol in query",
@@ -414,7 +414,6 @@ func TestGetErrorRates(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: false,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = false
 				cfg.MetricNamespace = "span_metrics"
 				cfg.NormalizeCalls = true
 				return cfg
@@ -424,9 +423,9 @@ func TestGetErrorRates(t *testing.T) {
 			wantLabels: map[string]string{
 				"service_name": "emailservice",
 			},
-			wantPromQlQuery: `sum(rate(calls{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
+			wantPromQlQuery: `sum(rate(span_metrics_calls_total{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
 				`span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name) / ` +
-				`sum(rate(calls{service_name =~ "emailservice", span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name)`,
+				`sum(rate(span_metrics_calls_total{service_name =~ "emailservice", span_kind =~ "SPAN_KIND_SERVER"}[10m])) by (service_name)`,
 		},
 		{
 			name:             "enable support for spanmetrics connector with a metric namespace",
@@ -434,13 +433,13 @@ func TestGetErrorRates(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.MetricNamespace = "span_metrics"
 				return cfg
 			},
 			wantName:        "service_operation_error_rate",
 			wantDescription: "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(span_metrics_calls{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
@@ -453,13 +452,13 @@ func TestGetErrorRates(t *testing.T) {
 			spanKinds:        []string{"SPAN_KIND_SERVER"},
 			groupByOperation: true,
 			updateConfig: func(cfg config.Configuration) config.Configuration {
-				cfg.SupportSpanmetricsConnector = true
 				cfg.NormalizeCalls = true
 				return cfg
 			},
 			wantName:        "service_operation_error_rate",
 			wantDescription: "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
+				"span_name":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls_total{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
@@ -482,7 +481,7 @@ func TestGetErrorRates(t *testing.T) {
 
 			m, err := reader.GetErrorRates(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -699,9 +698,8 @@ func TestInvalidLatencyUnit(t *testing.T) {
 	tracer, _, closer := tracerProvider(t)
 	defer closer()
 	cfg := config.Configuration{
-		SupportSpanmetricsConnector: true,
-		NormalizeDuration:           true,
-		LatencyUnit:                 "something invalid",
+		NormalizeDuration: true,
+		LatencyUnit:       "something invalid",
 	}
 	_, _ = NewMetricsReader(cfg, zap.NewNop(), tracer)
 }
@@ -900,8 +898,8 @@ func startMockPrometheusServer(t *testing.T, wantPromQlQuery string, wantWarning
 		assert.Equal(t, wantPromQlQuery, promQuery)
 
 		mockResponsePayloadFile := "testdata/service_datapoint_response.json"
-		if strings.Contains(promQuery, "by (service_name,operation") {
-			mockResponsePayloadFile = "testdata/service_operation_datapoint_response.json"
+		if strings.Contains(promQuery, "by (service_name,span_name") {
+			mockResponsePayloadFile = "testdata/service_span_name_datapoint_response.json"
 		}
 		sendResponse(t, w, mockResponsePayloadFile)
 	}))
@@ -947,12 +945,17 @@ func prepareMetricsReaderAndServer(t *testing.T, config config.Configuration, wa
 	return reader, mockPrometheus
 }
 
-func assertMetrics(t *testing.T, gotMetrics *metrics.MetricFamily, wantLabels map[string]string, wantName, wantDescription string) {
+func assertMetrics(t *testing.T, testName string, gotMetrics *metrics.MetricFamily, wantLabels map[string]string, wantName, wantDescription string) {
 	assert.Len(t, gotMetrics.Metrics, 1)
 	assert.Equal(t, wantName, gotMetrics.Name)
 	assert.Equal(t, wantDescription, gotMetrics.Help)
 	mps := gotMetrics.Metrics[0].MetricPoints
 	assert.Len(t, mps, 1)
+
+	// logging for expected and actual labels
+	fmt.Printf("Test Name: %s\n", testName)
+	fmt.Printf("Expected labels: %v\n", wantLabels)
+	fmt.Printf("Actual labels: %v\n", gotMetrics.Metrics[0].Labels)
 
 	// There is no guaranteed order of labels, so we need to take the approach of using a map of expected values.
 	labels := gotMetrics.Metrics[0].Labels
@@ -963,6 +966,11 @@ func assertMetrics(t *testing.T, gotMetrics *metrics.MetricFamily, wantLabels ma
 		delete(wantLabels, l.Name)
 	}
 	assert.Empty(t, wantLabels)
+
+	// Additional logging to show that all expected labels were found and matched
+	fmt.Printf("Remaining expected labels after matching: %v\n", wantLabels)
+	fmt.Printf("\n")
+
 	assert.Equal(t, int64(1620351786), mps[0].Timestamp.GetSeconds())
 
 	actualVal := mps[0].Value.(*metrics.MetricPoint_GaugeValue).GaugeValue.Value.(*metrics.GaugeValue_DoubleValue).DoubleValue

docker-compose/monitor/Makefile
@@ -12,17 +12,6 @@ dev: export JAEGER_IMAGE_TAG = dev
 dev:
 	docker compose -f docker-compose.yml up
 
-# starts older spanmetrics processor setup, for example,
-# to test backwards compatibility of Jaeger with spanmetrics processor.
-.PHONY: dev-processor
-dev-processor: export JAEGER_IMAGE_TAG = dev
-# Fix to a version before the breaking changes were introduced.
-dev-processor: export OTEL_IMAGE_TAG = 0.70.0
-dev-processor: export OTEL_CONFIG_SRC = ./otel-collector-config-processor.yml
-dev-processor: export PROMETHEUS_QUERY_SUPPORT_SPANMETRICS_CONNECTOR = false
-dev-processor:
-	docker compose -f docker-compose.yml up
-
 .PHONY: clean-jaeger
 clean-jaeger:
 	# Also cleans up intermediate cached containers.

docker-compose/monitor/otel-collector-config-processor.yml
@@ -1,42 +1,0 @@
-receivers:
-  jaeger:
-    protocols:
-      thrift_http:
-        endpoint: "0.0.0.0:14278"
-
-  otlp:
-    protocols:
-      grpc:
-      http:
-
-  # Dummy receiver that's never used, because a pipeline is required to have one.
-  otlp/spanmetrics:
-    protocols:
-      grpc:
-        endpoint: "localhost:65535"
-
-exporters:
-  prometheus:
-    endpoint: "0.0.0.0:8889"
-
-  otlp:
-    endpoint: jaeger:4317
-    tls:
-      insecure: true
-
-processors:
-  batch:
-  spanmetrics:
-    metrics_exporter: prometheus
-
-service:
-  pipelines:
-    traces:
-      receivers: [otlp, jaeger]
-      processors: [spanmetrics, batch]
-      exporters: [otlp]
-    # The exporter name in this pipeline must match the spanmetrics.metrics_exporter name.
-    # The receiver is just a dummy and never used; added to pass validation requiring at least one receiver in a pipeline.
-    metrics/spanmetrics:
-      receivers: [otlp/spanmetrics]
-      exporters: [prometheus]

pkg/prometheus/config/config.go
@@ -28,9 +28,8 @@ type Configuration struct {
 	TokenFilePath            string
 	TokenOverrideFromContext bool
 
-	SupportSpanmetricsConnector bool
-	MetricNamespace             string
-	LatencyUnit                 string
-	NormalizeCalls              bool
-	NormalizeDuration           bool
+	MetricNamespace   string
+	LatencyUnit       string
+	NormalizeCalls    bool
+	NormalizeDuration bool
 }

plugin/metrics/prometheus/factory_test.go
@@ -53,22 +53,11 @@ func TestWithDefaultConfiguration(t *testing.T) {
 	assert.Equal(t, "http://localhost:9090", f.options.Primary.ServerURL)
 	assert.Equal(t, 30*time.Second, f.options.Primary.ConnectTimeout)
 
-	assert.True(t, f.options.Primary.SupportSpanmetricsConnector)
 	assert.Empty(t, f.options.Primary.MetricNamespace)
 	assert.Equal(t, "ms", f.options.Primary.LatencyUnit)
 }
 
 func TestWithConfiguration(t *testing.T) {
-	t.Run("still supports the deprecated spanmetrics processor", func(t *testing.T) {
-		f := NewFactory()
-		v, command := config.Viperize(f.AddFlags)
-		err := command.ParseFlags([]string{
-			"--prometheus.query.support-spanmetrics-connector=false",
-		})
-		require.NoError(t, err)
-		f.InitFromViper(v, zap.NewNop())
-		assert.False(t, f.options.Primary.SupportSpanmetricsConnector)
-	})
 	t.Run("with custom configuration and no space in token file path", func(t *testing.T) {
 		f := NewFactory()
 		v, command := config.Viperize(f.AddFlags)
@@ -99,13 +88,11 @@ func TestWithConfiguration(t *testing.T) {
 		f := NewFactory()
 		v, command := config.Viperize(f.AddFlags)
 		err := command.ParseFlags([]string{
-			"--prometheus.query.support-spanmetrics-connector=true",
 			"--prometheus.query.namespace=mynamespace",
 			"--prometheus.query.duration-unit=ms",
 		})
 		require.NoError(t, err)
 		f.InitFromViper(v, zap.NewNop())
-		assert.True(t, f.options.Primary.SupportSpanmetricsConnector)
 		assert.Equal(t, "mynamespace", f.options.Primary.MetricNamespace)
 		assert.Equal(t, "ms", f.options.Primary.LatencyUnit)
 	})

commit 108c29bd846c2b4001468d0207554b61b33c1058

Restore "operation" name in the metrics response (#5673)
## Which problem is this PR solving?
- Resolves #5672
- Due to oversight, a bug was introduced in #5539 which changed the
attribute name expected by the UI from `operation` to `span_name`

## Description of the changes
- Revert those changes

## How was this change tested?
`(cd docker-compose/monitor/ && make build dev)`

<img width="1910" alt="image"
src="https://github.com/jaegertracing/jaeger/assets/3523016/79400073-75f1-4db5-8bef-9a4c439d2852">


## Checklist
- [ ] I have read
https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
- [ ] I have signed all commits
- [ ] I have added unit tests for the new functionality
- [ ] I have run lint and test steps successfully
  - for `jaeger`: `make lint test`
  - for `jaeger-ui`: `yarn lint` and `yarn test`

Signed-off-by: Yuri Shkuro <github@ysh.us>

plugin/metrics/prometheus/metricsstore/dbmodel/to_domain.go
@@ -31,8 +31,8 @@ type Translator struct {
 // New returns a new Translator.
 func New(spanNameLabel string) Translator {
 	return Translator{
-		// "span_name" is the label name that Jaeger UI expects.
-		labelMap: map[string]string{spanNameLabel: "span_name"},
+		// "operation" is the label name that Jaeger UI expects.
+		labelMap: map[string]string{spanNameLabel: "operation"},
 	}
 }
 

plugin/metrics/prometheus/metricsstore/reader.go
@@ -55,7 +55,7 @@ type (
 		metricsTranslator dbmodel.Translator
 		latencyMetricName string
 		callsMetricName   string
-		operationLabel    string
+		operationLabel    string // name of the attribute that contains span name / operation
 	}
 
 	promQueryParams struct {
@@ -90,7 +90,7 @@ func NewMetricsReader(cfg config.Configuration, logger *zap.Logger, tracer trace
 		return nil, fmt.Errorf("failed to initialize prometheus client: %w", err)
 	}
 
-	operationLabel := "span_name"
+	const operationLabel = "span_name"
 
 	mr := &MetricsReader{
 		client: promapi.NewAPI(client),

plugin/metrics/prometheus/metricsstore/dbmodel/to_domain_test.go
@@ -48,7 +48,7 @@ func TestToDomainMetricsFamily(t *testing.T) {
 
 	wantMetricLabels := map[string]string{
 		"label_key": "label_value",
-		"span_name": "span_name_value", // assert the name is translated to a Jaeger-friendly label.
+		"operation": "span_name_value", // assert the name is translated to a Jaeger-friendly label.
 	}
 	assert.Len(t, mf.Metrics, 1)
 	for _, ml := range mf.Metrics[0].Labels {

plugin/metrics/prometheus/metricsstore/reader_test.go
@@ -16,7 +16,6 @@ package metricsstore
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -183,7 +182,7 @@ func TestGetLatencies(t *testing.T) {
 			wantName:         "service_operation_latencies",
 			wantDescription:  "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_bucket{service_name =~ "emailservice", ` +
@@ -215,7 +214,7 @@ func TestGetLatencies(t *testing.T) {
 			wantName:        "service_operation_latencies",
 			wantDescription: "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(span_metrics_duration_bucket{service_name =~ "emailservice", ` +
@@ -234,7 +233,7 @@ func TestGetLatencies(t *testing.T) {
 			wantName:        "service_operation_latencies",
 			wantDescription: "0.95th quantile latency, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `histogram_quantile(0.95, sum(rate(duration_seconds_bucket{service_name =~ "emailservice", ` +
@@ -257,7 +256,7 @@ func TestGetLatencies(t *testing.T) {
 
 			m, err := reader.GetLatencies(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -286,7 +285,7 @@ func TestGetCallRates(t *testing.T) {
 			wantName:         "service_operation_call_rate",
 			wantDescription:  "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls{service_name =~ "emailservice", ` +
@@ -317,7 +316,7 @@ func TestGetCallRates(t *testing.T) {
 			wantName:        "service_operation_call_rate",
 			wantDescription: "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(span_metrics_calls{service_name =~ "emailservice", ` +
@@ -335,7 +334,7 @@ func TestGetCallRates(t *testing.T) {
 			wantName:        "service_operation_call_rate",
 			wantDescription: "calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls_total{service_name =~ "emailservice", ` +
@@ -357,7 +356,7 @@ func TestGetCallRates(t *testing.T) {
 
 			m, err := reader.GetCallRates(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -387,7 +386,7 @@ func TestGetErrorRates(t *testing.T) {
 			wantName:         "service_operation_error_rate",
 			wantDescription:  "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
@@ -439,7 +438,7 @@ func TestGetErrorRates(t *testing.T) {
 			wantName:        "service_operation_error_rate",
 			wantDescription: "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(span_metrics_calls{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
@@ -458,7 +457,7 @@ func TestGetErrorRates(t *testing.T) {
 			wantName:        "service_operation_error_rate",
 			wantDescription: "error rate, computed as a fraction of errors/sec over calls/sec, grouped by service & operation",
 			wantLabels: map[string]string{
-				"span_name":    "/OrderResult",
+				"operation":    "/OrderResult",
 				"service_name": "emailservice",
 			},
 			wantPromQlQuery: `sum(rate(calls_total{service_name =~ "emailservice", status_code = "STATUS_CODE_ERROR", ` +
@@ -481,7 +480,7 @@ func TestGetErrorRates(t *testing.T) {
 
 			m, err := reader.GetErrorRates(context.Background(), &params)
 			require.NoError(t, err)
-			assertMetrics(t, tc.name, m, tc.wantLabels, tc.wantName, tc.wantDescription)
+			assertMetrics(t, m, tc.wantLabels, tc.wantName, tc.wantDescription)
 			assert.Len(t, exp.GetSpans(), 1, "HTTP request was traced and span reported")
 		})
 	}
@@ -945,7 +944,7 @@ func prepareMetricsReaderAndServer(t *testing.T, config config.Configuration, wa
 	return reader, mockPrometheus
 }
 
-func assertMetrics(t *testing.T, testName string, gotMetrics *metrics.MetricFamily, wantLabels map[string]string, wantName, wantDescription string) {
+func assertMetrics(t *testing.T, gotMetrics *metrics.MetricFamily, wantLabels map[string]string, wantName, wantDescription string) {
 	assert.Len(t, gotMetrics.Metrics, 1)
 	assert.Equal(t, wantName, gotMetrics.Name)
 	assert.Equal(t, wantDescription, gotMetrics.Help)
@@ -953,9 +952,8 @@ func assertMetrics(t *testing.T, testName string, gotMetrics *metrics.MetricFami
 	assert.Len(t, mps, 1)
 
 	// logging for expected and actual labels
-	fmt.Printf("Test Name: %s\n", testName)
-	fmt.Printf("Expected labels: %v\n", wantLabels)
-	fmt.Printf("Actual labels: %v\n", gotMetrics.Metrics[0].Labels)
+	t.Logf("Expected labels: %v\n", wantLabels)
+	t.Logf("Actual labels: %v\n", gotMetrics.Metrics[0].Labels)
 
 	// There is no guaranteed order of labels, so we need to take the approach of using a map of expected values.
 	labels := gotMetrics.Metrics[0].Labels
@@ -968,8 +966,8 @@ func assertMetrics(t *testing.T, testName string, gotMetrics *metrics.MetricFami
 	assert.Empty(t, wantLabels)
 
 	// Additional logging to show that all expected labels were found and matched
-	fmt.Printf("Remaining expected labels after matching: %v\n", wantLabels)
-	fmt.Printf("\n")
+	t.Logf("Remaining expected labels after matching: %v\n", wantLabels)
+	t.Logf("\n")
 
 	assert.Equal(t, int64(1620351786), mps[0].Timestamp.GetSeconds())
 