8032ce222db94be81aa4440b88dd4d369482b30a
Wise-Wizard <saransh.shankar@gmail.com>
2024-06-23 18:16:57 +0530

    Created rough implementation of OTEL SDK
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- internal/metrics/otelmetrics/factory.go
+++ internal/metrics/otelmetrics/factory.go
@@ -7,22 +7,40 @@ import (
 	"context"
 	"log"
 
+	"github.com/jaegertracing/jaeger/pkg/metrics"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric"
-
-	"github.com/jaegertracing/jaeger/pkg/metrics"
+	metric "go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
-type otelFactory struct{}
+type otelFactory struct {
+	meter metric.Meter
+}
 
 func NewFactory() metrics.Factory {
-	return &otelFactory{}
+	res, err := resource.New(context.Background(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("jaeger-V2"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Could not create resource: %v", err)
+	}
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(res),
+		sdkmetric.WithReader(sdkmetric.NewManualReader()),
+	)
+	otel.SetMeterProvider(meterProvider)
+	return &otelFactory{
+		meter: otel.Meter("jaeger-V2"),
+	}
 }
 
 func (f *otelFactory) Counter(opts metrics.Options) metrics.Counter {
-	meter := otel.Meter("jaeger-V2")
-	counter, err := meter.Int64Counter(opts.Name)
+	counter, err := f.meter.Int64Counter(opts.Name)
 	if err != nil {
 		log.Printf("Error creating OTEL counter: %v", err)
 		return metrics.NullCounter
