d6347c440c10f8002f997f392b06124190d0eac4
Wise-Wizard <saransh.shankar@gmail.com>
2024-06-26 18:36:44 +0530

    Added Namespace Configuration
    Signed-off-by: Wise-Wizard <saransh.shankar@gmail.com>

--- internal/metrics/otelmetrics/factory.go
+++ internal/metrics/otelmetrics/factory.go
@@ -6,25 +6,34 @@ package otelmetrics
 import (
 	"context"
 	"log"
+	"strings"
 
 	"go.opentelemetry.io/otel/attribute"
-	metric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/jaegertracing/jaeger/pkg/metrics"
 )
 
 type otelFactory struct {
-	meter metric.Meter
+	meter      metric.Meter
+	scope      string
+	separator  string
+	normalizer *strings.Replacer
+	tags       map[string]string
 }
 
 func NewFactory(meterProvider metric.MeterProvider) metrics.Factory {
 	return &otelFactory{
-		meter: meterProvider.Meter("jaeger-v2"),
+		meter:      meterProvider.Meter("jaeger-v2"),
+		separator:  ".",
+		normalizer: strings.NewReplacer(" ", "_", ".", "_", "-", "_"),
+		tags:       make(map[string]string),
 	}
 }
 
 func (f *otelFactory) Counter(opts metrics.Options) metrics.Counter {
-	counter, err := f.meter.Int64Counter(opts.Name)
+	name := f.subScope(opts.Name)
+	counter, err := f.meter.Int64Counter(name)
 	if err != nil {
 		log.Printf("Error creating OTEL counter: %v", err)
 		return metrics.NullCounter
@@ -32,27 +41,28 @@ func (f *otelFactory) Counter(opts metrics.Options) metrics.Counter {
 	return &otelCounter{
 		counter:  counter,
 		fixedCtx: context.Background(),
-		option:   attributeSetOption(opts.Tags),
+		option:   attributeSetOption(f.mergeTags(opts.Tags)),
 	}
 }
 
 func (f *otelFactory) Gauge(opts metrics.Options) metrics.Gauge {
-	gauge, err := f.meter.Int64Gauge(opts.Name)
+	name := f.subScope(opts.Name)
+	gauge, err := f.meter.Int64Gauge(name)
 	if err != nil {
 		log.Printf("Error creating OTEL gauge: %v", err)
 		return metrics.NullGauge
-
 	}
 
 	return &otelGauge{
 		gauge:    gauge,
 		fixedCtx: context.Background(),
-		option:   attributeSetOption(opts.Tags),
+		option:   attributeSetOption(f.mergeTags(opts.Tags)),
 	}
 }
 
 func (f *otelFactory) Histogram(opts metrics.HistogramOptions) metrics.Histogram {
-	histogram, err := f.meter.Float64Histogram(opts.Name)
+	name := f.subScope(opts.Name)
+	histogram, err := f.meter.Float64Histogram(name)
 	if err != nil {
 		log.Printf("Error creating OTEL histogram: %v", err)
 		return metrics.NullHistogram
@@ -61,12 +71,13 @@ func (f *otelFactory) Histogram(opts metrics.HistogramOptions) metrics.Histogram
 	return &otelHistogram{
 		histogram: histogram,
 		fixedCtx:  context.Background(),
-		option:    attributeSetOption(opts.Tags),
+		option:    attributeSetOption(f.mergeTags(opts.Tags)),
 	}
 }
 
 func (f *otelFactory) Timer(opts metrics.TimerOptions) metrics.Timer {
-	timer, err := f.meter.Float64Histogram(opts.Name, metric.WithUnit("s"))
+	name := f.subScope(opts.Name)
+	timer, err := f.meter.Float64Histogram(name, metric.WithUnit("s"))
 	if err != nil {
 		log.Printf("Error creating OTEL timer: %v", err)
 		return metrics.NullTimer
@@ -74,12 +85,43 @@ func (f *otelFactory) Timer(opts metrics.TimerOptions) metrics.Timer {
 	return &otelTimer{
 		histogram: timer,
 		fixedCtx:  context.Background(),
-		option:    attributeSetOption(opts.Tags),
+		option:    attributeSetOption(f.mergeTags(opts.Tags)),
 	}
 }
 
 func (f *otelFactory) Namespace(opts metrics.NSOptions) metrics.Factory {
-	return f
+	return &otelFactory{
+		meter:      f.meter,
+		scope:      f.subScope(opts.Name),
+		separator:  f.separator,
+		normalizer: f.normalizer,
+		tags:       f.mergeTags(opts.Tags),
+	}
+}
+
+func (f *otelFactory) subScope(name string) string {
+	if f.scope == "" {
+		return f.normalize(name)
+	}
+	if name == "" {
+		return f.normalize(f.scope)
+	}
+	return f.normalize(f.scope + f.separator + name)
+}
+
+func (f *otelFactory) normalize(v string) string {
+	return f.normalizer.Replace(v)
+}
+
+func (f *otelFactory) mergeTags(tags map[string]string) map[string]string {
+	merged := make(map[string]string)
+	for k, v := range f.tags {
+		merged[k] = v
+	}
+	for k, v := range tags {
+		merged[k] = v
+	}
+	return merged
 }
 
 func attributeSetOption(tags map[string]string) metric.MeasurementOption {
