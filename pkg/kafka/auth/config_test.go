ee78dbe2dc84d6eef1bef7fac732459583fde9ec
mehul gautam <mehulsharma4786@gamil.com>
2024-06-27 16:07:44 +0000

    updated
    Signed-off-by: mehul gautam <mehulsharma4786@gamil.com>

--- pkg/kafka/auth/config_test.go
+++ pkg/kafka/auth/config_test.go
@@ -1,82 +1,110 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
 package auth
 
 import (
 	"testing"
+
+	"github.com/Shopify/sarama"
 	"github.com/spf13/viper"
-	"github.com/jaegertracing/jaeger/pkg/config/tlscfg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	//"github.com/davecgh/go-spew/spew"
+	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/pkg/config/tlscfg"
 )
 
-
 func Test_InitFromViper(t *testing.T) {
-	//kebrose_test
 	v := viper.New()
 	configPrefix := "kafka.auth."
-	v.Set(configPrefix + suffixAuthentication, "kerberos")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosServiceName, "kafka")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosRealm, "EXAMPLE.COM")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosUseKeyTab, true)
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosUsername, "user")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosPassword, "password")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosConfig, "/path/to/krb5.conf")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosKeyTab, "/path/to/keytab")
-	v.Set(configPrefix + kerberosPrefix + suffixKerberosDisablePAFXFAST, true)
-	v.Set(configPrefix + plainTextPrefix + suffixPlainTextUsername, "user")
-	v.Set(configPrefix + plainTextPrefix + suffixPlainTextPassword, "password")
-	v.Set(configPrefix + plainTextPrefix + suffixPlainTextMechanism, "SCRAM-SHA-256")
+	v.Set(configPrefix+suffixAuthentication, "tls")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosServiceName, "kafka")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosRealm, "EXAMPLE.COM")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosUseKeyTab, true)
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosUsername, "user")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosPassword, "password")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosConfig, "/path/to/krb5.conf")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosKeyTab, "/path/to/keytab")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosDisablePAFXFAST, true)
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextUsername, "user")
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextPassword, "password")
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextMechanism, "SCRAM-SHA-256")
 
 	authConfig := &AuthenticationConfig{}
 	err := authConfig.InitFromViper(configPrefix, v)
 	require.NoError(t, err)
 	expectedConfig := &AuthenticationConfig{
-		Authentication: "kerberos",
+		Authentication: "tls",
 		Kerberos: KerberosConfig{
-			ServiceName:      "kafka",
-			Realm:            "EXAMPLE.COM",
-			UseKeyTab:        true,
-			Username:         "user",
-			Password:         "password",
-			ConfigPath:       "/path/to/krb5.conf",
-			KeyTabPath:       "/path/to/keytab",
-			DisablePAFXFast:  true,
+			ServiceName:     "kafka",
+			Realm:           "EXAMPLE.COM",
+			UseKeyTab:       true,
+			Username:        "user",
+			Password:        "password",
+			ConfigPath:      "/path/to/krb5.conf",
+			KeyTabPath:      "/path/to/keytab",
+			DisablePAFXFast: true,
 		},
-		TLS: tlscfg.Options{},
+		TLS: tlscfg.Options{
+			Enabled: true,
+		},
 		PlainText: PlainTextConfig{
-			Username:   "user",
-			Password:   "password",
-			Mechanism:  "SCRAM-SHA-256",
+			Username:  "user",
+			Password:  "password",
+			Mechanism: "SCRAM-SHA-256",
 		},
 	}
 	assert.Equal(t, expectedConfig, authConfig)
-	//no authentication test
-	v.Set(configPrefix + suffixAuthentication, "")
+}
+
+func TestSetConfiguration(t *testing.T) {
+	logger, _ := zap.NewDevelopment()
+	saramaConfig := sarama.NewConfig()
+	v := viper.New()
+	configPrefix := "kafka.auth."
+	v.Set(configPrefix+suffixAuthentication, "kerberos")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosServiceName, "kafka")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosRealm, "EXAMPLE.COM")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosUseKeyTab, true)
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosUsername, "user")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosPassword, "password")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosConfig, "/path/to/krb5.conf")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosKeyTab, "/path/to/keytab")
+	v.Set(configPrefix+kerberosPrefix+suffixKerberosDisablePAFXFAST, true)
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextUsername, "user")
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextPassword, "password")
+	v.Set(configPrefix+plainTextPrefix+suffixPlainTextMechanism, "SCRAM-SHA-256")
+
+	authConfig := &AuthenticationConfig{}
+	err := authConfig.InitFromViper(configPrefix, v)
+	require.NoError(t, err)
+	require.NoError(t, authConfig.SetConfiguration(saramaConfig, logger))
+	// plaintest test
+	v.Set(configPrefix+suffixAuthentication, "plaintext")
 	authConfig = &AuthenticationConfig{}
 	err = authConfig.InitFromViper(configPrefix, v)
 	require.NoError(t, err)
-
-	expectedConfig.Authentication = ""
-	assert.Equal(t, expectedConfig, authConfig)
-
-	//plain text test
-	v.Set(configPrefix + suffixAuthentication, "plaintext")
+	require.NoError(t, authConfig.SetConfiguration(saramaConfig, logger))
+	// none value
+	v.Set(configPrefix+suffixAuthentication, " ")
 	authConfig = &AuthenticationConfig{}
 	err = authConfig.InitFromViper(configPrefix, v)
 	require.NoError(t, err)
-	expectedConfig.Authentication = "plaintext"
-	assert.Equal(t, expectedConfig, authConfig)
-	//tls test
-	v.Set(configPrefix + suffixAuthentication, "tls")
+	require.NoError(t, authConfig.SetConfiguration(saramaConfig, logger))
+	// tls value
+	// v.Set(configPrefix + suffixAuthentication, "tls")
+	// authConfig = &AuthenticationConfig{}
+	// err = authConfig.InitFromViper(configPrefix, v)
+	// require.NoError(t, err)
+	// _,err=authConfig.TLS.Config(zap.NewNop())
+	// // require.NoError(t, err)
+	// // require.NoError(t,authConfig.SetConfiguration(saramaConfig, logger))
+	// default value
+	v.Set(configPrefix+suffixAuthentication, "error")
 	authConfig = &AuthenticationConfig{}
 	err = authConfig.InitFromViper(configPrefix, v)
 	require.NoError(t, err)
-
-	expectedConfig.Authentication = "tls"
-	tlsClientConfig := tlscfg.ClientFlagsConfig{
-		Prefix: configPrefix,
-	}
-	expectedConfig.TLS, _ = tlsClientConfig.InitFromViper(v)
-	expectedConfig.TLS.Enabled = true
-	assert.Equal(t, expectedConfig, authConfig)
+	expected_error := "Unknown/Unsupported authentication method error to kafka cluster"
+	require.Error(t, authConfig.SetConfiguration(saramaConfig, logger), expected_error)
 }
