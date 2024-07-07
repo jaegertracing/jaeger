16ae07e4c8eee2269a21dccf07c397ba6d49c2d0
Yuri Shkuro <github@ysh.us>
2024-07-07 18:59:02 -0400

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/jaeger/internal/integration/e2e_integration_test.go
+++ cmd/jaeger/internal/integration/e2e_integration_test.go
@@ -3,5 +3,8 @@ package integration
 import "testing"
 
 func TestCreateStorageCleanerConfig(t *testing.T) {
+	// Ensure that we can parse the existing configs correctly.
+	// This is faster to run than the full integration test.
 	createStorageCleanerConfig(t, "../../config-elasticsearch.yaml", "elasticsearch")
+	createStorageCleanerConfig(t, "../../config-opensearch.yaml", "opensearch")
 }
