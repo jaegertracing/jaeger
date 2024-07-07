ebd8dbe785617e555e151577e44b5f379886980b
mehul gautam <mehulsharma4786@gamil.com>
2024-07-07 14:27:02 +0000

    testing
    Signed-off-by: mehul gautam <mehulsharma4786@gamil.com>

--- plugin/storage/integration/es_index_cleaner_test.go
+++ plugin/storage/integration/es_index_cleaner_test.go
@@ -32,7 +32,7 @@ const (
 	samplingIndexName     = "jaeger-sampling-2019-01-01"
 	spanIndexName         = "jaeger-span-2019-01-01"
 	serviceIndexName      = "jaeger-service-2019-01-01"
-	indexCleanerImage     = "jaegertracing/jaeger-es-index-cleaner:local-test"
+	indexCleanerImage     = "localhost:5000/jaegertracing/jaeger-es-index-cleaner:local-test"
 	rolloverImage         = "jaegertracing/jaeger-es-rollover:1.57.0"
 	rolloverNowEnvVar     = `CONDITIONS='{"max_age":"0s"}'`
 )
