9996f30105cd1473e7b41448fc793277bdd5477e
mehul gautam <mehulsharma4786@gamil.com>
2024-07-14 22:24:44 +0000

    fixed
    Signed-off-by: mehul gautam <mehulsharma4786@gamil.com>

--- scripts/hotrod-integration-test.sh
+++ scripts/hotrod-integration-test.sh
@@ -2,43 +2,47 @@
 
 set -euxf -o pipefail
 
+docker_compose_file="./examples/hotrod/docker-compose.yml"
+export REGISTRY="localhost:5000/"
+platforms="linux/amd64,linux/s390x,linux/ppc64le,linux/arm64"
+
+teardown() {
+  echo "Tearing down..."
+  docker compose -f "$docker_compose_file" down
+}
+trap teardown EXIT
+
 make build-examples GOOS=linux GOARCH=amd64
 make build-examples GOOS=linux GOARCH=s390x
 make build-examples GOOS=linux GOARCH=ppc64le
 make build-examples GOOS=linux GOARCH=arm64
 
-
-platforms="linux/amd64,linux/s390x,linux/ppc64le,linux/arm64"
 make prepare-docker-buildx
 
-# build image locally (-l) for integration test
+# Build image locally (-l) for integration test
 bash scripts/build-upload-a-docker-image.sh -l -c example-hotrod -d examples/hotrod -p "${platforms}"
 
-export HOTROD_IMAGE="localhost:5000/jaegertracing/example-hotrod"
-docker compose -f ./examples/hotrod/docker-compose.yml up -d
+docker compose -f "$docker_compose_file" up -d
 
 i=0
-while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:8080)" != "200" && ${i} -lt 30 ]]; do
+while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:8080)" != "200" && $i -lt 30 ]]; do
   sleep 1
   i=$((i+1))
 done
+
 body=$(curl localhost:8080)
 if [[ $body != *"Rides On Demand"* ]]; then
   echo "String \"Rides On Demand\" is not present on the index page"
   exit 1
 fi
 
-curl -X POST "http://localhost:8080/dispatch?customer=123"
-# Extract the logs from the docker compose service
-logs=$(docker compose -f ./examples/hotrod/docker-compose.yml logs hotrod)
-
-# Extract the trace_id from the logs
-TRACE_ID=$(echo "$logs" | grep -oP '(?<="trace_id": ")[^"]+' | tail -n 1)
+response=$(curl -i -X POST "http://localhost:8080/dispatch?customer=123")
+TRACE_ID=$(echo "$response" | grep -Fi "Traceresponse" | awk '{print $2}' | cut -d '-' -f 2)
 
 JAEGER_QUERY_URL="http://localhost:16686"
-EXPECTED_SPANS=10  # Change this to the expected number of spans
-MAX_RETRIES=30     
-SLEEP_INTERVAL=10  
+EXPECTED_SPANS=10
+MAX_RETRIES=30
+SLEEP_INTERVAL=10
 
 # Function to poll Jaeger for the trace
 poll_jaeger() {
