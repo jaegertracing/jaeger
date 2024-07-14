b15f33eac504cd269396ff1f54aeaf304ee31fae
pushkarm029 <pushkarmishra029@gmail.com>
2024-07-14 15:06:13 +0530

    fix
    Signed-off-by: pushkarm029 <pushkarmishra029@gmail.com>

--- cmd/jaeger/internal/extension/remotesampling/extension_test.go
+++ cmd/jaeger/internal/extension/remotesampling/extension_test.go
@@ -5,17 +5,23 @@ package remotesampling
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/storagetest"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/extension"
+	noopmetric "go.opentelemetry.io/otel/metric/noop"
 	nooptrace "go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
 
 	"github.com/jaegertracing/jaeger/cmd/jaeger/internal/extension/jaegerstorage"
-	memoryCfg "github.com/jaegertracing/jaeger/plugin/storage/memory"
+	"github.com/jaegertracing/jaeger/plugin/sampling/strategyprovider/adaptive"
+	"github.com/jaegertracing/jaeger/plugin/storage/memory"
 )
 
 type storageHost struct {
@@ -58,28 +64,34 @@ func (samplingHost) GetExporters() map[component.DataType]map[component.ID]compo
 	return nil
 }
 
-func makeStorageExtension(t *testing.T, memstoreName string) storageHost {
+func makeStorageExtension(t *testing.T, memstoreName string) component.Host {
+	telemetrySettings := component.TelemetrySettings{
+		Logger:         zaptest.NewLogger(t),
+		TracerProvider: nooptrace.NewTracerProvider(),
+		MeterProvider:  noopmetric.NewMeterProvider(),
+	}
 	extensionFactory := jaegerstorage.NewFactory()
 	storageExtension, err := extensionFactory.CreateExtension(
 		context.Background(),
 		extension.Settings{
-			TelemetrySettings: component.TelemetrySettings{
-				Logger:         zap.L(),
-				TracerProvider: nooptrace.NewTracerProvider(),
-			},
+			TelemetrySettings: telemetrySettings,
 		},
 		&jaegerstorage.Config{Backends: map[string]jaegerstorage.Backend{
-			memstoreName: {Memory: &memoryCfg.Configuration{MaxTraces: 10000}},
+			memstoreName: {Memory: &memory.Configuration{MaxTraces: 10000}},
 		}},
 	)
 	require.NoError(t, err)
-	host := storageHost{t: t, storageExtension: storageExtension}
+
+	host := storagetest.NewStorageHost()
+	host.WithExtension(jaegerstorage.ID, storageExtension)
 
 	err = storageExtension.Start(context.Background(), host)
 	require.NoError(t, err)
 	t.Cleanup(func() { require.NoError(t, storageExtension.Shutdown(context.Background())) })
 	return host
 }
+
+var _ component.Config = (*Config)(nil)
 
 func makeRemoteSamplingExtension(t *testing.T, cfg component.Config) samplingHost {
 	extensionFactory := NewFactory()
@@ -95,30 +107,31 @@ func makeRemoteSamplingExtension(t *testing.T, cfg component.Config) samplingHos
 	)
 	require.NoError(t, err)
 	host := samplingHost{t: t, samplingExtension: samplingExtension}
+	storageHost := makeStorageExtension(t, "foobar")
 
-	err = samplingExtension.Start(context.Background(), host)
+	err = samplingExtension.Start(context.Background(), storageHost)
 	require.NoError(t, err)
 	t.Cleanup(func() { require.NoError(t, samplingExtension.Shutdown(context.Background())) })
 	return host
 }
 
-// func TestStartFileBasedProvider(t *testing.T) {
-// 	factory := NewFactory()
-// 	cfg := factory.CreateDefaultConfig().(*Config)
-// 	cfg.File.Path = filepath.Join("..", "..", "..", "sampling-strategies.json")
-// 	cfg.Adaptive = nil
-// 	cfg.HTTP = nil
-// 	cfg.GRPC = nil
-// 	require.NoError(t, cfg.Validate())
+func TestStartFileBasedProvider(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.File.Path = filepath.Join("..", "..", "..", "sampling-strategies.json")
+	cfg.Adaptive = nil
+	cfg.HTTP = nil
+	cfg.GRPC = nil
+	require.NoError(t, cfg.Validate())
 
-// 	ext, err := factory.CreateExtension(context.Background(), extension.Settings{
-// 		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
-// 	}, cfg)
-// 	require.NoError(t, err)
-// 	host := makeStorageExtension(t, "foobar")
-// 	require.NoError(t, ext.Start(context.Background(), host))
-// 	require.NoError(t, ext.Shutdown(context.Background()))
-// }
+	ext, err := factory.CreateExtension(context.Background(), extension.Settings{
+		TelemetrySettings: componenttest.NewNopTelemetrySettings(),
+	}, cfg)
+	require.NoError(t, err)
+	host := makeStorageExtension(t, "foobar")
+	require.NoError(t, ext.Start(context.Background(), host))
+	require.NoError(t, ext.Shutdown(context.Background()))
+}
 
 func TestStartAdaptiveProvider(t *testing.T) {
 	factory := NewFactory()
@@ -138,95 +151,29 @@ func TestStartAdaptiveProvider(t *testing.T) {
 	require.NoError(t, ext.Shutdown(context.Background()))
 }
 
-// storageExtension, err := extensionFactory.CreateExtension(
-// 	context.Background(),
-// 	extension.Settings{
-// 		TelemetrySettings: component.TelemetrySettings{
-// 			Logger:         zap.L(),
-// 			TracerProvider: nooptrace.NewTracerProvider(),
-// 		},
-// 	},
-// 	&jaegerstorage.Config{Memory: map[string]memoryCfg.Configuration{
-// 		memstoreName: {MaxTraces: 10000},
-// 	}})
-
 func TestGetAdaptiveSamplingComponents(t *testing.T) {
-	host := makeStorageExtension(t, "foobar")
-	_, err := GetAdaptiveSamplingComponents(host)
-	require.Error(t, err)
+	// Success case
+	host := makeRemoteSamplingExtension(t, &Config{
+		Adaptive: &AdaptiveConfig{
+			SamplingStore: "foobar",
+			Options: adaptive.Options{
+				FollowerLeaseRefreshInterval: 1,
+				LeaderLeaseRefreshInterval:   1,
+				AggregationBuckets:           1,
+			},
+		},
+	})
 
-	samplingG := makeRemoteSamplingExtension(t, &Config{})
-	_, err = GetAdaptiveSamplingComponents(samplingG)
+	comps, err := GetAdaptiveSamplingComponents(host)
 	require.NoError(t, err)
+	require.NotNil(t, comps.DistLock)
+	require.NotNil(t, comps.SamplingStore)
+	require.Equal(t, comps.Options.FollowerLeaseRefreshInterval, time.Duration(1))
+	require.Equal(t, comps.Options.LeaderLeaseRefreshInterval, time.Duration(1))
+	require.Equal(t, comps.Options.AggregationBuckets, 1)
 
-	// host = makeRemoteSamplingExtension(t, &Config{})
-	// t.Log(host.GetExtensions())
-	// _, err = GetAdaptiveSamplingComponents(host)
-	// require.NoError(t, err)
-
-	// tests := []struct {
-	// 	name          string
-	// 	setupHost     func(*testing.T) component.Host
-	// 	expectedError string
-	// }{
-	// 	{
-	// 		name: "successful retrieval",
-	// 		setupHost: func(t *testing.T) component.Host {
-	// 			factory := NewFactory()
-	// 			cfg := factory.CreateDefaultConfig().(*Config)
-	// 			cfg.File = nil
-	// 			cfg.Adaptive.SamplingStore = "foobar"
-	// 			cfg.HTTP = nil
-	// 			cfg.GRPC = nil
-	// 			require.NoError(t, cfg.Validate())
-
-	// 			ext, err := factory.CreateExtension(context.Background(), extension.Settings{
-	// 				TelemetrySettings: componenttest.NewNopTelemetrySettings(),
-	// 			}, cfg)
-	// 			require.NoError(t, err)
-	// 			host := makeStorageExtension(t, "foobar")
-	// 			require.NoError(t, ext.Start(context.Background(), host))
-	// 			t.Cleanup(func() {
-	// 				require.NoError(t, ext.Shutdown(context.Background()))
-	// 			})
-	// 			return host
-	// 		},
-	// 		expectedError: "",
-	// 	},
-	// 	{
-	// 		name: "extension not found",
-	// 		setupHost: func(t *testing.T) component.Host {
-	// 			return storageHost{t: t}
-	// 		},
-	// 		expectedError: "cannot find extension 'jaeger.remote_sampling' (make sure it's defined earlier in the config)",
-	// 	},
-	// 	// {
-	// 	// 	name: "incorrect extension type",
-	// 	// 	setupHost: func(t *testing.T) component.Host {
-	// 	// 		host := storageHost{t: t}
-	// 	// 		host.storageExtension = componenttest.NewNopExtension()
-	// 	// 		return host
-	// 	// 	},
-	// 	// 	expectedError: "extension 'jaeger.remote_sampling' is not of type 'jaeger.remote_sampling'",
-	// 	// },
-	// }
-
-	// for _, tt := range tests {
-	// 	t.Run(tt.name, func(t *testing.T) {
-	// 		host := tt.setupHost(t)
-	// 		components, err := GetAdaptiveSamplingComponents(host)
-
-	// 		if tt.expectedError != "" {
-	// 			require.Error(t, err)
-	// 			require.Contains(t, err.Error(), tt.expectedError)
-	// 			require.Nil(t, components)
-	// 		} else {
-	// 			require.NoError(t, err)
-	// 			require.NotNil(t, components)
-	// 			require.NotNil(t, components.SamplingStore)
-	// 			require.NotNil(t, components.DistLock)
-	// 			require.NotNil(t, components.Options)
-	// 		}
-	// 	})
-	// }
+	// Error case
+	host = makeRemoteSamplingExtension(t, &Config{})
+	_, err = GetAdaptiveSamplingComponents(host)
+	require.ErrorContains(t, err, "extension 'remote_sampling' is not configured for adaptive sampling")
 }
--- cmd/jaeger/internal/processors/adaptivesampling/processor_test.go
+++ cmd/jaeger/internal/processors/adaptivesampling/processor_test.go
@@ -7,47 +7,92 @@ import (
 	"context"
 	"testing"
 
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/storagetest"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	noopmetric "go.opentelemetry.io/otel/metric/noop"
+	nooptrace "go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 
+	"github.com/jaegertracing/jaeger/cmd/jaeger/internal/extension/jaegerstorage"
 	"github.com/jaegertracing/jaeger/cmd/jaeger/internal/extension/remotesampling"
+	"github.com/jaegertracing/jaeger/plugin/sampling/strategyprovider/adaptive"
+	"github.com/jaegertracing/jaeger/plugin/storage/memory"
 )
 
-type storageHost struct {
-	extension component.Component
+type samplingHost struct {
+	t                 *testing.T
+	samplingExtension component.Component
 }
 
-func (storageHost) ReportFatalError(error) {
-}
-
-func (host storageHost) GetExtensions() map[component.ID]component.Component {
+func (host samplingHost) GetExtensions() map[component.ID]component.Component {
 	return map[component.ID]component.Component{
-		remotesampling.ID: host.extension,
+		remotesampling.ID: host.samplingExtension,
 	}
 }
 
-func (storageHost) GetFactory(_ component.Kind, _ component.Type) component.Factory {
+func (host samplingHost) ReportFatalError(err error) {
+	host.t.Fatal(err)
+}
+
+func (samplingHost) GetFactory(_ component.Kind, _ component.Type) component.Factory { return nil }
+func (samplingHost) GetExporters() map[component.DataType]map[component.ID]component.Component {
 	return nil
 }
 
-func (storageHost) GetExporters() map[component.DataType]map[component.ID]component.Component {
-	return nil
+func makeStorageExtension(t *testing.T, memstoreName string) component.Host {
+	telemetrySettings := component.TelemetrySettings{
+		Logger:         zaptest.NewLogger(t),
+		TracerProvider: nooptrace.NewTracerProvider(),
+		MeterProvider:  noopmetric.NewMeterProvider(),
+	}
+	extensionFactory := jaegerstorage.NewFactory()
+	storageExtension, err := extensionFactory.CreateExtension(
+		context.Background(),
+		extension.Settings{
+			TelemetrySettings: telemetrySettings,
+		},
+		&jaegerstorage.Config{Backends: map[string]jaegerstorage.Backend{
+			memstoreName: {Memory: &memory.Configuration{MaxTraces: 10000}},
+		}},
+	)
+	require.NoError(t, err)
+
+	host := storagetest.NewStorageHost()
+	host.WithExtension(jaegerstorage.ID, storageExtension)
+
+	err = storageExtension.Start(context.Background(), host)
+	require.NoError(t, err)
+	t.Cleanup(func() { require.NoError(t, storageExtension.Shutdown(context.Background())) })
+	return host
 }
 
-// type fakeRSExtension struct{}
+var _ component.Config = (*Config)(nil)
 
-// var _ remotesampling.Extension = (*fakeRSExtension)(nil)
+func makeRemoteSamplingExtension(t *testing.T, cfg component.Config) samplingHost {
+	extensionFactory := remotesampling.NewFactory()
+	samplingExtension, err := extensionFactory.CreateExtension(
+		context.Background(),
+		extension.Settings{
+			TelemetrySettings: component.TelemetrySettings{
+				Logger:         zap.L(),
+				TracerProvider: nooptrace.NewTracerProvider(),
+			},
+		},
+		cfg,
+	)
+	require.NoError(t, err)
+	host := samplingHost{t: t, samplingExtension: samplingExtension}
+	storageHost := makeStorageExtension(t, "foobar")
 
-// func (f *fakeRSExtension) Start(ctx context.Context, host component.Host) error {
-// 	return nil
-// }
-
-// func (f *fakeRSExtension) Shutdown(ctx context.Context) error {
-// 	return nil
-// }
+	err = samplingExtension.Start(context.Background(), storageHost)
+	require.NoError(t, err)
+	t.Cleanup(func() { require.NoError(t, samplingExtension.Shutdown(context.Background())) })
+	return host
+}
 
 func TestNewTraceProcessor(t *testing.T) {
 	telemetrySettings := component.TelemetrySettings{
@@ -59,32 +104,38 @@ func TestNewTraceProcessor(t *testing.T) {
 	require.NotNil(t, newTraceProcessor)
 }
 
-func TestTraceProcessorStart(t *testing.T) {
+func TestTraceProcessor(t *testing.T) {
 	telemetrySettings := component.TelemetrySettings{
 		Logger: zaptest.NewLogger(t, zaptest.WrapOptions(zap.AddCaller())),
 	}
 	config, ok := createDefaultConfig().(*Config)
 	require.True(t, ok)
 	traceProcessor := newTraceProcessor(*config, telemetrySettings)
-	err := traceProcessor.start(context.Background(), componenttest.NewNopHost())
+
+	host := makeRemoteSamplingExtension(t, &remotesampling.Config{
+		Adaptive: &remotesampling.AdaptiveConfig{
+			SamplingStore: "foobar",
+			Options:       adaptive.DefaultOptions(),
+		},
+	})
+
+	err := traceProcessor.start(context.Background(), host)
+	require.NoError(t, err)
+
+	twww := makeTracesOneSpan()
+	trace, err := traceProcessor.processTraces(context.Background(), twww)
+	require.NoError(t, err)
+	require.NotNil(t, trace)
+
+	err = traceProcessor.close(context.Background())
 	require.NoError(t, err)
 }
 
-// func TestTraceProcessorStarttw(t *testing.T) {
-// 	telemetrySettings := component.TelemetrySettings{
-// 		Logger: zaptest.NewLogger(t, zaptest.WrapOptions(zap.AddCaller())),
-// 	}
-// 	traceProcessor := newTraceProcessor(Config{}, telemetrySettings)
-// 	getAdaptiveSamplingComponents := remotesampling.AdaptiveSamplingComponents{
-// 		SamplingStore: ,
-// 	}
-// 	testHost := storageHost{
-// 		extension: &remotesampling.RsExtension{
-// 			AdaptiveStore: getAdaptiveSamplingComponents.SamplingStore,
-// 			DistLock:      getAdaptiveSamplingComponents.DistLock,
-// 		},
-// 	}
-
-// 	err := traceProcessor.start(context.Background(), testHost)
-// 	require.NoError(t, err)
-// }
+func makeTracesOneSpan() ptrace.Traces {
+	traces := ptrace.NewTraces()
+	rSpans := traces.ResourceSpans().AppendEmpty()
+	sSpans := rSpans.ScopeSpans().AppendEmpty()
+	span := sSpans.Spans().AppendEmpty()
+	span.SetName("test")
+	return traces
+}
