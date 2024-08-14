69db76e892ec5dc271374fdef18473d5e3b119df
Yuri Shkuro <github@ysh.us>
2024-08-14 00:09:47 -0400

    apply change to license headers
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/internal/flags/doc.go
+++ cmd/internal/flags/doc.go
@@ -1,17 +1,6 @@
 // Copyright (c) 2019 The Jaeger Authors.
 // Copyright (c) 2017 Uber Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 // Package flags defines command line flags that are shared by several jaeger components.
 // They are defined in this shared location so that if several components are wired into
