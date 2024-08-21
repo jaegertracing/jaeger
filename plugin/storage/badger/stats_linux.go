7fdca722a71b2ad6d282173f996a6c95a85c0431
Yuri Shkuro <github@ysh.us>
2024-08-20 22:36:06 -0400

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- plugin/storage/badger/stats_linux.go
+++ plugin/storage/badger/stats_linux.go
@@ -19,8 +19,10 @@ func (f *Factory) diskStatisticsUpdate() error {
 	_ = unix.Statfs(f.Options.GetPrimary().ValueDirectory, &valDirStatfs)
 
 	// Using Bavail instead of Bfree to get non-priviledged user space available
-	f.metrics.ValueLogSpaceAvailable.Update(int64(valDirStatfs.Bavail) * int64(valDirStatfs.Bsize))
-	f.metrics.KeyLogSpaceAvailable.Update(int64(keyDirStatfs.Bavail) * int64(keyDirStatfs.Bsize))
+	//nolint: gosec // G115
+	f.metrics.ValueLogSpaceAvailable.Update(int64(valDirStatfs.Bavail) * valDirStatfs.Bsize)
+	//nolint: gosec // G115
+	f.metrics.KeyLogSpaceAvailable.Update(int64(keyDirStatfs.Bavail) * keyDirStatfs.Bsize)
 
 	/*
 	 TODO If we wanted to clean up oldest data to free up diskspace, we need at a minimum an index to the StartTime
