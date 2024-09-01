8dd105380eafbd90e6590e9d3602ad1acc29e79a
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-09-01 18:52:33 -0400

    Fix godoc Comment
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/ui/doc.go
+++ cmd/query/app/ui/doc.go
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package ui bundles UI assets packaged with stdlib/embed.
-// If the real UI assets are available, they are used in the
-// build-ui Makefile target. Otherwise, the non-functional
-// index.html is used.
+// If the real UI assets are available, they are embedded and
+// used at runtime. Otherwise, the non-functional index.html is used.
 package ui
