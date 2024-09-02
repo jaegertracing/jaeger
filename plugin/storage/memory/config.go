d3151ff96160f0a527573177df521bf7503763cf
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-09-02 15:02:48 -0400

    Implement Validate Method For Configuration
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- plugin/storage/memory/config.go
+++ plugin/storage/memory/config.go
@@ -3,7 +3,16 @@
 
 package memory
 
-// Configuration describes the options to customize the storage behavior
+import "github.com/asaskevich/govalidator"
+
+// Configuration describes the options to customize the storage behavior.
 type Configuration struct {
+	// MaxTraces is the maximum amount of traces to store to store in memory.
+	// If MaxTraces is set to 0 (default), the number of traces stored will be unbounded.
 	MaxTraces int `mapstructure:"max_traces"`
+}
+
+func (c *Configuration) Validate() error {
+	_, err := govalidator.ValidateStruct(c)
+	return err
 }
