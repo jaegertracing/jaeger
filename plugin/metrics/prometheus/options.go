24714127446073f631f295a844fe6742015a976f
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-09-21 18:02:28 -0400

    Drive By: Remove Unused Configuration
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- plugin/metrics/prometheus/options.go
+++ plugin/metrics/prometheus/options.go
@@ -32,7 +32,6 @@ const (
 	defaultConnectTimeout = 30 * time.Second
 	defaultTokenFilePath  = ""
 
-	defaultSupportSpanmetricsConnector = true
 	// the default configuration here matches the default namespace in the span metrics connector
 	defaultMetricNamespace   = "traces_span_metrics"
 	defaultLatencyUnit       = "ms"
