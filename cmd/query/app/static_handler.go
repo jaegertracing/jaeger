12e794ea1c33f0741ac531e2805c9f63cd051af9
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-09-23 20:44:11 -0400

    Embed UIConfig Into StaticAssetsHandlerOptions
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/static_handler.go
+++ cmd/query/app/static_handler.go
@@ -64,7 +64,7 @@ type StaticAssetsHandler struct {
 
 // StaticAssetsHandlerOptions defines options for NewStaticAssetsHandler
 type StaticAssetsHandlerOptions struct {
-	UIConfig            UIConfig
+	UIConfig
 	BasePath            string
 	StorageCapabilities querysvc.StorageCapabilities
 	Logger              *zap.Logger
@@ -92,8 +92,8 @@ func NewStaticAssetsHandler(staticAssetsRoot string, options StaticAssetsHandler
 		return nil, err
 	}
 
-	options.Logger.Info("Using UI configuration", zap.String("path", options.UIConfig.ConfigFile))
-	watcher, err := fswatcher.New([]string{options.UIConfig.ConfigFile}, h.reloadUIConfig, h.options.Logger)
+	options.Logger.Info("Using UI configuration", zap.String("path", options.ConfigFile))
+	watcher, err := fswatcher.New([]string{options.ConfigFile}, h.reloadUIConfig, h.options.Logger)
 	if err != nil {
 		return nil, err
 	}
@@ -110,7 +110,7 @@ func (sH *StaticAssetsHandler) loadAndEnrichIndexHTML(open func(string) (http.Fi
 		return nil, fmt.Errorf("cannot load index.html: %w", err)
 	}
 	// replace UI config
-	if configObject, err := loadUIConfig(sH.options.UIConfig.ConfigFile); err != nil {
+	if configObject, err := loadUIConfig(sH.options.ConfigFile); err != nil {
 		return nil, err
 	} else if configObject != nil {
 		indexBytes = configObject.regexp.ReplaceAll(indexBytes, configObject.config)
@@ -138,13 +138,13 @@ func (sH *StaticAssetsHandler) loadAndEnrichIndexHTML(open func(string) (http.Fi
 }
 
 func (sH *StaticAssetsHandler) reloadUIConfig() {
-	sH.options.Logger.Info("reloading UI config", zap.String("filename", sH.options.UIConfig.ConfigFile))
+	sH.options.Logger.Info("reloading UI config", zap.String("filename", sH.options.ConfigFile))
 	content, err := sH.loadAndEnrichIndexHTML(sH.assetsFS.Open)
 	if err != nil {
 		sH.options.Logger.Error("error while reloading the UI config", zap.Error(err))
 	}
 	sH.indexHTML.Store(content)
-	sH.options.Logger.Info("reloaded UI config", zap.String("filename", sH.options.UIConfig.ConfigFile))
+	sH.options.Logger.Info("reloaded UI config", zap.String("filename", sH.options.ConfigFile))
 }
 
 func loadIndexHTML(open func(string) (http.File, error)) ([]byte, error) {
@@ -201,7 +201,7 @@ func loadUIConfig(uiConfig string) (*loadedConfig, error) {
 }
 
 func (sH *StaticAssetsHandler) loggingHandler(handler http.Handler) http.Handler {
-	if !sH.options.UIConfig.LogAccess {
+	if !sH.options.LogAccess {
 		return handler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
