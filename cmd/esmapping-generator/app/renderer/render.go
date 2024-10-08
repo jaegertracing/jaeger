37cfcbeb6b785a4e3ed917547cc75f319ce89885
Saumya Shah <saumyabshah90@gmail.com>
2024-10-08 19:15:13 +0530

    update: dependents of mappings.go to fit enums now
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- cmd/esmapping-generator/app/renderer/render.go
+++ cmd/esmapping-generator/app/renderer/render.go
@@ -4,6 +4,7 @@
 package renderer
 
 import (
+  "fmt"
 	"strconv"
 
 	"github.com/jaegertracing/jaeger/cmd/esmapping-generator/app"
@@ -11,11 +12,11 @@ import (
 	"github.com/jaegertracing/jaeger/plugin/storage/es/mappings"
 )
 
-var supportedMappings = map[string]struct{}{
-	"jaeger-span":         {},
-	"jaeger-service":      {},
-	"jaeger-dependencies": {},
-	"jaeger-sampling":     {},
+var supportedMappings = map[mappings.MappingType]struct{}{
+	mappings.SpanMapping:         {},
+	mappings.ServiceMapping:      {},
+	mappings.DependenciesMapping: {},
+	mappings.SamplingMapping:     {},
 }
 
 // GetMappingAsString returns rendered index templates as string
@@ -34,11 +35,37 @@ func GetMappingAsString(builder es.TemplateBuilder, opt *app.Options) (string, e
 		UseILM:          enableILM,
 		ILMPolicyName:   opt.ILMPolicyName,
 	}
-	return mappingBuilder.GetMapping(opt.Mapping)
+
+	mappingType, err := stringToMappingType(opt.Mapping)
+	if err != nil {
+		return "", err
+	}
+
+	return mappingBuilder.GetMapping(mappingType)
 }
 
 // IsValidOption checks if passed option is a valid index template.
 func IsValidOption(val string) bool {
-	_, ok := supportedMappings[val]
+	mappingType, err := stringToMappingType(val)
+	if err != nil {
+		return false
+	}
+	_, ok := supportedMappings[mappingType]
 	return ok
+}
+
+// stringToMappingType converts a string to a MappingType
+func stringToMappingType(val string) (mappings.MappingType, error) {
+	switch val {
+	case "jaeger-span":
+		return mappings.SpanMapping, nil
+	case "jaeger-service":
+		return mappings.ServiceMapping, nil
+	case "jaeger-dependencies":
+		return mappings.DependenciesMapping, nil
+	case "jaeger-sampling":
+		return mappings.SamplingMapping, nil
+	default:
+		return mappings.SpanMapping, fmt.Errorf("invalid mapping type: %s", val)
+	}
 }
