9be31d9681c83114c940e4ae18038e060ecebc54
Andreas Gerstmayr <agerstmayr@redhat.com>
2024-10-10 19:13:41 +0200

    adjust scripts/rebuild-ui.sh
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- scripts/rebuild-ui.sh
+++ scripts/rebuild-ui.sh
@@ -32,4 +32,4 @@ if [[ "$last_tag" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]];  then
 fi
 
 # do a regular full build
-yarn install --frozen-lockfile && cd packages/jaeger-ui && yarn build
+npm ci && cd packages/jaeger-ui && npm run build
