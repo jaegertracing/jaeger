dab18f14292f8be61a0933586e4b5c9871e1b0a0
Andreas Gerstmayr <agerstmayr@redhat.com>
2024-10-11 13:36:03 +0200

    unshallow jaeger-ui submodule to receive tags
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- scripts/rebuild-ui.sh
+++ scripts/rebuild-ui.sh
@@ -7,7 +7,7 @@ set -euxf -o pipefail
 
 cd jaeger-ui
 
-git fetch --all --tags
+git fetch --all --unshallow --tags
 git log --oneline --decorate=full -n 10 | cat
 
 last_tag=$(git describe --tags --dirty 2>/dev/null)
