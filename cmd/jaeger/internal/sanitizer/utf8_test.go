c4797d5a833e3204f046230a9e5eec884ffa98c3
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-10-12 15:59:54 -0400

    Add Unit Tests For New Function
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ cmd/jaeger/internal/sanitizer/utf8_test.go
@@ -0,0 +1,300 @@
+package sanitizer
+
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+func invalidUTF8() string {
+	s, _ := hex.DecodeString("fefeffff")
+	return string(s)
+}
+
+var utf8EncodingTests = []struct {
+	name              string
+	key               string
+	value             string
+	expectedKey       string
+	expectedValue     pcommon.Value
+	expectedValueInit func(pcommon.Value)
+}{
+	{
+		name:          "valid key + valid value",
+		key:           "key",
+		value:         "value",
+		expectedKey:   "key",
+		expectedValue: pcommon.NewValueStr("value"),
+	},
+	{
+		name:          "invalid key + valid value",
+		key:           invalidUTF8(),
+		value:         "value",
+		expectedKey:   "InvalidTagKey",
+		expectedValue: pcommon.NewValueBytes(),
+		expectedValueInit: func(v pcommon.Value) {
+			expected := []byte(fmt.Sprintf("%s:value", invalidUTF8()))
+			v.Bytes().Append(expected...)
+		},
+	},
+	{
+		name:          "valid key + invalid value",
+		key:           "key",
+		value:         invalidUTF8(),
+		expectedKey:   "key",
+		expectedValue: pcommon.NewValueBytes(),
+		expectedValueInit: func(v pcommon.Value) {
+			expected := []byte(invalidUTF8())
+			v.Bytes().Append(expected...)
+		},
+	},
+	{
+		name:          "invalid key + invalid value",
+		key:           invalidUTF8(),
+		value:         invalidUTF8(),
+		expectedKey:   "InvalidTagKey",
+		expectedValue: pcommon.NewValueBytes(),
+		expectedValueInit: func(v pcommon.Value) {
+			expected := []byte(fmt.Sprintf("%s:%s", invalidUTF8(), invalidUTF8()))
+			v.Bytes().Append(expected...)
+		},
+	},
+}
+
+func TestUTF8Sanitizer_SanitizesResourceSpanAttributes(t *testing.T) {
+	for _, test := range utf8EncodingTests {
+		t.Run(test.name, func(t *testing.T) {
+			traces := ptrace.NewTraces()
+			traces.
+				ResourceSpans().
+				AppendEmpty().
+				Resource().
+				Attributes().
+				PutStr(test.key, test.value)
+			sanitizer := NewUTF8Sanitizer()
+			sanitized := sanitizer(traces)
+			value, ok := sanitized.
+				ResourceSpans().
+				At(0).
+				Resource().
+				Attributes().
+				Get(test.expectedKey)
+			require.True(t, ok)
+			if test.expectedValueInit != nil {
+				test.expectedValueInit(test.expectedValue)
+			}
+			require.Equal(t, test.expectedValue, value)
+		})
+	}
+}
+
+func TestUTF8Sanitizer_SanitizesScopeSpanAttributes(t *testing.T) {
+	for _, test := range utf8EncodingTests {
+		t.Run(test.name, func(t *testing.T) {
+			traces := ptrace.NewTraces()
+			traces.
+				ResourceSpans().
+				AppendEmpty().
+				ScopeSpans().
+				AppendEmpty().
+				Scope().
+				Attributes().
+				PutStr(test.key, test.value)
+			sanitizer := NewUTF8Sanitizer()
+			sanitized := sanitizer(traces)
+			value, ok := sanitized.
+				ResourceSpans().
+				At(0).
+				ScopeSpans().
+				At(0).
+				Scope().
+				Attributes().
+				Get(test.expectedKey)
+			require.True(t, ok)
+			if test.expectedValueInit != nil {
+				test.expectedValueInit(test.expectedValue)
+			}
+			require.Equal(t, test.expectedValue, value)
+		})
+	}
+}
+
+func TestUTF8Sanitizer_SanitizesSpanAttributes(t *testing.T) {
+	for _, test := range utf8EncodingTests {
+		t.Run(test.name, func(t *testing.T) {
+			traces := ptrace.NewTraces()
+			traces.
+				ResourceSpans().
+				AppendEmpty().
+				ScopeSpans().
+				AppendEmpty().
+				Spans().
+				AppendEmpty().
+				Attributes().
+				PutStr(test.key, test.value)
+			sanitizer := NewUTF8Sanitizer()
+			sanitized := sanitizer(traces)
+			value, ok := sanitized.
+				ResourceSpans().
+				At(0).
+				ScopeSpans().
+				At(0).
+				Spans().
+				At(0).
+				Attributes().
+				Get(test.expectedKey)
+			require.True(t, ok)
+			if test.expectedValueInit != nil {
+				test.expectedValueInit(test.expectedValue)
+			}
+			require.Equal(t, test.expectedValue, value)
+		})
+	}
+}
+
+func TestUTF8Sanitizer_SanitizesInvalidSpanName(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		ScopeSpans().
+		AppendEmpty().
+		Spans().
+		AppendEmpty().
+		SetName(invalidUTF8())
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	name := sanitized.
+		ResourceSpans().
+		At(0).
+		ScopeSpans().
+		At(0).
+		Spans().
+		At(0).
+		Name()
+	require.Equal(t, "InvalidOperationName", name)
+}
+
+func TestUTF8Sanitizer_DoesNotSanitizInvalidSpanName(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		ScopeSpans().
+		AppendEmpty().
+		Spans().
+		AppendEmpty().
+		SetName("name")
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	name := sanitized.
+		ResourceSpans().
+		At(0).
+		ScopeSpans().
+		At(0).
+		Spans().
+		At(0).
+		Name()
+	require.Equal(t, "name", name)
+}
+
+func TestUTF8Sanitizer_SanitizesInvalidServiceName(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		ScopeSpans().
+		AppendEmpty().
+		Spans().
+		AppendEmpty().
+		Attributes().
+		PutStr("service.name", invalidUTF8())
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	value, ok := sanitized.
+		ResourceSpans().
+		At(0).
+		ScopeSpans().
+		At(0).
+		Spans().
+		At(0).
+		Attributes().
+		Get("service.name")
+	require.True(t, ok)
+	require.Equal(t, []byte(invalidUTF8()), value.Bytes().AsRaw())
+}
+
+func TestUTF8Sanitizer_DoesNotSanitizeValidServiceName(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		ScopeSpans().
+		AppendEmpty().
+		Spans().
+		AppendEmpty().
+		Attributes().
+		PutStr("service.name", "name")
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	value, ok := sanitized.
+		ResourceSpans().
+		At(0).
+		ScopeSpans().
+		At(0).
+		Spans().
+		At(0).
+		Attributes().
+		Get("service.name")
+	require.True(t, ok)
+	require.Equal(t, "name", value.Str())
+}
+
+func TestUTF8Sanitizer_DoesNotSanitizeNonStringServiceName(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		ScopeSpans().
+		AppendEmpty().
+		Spans().
+		AppendEmpty().
+		Attributes().
+		PutInt("service.name", 1)
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	value, ok := sanitized.
+		ResourceSpans().
+		At(0).
+		ScopeSpans().
+		At(0).
+		Spans().
+		At(0).
+		Attributes().
+		Get("service.name")
+	require.True(t, ok)
+	require.EqualValues(t, 1, value.Int())
+}
+
+func TestUTF8Sanitizer_RemovesInvalidKeys(t *testing.T) {
+	traces := ptrace.NewTraces()
+	traces.
+		ResourceSpans().
+		AppendEmpty().
+		Resource().
+		Attributes().
+		PutStr(invalidUTF8(), "value")
+	sanitizer := NewUTF8Sanitizer()
+	sanitized := sanitizer(traces)
+	_, ok := sanitized.
+		ResourceSpans().
+		At(0).
+		Resource().
+		Attributes().
+		Get(invalidUTF8())
+	require.False(t, ok)
+}
