2lambda123/jaegertracing-jaeger

Enable Lint Rule: import-shadowing
Signed-off-by: Meet Soni <meetsoni3017@gmail.com>

cmd/jaeger/internal/integration/span_writer.go
@@ -48,17 +48,17 @@ func createSpanWriter(logger *zap.Logger, port int) (*spanWriter, error) {
 	set := exportertest.NewNopSettings()
 	set.Logger = logger
 
-	exporter, err := factory.CreateTracesExporter(context.Background(), set, cfg)
+	tracesExporter, err := factory.CreateTracesExporter(context.Background(), set, cfg)
 	if err != nil {
 		return nil, err
 	}
-	if err := exporter.Start(context.Background(), componenttest.NewNopHost()); err != nil {
+	if err := tracesExporter.Start(context.Background(), componenttest.NewNopHost()); err != nil {
 		return nil, err
 	}
 
 	return &spanWriter{
 		logger:   logger,
-		exporter: exporter,
+		exporter: tracesExporter,
 	}, nil
 }
 

jaegertracing/jaeger

cmd/jaeger/internal/integration/span_writer.go
@@ -48,17 +48,17 @@ func createSpanWriter(logger *zap.Logger, port int) (*spanWriter, error) {
 	set := exportertest.NewNopSettings()
 	set.Logger = logger
 
-	exporter, err := factory.CreateTraces(context.Background(), set, cfg)
+	exp, err := factory.CreateTracesExporter(context.Background(), set, cfg)
 	if err != nil {
 		return nil, err
 	}
-	if err := exporter.Start(context.Background(), componenttest.NewNopHost()); err != nil {
+	if err := exp.Start(context.Background(), componenttest.NewNopHost()); err != nil {
 		return nil, err
 	}
 
 	return &spanWriter{
 		logger:   logger,
-		exporter: exporter,
+		exporter: exp,
 	}, nil
 }
 