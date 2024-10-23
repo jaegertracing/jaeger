e2b847b849f552fc057c423120021b590df3e722
Yuri Shkuro <github@ysh.us>
2024-10-22 22:38:12 -0400

    replace deprecated methods
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/jaeger/internal/exporters/storageexporter/exporter_test.go
+++ cmd/jaeger/internal/exporters/storageexporter/exporter_test.go
@@ -119,7 +119,7 @@ func TestExporter(t *testing.T) {
 	err := config.Validate()
 	require.NoError(t, err)
 
-	tracesExporter, err := exporterFactory.CreateTracesExporter(ctx, exporter.Settings{
+	tracesExporter, err := exporterFactory.CreateTraces(ctx, exporter.Settings{
 		ID:                ID,
 		TelemetrySettings: telemetrySettings,
 		BuildInfo:         component.NewDefaultBuildInfo(),
@@ -172,7 +172,7 @@ func makeStorageExtension(t *testing.T, memstoreName string) component.Host {
 		MeterProvider: noopmetric.NewMeterProvider(),
 	}
 	extensionFactory := jaegerstorage.NewFactory()
-	storageExtension, err := extensionFactory.CreateExtension(
+	storageExtension, err := extensionFactory.Create(
 		context.Background(),
 		extension.Settings{
 			TelemetrySettings: telemetrySettings,
