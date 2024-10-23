b004ed86da4004070a80c5801f074ee523abed1f
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-10-23 19:56:42 -0400

    Fix Punctuation
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/flags.go
+++ cmd/query/app/flags.go
@@ -105,7 +105,7 @@ func (qOpts *QueryOptions) InitFromViper(v *viper.Viper, logger *zap.Logger) (*Q
 	// TODO: drop support for same host ports
 	// https://github.com/jaegertracing/jaeger/issues/6117
 	if qOpts.HTTP.Endpoint == qOpts.GRPC.NetAddr.Endpoint {
-		logger.Warn("using the same port for gRPC and HTTP is deprecated; please use dedicated ports instead; Support for shared port will be removed in Feb 2025")
+		logger.Warn("using the same port for gRPC and HTTP is deprecated; please use dedicated ports instead; support for shared port will be removed in Feb 2025")
 	}
 	tlsGrpc, err := tlsGRPCFlagsConfig.InitFromViper(v)
 	if err != nil {
