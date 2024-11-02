9c1c3ceac3c3c02af0970cabeac8d91aa5b83529
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2024-11-02 23:06:48 +0530

    Crossdock sampling redirection to collector  (#6115)
    ## Which problem is this PR solving?
    - Resolve #6083 
    ## Description of the changes
    - 
    
    ## How was this change tested?
    - make test 
    
    ## Checklist
    - [ ] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [ ] I have signed all commits
    - [ ] I have added unit tests for the new functionality
    - [ ] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `yarn lint` and `yarn test`
    
    ---------
    
    Signed-off-by: chahatsagarmain <chahatsagar2003@gmail.com>

--- crossdock/services/agent_test.go
+++ crossdock/services/agent_test.go
@@ -52,10 +52,14 @@ func TestGetSamplingRateInternal(t *testing.T) {
 	}
 }
 
-type testAgentHandler struct{}
+type testAgentHandler struct {
+	logger *zap.Logger
+}
 
-func (*testAgentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *testAgentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	svc := r.FormValue("service")
+	h.logger.Info("Handling sampling request",
+		zap.String("service", svc))
 	body := []byte("bad json")
 	if svc == "crossdock-svc" {
 		response := api_v2.SamplingStrategyResponse{
@@ -77,16 +81,19 @@ func (*testAgentHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestGetSamplingRate(t *testing.T) {
-	handler := &testAgentHandler{}
+	logger := zap.NewExample()
+	handler := &testAgentHandler{logger: logger}
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
 	// Test with no http server
-	agent := NewAgentService("", zap.NewNop())
+	agent := NewCollectorService("", zap.NewNop())
 	_, err := agent.GetSamplingRate("svc", "op")
 	require.Error(t, err)
 
-	agent = NewAgentService(server.URL, zap.NewNop())
+	agent = NewCollectorService(server.URL, zap.NewNop())
+	logger.Info("Testing with mock server",
+		zap.String("url", server.URL))
 	rate, err := agent.GetSamplingRate("svc", "op")
 	require.NoError(t, err)
 	assert.InDelta(t, 1.0, rate, 0.01)
@@ -100,7 +107,7 @@ func TestGetSamplingRateReadAllErr(t *testing.T) {
 		w.Header().Set("Content-Length", "1")
 	}))
 	defer server.Close()
-	agent := NewAgentService(server.URL, zap.NewNop())
+	agent := NewCollectorService(server.URL, zap.NewNop())
 	_, err := agent.GetSamplingRate("svc", "op")
 	require.EqualError(t, err, "unexpected EOF")
 }
