b6a6fc6db6c847ff235c12dbad0d4301266c2800
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-11-28 08:45:03 -0500

    Implement Decorator Factory
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ storage/decorator.go
@@ -0,0 +1,42 @@
+package storage
+
+import (
+	"github.com/jaegertracing/jaeger/pkg/metrics"
+	"github.com/jaegertracing/jaeger/storage/dependencystore"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
+	"github.com/jaegertracing/jaeger/storage/spanstore/spanstoremetrics"
+	"go.uber.org/zap"
+)
+
+type DecoratorFactory struct {
+	delegate       Factory
+	metricsFactory metrics.Factory
+}
+
+func NewDecoratorFactory(sf Factory, mf metrics.Factory) *DecoratorFactory {
+	return &DecoratorFactory{
+		delegate:       sf,
+		metricsFactory: mf,
+	}
+}
+
+func (df *DecoratorFactory) Initialize(metricsFactory metrics.Factory, logger *zap.Logger) error {
+	return df.delegate.Initialize(metricsFactory, logger)
+}
+
+func (df *DecoratorFactory) CreateSpanReader() (spanstore.Reader, error) {
+	sr, err := df.delegate.CreateSpanReader()
+	if err != nil {
+		return sr, err
+	}
+	sr = spanstoremetrics.NewReaderDecorator(sr, df.metricsFactory)
+	return sr, nil
+}
+
+func (df *DecoratorFactory) CreateSpanWriter() (spanstore.Writer, error) {
+	return df.delegate.CreateSpanWriter()
+}
+
+func (df *DecoratorFactory) CreateDependencyReader() (dependencystore.Reader, error) {
+	return df.delegate.CreateDependencyReader()
+}
