e643d6cf5243f7dec6536e57ede76d1abf582925
Yuri Shkuro <github@ysh.us>
2024-11-28 18:30:50 -0500

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- pkg/telemetry/settings_test.go
+++ pkg/telemetry/settings_test.go
@@ -7,7 +7,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componentstatus"
+	"go.opentelemetry.io/collector/component/componenttest"
+	noopmetric "go.opentelemetry.io/otel/metric/noop"
+	nooptrace "go.opentelemetry.io/otel/trace/noop"
+	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger/pkg/healthcheck"
 	"github.com/jaegertracing/jaeger/pkg/telemetry"
@@ -71,6 +76,21 @@ func TestHCAdapter(t *testing.T) {
 			assert.Equal(t, tt.expectedHC, hc.Get())
 		})
 	}
+}
+
+func TestFromOtelComponent(t *testing.T) {
+	otelTelset := component.TelemetrySettings{
+		Logger:         zap.NewNop(),
+		MeterProvider:  noopmetric.NewMeterProvider(),
+		TracerProvider: nooptrace.NewTracerProvider(),
+	}
+	host := componenttest.NewNopHost()
+	telset := telemetry.FromOtelComponent(otelTelset, host)
+	assert.Equal(t, otelTelset.Logger, telset.Logger)
+	assert.Equal(t, otelTelset.MeterProvider, telset.MeterProvider)
+	assert.Equal(t, otelTelset.TracerProvider, telset.TracerProvider)
+	assert.Equal(t, host, telset.Host)
+	assert.NotNil(t, telset.ReportStatus)
 }
 
 func TestMain(m *testing.M) {
