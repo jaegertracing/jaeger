10bd9aa2ba980c6ae2739a2ed9846c823227f950
Alok Kumar Singh <dev.alok.singh123@gmail.com>
2024-11-29 21:42:54 +0530

    lint fixes
    Signed-off-by: Alok Kumar Singh <dev.alok.singh123@gmail.com>

--- pkg/cassandra/config/schema.go
+++ pkg/cassandra/config/schema.go
@@ -45,7 +45,7 @@ func (sc *schemaCreator) constructTemplateParams() templateParams {
 	}
 }
 
-func (sc *schemaCreator) getQueryFileAsBytes(fileName string, params templateParams) ([]byte, error) {
+func (_ *schemaCreator) getQueryFileAsBytes(fileName string, params templateParams) ([]byte, error) {
 	tmpl, err := template.ParseFS(schemaFile, fileName)
 	if err != nil {
 		return nil, err
@@ -60,7 +60,7 @@ func (sc *schemaCreator) getQueryFileAsBytes(fileName string, params templatePar
 	return result.Bytes(), nil
 }
 
-func (sc *schemaCreator) getQueriesFromBytes(queryFile []byte) ([]string, error) {
+func (_ *schemaCreator) getQueriesFromBytes(queryFile []byte) ([]string, error) {
 	lines := bytes.Split(queryFile, []byte("\n"))
 
 	var extractedLines [][]byte
