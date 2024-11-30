e9fac0575b5cc3b5666b6432f25759d6e09dfd0d
Alok Kumar Singh <62210712+akstron@users.noreply.github.com>
2024-11-30 11:46:50 -0800

    Create Cassandra db schema on session initialization (#5922)
    Create Schema (if not present) on Session Initialization
    
    Once a session is established with cassandra db, the added code parses
    the template file containing queries for creating schema and create
    queries out of it. Post which it executes those queries to create the
    required types and tables.
    
    ## Which problem is this PR solving?
    Resolves #5797 
    
    ## Description of the changes
    - The PR includes the following changes:
    - 1. Embedding template files into binary
    - 2. Creation of database schema in initialization steps once session to
    database is established.
    
    ## How was this change tested?
    -  Schema rendering is being tested with unit test.
    -  bash scripts/cassandra-integration-test.sh -s 4 v004 v2
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `yarn lint` and `yarn test`
    
    ---------
    
    Signed-off-by: Alok Kumar Singh <dev.alok.singh123@gmail.com>
    Signed-off-by: Alok Kumar Singh <62210712+akstron@users.noreply.github.com>
    Signed-off-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    Co-authored-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>
    Co-authored-by: Yuri Shkuro <github@ysh.us>

---
+++ plugin/storage/cassandra/schema/schema_test.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package schema
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQueryGenerationFromBytes(t *testing.T) {
+	queriesAsString := `
+query1 -- comment (this should be removed)
+query1-continue
+query1-finished; --
+
+
+query2;
+query-3 query-3-continue query-3-finished;
+`
+	expGeneratedQueries := []string{
+		`query1
+query1-continue
+query1-finished;
+`,
+		`query2;
+`,
+		`query-3 query-3-continue query-3-finished;
+`,
+	}
+
+	sc := Creator{}
+	queriesAsBytes := []byte(queriesAsString)
+	queries, err := sc.getQueriesFromBytes(queriesAsBytes)
+	require.NoError(t, err)
+
+	require.Equal(t, len(expGeneratedQueries), len(queries))
+
+	for i := range len(expGeneratedQueries) {
+		require.Equal(t, expGeneratedQueries[i], queries[i])
+	}
+}
+
+func TestInvalidQueryTemplate(t *testing.T) {
+	queriesAsString := `
+	query1 -- comment (this should be removed)
+	query1-continue
+	query1-finished; --
+
+
+	query2;
+	query-3 query-3-continue query-3-finished -- missing semicolon
+	`
+	sc := Creator{}
+	queriesAsBytes := []byte(queriesAsString)
+	_, err := sc.getQueriesFromBytes(queriesAsBytes)
+	require.Error(t, err)
+}
