d2d194ed1f04807c645ec5e00fe0ff8340bac261
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-08 14:50:48 -0500

    Add Test For New Helper
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ storage_v2/tracestore/reader_test.go
@@ -0,0 +1,34 @@
+package tracestore
+
+import (
+	"testing"
+	"time"
+
+	"github.com/crossdock/crossdock-go/require"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
+)
+
+func TestToSpanStoreQueryParameters(t *testing.T) {
+	now := time.Now()
+	query := &TraceQueryParameters{
+		ServiceName:   "service",
+		OperationName: "operation",
+		Tags:          map[string]string{"tag-a": "val-a"},
+		StartTimeMin:  now,
+		StartTimeMax:  now.Add(time.Minute),
+		DurationMin:   time.Minute,
+		DurationMax:   time.Hour,
+		NumTraces:     10,
+	}
+	expected := &spanstore.TraceQueryParameters{
+		ServiceName:   "service",
+		OperationName: "operation",
+		Tags:          map[string]string{"tag-a": "val-a"},
+		StartTimeMin:  now,
+		StartTimeMax:  now.Add(time.Minute),
+		DurationMin:   time.Minute,
+		DurationMax:   time.Hour,
+		NumTraces:     10,
+	}
+	require.Equal(t, expected, query.ToSpanStoreQueryParameters())
+}
