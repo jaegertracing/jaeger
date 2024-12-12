a31f0ac075e69e43c27cbfb794c8445b86d1065f
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-11 22:36:00 -0500

    Add Adjuster Interface To Operate On OTLP Data Format
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ cmd/query/app/querysvc/adjuster/adjuster.go
@@ -0,0 +1,57 @@
+package adjuster
+
+import (
+	"errors"
+
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+// Adjuster defines an interface for modifying a trace object.
+// It returns the adjusted trace object, which is also updated in place.
+// If the adjuster encounters an issue that prevents it from applying
+// modifications, it should return the original trace object along with an error.
+type Adjuster interface {
+	Adjust(ptrace.Traces) (ptrace.Traces, error)
+}
+
+// Func is a type alias that wraps a function and makes an Adjuster from it.
+type Func func(trace ptrace.Traces) (ptrace.Traces, error)
+
+// Adjust implements Adjuster interface for the Func alias.
+func (f Func) Adjust(trace ptrace.Traces) (ptrace.Traces, error) {
+	return f(trace)
+}
+
+// Sequence creates an adjuster that combines a series of adjusters
+// applied in order. Errors from each step are accumulated and returned
+// in the end as a single wrapper error. Errors do not interrupt the
+// sequence of adapters.
+func Sequence(adjusters ...Adjuster) Adjuster {
+	return sequence{adjusters: adjusters}
+}
+
+// FailFastSequence is similar to Sequence() but returns immediately
+// if any adjuster returns an error.
+func FailFastSequence(adjusters ...Adjuster) Adjuster {
+	return sequence{adjusters: adjusters, failFast: true}
+}
+
+type sequence struct {
+	adjusters []Adjuster
+	failFast  bool
+}
+
+func (c sequence) Adjust(trace ptrace.Traces) (ptrace.Traces, error) {
+	var errs []error
+	for _, adjuster := range c.adjusters {
+		var err error
+		trace, err = adjuster.Adjust(trace)
+		if err != nil {
+			if c.failFast {
+				return trace, err
+			}
+			errs = append(errs, err)
+		}
+	}
+	return trace, errors.Join(errs...)
+}
