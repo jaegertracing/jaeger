ccaf396c853c6b8ac0bcf72f4b0d7c91eaac2501
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-13 11:04:21 -0500

    Add More Tests
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/ipattribute_test.go
+++ cmd/query/app/querysvc/adjuster/ipattribute_test.go
@@ -12,13 +12,19 @@ import (
 
 func TestIPAttributeAdjuster(t *testing.T) {
 	traces := ptrace.NewTraces()
-	spans := traces.ResourceSpans().AppendEmpty().ScopeSpans().AppendEmpty().Spans()
+	resourceSpans := traces.ResourceSpans().AppendEmpty()
+
+	resourceSpans.Resource().Attributes().PutInt("a", 42)
+	resourceSpans.Resource().Attributes().PutInt("ip", 1<<26|2<<16|3<<8|4)
+	resourceSpans.Resource().Attributes().PutStr("peer.ipv4", "something")
+
+	spans := resourceSpans.ScopeSpans().AppendEmpty().Spans()
 
 	spanA := spans.AppendEmpty()
 	spanA.Attributes().PutInt("a", 42)
 	spanA.Attributes().PutStr("ip", "not integer")
 	spanA.Attributes().PutInt("ip", 1<<24|2<<16|3<<8|4)
-	spanA.Attributes().PutStr("peer.ipv4", "something")
+	spanA.Attributes().PutStr("peer.ipv4", "something else")
 
 	spanB := spans.AppendEmpty()
 	spanB.Attributes().PutDouble("ip", 1<<25|2<<16|3<<8|4)
@@ -26,10 +32,25 @@ func TestIPAttributeAdjuster(t *testing.T) {
 	trace, err := IPAttribute().Adjust(traces)
 	require.NoError(t, err)
 
-	span := trace.ResourceSpans().At(0).ScopeSpans().At(0).Spans()
+	resourceSpan := trace.ResourceSpans().At(0)
+	resourceAttributes := resourceSpan.Resource().Attributes()
+
+	val, ok := resourceAttributes.Get("a")
+	require.True(t, ok)
+	require.EqualValues(t, 42, val.Int())
+
+	val, ok = resourceAttributes.Get("ip")
+	require.True(t, ok)
+	require.EqualValues(t, "4.2.3.4", val.Str())
+
+	val, ok = resourceAttributes.Get("peer.ipv4")
+	require.True(t, ok)
+	require.EqualValues(t, "something", val.Str())
+
+	span := resourceSpan.ScopeSpans().At(0).Spans()
 	attributesA := span.At(0).Attributes()
 
-	val, ok := attributesA.Get("a")
+	val, ok = attributesA.Get("a")
 	require.True(t, ok)
 	require.EqualValues(t, 42, val.Int())
 
@@ -39,7 +60,7 @@ func TestIPAttributeAdjuster(t *testing.T) {
 
 	val, ok = attributesA.Get("peer.ipv4")
 	require.True(t, ok)
-	require.EqualValues(t, "something", val.Str())
+	require.EqualValues(t, "something else", val.Str())
 
 	attributesB := span.At(1).Attributes()
 
