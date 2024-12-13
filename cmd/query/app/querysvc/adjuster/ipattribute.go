a4b3ae6c9fbdcd873efdc8d7a20bc0aab17d2dea
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-13 11:22:18 -0500

    Make Adjuster Struct To Simplify Body
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/ipattribute.go
+++ cmd/query/app/querysvc/adjuster/ipattribute.go
@@ -21,56 +21,57 @@ var ipAttributesToCorrect = map[string]struct{}{
 // which usually contain IPv4 packed into uint32, with their string
 // representation (e.g. "8.8.8.8"").
 func IPAttribute() Adjuster {
-	adjustAttributes := func(attributes pcommon.Map) {
-		adjusted := make(map[string]string)
-
-		attributes.Range(func(k string, v pcommon.Value) bool {
-			if _, ok := ipAttributesToCorrect[k]; !ok {
-				return true
-			}
-			var value uint32
-			switch v.Type() {
-			case pcommon.ValueTypeInt:
-				//nolint: gosec // G115
-				value = uint32(v.Int())
-			case pcommon.ValueTypeDouble:
-				value = uint32(v.Double())
-			default:
-				return true
-			}
-			var buf [4]byte
-			binary.BigEndian.PutUint32(buf[:], value)
-			var sBuf bytes.Buffer
-			for i, b := range buf {
-				if i > 0 {
-					sBuf.WriteRune('.')
-				}
-				sBuf.WriteString(strconv.FormatUint(uint64(b), 10))
-			}
-			adjusted[k] = sBuf.String()
-			return true
-		})
-
-		for k, v := range adjusted {
-			attributes.PutStr(k, v)
-		}
-	}
-
 	return Func(func(traces ptrace.Traces) (ptrace.Traces, error) {
+		adjuster := ipAttributeAdjuster{}
 		resourceSpans := traces.ResourceSpans()
 		for i := 0; i < resourceSpans.Len(); i++ {
 			rs := resourceSpans.At(i)
-			adjustAttributes(rs.Resource().Attributes())
+			adjuster.adjust(rs.Resource().Attributes())
 			scopeSpans := rs.ScopeSpans()
 			for j := 0; j < scopeSpans.Len(); j++ {
 				ss := scopeSpans.At(j)
 				spans := ss.Spans()
 				for k := 0; k < spans.Len(); k++ {
 					span := spans.At(k)
-					adjustAttributes(span.Attributes())
+					adjuster.adjust(span.Attributes())
 				}
 			}
 		}
 		return traces, nil
 	})
+}
+
+type ipAttributeAdjuster struct{}
+
+func (ipAttributeAdjuster) adjust(attributes pcommon.Map) {
+	adjusted := make(map[string]string)
+	attributes.Range(func(k string, v pcommon.Value) bool {
+		if _, ok := ipAttributesToCorrect[k]; !ok {
+			return true
+		}
+		var value uint32
+		switch v.Type() {
+		case pcommon.ValueTypeInt:
+			//nolint: gosec // G115
+			value = uint32(v.Int())
+		case pcommon.ValueTypeDouble:
+			value = uint32(v.Double())
+		default:
+			return true
+		}
+		var buf [4]byte
+		binary.BigEndian.PutUint32(buf[:], value)
+		var sBuf bytes.Buffer
+		for i, b := range buf {
+			if i > 0 {
+				sBuf.WriteRune('.')
+			}
+			sBuf.WriteString(strconv.FormatUint(uint64(b), 10))
+		}
+		adjusted[k] = sBuf.String()
+		return true
+	})
+	for k, v := range adjusted {
+		attributes.PutStr(k, v)
+	}
 }
