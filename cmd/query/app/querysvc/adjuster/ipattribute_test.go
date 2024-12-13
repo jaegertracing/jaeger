602d9c7b10ff28ac9a9cd255a1b0f6f9530e480e
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-13 11:26:12 -0500

    Fix Test
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/ipattribute_test.go
+++ cmd/query/app/querysvc/adjuster/ipattribute_test.go
@@ -18,7 +18,7 @@ func TestIPAttributeAdjuster(t *testing.T) {
 
 	resourceAttributes := resourceSpans.Resource().Attributes()
 	resourceAttributes.PutInt("a", 42)
-	resourceAttributes.PutInt("ip", 1<<26|2<<16|3<<8|4)
+	resourceAttributes.PutInt("ip", 1<<24|2<<16|3<<8|4)
 	resourceAttributes.PutStr("peer.ipv4", "something")
 
 	spans := resourceSpans.ScopeSpans().AppendEmpty().Spans()
@@ -39,12 +39,12 @@ func TestIPAttributeAdjuster(t *testing.T) {
 
 	createSpan(map[string]any{
 		"a":         42,
-		"ip":        int(1<<24 | 2<<16 | 3<<8 | 4),
+		"ip":        int(1<<25 | 2<<16 | 3<<8 | 4),
 		"peer.ipv4": "something else",
 	})
 
 	createSpan(map[string]any{
-		"ip": float64(1<<25 | 2<<16 | 3<<8 | 4),
+		"ip": float64(1<<26 | 2<<16 | 3<<8 | 4),
 	})
 
 	assertAttribute := func(attributes pcommon.Map, key string, expected any) {
@@ -65,7 +65,7 @@ func TestIPAttributeAdjuster(t *testing.T) {
 	assert.Equal(t, 3, resourceSpan.Resource().Attributes().Len())
 
 	assertAttribute(resourceSpan.Resource().Attributes(), "a", 42)
-	assertAttribute(resourceSpan.Resource().Attributes(), "ip", "4.2.3.4")
+	assertAttribute(resourceSpan.Resource().Attributes(), "ip", "1.2.3.4")
 	assertAttribute(resourceSpan.Resource().Attributes(), "peer.ipv4", "something")
 
 	gotSpans := resourceSpan.ScopeSpans().At(0).Spans()
@@ -73,9 +73,9 @@ func TestIPAttributeAdjuster(t *testing.T) {
 
 	assert.Equal(t, 3, gotSpans.At(0).Attributes().Len())
 	assertAttribute(gotSpans.At(0).Attributes(), "a", 42)
-	assertAttribute(gotSpans.At(0).Attributes(), "ip", "1.2.3.4")
+	assertAttribute(gotSpans.At(0).Attributes(), "ip", "2.2.3.4")
 	assertAttribute(gotSpans.At(0).Attributes(), "peer.ipv4", "something else")
 
 	assert.Equal(t, 1, gotSpans.At(1).Attributes().Len())
-	assertAttribute(gotSpans.At(1).Attributes(), "ip", "2.2.3.4")
+	assertAttribute(gotSpans.At(1).Attributes(), "ip", "4.2.3.4")
 }
