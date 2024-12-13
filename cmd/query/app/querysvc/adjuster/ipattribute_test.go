00d18358ec1bd115757673005b8b0f2972d08eca
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-13 11:25:03 -0500

    Clean Up Test File
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/ipattribute_test.go
+++ cmd/query/app/querysvc/adjuster/ipattribute_test.go
@@ -6,7 +6,9 @@ package adjuster
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
@@ -14,57 +16,66 @@ func TestIPAttributeAdjuster(t *testing.T) {
 	traces := ptrace.NewTraces()
 	resourceSpans := traces.ResourceSpans().AppendEmpty()
 
-	resourceSpans.Resource().Attributes().PutInt("a", 42)
-	resourceSpans.Resource().Attributes().PutInt("ip", 1<<26|2<<16|3<<8|4)
-	resourceSpans.Resource().Attributes().PutStr("peer.ipv4", "something")
+	resourceAttributes := resourceSpans.Resource().Attributes()
+	resourceAttributes.PutInt("a", 42)
+	resourceAttributes.PutInt("ip", 1<<26|2<<16|3<<8|4)
+	resourceAttributes.PutStr("peer.ipv4", "something")
 
 	spans := resourceSpans.ScopeSpans().AppendEmpty().Spans()
 
-	spanA := spans.AppendEmpty()
-	spanA.Attributes().PutInt("a", 42)
-	spanA.Attributes().PutStr("ip", "not integer")
-	spanA.Attributes().PutInt("ip", 1<<24|2<<16|3<<8|4)
-	spanA.Attributes().PutStr("peer.ipv4", "something else")
+	createSpan := func(attrs map[string]any) {
+		span := spans.AppendEmpty()
+		for key, value := range attrs {
+			switch v := value.(type) {
+			case int:
+				span.Attributes().PutInt(key, int64(v))
+			case string:
+				span.Attributes().PutStr(key, v)
+			case float64:
+				span.Attributes().PutDouble(key, v)
+			}
+		}
+	}
 
-	spanB := spans.AppendEmpty()
-	spanB.Attributes().PutDouble("ip", 1<<25|2<<16|3<<8|4)
+	createSpan(map[string]any{
+		"a":         42,
+		"ip":        int(1<<24 | 2<<16 | 3<<8 | 4),
+		"peer.ipv4": "something else",
+	})
+
+	createSpan(map[string]any{
+		"ip": float64(1<<25 | 2<<16 | 3<<8 | 4),
+	})
+
+	assertAttribute := func(attributes pcommon.Map, key string, expected any) {
+		val, ok := attributes.Get(key)
+		require.True(t, ok)
+		switch v := expected.(type) {
+		case int:
+			require.EqualValues(t, v, val.Int())
+		case string:
+			require.EqualValues(t, v, val.Str())
+		}
+	}
 
 	trace, err := IPAttribute().Adjust(traces)
 	require.NoError(t, err)
 
 	resourceSpan := trace.ResourceSpans().At(0)
-	resourceAttributes := resourceSpan.Resource().Attributes()
+	assert.Equal(t, 3, resourceSpan.Resource().Attributes().Len())
 
-	val, ok := resourceAttributes.Get("a")
-	require.True(t, ok)
-	require.EqualValues(t, 42, val.Int())
+	assertAttribute(resourceSpan.Resource().Attributes(), "a", 42)
+	assertAttribute(resourceSpan.Resource().Attributes(), "ip", "4.2.3.4")
+	assertAttribute(resourceSpan.Resource().Attributes(), "peer.ipv4", "something")
 
-	val, ok = resourceAttributes.Get("ip")
-	require.True(t, ok)
-	require.EqualValues(t, "4.2.3.4", val.Str())
+	gotSpans := resourceSpan.ScopeSpans().At(0).Spans()
+	assert.Equal(t, 2, gotSpans.Len())
 
-	val, ok = resourceAttributes.Get("peer.ipv4")
-	require.True(t, ok)
-	require.EqualValues(t, "something", val.Str())
+	assert.Equal(t, 3, gotSpans.At(0).Attributes().Len())
+	assertAttribute(gotSpans.At(0).Attributes(), "a", 42)
+	assertAttribute(gotSpans.At(0).Attributes(), "ip", "1.2.3.4")
+	assertAttribute(gotSpans.At(0).Attributes(), "peer.ipv4", "something else")
 
-	span := resourceSpan.ScopeSpans().At(0).Spans()
-	attributesA := span.At(0).Attributes()
-
-	val, ok = attributesA.Get("a")
-	require.True(t, ok)
-	require.EqualValues(t, 42, val.Int())
-
-	val, ok = attributesA.Get("ip")
-	require.True(t, ok)
-	require.EqualValues(t, "1.2.3.4", val.Str())
-
-	val, ok = attributesA.Get("peer.ipv4")
-	require.True(t, ok)
-	require.EqualValues(t, "something else", val.Str())
-
-	attributesB := span.At(1).Attributes()
-
-	val, ok = attributesB.Get("ip")
-	require.True(t, ok)
-	require.EqualValues(t, "2.2.3.4", val.Str())
+	assert.Equal(t, 1, gotSpans.At(1).Attributes().Len())
+	assertAttribute(gotSpans.At(1).Attributes(), "ip", "2.2.3.4")
 }
