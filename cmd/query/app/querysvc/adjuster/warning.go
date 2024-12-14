10cb6a963deeb79e6fae1651b2d25f53d2afc871
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2024-12-14 17:55:22 -0500

    [v2][adjuster] Implement otel attribute adjuster to operate on otlp data model (#6358)
    ## Which problem is this PR solving?
    - Towards https://github.com/jaegertracing/jaeger/issues/6344
    
    ## Description of the changes
    - This PR implements the `OTelTag` adjuster to operate on the OTLP data
    model. In the OTLP model, tags are dubbed as attributes so the adjuster
    was renamed to `OTELAttribute`.
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ cmd/query/app/querysvc/adjuster/warning.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package adjuster
+
+import (
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+const (
+	adjusterWarningAttribute = "jaeger.adjuster.warning"
+)
+
+func addWarning(span ptrace.Span, warning string) {
+	var warnings pcommon.Slice
+	if currWarnings, ok := span.Attributes().Get(adjusterWarningAttribute); ok {
+		warnings = currWarnings.Slice()
+	} else {
+		warnings = span.Attributes().PutEmptySlice(adjusterWarningAttribute)
+	}
+	warnings.AppendEmpty().SetStr(warning)
+}
