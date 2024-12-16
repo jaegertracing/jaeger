ee9ccb00de3d2c13a477952efb399a42c9ad5ddf
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-16 18:50:58 -0500

    Add Interface Checks To Adjusters
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/spanlinks.go
+++ cmd/query/app/querysvc/adjuster/spanlinks.go
@@ -7,6 +7,8 @@ import (
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
+var _ Adjuster = (*LinksAdjuster)(nil)
+
 // SpanLinks creates an adjuster that removes span links with empty trace IDs.
 func SpanLinks() LinksAdjuster {
 	return LinksAdjuster{}
