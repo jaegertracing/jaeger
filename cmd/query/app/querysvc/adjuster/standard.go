c42b6c6eab343d1b0cb9d7e0be313d6f30249d5e
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-23 11:48:35 -0500

    Clean Up godoc Comments
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/query/app/querysvc/adjuster/standard.go
+++ cmd/query/app/querysvc/adjuster/standard.go
@@ -4,14 +4,14 @@ import (
 	"time"
 )
 
-// StandardAdjusters is a list of adjusters applied by the query service
+// StandardAdjusters returns a list of adjusters applied by the query service
 // before returning the data to the API clients.
 func StandardAdjusters(maxClockSkewAdjust time.Duration) []Adjuster {
 	return []Adjuster{
 		SpanIDUniquifier(),
 		SortAttributesAndEvents(),
-		SpanHash(),                    // requires SortTagsAndLogFields for deterministic results
-		ClockSkew(maxClockSkewAdjust), // adds warnings (which affect SpanHash) on dupe span IDs
+		SpanHash(),                    // requires SortAttributesAndEvents for deterministic results
+		ClockSkew(maxClockSkewAdjust), // adds warnings (which affect SpanHash) on duplicate span IDs
 		IPAttribute(),
 		ResourceAttributes(),
 		SpanLinks(),
