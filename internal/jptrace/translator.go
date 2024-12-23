52c01c69b90b21f5b2ab9d148b6e2948c6eb458a
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-23 11:25:42 -0500

    Flip Map Conversion
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/translator.go
+++ internal/jptrace/translator.go
@@ -5,7 +5,6 @@ package jptrace
 
 import (
 	jaegerTranslator "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/jaeger"
-	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
 	"github.com/jaegertracing/jaeger/model"
@@ -15,32 +14,31 @@ import (
 // to Jaeger model batches ([]*model.Batch).
 func ProtoFromTraces(traces ptrace.Traces) []*model.Batch {
 	batches := jaegerTranslator.ProtoFromTraces(traces)
-	spanMap := createSpanMapFromTraces(traces)
-	addWarningsToBatches(batches, spanMap)
+	spanMap := createSpanMapFromBatches(batches)
+	transferWarnings(traces, spanMap)
 	return batches
 }
 
-func createSpanMapFromTraces(traces ptrace.Traces) map[pcommon.SpanID]ptrace.Span {
-	spanMap := make(map[pcommon.SpanID]ptrace.Span)
+func createSpanMapFromBatches(batches []*model.Batch) map[model.SpanID]*model.Span {
+	spanMap := make(map[model.SpanID]*model.Span)
+	for _, batch := range batches {
+		for _, span := range batch.Spans {
+			spanMap[span.SpanID] = span
+		}
+	}
+	return spanMap
+}
+
+func transferWarnings(traces ptrace.Traces, spanMap map[model.SpanID]*model.Span) {
 	resources := traces.ResourceSpans()
 	for i := 0; i < resources.Len(); i++ {
 		scopes := resources.At(i).ScopeSpans()
 		for j := 0; j < scopes.Len(); j++ {
 			spans := scopes.At(j).Spans()
 			for k := 0; k < spans.Len(); k++ {
-				span := spans.At(k)
-				spanMap[span.SpanID()] = span
-			}
-		}
-	}
-	return spanMap
-}
-
-func addWarningsToBatches(batches []*model.Batch, spanMap map[pcommon.SpanID]ptrace.Span) {
-	for _, batch := range batches {
-		for _, span := range batch.Spans {
-			if otelSpan, ok := spanMap[span.SpanID.ToOTELSpanID()]; ok {
+				otelSpan := spans.At(k)
 				warnings := GetWarnings(otelSpan)
+				span := spanMap[model.SpanIDFromOTEL(otelSpan.SpanID())]
 				span.Warnings = append(span.Warnings, warnings...)
 				// filter out the warning tag
 				span.Tags = filterTags(span.Tags, warningsAttribute)
