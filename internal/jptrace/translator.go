e6f51e023ebba4883dc396a19a06cf040d288d0b
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-23 09:41:26 -0500

    Change Implementation To Be More Robust
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/translator.go
+++ internal/jptrace/translator.go
@@ -4,29 +4,45 @@
 package jptrace
 
 import (
-	otlp2jaeger "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/jaeger"
+	jaegerTranslator "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/jaeger"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
 	"github.com/jaegertracing/jaeger/model"
 )
 
+// ProtoFromTraces converts OpenTelemetry traces (ptrace.Traces)
+// to Jaeger model batches ([]*model.Batch).
 func ProtoFromTraces(traces ptrace.Traces) []*model.Batch {
-	batches := otlp2jaeger.ProtoFromTraces(traces)
-	for i, batch := range batches {
-		scopes := traces.ResourceSpans().At(i).ScopeSpans()
-		spanIndex := 0
+	batches := jaegerTranslator.ProtoFromTraces(traces)
+	spanMap := createSpanMapFromTraces(traces)
+	addWarningsToBatches(batches, spanMap)
+	return batches
+}
+
+func createSpanMapFromTraces(traces ptrace.Traces) map[pcommon.SpanID]ptrace.Span {
+	spanMap := make(map[pcommon.SpanID]ptrace.Span)
+	resources := traces.ResourceSpans()
+	for i := 0; i < resources.Len(); i++ {
+		scopes := resources.At(i).ScopeSpans()
 		for j := 0; j < scopes.Len(); j++ {
 			spans := scopes.At(j).Spans()
 			for k := 0; k < spans.Len(); k++ {
 				span := spans.At(k)
-				// add warnings
-				warnings := GetWarnings(span)
-				if len(warnings) > 0 {
-					batch.Spans[spanIndex].Warnings = warnings
-				}
-				spanIndex++
+				spanMap[span.SpanID()] = span
 			}
 		}
 	}
-	return batches
+	return spanMap
+}
+
+func addWarningsToBatches(batches []*model.Batch, spanMap map[pcommon.SpanID]ptrace.Span) {
+	for i, batch := range batches {
+		for j, span := range batch.Spans {
+			if span, ok := spanMap[span.SpanID.ToOTELSpanID()]; ok {
+				warnings := GetWarnings(span)
+				batches[i].Spans[j].Warnings = append(batches[i].Spans[j].Warnings, warnings...)
+			}
+		}
+	}
 }
--- internal/jptrace/translator_test.go
+++ internal/jptrace/translator_test.go
@@ -1,38 +1,34 @@
-// Copyright (c) 2024 The Jaeger Authors.
-// SPDX-License-Identifier: Apache-2.0
-
 package jptrace
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
-func TestProtoFromTraces_Warnings(t *testing.T) {
+func TestProtoFromTraces_AddsWarnings(t *testing.T) {
 	traces := ptrace.NewTraces()
 	rs1 := traces.ResourceSpans().AppendEmpty()
 	ss1 := rs1.ScopeSpans().AppendEmpty()
 	span1 := ss1.Spans().AppendEmpty()
 	span1.SetName("test-span-1")
+	span1.SetSpanID(pcommon.SpanID([8]byte{1, 2, 3, 4, 5, 6, 7, 8}))
 	AddWarning(span1, "test-warning-1")
 	AddWarning(span1, "test-warning-2")
 
 	ss2 := rs1.ScopeSpans().AppendEmpty()
 	span2 := ss2.Spans().AppendEmpty()
 	span2.SetName("test-span-2")
+	span2.SetSpanID(pcommon.SpanID([8]byte{9, 10, 11, 12, 13, 14, 15, 16}))
 
 	rs2 := traces.ResourceSpans().AppendEmpty()
 	ss3 := rs2.ScopeSpans().AppendEmpty()
 	span3 := ss3.Spans().AppendEmpty()
 	span3.SetName("test-span-3")
+	span3.SetSpanID(pcommon.SpanID([8]byte{17, 18, 19, 20, 21, 22, 23, 24}))
 	AddWarning(span3, "test-warning-3")
-
-	ss4 := rs2.ScopeSpans().AppendEmpty()
-	span4 := ss4.Spans().AppendEmpty()
-	span4.SetName("test-span-4")
-	AddWarning(span4, "test-warning-4")
 
 	batches := ProtoFromTraces(traces)
 
@@ -44,9 +40,7 @@ func TestProtoFromTraces_Warnings(t *testing.T) {
 	assert.Equal(t, "test-span-2", batches[0].Spans[1].OperationName)
 	assert.Empty(t, batches[0].Spans[1].Warnings)
 
-	assert.Len(t, batches[1].Spans, 2)
+	assert.Len(t, batches[1].Spans, 1)
 	assert.Equal(t, "test-span-3", batches[1].Spans[0].OperationName)
 	assert.Equal(t, []string{"test-warning-3"}, batches[1].Spans[0].Warnings)
-	assert.Equal(t, "test-span-4", batches[1].Spans[1].OperationName)
-	assert.Equal(t, []string{"test-warning-4"}, batches[1].Spans[1].Warnings)
 }
