1c9b19b456b107c4edd2b095e1a0aa4adcf6ccf4
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-23 22:16:39 -0500

    Fix Include
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/warning_test.go
+++ internal/jptrace/warning_test.go
@@ -6,8 +6,8 @@ package jptrace
 import (
 	"testing"
 
-	"github.com/crossdock/crossdock-go/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
