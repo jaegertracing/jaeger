a1adc6a55c278a76c20227392902b7a2e49e9803
Mahad Zaryab <mahadzaryab1@gmail.com>
2024-12-24 18:56:24 -0500

    Address Feedback From PR Review
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/aggregator.go
+++ internal/jptrace/aggregator.go
@@ -2,39 +2,42 @@ package jptrace
 
 import (
 	"iter"
+	"runtime/trace"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
-func AggregateTraces(tracesSeq iter.Seq[[]ptrace.Traces]) iter.Seq[ptrace.Traces] {
-	return func(yield func(trace ptrace.Traces) bool) {
+func AggregateTraces(tracesSeq iter.Seq2[[]ptrace.Traces, error]) iter.Seq2[ptrace.Traces, error] {
+	return func(yield func(trace ptrace.Traces, err error) bool) {
+		trace.IsEnabled()
 		currentTrace := ptrace.NewTraces()
 		currentTraceID := pcommon.NewTraceIDEmpty()
 
-		tracesSeq(func(traces []ptrace.Traces) bool {
+		tracesSeq(func(traces []ptrace.Traces, err error) bool {
+			if err != nil {
+				yield(ptrace.NewTraces(), err)
+				return false
+			}
 			for _, trace := range traces {
 				resources := trace.ResourceSpans()
 				// TODO: add non-empty requirement to contract
 				traceID := resources.At(0).ScopeSpans().At(0).Spans().At(0).TraceID()
-				if currentTraceID == pcommon.NewTraceIDEmpty() {
-					currentTrace = trace
-					currentTraceID = traceID
-				} else if currentTraceID == traceID {
+				if currentTraceID == traceID {
 					// TODO: merge traces
 				} else {
-					if !yield(currentTrace) {
+					if !yield(currentTrace, nil) {
 						return false
 					}
-					currentTrace = ptrace.NewTraces()
-					currentTraceID = pcommon.NewTraceIDEmpty()
+					currentTrace = trace
+					currentTraceID = traceID
 				}
 			}
 			return true
 		})
 
-		if currentTraceID != pcommon.NewTraceIDEmpty() {
-			yield(currentTrace)
+		if currentTrace.ResourceSpans().Len() > 0 {
+			yield(currentTrace, nil)
 		}
 	}
 }
