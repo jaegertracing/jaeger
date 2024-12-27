aa04166029670c80cc421995c46bc2d323160a45
Yuri Shkuro <github@ysh.us>
2024-12-27 14:53:53 -0500

    fixes
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- cmd/jaeger/internal/integration/e2e_integration.go
+++ cmd/jaeger/internal/integration/e2e_integration.go
@@ -4,17 +4,15 @@
 package integration
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
@@ -53,61 +51,6 @@ type E2EStorageIntegration struct {
 	EnvVarOverrides map[string]string
 }
 
-// Binary is a wrapper around exec.Cmd to help running binaries in tests.
-type Binary struct {
-	Name string
-	exec.Cmd
-}
-
-func (b *Binary) Start(t *testing.T) {
-	outFile, err := os.OpenFile(
-		filepath.Join(t.TempDir(), "output_logs.txt"),
-		os.O_CREATE|os.O_WRONLY,
-		os.ModePerm,
-	)
-	require.NoError(t, err)
-	t.Logf("Writing the %s output logs into %s", b.Name, outFile.Name())
-
-	errFile, err := os.OpenFile(
-		filepath.Join(t.TempDir(), "error_logs.txt"),
-		os.O_CREATE|os.O_WRONLY,
-		os.ModePerm,
-	)
-	require.NoError(t, err)
-	t.Logf("Writing the %s error logs into %s", b.Name, errFile.Name())
-
-	b.Stdout = outFile
-	b.Stderr = errFile
-
-	t.Logf("Running command: %v", b.Args)
-	require.NoError(t, b.Cmd.Start())
-	t.Cleanup(func() {
-		if err := b.Process.Kill(); err != nil {
-			t.Errorf("Failed to kill %s process: %v", b.Name, err)
-		}
-		if t.Failed() {
-			// A special annotation to create a foldable section in the GitHub Actions runner output.
-			// https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#grouping-log-lines
-			fmt.Printf("::group::🚧 🚧 🚧 %s binary logs\n", b.Name)
-			outLogs, err := os.ReadFile(outFile.Name())
-			if err != nil {
-				t.Errorf("Failed to read output logs: %v", err)
-			} else {
-				fmt.Printf("🚧 🚧 🚧 %s output logs:\n%s", b.Name, outLogs)
-			}
-
-			errLogs, err := os.ReadFile(errFile.Name())
-			if err != nil {
-				t.Errorf("Failed to read error logs: %v", err)
-			} else {
-				fmt.Printf("🚧 🚧 🚧 %s error logs:\n%s", b.Name, errLogs)
-			}
-			// End of Github Actions foldable section annotation.
-			fmt.Println("::endgroup::")
-		}
-	})
-}
-
 // e2eInitialize starts the Jaeger-v2 collector with the provided config file,
 // it also initialize the SpanWriter and SpanReader below.
 // This function should be called before any of the tests start.
@@ -144,74 +87,15 @@ func (s *E2EStorageIntegration) e2eInitialize(t *testing.T, storage string) {
 	}
 	cmd.Start(t)
 
-	// Wait for the binary to start and become ready to serve requests.
-	require.Eventually(t, func() bool { return s.doHealthCheck(t) },
-		60*time.Second, 3*time.Second, "%s did not start", s.BinaryName)
-	t.Logf("%s is ready", s.BinaryName)
-
 	s.SpanWriter, err = createSpanWriter(logger, otlpPort)
 	require.NoError(t, err)
-	traceReader, err := createTraceReader(logger, ports.QueryGRPC)
+	s.TraceReader, err = createTraceReader(logger, ports.QueryGRPC)
 	require.NoError(t, err)
-	s.TraceReader = traceReader
 
 	t.Cleanup(func() {
-		// Call e2eCleanUp to close the SpanReader and SpanWriter gRPC connection.
-		s.e2eCleanUp(t)
+		require.NoError(t, s.TraceReader.(io.Closer).Close())
+		require.NoError(t, s.SpanWriter.(io.Closer).Close())
 	})
-}
-
-func (s *E2EStorageIntegration) doHealthCheck(t *testing.T) bool {
-	healthCheckPort := s.HealthCheckPort
-	if healthCheckPort == 0 {
-		healthCheckPort = ports.CollectorV2HealthChecks
-	}
-	healthCheckEndpoint := fmt.Sprintf("http://localhost:%d/status", healthCheckPort)
-	t.Logf("Checking if %s is available on %s", s.BinaryName, healthCheckEndpoint)
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, healthCheckEndpoint, nil)
-	if err != nil {
-		t.Logf("HTTP request creation failed: %v", err)
-		return false
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Logf("HTTP request failed: %v", err)
-		return false
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Logf("Failed to read HTTP response body: %v", err)
-		return false
-	}
-	if resp.StatusCode != http.StatusOK {
-		t.Logf("HTTP response not OK: %v", string(body))
-		return false
-	}
-
-	var healthResponse struct {
-		Status string `json:"status"`
-	}
-	if err := json.NewDecoder(bytes.NewReader(body)).Decode(&healthResponse); err != nil {
-		t.Logf("Failed to decode JSON response '%s': %v", string(body), err)
-		return false
-	}
-
-	// Check if the status field in the JSON is "StatusOK"
-	if healthResponse.Status != "StatusOK" {
-		t.Logf("Received non-OK status %s: %s", healthResponse.Status, string(body))
-		return false
-	}
-	return true
-}
-
-// e2eCleanUp closes the SpanReader and SpanWriter gRPC connection.
-// This function should be called after all the tests are finished.
-func (s *E2EStorageIntegration) e2eCleanUp(t *testing.T) {
-	require.NoError(t, s.TraceReader.(io.Closer).Close())
-	require.NoError(t, s.SpanWriter.(io.Closer).Close())
 }
 
 func createStorageCleanerConfig(t *testing.T, configFile string, storage string) string {
@@ -260,7 +144,9 @@ func createStorageCleanerConfig(t *testing.T, configFile string, storage string)
 
 	newData, err := yaml.Marshal(config)
 	require.NoError(t, err)
-	tempFile := filepath.Join(t.TempDir(), "storageCleaner_config.yaml")
+	fileExt := filepath.Ext(filepath.Base(configFile))
+	fileName := strings.TrimSuffix(filepath.Base(configFile), fileExt)
+	tempFile := filepath.Join(t.TempDir(), fileName+"_with_storageCleaner"+fileExt)
 	err = os.WriteFile(tempFile, newData, 0o600)
 	require.NoError(t, err)
 
