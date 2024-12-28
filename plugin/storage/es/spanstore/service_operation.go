1cba60a35d6be2f31eb711c5ba36866a422465cc
Manik2708 <mehtamanik96@gmail.com>
2024-12-28 21:45:55 +0530

    conflicts resolved
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

--- plugin/storage/es/spanstore/service_operation.go
+++ plugin/storage/es/spanstore/service_operation.go
@@ -19,7 +19,8 @@ import (
 	"github.com/jaegertracing/jaeger/model"
 	"github.com/jaegertracing/jaeger/pkg/cache"
 	"github.com/jaegertracing/jaeger/pkg/es"
-	"github.com/jaegertracing/jaeger/plugin/storage/es/spanstore/dbmodel"
+	"github.com/jaegertracing/jaeger/plugin/storage/es/spanstore/internal/dbmodel"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
 )
 
 const (
