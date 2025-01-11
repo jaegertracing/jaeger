797107c89a0273b087eaf461a070b94c5a246d03
cs-308-2023 <adityaruhela2003@gmail.com>
2025-01-11 23:30:39 +0530

    Added explanation comments
    Signed-off-by: cs-308-2023 <adityaruhela2003@gmail.com>

--- internal/jptrace/attributes.go
+++ internal/jptrace/attributes.go
@@ -1,7 +1,13 @@
 package jptrace
 
 const(
-
+	// WarningsAttribute is the name of the span attribute where we can
+	// store various warnings produced from transformations,
+	// such as inbound sanitizers and outbound adjusters.
+	// The value type of the attribute is a string slice.
 	WarningsAttribute = "@jaeger@warnings"
+	// FormatAttribute is a key for span attribute that records the original
+	// wire format in which the span was received by Jaeger, 
+	// e.g. proto, thrift, json.
 	FormatAttribute = "@jaeger@format"
 )
