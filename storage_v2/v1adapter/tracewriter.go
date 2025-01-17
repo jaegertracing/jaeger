9e0d66e878020977dd3333a0bdd17f16b0e1700f
Manik2708 <mehtamanik96@gmail.com>
2025-01-17 15:25:50 +0530

    [storage_v2]Upgraded integration tests to use Dependency Writer of storage_v2
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

--- storage_v2/v1adapter/tracewriter.go
+++ storage_v2/v1adapter/tracewriter.go
@@ -6,9 +6,12 @@ package v1adapter
 import (
 	"context"
 	"errors"
+	"time"
 
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
+	"github.com/jaegertracing/jaeger/model"
+	"github.com/jaegertracing/jaeger/storage/dependencystore"
 	"github.com/jaegertracing/jaeger/storage/spanstore"
 	"github.com/jaegertracing/jaeger/storage_v2/tracestore"
 )
@@ -48,4 +51,18 @@ func (t *TraceWriter) WriteTraces(ctx context.Context, td ptrace.Traces) error {
 		}
 	}
 	return errors.Join(errs...)
+}
+
+type DependencyWriter struct {
+	writer dependencystore.Writer
+}
+
+func NewDependencyWriter(writer dependencystore.Writer) *DependencyWriter {
+	return &DependencyWriter{
+		writer: writer,
+	}
+}
+
+func (dw *DependencyWriter) WriteDependencies(ts time.Time, dependencies []model.DependencyLink) error {
+	return dw.writer.WriteDependencies(ts, dependencies)
 }
