43b76043afb08af2c9d590a4534a3c59182b734a
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-01-18 11:13:13 -0500

    Fix Cassandra Integration Test
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- plugin/storage/integration/cassandra_test.go
+++ plugin/storage/integration/cassandra_test.go
@@ -24,7 +24,8 @@ import (
 
 type CassandraStorageIntegration struct {
 	StorageIntegration
-	factory *cassandra.Factory
+	factory        *cassandra.Factory
+	archiveFactory *cassandra.Factory
 }
 
 func newCassandraStorageIntegration() *CassandraStorageIntegration {
@@ -43,9 +44,9 @@ func (s *CassandraStorageIntegration) cleanUp(t *testing.T) {
 	require.NoError(t, s.factory.Purge(context.Background()))
 }
 
-func (*CassandraStorageIntegration) initializeCassandraFactory(t *testing.T, flags []string) *cassandra.Factory {
+func (*CassandraStorageIntegration) initializeCassandraFactory(t *testing.T, flags []string, factoryInit func() *cassandra.Factory) *cassandra.Factory {
 	logger := zaptest.NewLogger(t, zaptest.WrapOptions(zap.AddCaller()))
-	f := cassandra.NewFactory()
+	f := factoryInit()
 	v, command := config.Viperize(f.AddFlags)
 	require.NoError(t, command.ParseFlags(flags))
 	f.InitFromViper(v, logger)
@@ -64,14 +65,17 @@ func (s *CassandraStorageIntegration) initializeCassandra(t *testing.T) {
 		"--cassandra.password=" + password,
 		"--cassandra.username=" + username,
 		"--cassandra.keyspace=jaeger_v1_dc1",
+	}, cassandra.NewFactory)
+	af := s.initializeCassandraFactory(t, []string{
 		"--cassandra-archive.keyspace=jaeger_v1_dc1_archive",
 		"--cassandra-archive.enabled=true",
 		"--cassandra-archive.servers=127.0.0.1",
 		"--cassandra-archive.basic.allowed-authenticators=org.apache.cassandra.auth.PasswordAuthenticator",
 		"--cassandra-archive.password=" + password,
 		"--cassandra-archive.username=" + username,
-	})
+	}, cassandra.NewArchiveFactory)
 	s.factory = f
+	s.archiveFactory = af
 	var err error
 	spanWriter, err := f.CreateSpanWriter()
 	require.NoError(t, err)
@@ -79,9 +83,9 @@ func (s *CassandraStorageIntegration) initializeCassandra(t *testing.T) {
 	spanReader, err := f.CreateSpanReader()
 	require.NoError(t, err)
 	s.TraceReader = v1adapter.NewTraceReader(spanReader)
-	s.ArchiveSpanReader, err = f.CreateArchiveSpanReader()
+	s.ArchiveSpanReader, err = af.CreateSpanReader()
 	require.NoError(t, err)
-	s.ArchiveSpanWriter, err = f.CreateArchiveSpanWriter()
+	s.ArchiveSpanWriter, err = af.CreateSpanWriter()
 	require.NoError(t, err)
 	s.SamplingStore, err = f.CreateSamplingStore(0)
 	require.NoError(t, err)
