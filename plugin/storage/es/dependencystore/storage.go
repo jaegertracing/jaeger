2da40c97de8d142b3f0fd9ddc59df1973b967913
Manik2708 <mehtamanik96@gmail.com>
2025-01-25 14:15:17 +0530

    goleak
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

--- plugin/storage/es/dependencystore/storage.go
+++ plugin/storage/es/dependencystore/storage.go
@@ -137,7 +137,7 @@ func (s *DependencyStore) getWriteIndex(ts time.Time) string {
 	return indexWithDate(s.dependencyIndexPrefix, s.indexDateLayout, ts)
 }
 
-func (s *DependencyStore) CreatePolicy() error {
+func (s *DependencyStore) InitializePolicyManager() error {
 	policyManager := ilm.NewPolicyManager(s.client, s.dependencyIndexPrefix)
 	return policyManager.Init()
 }
