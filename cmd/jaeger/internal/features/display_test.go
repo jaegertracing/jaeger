4b032d3bac49308adc88dd428eb0104174e92086
cs-308-2023 <adityaruhela2003@gmail.com>
2025-01-26 21:09:54 +0530

    Lint
    Signed-off-by: cs-308-2023 <adityaruhela2003@gmail.com>

--- cmd/jaeger/internal/features/display_test.go
+++ cmd/jaeger/internal/features/display_test.go
@@ -7,9 +7,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/featuregate"
 
 	"github.com/jaegertracing/jaeger/pkg/testutils"
-	"go.opentelemetry.io/collector/featuregate"
 )
 
 func TestMain(m *testing.M) {
@@ -17,16 +17,15 @@ func TestMain(m *testing.M) {
 }
 
 func TestDisplay(t *testing.T) {
-	featuregate.GlobalRegistry().MustRegister( 
-		"jaeger.sampling.includeDefaultOpStrategies", 
-		featuregate.StageBeta, // enabed by default 
-		featuregate.WithRegisterFromVersion("v2.2.0"), 
-		featuregate.WithRegisterToVersion("v2.5.0"), 
-		featuregate.WithRegisterDescription("Forces service strategy to be merged with default strategy, including per-operation overrides."), 
-		featuregate.WithRegisterReferenceURL("https://github.com/jaegertracing/jaeger/issues/5270"), 
-	) 
+	featuregate.GlobalRegistry().MustRegister(
+		"jaeger.sampling.includeDefaultOpStrategies",
+		featuregate.StageBeta, // enabed by default
+		featuregate.WithRegisterFromVersion("v2.2.0"),
+		featuregate.WithRegisterToVersion("v2.5.0"),
+		featuregate.WithRegisterDescription("Forces service strategy to be merged with default strategy, including per-operation overrides."),
+		featuregate.WithRegisterReferenceURL("https://github.com/jaegertracing/jaeger/issues/5270"),
+	)
 	out := DisplayFeatures()
 	expected_out := "Feature:\tjaeger.sampling.includeDefaultOpStrategies\nDefault state:\tOn\nDescription:\tForces service strategy to be merged with default strategy, including per-operation overrides.\nReferenceURL:\thttps://github.com/jaegertracing/jaeger/issues/5270\n\n"
 	require.Equal(t, expected_out, out)
-	
 }
