68655a826b751d9cbae435619b401e6897ab3984
Emmanuel Emonueje Ebenezer <eebenezer949@gmail.com>
2025-01-29 02:56:46 +0100

    Use v2 api for es archive trace reader-writer
    Signed-off-by: Emmanuel Emonueje Ebenezer <eebenezer949@gmail.com>

--- plugin/storage/integration/elasticsearch_test.go
+++ plugin/storage/integration/elasticsearch_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/jaegertracing/jaeger/pkg/testutils"
 	"github.com/jaegertracing/jaeger/plugin/storage/es"
 	"github.com/jaegertracing/jaeger/storage/dependencystore"
-	"github.com/jaegertracing/jaeger/storage/spanstore"
+	"github.com/jaegertracing/jaeger/storage_v2/tracestore"
 	"github.com/jaegertracing/jaeger/storage_v2/v1adapter"
 )
 
@@ -54,8 +54,8 @@ type ESStorageIntegration struct {
 	client   *elastic.Client
 	v8Client *elasticsearch8.Client
 
-	ArchiveSpanReader spanstore.Reader
-	ArchiveSpanWriter spanstore.Writer
+	ArchiveTraceReader tracestore.Reader
+	ArchiveTraceWriter tracestore.Writer
 
 	factory        *es.Factory
 	archiveFactory *es.Factory
@@ -265,23 +265,32 @@ func (s *ESStorageIntegration) testArchiveTrace(t *testing.T) {
 	s.skipIfNeeded(t)
 	defer s.cleanUp(t)
 	tID := model.NewTraceID(uint64(11), uint64(22))
-	expected := &model.Span{
-		OperationName: "archive_span",
-		StartTime:     time.Now().Add(-maxSpanAge * 5).Truncate(time.Microsecond),
-		TraceID:       tID,
-		SpanID:        model.NewSpanID(55),
-		References:    []model.SpanRef{},
-		Process:       model.NewProcess("archived_service", model.KeyValues{}),
+	expected := &model.Trace{
+		Spans: []*model.Span{
+			{
+				OperationName: "archive_span",
+				StartTime:     time.Now().Add(-maxSpanAge * 5).Truncate(time.Microsecond),
+				TraceID:       tID,
+				SpanID:        model.NewSpanID(55),
+				References:    []model.SpanRef{},
+				Process:       model.NewProcess("archived_service", model.KeyValues{}),
+			},
+		},
 	}
 
-	require.NoError(t, s.ArchiveSpanWriter.WriteSpan(context.Background(), expected))
+	require.NoError(t, s.ArchiveTraceWriter.WriteTraces(context.Background(), v1adapter.V1TraceToOtelTrace(expected)))
 
 	var actual *model.Trace
 	found := s.waitForCondition(t, func(_ *testing.T) bool {
 		var err error
-		actual, err = s.ArchiveSpanReader.GetTrace(context.Background(), spanstore.GetTraceParameters{TraceID: tID})
+		iterTraces := s.ArchiveTraceReader.GetTraces(context.Background(), tracestore.GetTraceParams{TraceID: v1adapter.FromV1TraceID(tID)})
+		traces, err := v1adapter.V1TracesFromSeq2(iterTraces)
+		if len(traces) == 0 {
+			return false
+		}
+		actual = traces[0]
 		return err == nil && len(actual.Spans) == 1
 	})
 	require.True(t, found)
-	CompareTraces(t, &model.Trace{Spans: []*model.Span{expected}}, actual)
+	CompareTraces(t, expected, actual)
 }
