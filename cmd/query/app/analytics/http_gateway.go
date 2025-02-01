c032479dbb6317827f38c7aff6d5bc2e2c4de1da
adityachopra29 <adityachopra2912@gmail.com>
2025-02-02 02:23:02 +0530

    add deep dependencies endpoint
    Signed-off-by: adityachopra29 <adityachopra2912@gmail.com>

---
+++ cmd/query/app/analytics/http_gateway.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package analytics
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+
+	"github.com/jaegertracing/jaeger/cmd/query/app/querysvc"
+)
+
+const (
+	// paramOpsName = ""
+	// paramServiceName = ""
+
+	routeGetDeepDependencies  = "/api/deep-dependencies/"
+)
+
+// HTTPGateway exposes analytics HTTP endpoints.
+type HTTPGateway struct {
+	QueryService *querysvc.QueryService
+	Logger       *zap.Logger
+}
+
+func (h *HTTPGateway) RegisterRoutes(router *mux.Router) {
+	h.addRoute(router, h.getDeepDependencies, routeGetDeepDependencies).Methods(http.MethodGet)
+}
+
+// addRoute adds a new endpoint to the router with given path and handler function.
+// This code is mostly copied from ../http_handler.
+func (*HTTPGateway) addRoute(
+	router *mux.Router,
+	f func(http.ResponseWriter, *http.Request),
+	route string,
+	_ ...any, /* args */
+) *mux.Route {
+	var handler http.Handler = http.HandlerFunc(f)
+	handler = otelhttp.WithRouteTag(route, handler)
+	handler = spanNameHandler(route, handler)
+	return router.HandleFunc(route, handler.ServeHTTP)
+}
+
+func spanNameHandler(spanName string, handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		span := trace.SpanFromContext(r.Context())
+		span.SetName(spanName)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func (h * HTTPGateway) getDeepDependencies(w http.ResponseWriter, r *http.Request) {
+	dependencies := TDdgPayload{
+		Dependencies: []TDdgPayloadPath{
+			{
+				Path: []TDdgPayloadEntry{
+					{Service: "serviceA", Operation: "opA"},
+					{Service: "serviceB", Operation: "opB"},
+				},
+				Attributes: []Attribute{
+					{Key: "exemplar_trace_id", Value: "abc123"},
+				},
+			},
+		},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(dependencies); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+
+
+}
+
+// Structs that need to be used or created for deep dependencies
+type TDdgPayloadEntry struct {
+	Operation string `json:"operation"`
+	Service   string `json:"service"`
+}
+
+type Attribute struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type TDdgPayloadPath struct {
+	Path       []TDdgPayloadEntry `json:"path"`
+	Attributes []Attribute        `json:"attributes"`
+}
+
+type TDdgPayload struct {
+	Dependencies []TDdgPayloadPath `json:"dependencies"`
+}
