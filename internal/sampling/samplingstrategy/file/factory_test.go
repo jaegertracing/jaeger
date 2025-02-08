4a1b518e2a5250a3d7274d6ff7b9ed4e28cab38c
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-02-08 10:58:34 -0500

    Move Configurable To Internal
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/sampling/samplingstrategy/file/factory_test.go
+++ internal/sampling/samplingstrategy/file/factory_test.go
@@ -10,14 +10,14 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger/internal/sampling/samplingstrategy"
+	"github.com/jaegertracing/jaeger/internal/storage/v1"
 	"github.com/jaegertracing/jaeger/pkg/config"
 	"github.com/jaegertracing/jaeger/pkg/metrics"
-	"github.com/jaegertracing/jaeger/plugin"
 )
 
 var (
 	_ samplingstrategy.Factory = new(Factory)
-	_ plugin.Configurable      = new(Factory)
+	_ storage.Configurable     = new(Factory)
 )
 
 func TestFactory(t *testing.T) {
