8fdd9c6bdb6039ca1b326e6bbf540f895f68b819
cs-308-2023 <adityaruhela2003@gmail.com>
2025-02-12 22:47:13 +0530

    Not be empty
    Signed-off-by: cs-308-2023 <adityaruhela2003@gmail.com>

--- cmd/query/app/qualitymetrics/data_test.go
+++ cmd/query/app/qualitymetrics/data_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestGetSampleData(t *testing.T) {
 	data := GetSampleData()
-	assert.NotEmpty(t, data.TraceQualityDocumentationLink, "TraceQualityDocumentationLink should match")
+	assert.NotEmpty(t, data.TraceQualityDocumentationLink, "TraceQualityDocumentationLink should not be empty")
 }
 
 func TestMain(m *testing.M) {
