151b2f35c848fda880950e02832b594ece9b23ef
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-02-28 10:20:50 -0500

    Address Feedback From Last PR Review
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/attributes_test.go
+++ internal/jptrace/attributes_test.go
@@ -62,45 +62,27 @@ func TestAttributesToStringMap(t *testing.T) {
 
 func TestMapToAttributes(t *testing.T) {
 	tests := []struct {
-		name      string
-		tags      map[string]string
-		requireFn func(t *testing.T, result pcommon.Map)
+		name string
+		tags map[string]string
 	}{
 		{
 			name: "empty map",
 			tags: map[string]string{},
-			requireFn: func(t *testing.T, result pcommon.Map) {
-				require.Equal(t, 0, result.Len(), "Expected map to be empty")
-			},
 		},
 		{
 			name: "single tag",
 			tags: map[string]string{"key1": "value1"},
-			requireFn: func(t *testing.T, result pcommon.Map) {
-				val, exists := result.Get("key1")
-				require.True(t, exists)
-				require.Equal(t, "value1", val.Str())
-			},
 		},
 		{
 			name: "multiple tags",
 			tags: map[string]string{"key1": "value1", "key2": "value2"},
-			requireFn: func(t *testing.T, result pcommon.Map) {
-				val, exists := result.Get("key1")
-				require.True(t, exists)
-				require.Equal(t, "value1", val.Str())
-
-				val, exists = result.Get("key2")
-				require.True(t, exists)
-				require.Equal(t, "value2", val.Str())
-			},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			result := MapToAttributes(test.tags)
-			test.requireFn(t, result)
+			require.Equal(t, test.tags, AttributesToStringMap(result))
 		})
 	}
 }
