d68e03695e7de72fd5803bc7dae2af83f67c57a9
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2025-03-09 12:35:14 -0300

    Update cmd/es-index-cleaner/app/cutoff_time.go
    Signed-off-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>

--- cmd/es-index-cleaner/app/cutoff_time.go
+++ cmd/es-index-cleaner/app/cutoff_time.go
@@ -9,6 +9,7 @@ import (
 
 func CalculateDeletionCutoff(currTime time.Time, numOfDays int, relativeIndexEnabled bool) time.Time {
 	year, month, day := currTime.Date()
+	// tomorrow midnight
 	cutoffTime := time.Date(year, month, day, 0, 0, 0, 0, time.UTC).AddDate(0, 0, 1)
 	if relativeIndexEnabled {
 		cutoffTime = currTime
