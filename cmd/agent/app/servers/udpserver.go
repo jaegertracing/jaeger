c54ef8faea00eb81c340f03aba4454b3cbd1daed
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2025-03-16 00:22:38 -0300

    [agent] Refactor UDP server (#6852)
    ## Which problem is this PR solving?
    - Simplify code, part of #6704
    
    ## Description of the changes
    - Remove the need for custom buffer type and use `bytes.Buffer`
    - Rename TBufferedServer to UDPServer since there's nothing
    Thrift-specific about it
    - Simplify tests of UDPServer to not depend on external Thrift types
    
    ## How was this change tested?
    - CI
    
    ---------
    
    Signed-off-by: Yuri Shkuro <github@ysh.us>
    Signed-off-by: amol-verma-allen <amol.verma@allen.in>

--- cmd/agent/app/servers/udpserver.go
+++ cmd/agent/app/servers/udpserver.go
@@ -5,6 +5,7 @@
 package servers
 
 import (
+	"bytes"
 	"io"
 	"sync"
 	"sync/atomic"
@@ -12,24 +13,32 @@ import (
 	"github.com/jaegertracing/jaeger/pkg/metrics"
 )
 
-// ThriftTransport is a subset of thrift.TTransport methods, for easier mocking.
-type ThriftTransport interface {
+// TBufferedServer is an alias for UDPServer, for backwards compatibility.
+type TBufferedServer = UDPServer
+
+// NewTBufferedServer is an alias for NewUDPServer, for backwards compatibility.
+var NewTBufferedServer = NewUDPServer
+
+// UDPConn is a an abstraction of *net.UDPConn, for easier mocking.
+type UDPConn interface {
 	io.Reader
 	io.Closer
 }
 
-// TBufferedServer is a custom thrift server that reads traffic using the transport provided
-// and places messages into a buffered channel to be processed by the processor provided
-type TBufferedServer struct {
+// UDPServer reads packets from a UDP connection into bytes.Buffer and places
+// each buffer into a bounded channel to be consumed by the receiver.
+// After consuming the buffer, the receiver SHOULD call DataRecd() to signal
+// that the buffer is no longer in use and to return it to the pool.
+type UDPServer struct {
 	// NB. queueLength HAS to be at the top of the struct or it will SIGSEV for certain architectures.
 	// See https://github.com/golang/go/issues/13868
 	queueSize     int64
-	dataChan      chan *ReadBuf
+	dataChan      chan *bytes.Buffer
 	maxPacketSize int
 	maxQueueSize  int
 	serving       uint32
-	transport     ThriftTransport
-	readBufPool   *sync.Pool
+	transport     UDPConn
+	readBufPool   sync.Pool
 	metrics       struct {
 		// Size of the current server queue
 		QueueSize metrics.Gauge `metric:"thrift.udp.server.queue_size"`
@@ -58,86 +67,117 @@ const (
 	stateInit
 )
 
-// NewTBufferedServer creates a TBufferedServer
-func NewTBufferedServer(
-	transport ThriftTransport,
+// NewUDPServer creates a UDPServer
+func NewUDPServer(
+	transport UDPConn,
 	maxQueueSize int,
 	maxPacketSize int,
 	mFactory metrics.Factory,
-) (*TBufferedServer, error) {
-	dataChan := make(chan *ReadBuf, maxQueueSize)
-
-	readBufPool := &sync.Pool{
-		New: func() any {
-			return &ReadBuf{bytes: make([]byte, maxPacketSize)}
-		},
-	}
-
-	res := &TBufferedServer{
-		dataChan:      dataChan,
+) (*UDPServer, error) {
+	srv := &UDPServer{
+		dataChan:      make(chan *bytes.Buffer, maxQueueSize),
 		transport:     transport,
 		maxQueueSize:  maxQueueSize,
 		maxPacketSize: maxPacketSize,
-		readBufPool:   readBufPool,
 		serving:       stateInit,
+		readBufPool: sync.Pool{
+			New: func() any {
+				return new(bytes.Buffer)
+			},
+		},
 	}
 
-	metrics.MustInit(&res.metrics, mFactory, nil)
-	return res, nil
+	metrics.MustInit(&srv.metrics, mFactory, nil)
+	return srv, nil
+}
+
+// packetReader is a helper for reading a single packet no larger than maxPacketSize
+// from the underlying reader. Without it the ReadFrom() method of bytes.Buffer would
+// read multiple packets and won't even stop at maxPacketSize.
+type packetReader struct {
+	maxPacketSize int
+	reader        io.LimitedReader
+	attempt       int
+}
+
+func (r *packetReader) Read(p []byte) (int, error) {
+	if r.attempt > 0 {
+		return 0, io.EOF
+	}
+	r.attempt = 1
+	return r.reader.Read(p)
+}
+
+func (r *packetReader) readPacket(buf *bytes.Buffer) (int, error) {
+	// reset the readers since we're reusing them to avoid allocations
+	r.attempt = 0
+	r.reader.N = int64(r.maxPacketSize)
+	// prepare the buffer for expected packet size
+	buf.Grow(r.maxPacketSize)
+	buf.Reset()
+	// use Buffer's ReadFrom() as otherwise it's hard to get it into the right state
+	n, err := buf.ReadFrom(r)
+	return int(n), err
 }
 
 // Serve initiates the readers and starts serving traffic
-func (s *TBufferedServer) Serve() {
+func (s *UDPServer) Serve() {
 	defer close(s.dataChan)
 	if !atomic.CompareAndSwapUint32(&s.serving, stateInit, stateServing) {
 		return // Stop already called
 	}
 
+	pr := &packetReader{
+		maxPacketSize: s.maxPacketSize,
+		reader: io.LimitedReader{
+			R: s.transport,
+		},
+	}
+
 	for s.IsServing() {
-		readBuf := s.readBufPool.Get().(*ReadBuf)
-		n, err := s.transport.Read(readBuf.bytes)
+		buf := s.readBufPool.Get().(*bytes.Buffer)
+		n, err := pr.readPacket(buf)
 		if err == nil {
-			readBuf.n = n
 			s.metrics.PacketSize.Update(int64(n))
 			select {
-			case s.dataChan <- readBuf:
+			case s.dataChan <- buf:
 				s.metrics.PacketsProcessed.Inc(1)
 				s.updateQueueSize(1)
 			default:
-				s.readBufPool.Put(readBuf)
+				s.readBufPool.Put(buf)
 				s.metrics.PacketsDropped.Inc(1)
 			}
 		} else {
-			s.readBufPool.Put(readBuf)
+			s.readBufPool.Put(buf)
 			s.metrics.ReadError.Inc(1)
 		}
 	}
 }
 
-func (s *TBufferedServer) updateQueueSize(delta int64) {
+func (s *UDPServer) updateQueueSize(delta int64) {
 	atomic.AddInt64(&s.queueSize, delta)
 	s.metrics.QueueSize.Update(atomic.LoadInt64(&s.queueSize))
 }
 
 // IsServing indicates whether the server is currently serving traffic
-func (s *TBufferedServer) IsServing() bool {
+func (s *UDPServer) IsServing() bool {
 	return atomic.LoadUint32(&s.serving) == stateServing
 }
 
 // Stop stops the serving of traffic and waits until the queue is
 // emptied by the readers
-func (s *TBufferedServer) Stop() {
+func (s *UDPServer) Stop() {
 	atomic.StoreUint32(&s.serving, stateStopped)
 	_ = s.transport.Close()
 }
 
 // DataChan returns the data chan of the buffered server
-func (s *TBufferedServer) DataChan() chan *ReadBuf {
+func (s *UDPServer) DataChan() chan *bytes.Buffer {
 	return s.dataChan
 }
 
 // DataRecd is called by the consumers every time they read a data item from DataChan
-func (s *TBufferedServer) DataRecd(buf *ReadBuf) {
+func (s *UDPServer) DataRecd(buf *bytes.Buffer) {
 	s.updateQueueSize(-1)
 	s.readBufPool.Put(buf)
 }
