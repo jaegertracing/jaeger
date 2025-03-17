c0be5ec4a1288bc1f79ded72c94019fc20349141
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-03-16 20:45:24 -0400

    Add Helper To Convert Map To Key Value List
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jpcommon/map.go
+++ internal/jpcommon/map.go
@@ -1,9 +1,7 @@
-// Copyright (c) 2025 The Jaeger Authors.
-// SPDX-License-Identifier: Apache-2.0
-
 package jpcommon
 
 import (
+	"github.com/jaegertracing/jaeger/proto-gen/storage/v2"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 )
 
@@ -22,4 +20,77 @@ func PlainMapToPcommonMap(attributesMap map[string]string) pcommon.Map {
 		attributes.PutStr(k, v)
 	}
 	return attributes
+}
+
+// ConvertMapToKeyValues converts a pcommon.Map to []*storage.KeyValue.
+func ConvertMapToKeyValueList(m pcommon.Map) []*storage.KeyValue {
+	keyValues := make([]*storage.KeyValue, 0, m.Len())
+	m.Range(func(k string, v pcommon.Value) bool {
+		keyValues = append(keyValues, &storage.KeyValue{
+			Key:   k,
+			Value: convertValueToAnyValue(v),
+		})
+		return true
+	})
+	return keyValues
+}
+
+func convertValueToAnyValue(v pcommon.Value) *storage.AnyValue {
+	switch v.Type() {
+	case pcommon.ValueTypeStr:
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_StringValue{
+				StringValue: v.Str(),
+			},
+		}
+	case pcommon.ValueTypeBool:
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_BoolValue{
+				BoolValue: v.Bool(),
+			},
+		}
+	case pcommon.ValueTypeInt:
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_IntValue{
+				IntValue: v.Int(),
+			},
+		}
+	case pcommon.ValueTypeDouble:
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_DoubleValue{
+				DoubleValue: v.Double(),
+			},
+		}
+	case pcommon.ValueTypeBytes:
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_BytesValue{
+				BytesValue: v.Bytes().AsRaw(),
+			},
+		}
+	case pcommon.ValueTypeSlice:
+		arr := v.Slice()
+		arrayValues := make([]*storage.AnyValue, 0, arr.Len())
+		for i := 0; i < arr.Len(); i++ {
+			arrayValues = append(arrayValues, convertValueToAnyValue(arr.At(i)))
+		}
+		return &storage.AnyValue{
+			Value: &storage.AnyValue_ArrayValue{
+				ArrayValue: &storage.ArrayValue{
+					Values: arrayValues,
+				},
+			},
+		}
+	case pcommon.ValueTypeMap:
+		kvList := &storage.KeyValueList{}
+		v.Map().Range(func(k string, val pcommon.Value) bool {
+			kvList.Values = append(kvList.Values, &storage.KeyValue{
+				Key:   k,
+				Value: convertValueToAnyValue(val),
+			})
+			return true
+		})
+		return &storage.AnyValue{Value: &storage.AnyValue_KvlistValue{KvlistValue: kvList}}
+	default:
+		return nil
+	}
 }
