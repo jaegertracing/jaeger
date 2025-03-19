66b49624bd959892ef400739112a2cdc99e985ff
Mohammed Shuraih Shaikh <shuraih15@gmail.com>
2025-03-19 15:10:14 +0530

    Replaced the use of model/converter/thrift/zipkin by the package provided by OTEL collector contrib (pkg/translator/zipkin/zipkinthriftconverter)
    Signed-off-by: Mohammed Shuraih Shaikh <shuraih15@gmail.com>

--- model/converter/thrift/zipkin/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2019 The Jaeger Authors.
-// Copyright (c) 2017 Uber Technologies, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package zipkin allows converting model.Trace to/from zipkin.thrift model.
-package zipkin
