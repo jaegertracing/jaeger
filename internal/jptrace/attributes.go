e439cd91831a5cca6c2c3e50a58b820defeb119c
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-03-19 17:59:58 -0400

    Revert Changes To attributes.go
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/jptrace/attributes.go
+++ internal/jptrace/attributes.go
@@ -1,9 +1,10 @@
-// Copyright (c) 2025 The Jaeger Authors.
+// Copyright (c) 2024 The Jaeger Authors.
 // SPDX-License-Identifier: Apache-2.0
-
 package jptrace
 
-import "go.opentelemetry.io/collector/pdata/pcommon"
+import (
+	"go.opentelemetry.io/collector/pdata/pcommon"
+)
 
 const (
 	// WarningsAttribute is the name of the span attribute where we can
