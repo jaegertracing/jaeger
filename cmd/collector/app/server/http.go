ad183f55f419b51a5aa1d726e8e094166833736c
danish9039 <danishsiddiqui040@gmail.com>
2025-03-22 19:53:43 +0530

    .
    Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

--- cmd/collector/app/server/http.go
+++ cmd/collector/app/server/http.go
@@ -14,10 +14,10 @@ import (
 
 	"github.com/jaegertracing/jaeger/cmd/collector/app/handler"
 	"github.com/jaegertracing/jaeger/internal/healthcheck"
+	"github.com/jaegertracing/jaeger/internal/httpmetrics"
 	"github.com/jaegertracing/jaeger/internal/recoveryhandler"
 	samplinghttp "github.com/jaegertracing/jaeger/internal/sampling/http"
 	"github.com/jaegertracing/jaeger/internal/sampling/samplingstrategy"
-	"github.com/jaegertracing/jaeger/internal/httpmetrics"
 	"github.com/jaegertracing/jaeger/pkg/metrics"
 	"github.com/jaegertracing/jaeger/pkg/telemetry"
 )
