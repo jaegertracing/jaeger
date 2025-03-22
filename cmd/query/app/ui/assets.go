e8c57060bcfbfd8ee3e58b237afc9bb0dbf22335
SACHIN KUMAR <118827645+sAchin-680@users.noreply.github.com>
2025-03-23 00:31:12 +0530

    Move pkg/gzipfs to internal/gzipfs (#6897)
    ## Which problem is this PR solving?
    - **Resolves** #6869
    
    ## Description of the changes
    - Moved `pkg/gzipfs/` to `internal/gzipfs/`
    - Updated all imports referencing `pkg/gzipfs/` to `internal/gzipfs/`
    
    ## How was this change tested?
    -
    
    ## Checklist
    - [ ] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [ ] I have signed all commits
    - [ ] I have added unit tests for the new functionality
    - [ ] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    Signed-off-by: sAchin-680 <mrmister680@gmail.com>
    Signed-off-by: amol-verma-allen <amol.verma@allen.in>

--- cmd/query/app/ui/assets.go
+++ cmd/query/app/ui/assets.go
@@ -9,7 +9,7 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/jaegertracing/jaeger/pkg/gzipfs"
+	"github.com/jaegertracing/jaeger/internal/gzipfs"
 	"github.com/jaegertracing/jaeger/pkg/httpfs"
 )
 
