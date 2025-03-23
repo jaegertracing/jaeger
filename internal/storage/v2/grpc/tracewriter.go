573d7adadb64d7e452d280f4e0ff817f8906b20d
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-03-23 12:27:00 -0400

    [grpc][v2] Implement v2 gRPC trace writer  (#6919)
    ## Which problem is this PR solving?
    - Towards #6789
    
    ## Description of the changes
    - This PR implements the v2 gRPC trace writer using OTEL's gRPC exporter
    client.
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>
    Signed-off-by: amol-verma-allen <amol.verma@allen.in>

---
+++ internal/storage/v2/grpc/tracewriter.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package grpc
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
+	"google.golang.org/grpc"
+
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+)
+
+var _ tracestore.Writer = (*TraceWriter)(nil)
+
+type TraceWriter struct {
+	client ptraceotlp.GRPCClient
+}
+
+// NewTraceWriter creates a TraceWriter that exports traces to a remote gRPC storage server.
+//
+// The provided gRPC connection is used exclusively for sending trace data to the backend.
+// To prevent recursive trace generation, this connection should not have instrumentation enabled.
+func NewTraceWriter(conn *grpc.ClientConn) *TraceWriter {
+	return &TraceWriter{
+		client: ptraceotlp.NewGRPCClient(conn),
+	}
+}
+
+func (tw *TraceWriter) WriteTraces(ctx context.Context, td ptrace.Traces) error {
+	req := ptraceotlp.NewExportRequestFromTraces(td)
+	_, err := tw.client.Export(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to export traces: %w", err)
+	}
+	return nil
+}
