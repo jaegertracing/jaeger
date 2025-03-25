19b97497a52b24a8ed810b80703077d94d923b84
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-03-25 15:47:13 +0100

    Support configuring prometheus.extra_query_parameters via CLI (#6931)
    ## Which problem is this PR solving?
    - All config options except for `extra_query_parameters` are
    configurable via CLI parameters
    
    ## Description of the changes
    - Support configuring prometheus.extra_query_parameters via CLI
    
    Related: #6360 
    
    ## How was this change tested?
    - unit test
    
    ## Checklist
    - [X] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [X] I have signed all commits
    - [X] I have added unit tests for the new functionality
    - [ ] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

---
+++ internal/storage/metricstore/prometheus/options_test.go
@@ -0,0 +1,78 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package prometheus
+
+import (
+	"errors"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/jaegertracing/jaeger/internal/config"
+)
+
+func TestCLI(t *testing.T) {
+	opts := NewOptions()
+	v, command := config.Viperize(opts.AddFlags)
+	err := command.ParseFlags([]string{
+		"--prometheus.query.extra-query-params=key1=value1",
+	})
+	require.NoError(t, err)
+
+	err = opts.InitFromViper(v)
+	require.NoError(t, err)
+	assert.Equal(t, map[string]string{"key1": "value1"}, opts.ExtraQueryParams)
+}
+
+func TestCLIError(t *testing.T) {
+	opts := NewOptions()
+	v, command := config.Viperize(opts.AddFlags)
+
+	err := command.ParseFlags([]string{
+		"--prometheus.query.extra-query-params=key1",
+	})
+	require.NoError(t, err)
+
+	err = opts.InitFromViper(v)
+	require.ErrorContains(t, err, "failed to parse extra query params: failed to parse 'key1'. Expected format: 'param1=value1,param2=value2'")
+}
+
+func TestParseKV(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected map[string]string
+		err      error
+	}{
+		{
+			input:    "",
+			expected: map[string]string{},
+			err:      nil,
+		},
+		{
+			input:    "key1=value1",
+			expected: map[string]string{"key1": "value1"},
+			err:      nil,
+		},
+		{
+			input:    "key1=value1,key2=value2",
+			expected: map[string]string{"key1": "value1", "key2": "value2"},
+			err:      nil,
+		},
+		{
+			input:    "key1=value1,key2",
+			expected: map[string]string{},
+			err:      errors.New("failed to parse 'key1=value1,key2'. Expected format: 'param1=value1,param2=value2'"),
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			kv, err := parseKV(test.input)
+			assert.Equal(t, test.expected, kv)
+			assert.Equal(t, test.err, err)
+		})
+	}
+}
