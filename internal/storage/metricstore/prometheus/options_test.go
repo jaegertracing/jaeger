18d34098cd3a1268f048a7be83ba6b8efa91ab7c
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-03-25 15:17:24 +0100

    add test for error case
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- internal/storage/metricstore/prometheus/options_test.go
+++ internal/storage/metricstore/prometheus/options_test.go
@@ -8,8 +8,36 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/jaegertracing/jaeger/internal/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestCLI(t *testing.T) {
+	opts := NewOptions()
+	v, command := config.Viperize(opts.AddFlags)
+	err := command.ParseFlags([]string{
+		"--prometheus.query.extra-query-params=key1=value1",
+	})
+	require.NoError(t, err)
+
+	err = opts.InitFromViper(v)
+	require.NoError(t, err)
+	assert.Equal(t, map[string]string{"key1": "value1"}, opts.ExtraQueryParams)
+}
+
+func TestCLIError(t *testing.T) {
+	opts := NewOptions()
+	v, command := config.Viperize(opts.AddFlags)
+
+	err := command.ParseFlags([]string{
+		"--prometheus.query.extra-query-params=key1",
+	})
+	require.NoError(t, err)
+
+	err = opts.InitFromViper(v)
+	require.ErrorContains(t, err, "failed to parse extra query params: failed to parse 'key1'. Expected format: 'param1=value1,param2=value2'")
+}
 
 func TestParseKV(t *testing.T) {
 	tests := []struct {
