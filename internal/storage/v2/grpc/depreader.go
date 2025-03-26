2046eb91964cf6eaa753849a3c904faee5778ada
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-03-26 10:45:07 -0400

    [grpc][v2] Implement v2 gRPC dependency reader  (#6933)
    ## Which problem is this PR solving?
    - Towards #6789
    
    ## Description of the changes
    - This PR implements the v2 gRPC dependency reader
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>
    Signed-off-by: amol-verma-allen <amol.verma@allen.in>

---
+++ internal/storage/v2/grpc/depreader.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package grpc
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+
+	"github.com/jaegertracing/jaeger-idl/model/v1"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore"
+	"github.com/jaegertracing/jaeger/proto-gen/storage/v2"
+)
+
+var _ depstore.Reader = (*DependencyReader)(nil)
+
+type DependencyReader struct {
+	client storage.DependencyReaderClient
+}
+
+// NewDependencyReader creates a DependencyReader that communicates with a remote gRPC storage server.
+// The provided gRPC connection is used exclusively for reading dependencies, meaning it is safe
+// to enable instrumentation on the connection.
+func NewDependencyReader(conn *grpc.ClientConn) *DependencyReader {
+	return &DependencyReader{
+		client: storage.NewDependencyReaderClient(conn),
+	}
+}
+
+func (dr *DependencyReader) GetDependencies(
+	ctx context.Context,
+	query depstore.QueryParameters,
+) ([]model.DependencyLink, error) {
+	resp, err := dr.client.GetDependencies(ctx, &storage.GetDependenciesRequest{
+		StartTime: query.StartTime,
+		EndTime:   query.EndTime,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dependencies: %w", err)
+	}
+	dependencies := make([]model.DependencyLink, len(resp.Dependencies))
+	for i, dep := range resp.Dependencies {
+		dependencies[i] = model.DependencyLink{
+			Parent:    dep.Parent,
+			Child:     dep.Child,
+			CallCount: dep.CallCount,
+			Source:    dep.Source,
+		}
+	}
+	return dependencies, nil
+}
