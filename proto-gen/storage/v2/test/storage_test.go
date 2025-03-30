38dc0a943b3abe95c4c878cd7364d7220dc8cc18
Yuri Shkuro <github@ysh.us>
2025-03-29 21:52:36 -0400

    fix
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- proto-gen/storage/v2/test/storage_test.go
+++ proto-gen/storage/v2/test/storage_test.go
@@ -6,14 +6,13 @@ import (
 	v1 "go.opentelemetry.io/proto/otlp/trace/v1"
 
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/pdata/ptrace"
 	"google.golang.org/protobuf/proto"
 )
 
 func TestTracesChunk(t *testing.T) {
-	traceA := ptrace.NewTraces()
-	spanA := traceA.ResourceSpans().AppendEmpty().ScopeSpans().AppendEmpty().Spans().AppendEmpty()
-	spanA.SetName("span-a")
+	traceId := [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	spanId := [8]byte{1, 2, 3, 4, 5, 6, 7, 8}
+	t.Logf("span-name: %x", ([]byte("span-a")))
 
 	c := new(TracesChunk)
 	c.Traces = []*v1.TracesData{
@@ -24,7 +23,9 @@ func TestTracesChunk(t *testing.T) {
 						{
 							Spans: []*v1.Span{
 								{
-									Name: "span-a",
+									Name:    "span-a",
+									TraceId: traceId[:],
+									SpanId:  spanId[:],
 								},
 							},
 						},
