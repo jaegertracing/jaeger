daeef3a7876f04b95aea03460910db4f9187cc49
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-02 15:34:27 -0400

    Add Test For NewFactory
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ internal/storage/v2/grpc/factory_test.go
@@ -0,0 +1,55 @@
+package grpc
+
+import (
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/jaegertracing/jaeger/internal/telemetry"
+	"github.com/jaegertracing/jaeger/internal/tenancy"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/config/configauth"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+	"google.golang.org/grpc"
+)
+
+func TestNewFactory_NonEmptyAuthenticator(t *testing.T) {
+	cfg := &Config{
+		ClientConfig: configgrpc.ClientConfig{
+			Auth: &configauth.Authentication{},
+		},
+	}
+	_, err := NewFactory(*cfg, telemetry.NoopSettings())
+	require.ErrorContains(t, err, "authenticator is not supported")
+}
+
+func TestNewFactory(t *testing.T) {
+	lis, err := net.Listen("tcp", ":0")
+	require.NoError(t, err, "failed to listen")
+
+	s := grpc.NewServer()
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+	defer s.Stop()
+
+	cfg := Config{
+		ClientConfig: configgrpc.ClientConfig{
+			Endpoint: lis.Addr().String(),
+		},
+		TimeoutConfig: exporterhelper.TimeoutConfig{
+			Timeout: 1 * time.Second,
+		},
+		Tenancy: tenancy.Options{
+			Enabled: true,
+		},
+	}
+	telset := telemetry.NoopSettings()
+	f, err := NewFactory(cfg, telset)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+}
