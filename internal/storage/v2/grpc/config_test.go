e9066f2806c1d24e79325d9286759edfc0091491
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-02 15:24:30 -0400

    Add Unit Test For config.go
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ internal/storage/v2/grpc/config_test.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package grpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefaultConfig(t *testing.T) {
+	cfg := DefaultConfig()
+	require.Equal(t, defaultConnectionTimeout, cfg.Timeout)
+}
