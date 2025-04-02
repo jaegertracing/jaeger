38d4a77280c0b9635a3e3826fde07d5764570ce5
pipiland <nguyen.t.dang.minh@gmail.com>
2025-04-02 22:46:01 +0300

    Move TraceReader tests to query_test
    Signed-off-by: pipiland <nguyen.t.dang.minh@gmail.com>

--- cmd/jaeger/internal/integration/query_test.go
+++ cmd/jaeger/internal/integration/query_test.go
@@ -4,9 +4,17 @@
 package integration
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 
 	"github.com/jaegertracing/jaeger/internal/storage/integration"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/v1adapter"
 )
 
 func TestJaegerQueryService(t *testing.T) {
@@ -21,5 +29,37 @@ func TestJaegerQueryService(t *testing.T) {
 		SkipStorageCleaner: true,
 	}
 	s.e2eInitialize(t, "memory")
-	s.RunTraceReaderSmokeTests(t)
+
+	runTraceReaderSmokeTests(context.Background(), s.TraceReader, t)
+}
+
+// RunTraceReaderSmokeTests runs tests on TraceReader to see it returns no error
+func runTraceReaderSmokeTests(ctx context.Context, traceReader tracestore.Reader, t *testing.T) {
+	t.Run("TraceReader.GetTraces", func(t *testing.T) {
+		iterTraces := traceReader.GetTraces(ctx)
+		_, err := v1adapter.V1TracesFromSeq2(iterTraces)
+		assert.NoError(t, err)
+	})
+
+	t.Run("TraceReader.GetServices", func(t *testing.T) {
+		_, err := traceReader.GetServices(ctx)
+		assert.NoError(t, err)
+	})
+
+	t.Run("TraceReader.GetOperations", func(t *testing.T) {
+		_, err := traceReader.GetOperations(ctx, tracestore.OperationQueryParams{ServiceName: "random-service-name"})
+		assert.NoError(t, err)
+	})
+
+	t.Run("TraceReader.FindTraces", func(t *testing.T) {
+		iter := traceReader.FindTraces(ctx, tracestore.TraceQueryParams{
+			ServiceName:  "random-service-name",
+			Attributes:   pcommon.NewMap(),
+			StartTimeMin: time.Now().Add(-2 * time.Hour),
+			StartTimeMax: time.Now(),
+		})
+		iter(func(_ []ptrace.Traces, err error) bool {
+			return assert.NoError(t, err)
+		})
+	})
 }
