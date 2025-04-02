76f5ec5aac111708c6ff5ae890fcfb2120f9bc6e
pipiland <nguyen.t.dang.minh@gmail.com>
2025-04-02 17:42:46 +0300

    Remove unnecessary pkg alias
    Signed-off-by: pipiland <nguyen.t.dang.minh@gmail.com>

--- cmd/query/app/querysvc/adjusters.go
+++ cmd/query/app/querysvc/adjusters.go
@@ -7,20 +7,20 @@ package querysvc
 import (
 	"time"
 
-	adjuster2 "github.com/jaegertracing/jaeger/cmd/query/app/querysvc/internal/adjuster"
+	"github.com/jaegertracing/jaeger/cmd/query/app/querysvc/internal/adjuster"
 )
 
 // StandardAdjusters is a list of model adjusters applied by the query service
 // before returning the data to the API clients.
-func StandardAdjusters(maxClockSkewAdjust time.Duration) []adjuster2.Adjuster {
-	return []adjuster2.Adjuster{
-		adjuster2.ZipkinSpanIDUniquifier(),
-		adjuster2.SortTagsAndLogFields(),
-		adjuster2.DedupeBySpanHash(),            // requires SortTagsAndLogFields for deterministic results
-		adjuster2.ClockSkew(maxClockSkewAdjust), // adds warnings (which affect SpanHash) on dupe span IDs
-		adjuster2.IPTagAdjuster(),
-		adjuster2.OTelTagAdjuster(),
-		adjuster2.SpanReferences(),
-		adjuster2.ParentReference(),
+func StandardAdjusters(maxClockSkewAdjust time.Duration) []adjuster.Adjuster {
+	return []adjuster.Adjuster{
+		adjuster.ZipkinSpanIDUniquifier(),
+		adjuster.SortTagsAndLogFields(),
+		adjuster.DedupeBySpanHash(),            // requires SortTagsAndLogFields for deterministic results
+		adjuster.ClockSkew(maxClockSkewAdjust), // adds warnings (which affect SpanHash) on dupe span IDs
+		adjuster.IPTagAdjuster(),
+		adjuster.OTelTagAdjuster(),
+		adjuster.SpanReferences(),
+		adjuster.ParentReference(),
 	}
 }
