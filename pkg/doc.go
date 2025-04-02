f18474c0c50bbbf8436adbf021321f637eedae64
pipiland <nguyen.t.dang.minh@gmail.com>
2025-04-02 22:07:07 +0300

    Delete empty package pkg
    Signed-off-by: pipiland <nguyen.t.dang.minh@gmail.com>

--- pkg/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019 The Jaeger Authors.
-// Copyright (c) 2017 Uber Technologies, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package pkg is the collection of utility packages used by the Jaeger components without being specific to its internals.
-//
-// Utility packages are kept separate from the Jaeger core codebase to keep it as small and concise as possible. If some utilities grow larger and their APIs stabilize, they may be moved to their own repository, to facilitate re-use by other projects. However that is not the priority.
-package pkg
