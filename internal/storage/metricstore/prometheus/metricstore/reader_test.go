6df586d19559cb4784d49e2d4c86adad8ae43452
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2025-04-03 20:35:49 +0530

    Add usetesting linter and fix lint issues (#6892)
    Signed-off-by: Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>

--- internal/storage/metricstore/prometheus/metricstore/reader_test.go
+++ internal/storage/metricstore/prometheus/metricstore/reader_test.go
@@ -769,9 +769,8 @@ func TestGetRoundTripperTLSConfig(t *testing.T) {
 func TestGetRoundTripperTokenFile(t *testing.T) {
 	const wantBearer = "token from file"
 
-	file, err := os.CreateTemp("", "token_")
+	file, err := os.Create(t.TempDir() + "token_")
 	require.NoError(t, err)
-	defer func() { require.NoError(t, os.Remove(file.Name())) }()
 
 	_, err = file.Write([]byte(wantBearer))
 	require.NoError(t, err)
@@ -804,9 +803,9 @@ func TestGetRoundTripperTokenFile(t *testing.T) {
 }
 
 func TestGetRoundTripperTokenFromContext(t *testing.T) {
-	file, err := os.CreateTemp("", "token_")
+	file, err := os.Create(t.TempDir() + "token_")
 	require.NoError(t, err)
-	defer func() { require.NoError(t, os.Remove(file.Name())) }()
+
 	_, err = file.Write([]byte("token from file"))
 	require.NoError(t, err)
 	require.NoError(t, file.Close())
