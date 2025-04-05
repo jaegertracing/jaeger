6c70db0c13d2b6a097669265b1cc1639e2fd77ab
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-04 22:36:04 -0400

    Add Interface Checks
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/grpc/server.go
+++ internal/storage/v2/grpc/server.go
@@ -1,14 +1,24 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
 package grpc
 
 import (
 	"context"
 
+	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
+
 	"github.com/jaegertracing/jaeger/internal/proto-gen/storage/v2"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
-	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
 )
 
-type Handler struct {
+var (
+	_ storage.TraceReaderServer      = (*Server)(nil)
+	_ storage.DependencyReaderServer = (*Server)(nil)
+	_ ptraceotlp.GRPCServer          = (*Server)(nil)
+)
+
+type Server struct {
 	storage.UnimplementedTraceReaderServer
 	storage.UnimplementedDependencyReaderServer
 	ptraceotlp.UnimplementedGRPCServer
@@ -16,13 +26,13 @@ type Handler struct {
 	traceReader tracestore.Reader
 }
 
-func NewHandler(traceReader tracestore.Reader) *Handler {
-	return &Handler{
+func NewServer(traceReader tracestore.Reader) *Server {
+	return &Server{
 		traceReader: traceReader,
 	}
 }
 
-func (h *Handler) GetServices(
+func (h *Server) GetServices(
 	ctx context.Context,
 	_ *storage.GetServicesRequest,
 ) (*storage.GetServicesResponse, error) {
