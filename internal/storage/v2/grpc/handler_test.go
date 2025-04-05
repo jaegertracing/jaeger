5c744700545b34e17cd08cc8a54de0d7a637bc8e
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-05 13:30:25 -0400

    Add Unit Test
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/grpc/handler_test.go
+++ internal/storage/v2/grpc/handler_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/jaegertracing/jaeger/internal/proto-gen/storage/v2"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
 	tracestoremocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore/mocks"
 )
 
@@ -45,6 +46,57 @@ func TestServer_GetServices(t *testing.T) {
 			resp, err := server.GetServices(context.Background(), &storage.GetServicesRequest{})
 			if test.expectedErr == nil {
 				require.Equal(t, test.expectedServices, resp.Services)
+			} else {
+				require.ErrorIs(t, err, test.expectedErr)
+			}
+		})
+	}
+}
+
+func TestServer_GetOperations(t *testing.T) {
+	params := tracestore.OperationQueryParams{
+		ServiceName: "service",
+		SpanKind:    "kind",
+	}
+	req := &storage.GetOperationsRequest{
+		Service:  "service",
+		SpanKind: "kind",
+	}
+	tests := []struct {
+		name               string
+		operations         []tracestore.Operation
+		err                error
+		expectedOperations []*storage.Operation
+		expectedErr        error
+	}{
+		{
+			name: "success",
+			operations: []tracestore.Operation{
+				{Name: "operation1", SpanKind: "kind"},
+				{Name: "operation2", SpanKind: "kind"},
+			},
+			expectedOperations: []*storage.Operation{
+				{Name: "operation1", SpanKind: "kind"},
+				{Name: "operation2", SpanKind: "kind"},
+			},
+		},
+		{
+			name:        "error",
+			err:         assert.AnError,
+			expectedErr: assert.AnError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reader := new(tracestoremocks.Reader)
+			reader.On("GetOperations", mock.Anything, params).
+				Return(test.operations, test.err).Once()
+
+			server := NewHandler(reader)
+			resp, err := server.GetOperations(context.Background(), req)
+			if test.expectedErr == nil {
+				require.Equal(t, test.expectedOperations, resp.Operations)
 			} else {
 				require.ErrorIs(t, err, test.expectedErr)
 			}
