f5a578296d37a17d283dc016d9388a912e486b91
pipiland <nguyen.t.dang.minh@gmail.com>
2025-04-05 21:11:01 +0300

    Simplify query_test: get rid of additional confix
    Signed-off-by: pipiland <nguyen.t.dang.minh@gmail.com>

--- cmd/jaeger/internal/integration/query_test.go
+++ cmd/jaeger/internal/integration/query_test.go
@@ -4,15 +4,18 @@
 package integration
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/jaegertracing/jaeger/internal/storage/integration"
+	"github.com/jaegertracing/jaeger/ports"
 )
 
 func TestJaegerQueryService(t *testing.T) {
 	integration.SkipUnlessEnv(t, "query")
 
-	// Start instance of Jaeger with jaeger_query reading from remote storage
+	// Start instance of Jaeger with jaeger_query reading from Remote Storage, which
+	// will be started in GRPCStorageIntegration below
 	query := &E2EStorageIntegration{
 		ConfigFile:         "../../config-query.yaml",
 		SkipStorageCleaner: true,
@@ -23,11 +26,17 @@ func TestJaegerQueryService(t *testing.T) {
 	query.e2eInitialize(t, "grpc")
 	t.Log("Query initialized")
 
-	// Start another instance of Jaeger receiving traces from OTLP and write traces to remote storage
+	// Start another instance of Jaeger receiving traces and write traces to Remote Storage
 	collector := &GRPCStorageIntegration{
 		E2EStorageIntegration: E2EStorageIntegration{
-			ConfigFile:         "../../config-remote-storage-without-query.yaml",
+			ConfigFile:         "../../config-remote-storage.yaml",
 			SkipStorageCleaner: true,
+			EnvVarOverrides: map[string]string{
+				// Run jaeger_query on different ports here to avoid conflict
+				// with jaeger_query instance of Jaeger above
+				"JAEGER_QUERY_GRPC_PORT": fmt.Sprintf("%d", ports.QueryGRPC+1000),
+				"JAEGER_QUERY_HTTP_PORT": fmt.Sprintf("%d", ports.QueryHTTP+1000),
+			},
 		},
 	}
 	collector.CleanUp = collector.cleanUp
