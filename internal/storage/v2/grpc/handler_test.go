06840acda4e6ea5cec5afaa19356125595904231
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-04-05 23:41:52 -0400

    [grpc][v2] Implement `GetTraces` in gRPC v2 handler (#6985)
    ## Which problem is this PR solving?
    - Towards #6979
    
    ## Description of the changes
    - Implement the `Traces` call in the gRPC v2 handler
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/grpc/handler_test.go
+++ internal/storage/v2/grpc/handler_test.go
@@ -5,18 +5,129 @@ package grpc
 
 import (
 	"context"
+	"iter"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 
+	"github.com/jaegertracing/jaeger/internal/jptrace"
 	"github.com/jaegertracing/jaeger/internal/proto-gen/storage/v2"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
 	tracestoremocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore/mocks"
 )
 
-func TestServer_GetServices(t *testing.T) {
+type testStream struct {
+	storage.TraceReader_GetTracesServer
+	sent    []*jptrace.TracesData
+	sendErr error
+}
+
+func (*testStream) Context() context.Context {
+	return context.Background()
+}
+
+func (f *testStream) Send(td *jptrace.TracesData) error {
+	if f.sendErr != nil {
+		return f.sendErr
+	}
+	f.sent = append(f.sent, td)
+	return nil
+}
+
+func TestHandler_GetTraces(t *testing.T) {
+	reader := new(tracestoremocks.Reader)
+	start := time.Now()
+	end := start.Add(time.Minute)
+	query := tracestore.GetTraceParams{
+		TraceID: pcommon.TraceID([16]byte{1}),
+		Start:   start,
+		End:     end,
+	}
+	trace := makeTestTrace()
+	td := jptrace.TracesData(trace)
+
+	tests := []struct {
+		name         string
+		traces       [][]ptrace.Traces
+		expectedSent []*jptrace.TracesData
+		sendErr      error
+		getTraceErr  error
+		expectedErr  error
+	}{
+		{
+			name:   "single trace",
+			traces: [][]ptrace.Traces{{trace}},
+			expectedSent: []*jptrace.TracesData{
+				&td,
+			},
+		},
+		{
+			name:         "multiple traces",
+			traces:       [][]ptrace.Traces{{trace, trace}},
+			expectedSent: []*jptrace.TracesData{&td, &td},
+		},
+		{
+			name:         "multiple chunks",
+			traces:       [][]ptrace.Traces{{trace, trace}, {trace, trace}},
+			expectedSent: []*jptrace.TracesData{&td, &td, &td, &td},
+		},
+		{
+			name:        "storage error",
+			getTraceErr: assert.AnError,
+			expectedErr: assert.AnError,
+		},
+		{
+			name:        "send error",
+			traces:      [][]ptrace.Traces{{trace, trace}},
+			sendErr:     assert.AnError,
+			expectedErr: assert.AnError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reader.On("GetTraces", mock.Anything, query).
+				Return(iter.Seq2[[]ptrace.Traces, error](func(yield func([]ptrace.Traces, error) bool) {
+					if test.getTraceErr != nil {
+						yield(nil, test.getTraceErr)
+						return
+					}
+					for _, traces := range test.traces {
+						if !yield(traces, nil) {
+							return
+						}
+					}
+				})).Once()
+
+			server := NewHandler(reader)
+			stream := &testStream{
+				sendErr: test.sendErr,
+			}
+			err := server.GetTraces(&storage.GetTracesRequest{
+				Query: []*storage.GetTraceParams{
+					{
+						TraceId:   []byte{1},
+						StartTime: start,
+						EndTime:   end,
+					},
+				},
+			}, stream)
+			if test.expectedErr != nil {
+				require.ErrorIs(t, err, test.expectedErr)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.expectedSent, stream.sent)
+			}
+		})
+	}
+}
+
+func TestHandler_GetServices(t *testing.T) {
 	tests := []struct {
 		name             string
 		services         []string
@@ -50,6 +161,7 @@ func TestServer_GetServices(t *testing.T) {
 			server := NewHandler(reader)
 			resp, err := server.GetServices(context.Background(), &storage.GetServicesRequest{})
 			if test.expectedErr == nil {
+				require.NoError(t, err)
 				require.Equal(t, test.expectedServices, resp.Services)
 			} else {
 				require.ErrorIs(t, err, test.expectedErr)
@@ -58,7 +170,7 @@ func TestServer_GetServices(t *testing.T) {
 	}
 }
 
-func TestServer_GetOperations(t *testing.T) {
+func TestHandler_GetOperations(t *testing.T) {
 	params := tracestore.OperationQueryParams{
 		ServiceName: "service",
 		SpanKind:    "kind",
@@ -106,6 +218,7 @@ func TestServer_GetOperations(t *testing.T) {
 			server := NewHandler(reader)
 			resp, err := server.GetOperations(context.Background(), req)
 			if test.expectedErr == nil {
+				require.NoError(t, err)
 				require.Equal(t, test.expectedOperations, resp.Operations)
 			} else {
 				require.ErrorIs(t, err, test.expectedErr)
