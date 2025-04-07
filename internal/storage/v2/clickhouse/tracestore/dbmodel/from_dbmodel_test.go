89dee0d7efb7d830c5129feaeb2252f4322b38fe
zhengkezhou1 <madzhou1@gmail.com>
2025-04-07 19:16:32 +0800

    from-to-from test
    Signed-off-by: zhengkezhou1 <madzhou1@gmail.com>

---
+++ internal/storage/v2/clickhouse/tracestore/dbmodel/from_dbmodel_test.go
@@ -0,0 +1,64 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package dbmodel
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestToPtrace(t *testing.T) {
+	dbTrace := jsonToDbTrace(t, "./fixtures/dbtrace.json")
+	expected := jsonToTrace(t, "./fixtures/ptrace.json")
+	actual := ToPTrace(dbTrace)
+
+	exceptedResourceSpans := expected.ResourceSpans().At(0)
+	expectedResourceAttr := exceptedResourceSpans.Resource().Attributes().AsRaw()
+	actualResourceSpans := actual.ResourceSpans().At(0)
+	actualResourceAttr := actualResourceSpans.Resource().Attributes().AsRaw()
+	require.Equal(t, expectedResourceAttr, actualResourceAttr)
+
+	exceptedScopeSpans := exceptedResourceSpans.ScopeSpans().At(0)
+	exceptedScopeAttr := exceptedScopeSpans.Scope().Attributes().AsRaw()
+	actualScopeSpans := actualResourceSpans.ScopeSpans().At(0)
+	actualScopeAttr := actualScopeSpans.Scope().Attributes().AsRaw()
+	require.Equal(t, exceptedScopeAttr, actualScopeAttr)
+
+	exceptedSpan := exceptedScopeSpans.Spans().At(0)
+	exceptedSpanAttr := exceptedSpan.Attributes().AsRaw()
+	actualSpan := actualScopeSpans.Spans().At(0)
+	actualSpanAttr := actualSpan.Attributes().AsRaw()
+	require.Equal(t, exceptedSpanAttr, actualSpanAttr)
+
+	exceptedEvents := exceptedSpan.Events()
+	actualEvents := actualSpan.Events()
+	require.Equal(t, exceptedEvents.Len(), actualEvents.Len())
+	for i := 0; i < exceptedEvents.Len(); i++ {
+		exceptedEvent := exceptedEvents.At(i)
+		exceptedEventAttr := exceptedEvent.Attributes().AsRaw()
+		actualEvent := actualEvents.At(i)
+		actualEventAttr := actualEvent.Attributes().AsRaw()
+		require.Equal(t, exceptedEventAttr, actualEventAttr)
+	}
+
+	exceptedLinks := exceptedSpan.Links()
+	actualLinks := actualSpan.Links()
+	require.Equal(t, exceptedLinks.Len(), actualLinks.Len())
+	for i := 0; i < exceptedEvents.Len(); i++ {
+		exceptedLink := exceptedEvents.At(i)
+		exceptedLinkAttr := exceptedLink.Attributes().AsRaw()
+		actualLink := actualEvents.At(i)
+		actualLinkAttr := actualLink.Attributes().AsRaw()
+		require.Equal(t, exceptedLinkAttr, actualLinkAttr)
+	}
+}
+
+func jsonToDbTrace(t *testing.T, filename string) (dbTrace Trace) {
+	traceBytes := readJSONBytes(t, filename)
+	err := json.Unmarshal(traceBytes, &dbTrace)
+	require.NoError(t, err, "Failed to read file %s", filename)
+	return dbTrace
+}
--- internal/storage/v2/clickhouse/tracestore/dbmodel/to_dbmodel.go
+++ internal/storage/v2/clickhouse/tracestore/dbmodel/to_dbmodel.go
@@ -3,7 +3,11 @@
 
 package dbmodel
 
-import "go.opentelemetry.io/collector/pdata/pcommon"
+import (
+	"github.com/ClickHouse/ch-go/proto"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
 
 // NestedAttributesGroup There is a one-to-many relationship between a NestedAttributesGroup and a pcommon.Map.
 // In the official ClickHouse implementation, ptrace.SpanEventSlice and ptrace.SpanLinkSlice are stored in a Nested format in the database.
@@ -61,7 +65,7 @@ func AttributesToGroup(attributes pcommon.Map) AttributesGroup {
 // For example, a pcommon.Map can contain key-value pairs such as:
 // string-string, string-bool, string-int64, string-float64. Clearly, the key-value pairs need to be classified based on the data type.
 func AttributesToMap(attrs pcommon.Map) map[pcommon.ValueType]map[string]string {
-	result := make(map[pcommon.ValueType]map[string]string, 8)
+	result := make(map[pcommon.ValueType]map[string]string)
 	for _, valueType := range []pcommon.ValueType{
 		ValueTypeBool, ValueTypeDouble, ValueTypeInt, ValueTypeStr,
 		ValueTypeMap, ValueTypeSlice, ValueTypeBytes,
@@ -78,4 +82,241 @@ func AttributesToMap(attrs pcommon.Map) map[pcommon.ValueType]map[string]string 
 		return true
 	})
 	return result
+}
+
+var (
+	scopeAttributesBytesKey   = new(proto.ColStr).LowCardinality().Array()
+	scopeAttributesBytesValue = new(proto.ColStr).LowCardinality().Array()
+	scopeAttributesSliceKey   = new(proto.ColStr).LowCardinality().Array()
+	scopeAttributesSliceValue = new(proto.ColJSONStr).LowCardinality().Array()
+	scopeAttributesMapKey     = new(proto.ColStr).LowCardinality().Array()
+	scopeAttributesMapValue   = new(proto.ColJSONStr).LowCardinality().Array()
+
+	ColumnScopeAttributesBytesKey   = "scopeAttributesBytesKey"
+	ColumnScopeAttributesBytesValue = "scopeAttributesBytesValue"
+	ColumnScopeAttributesSliceKey   = "scopeAttributesSliceKey"
+	ColumnScopeAttributesSliceValue = "scopeAttributesSliceValue"
+	ColumnScopeAttributesMapKey     = "scopeAttributesMapKey"
+	ColumnScopeAttributesMapValue   = "scopeAttributesMapValue"
+)
+
+var (
+	resourceAttributesBytesKey   = new(proto.ColStr).LowCardinality().Array()
+	resourceAttributesBytesValue = new(proto.ColStr).LowCardinality().Array()
+	resourceAttributesSliceKey   = new(proto.ColStr).LowCardinality().Array()
+	resourceAttributesSliceValue = new(proto.ColJSONStr).LowCardinality().Array()
+	resourceAttributesMapKey     = new(proto.ColStr).LowCardinality().Array()
+	resourceAttributesMapValue   = new(proto.ColJSONStr).LowCardinality().Array()
+
+	ColumnResourceAttributesBytesKey   = "resourceAttributesBytesKey"
+	ColumnResourceAttributesBytesValue = "resourceAttributesBytesValue"
+	ColumnResourceAttributesSliceKey   = "resourceAttributesSliceKey"
+	ColumnResourceAttributesSliceValue = "resourceAttributesSliceValue"
+	ColumnResourceAttributesMapKey     = "resourceAttributesMapKey"
+	ColumnResourceAttributesMapValue   = "resourceAttributesMapValue"
+)
+
+var (
+	spanAttributesBytesKey   = new(proto.ColStr).LowCardinality().Array()
+	spanAttributesBytesValue = new(proto.ColStr).LowCardinality().Array()
+	spanAttributesSliceKey   = new(proto.ColStr).LowCardinality().Array()
+	spanAttributesSliceValue = new(proto.ColJSONStr).LowCardinality().Array()
+	spanAttributesMapKey     = new(proto.ColStr).LowCardinality().Array()
+	spanAttributesMapValue   = new(proto.ColJSONStr).LowCardinality().Array()
+
+	ColumnSpanAttributesBytesKey   = "spanAttributesBytesKey"
+	ColumnSpanAttributesBytesValue = "spanAttributesBytesValue"
+	ColumnSpanAttributesSliceKey   = "spanAttributesSliceKey"
+	ColumnSpanAttributesSliceValue = "spanAttributesSliceValue"
+	ColumnSpanAttributesMapKey     = "spanAttributesMapKey"
+	ColumnSpanAttributesMapValue   = "spanAttributesMapValue"
+)
+
+var (
+	eventsAttributesBytesKeys    = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	eventsAttributesBytesValue   = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	eventsAttributesSlicesKeys   = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	eventsAttributesSlicesValues = proto.NewArray(new(proto.ColJSONStr).LowCardinality().Array())
+	eventsAttributesMapKeys      = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	eventsAttributeMapValues     = proto.NewArray(new(proto.ColJSONStr).LowCardinality().Array())
+
+	ColumnEventsAttributesBytesKeys   = "eventsAttributesBytesKeys"
+	ColumnEventsAttributesBytesValues = "eventsAttributesBytesValues"
+	ColumnEventsAttributesSliceKeys   = "eventsAttributesSliceKeys"
+	ColumnEventsAttributesSliceValues = "eventsAttributesSliceValues"
+	ColumnEventsAttributesMapKeys     = "eventsAttributesMapKeys"
+	ColumnEventsAttributesMapValues   = "eventsAttributesMapValues"
+)
+
+var (
+	linksAttributesBytesKeys    = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	linksAttributesBytesValue   = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	linksAttributesSlicesKeys   = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	linksAttributesSlicesValues = proto.NewArray(new(proto.ColJSONStr).LowCardinality().Array())
+	linksAttributesMapKeys      = proto.NewArray(new(proto.ColStr).LowCardinality().Array())
+	linksAttributeMapValues     = proto.NewArray(new(proto.ColJSONStr).LowCardinality().Array())
+
+	ColumnLinksAttributesBytesKeys   = "linksAttributesBytesKeys"
+	ColumnLinksAttributesBytesValues = "linksAttributesBytesValues"
+	ColumnLinksAttributesSliceKeys   = "linksAttributesSliceKeys"
+	ColumnLinksAttributesSliceValues = "linksAttributesSliceValues"
+	ColumnLinksAttributesMapKeys     = "linksAttributesMapKeys"
+	ColumnLinksAttributesMapValues   = "linksAttributesMapValues"
+)
+
+type AttributeType int32
+
+const (
+	AttributeTypeResource AttributeType = iota
+	AttributeTypeScope
+	AttributeTypeSpan
+	AttributeTypeEvent
+	AttributeTypeLink
+)
+
+// FromPtrace converts the OpenTelemetry Traces model into a ClickHouse-compatible format for batch insertion.
+// It maps the trace attributes, spans, links and events from the OTel model to the appropriate ClickHouse column types
+func FromPtrace(td ptrace.Traces) proto.Input {
+	for i := 0; i < td.ResourceSpans().Len(); i++ {
+		resourceSpans := td.ResourceSpans().At(i)
+		resourceGroup := AttributesToGroup(resourceSpans.Resource().Attributes())
+
+		for j := 0; j < resourceSpans.ScopeSpans().Len(); j++ {
+			scope := resourceSpans.ScopeSpans().At(j).Scope()
+			scopeGroup := AttributesToGroup(scope.Attributes())
+
+			spans := resourceSpans.ScopeSpans().At(j).Spans()
+			for k := 0; k < spans.Len(); k++ {
+				span := spans.At(k)
+				spanGroup := AttributesToGroup(span.Attributes())
+				AppendGroup(AttributeTypeResource, resourceGroup)
+				AppendGroup(AttributeTypeScope, scopeGroup)
+				AppendGroup(AttributeTypeSpan, spanGroup)
+
+				var eventNestedGroup NestedAttributesGroup
+				for l := 0; l < span.Events().Len(); l++ {
+					event := span.Events().At(l)
+					eventGroup := AttributesToGroup(event.Attributes())
+					eventNestedGroup.AttributesGroups = append(eventNestedGroup.AttributesGroups, eventGroup)
+				}
+				AppendGroups(AttributeTypeEvent, eventNestedGroup)
+
+				var linkNestedGroup NestedAttributesGroup
+				for l := 0; l < span.Links().Len(); l++ {
+					link := span.Links().At(l)
+					linkGroup := AttributesToGroup(link.Attributes())
+					linkNestedGroup.AttributesGroups = append(linkNestedGroup.AttributesGroups, linkGroup)
+				}
+				AppendGroups(AttributeTypeLink, linkNestedGroup)
+			}
+		}
+	}
+
+	return proto.Input{
+		{Name: ColumnResourceAttributesBytesKey, Data: resourceAttributesBytesKey},
+		{Name: ColumnResourceAttributesBytesValue, Data: resourceAttributesBytesValue},
+		{Name: ColumnResourceAttributesMapKey, Data: resourceAttributesMapKey},
+		{Name: ColumnResourceAttributesMapValue, Data: resourceAttributesMapValue},
+		{Name: ColumnResourceAttributesSliceKey, Data: resourceAttributesSliceKey},
+		{Name: ColumnResourceAttributesSliceValue, Data: resourceAttributesSliceValue},
+
+		{Name: ColumnScopeAttributesBytesKey, Data: scopeAttributesBytesKey},
+		{Name: ColumnScopeAttributesBytesValue, Data: scopeAttributesBytesValue},
+		{Name: ColumnScopeAttributesMapKey, Data: scopeAttributesMapKey},
+		{Name: ColumnScopeAttributesMapValue, Data: scopeAttributesMapValue},
+		{Name: ColumnScopeAttributesSliceKey, Data: scopeAttributesSliceKey},
+		{Name: ColumnScopeAttributesSliceValue, Data: scopeAttributesSliceValue},
+
+		{Name: ColumnSpanAttributesBytesKey, Data: spanAttributesBytesKey},
+		{Name: ColumnSpanAttributesBytesValue, Data: spanAttributesBytesValue},
+		{Name: ColumnSpanAttributesMapKey, Data: spanAttributesMapKey},
+		{Name: ColumnSpanAttributesMapValue, Data: spanAttributesMapValue},
+		{Name: ColumnSpanAttributesSliceKey, Data: spanAttributesSliceKey},
+		{Name: ColumnSpanAttributesSliceValue, Data: spanAttributesSliceValue},
+
+		{Name: ColumnEventsAttributesBytesKeys, Data: eventsAttributesBytesKeys},
+		{Name: ColumnEventsAttributesBytesValues, Data: eventsAttributesBytesValue},
+		{Name: ColumnEventsAttributesMapKeys, Data: eventsAttributesMapKeys},
+		{Name: ColumnEventsAttributesMapValues, Data: eventsAttributeMapValues},
+		{Name: ColumnEventsAttributesSliceKeys, Data: eventsAttributesSlicesKeys},
+		{Name: ColumnEventsAttributesSliceValues, Data: eventsAttributesSlicesValues},
+
+		{Name: ColumnLinksAttributesBytesKeys, Data: linksAttributesBytesKeys},
+		{Name: ColumnLinksAttributesBytesValues, Data: linksAttributesBytesValue},
+		{Name: ColumnLinksAttributesMapKeys, Data: linksAttributesMapKeys},
+		{Name: ColumnLinksAttributesMapValues, Data: linksAttributeMapValues},
+		{Name: ColumnLinksAttributesSliceKeys, Data: linksAttributesSlicesKeys},
+		{Name: ColumnLinksAttributesSliceValues, Data: linksAttributesSlicesValues},
+	}
+}
+
+const (
+	ValueTypeBool   = pcommon.ValueTypeBool
+	ValueTypeDouble = pcommon.ValueTypeDouble
+	ValueTypeInt    = pcommon.ValueTypeInt
+	ValueTypeStr    = pcommon.ValueTypeStr
+	ValueTypeMap    = pcommon.ValueTypeMap
+	ValueTypeSlice  = pcommon.ValueTypeSlice
+	ValueTypeBytes  = pcommon.ValueTypeBytes
+)
+
+// AppendGroups Writes a complete set of pcommon.Map to the database. NestedAttributesGroup and pcommon.Map have a one-to-many relationship.
+func AppendGroups(attributeType AttributeType, nestedGroup NestedAttributesGroup) {
+	var byteKeys [][]string
+	var bytesValues [][]string
+	var mapKeys [][]string
+	var mapValues [][]string
+	var sliceKeys [][]string
+	var sliceValues [][]string
+	for _, group := range nestedGroup.AttributesGroups {
+		byteKeys = append(byteKeys, group.BytesKeys)
+		bytesValues = append(bytesValues, group.BytesValues)
+		sliceKeys = append(sliceKeys, group.SliceKeys)
+		sliceValues = append(sliceValues, group.SliceValues)
+		mapKeys = append(mapKeys, group.MapKeys)
+		mapValues = append(mapValues, group.MapValues)
+	}
+	switch attributeType {
+	case AttributeTypeEvent:
+		eventsAttributesBytesKeys.Append(byteKeys)
+		eventsAttributesSlicesKeys.Append(sliceKeys)
+		eventsAttributesMapKeys.Append(mapKeys)
+		eventsAttributesBytesValue.Append(bytesValues)
+		eventsAttributesSlicesValues.Append(sliceValues)
+		eventsAttributeMapValues.Append(mapValues)
+	case AttributeTypeLink:
+		linksAttributesBytesKeys.Append(byteKeys)
+		linksAttributesSlicesKeys.Append(sliceKeys)
+		linksAttributesMapKeys.Append(mapKeys)
+		linksAttributesBytesValue.Append(bytesValues)
+		linksAttributesSlicesValues.Append(sliceValues)
+		linksAttributeMapValues.Append(mapValues)
+	}
+}
+
+// AppendGroup Writes a complete pcommon.Map to the database. AttributesGroup and pcommon.Map have a one-to-one relationship.
+func AppendGroup(attributeType AttributeType, group AttributesGroup) {
+	switch attributeType {
+	case AttributeTypeResource:
+		resourceAttributesBytesKey.Append(group.BytesKeys)
+		resourceAttributesSliceKey.Append(group.SliceKeys)
+		resourceAttributesMapKey.Append(group.MapKeys)
+		resourceAttributesBytesValue.Append(group.BytesValues)
+		resourceAttributesSliceValue.Append(group.SliceValues)
+		resourceAttributesMapValue.Append(group.MapValues)
+	case AttributeTypeScope:
+		scopeAttributesBytesKey.Append(group.BytesKeys)
+		scopeAttributesSliceKey.Append(group.SliceKeys)
+		scopeAttributesMapKey.Append(group.MapKeys)
+		scopeAttributesBytesValue.Append(group.BytesValues)
+		scopeAttributesSliceValue.Append(group.SliceValues)
+		scopeAttributesMapValue.Append(group.MapValues)
+	case AttributeTypeSpan:
+		spanAttributesBytesKey.Append(group.BytesKeys)
+		spanAttributesSliceKey.Append(group.SliceKeys)
+		spanAttributesMapKey.Append(group.MapKeys)
+		spanAttributesBytesValue.Append(group.BytesValues)
+		spanAttributesSliceValue.Append(group.SliceValues)
+		spanAttributesMapValue.Append(group.MapValues)
+	}
 }
