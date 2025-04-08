fe34670061f862069a361d8d13de39e9fec3ce36
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-04-08 19:36:00 -0400

    [grpc][v2] Implement `FindTraces` in gRPC v2 handler (#6992)
    ## Which problem is this PR solving?
    - Towards #6979
    
    ## Description of the changes
    - Implement the `FindTraces` call in the gRPC v2 handler
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>
    Signed-off-by: amol-verma-allen <amol.verma@allen.in>

--- internal/storage/v2/grpc/handler_test.go
+++ internal/storage/v2/grpc/handler_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	"google.golang.org/grpc"
 
 	"github.com/jaegertracing/jaeger/internal/jptrace"
 	"github.com/jaegertracing/jaeger/internal/proto-gen/storage/v2"
@@ -22,7 +23,7 @@ import (
 )
 
 type testStream struct {
-	storage.TraceReader_GetTracesServer
+	grpc.ServerStream
 	sent    []*jptrace.TracesData
 	sendErr error
 }
@@ -223,6 +224,304 @@ func TestHandler_GetOperations(t *testing.T) {
 			} else {
 				require.ErrorIs(t, err, test.expectedErr)
 			}
+		})
+	}
+}
+
+func TestHandler_FindTraces(t *testing.T) {
+	reader := new(tracestoremocks.Reader)
+	query := tracestore.TraceQueryParams{
+		ServiceName:   "service",
+		OperationName: "operation",
+		Attributes:    pcommon.NewMap(),
+	}
+	trace := makeTestTrace()
+	td := jptrace.TracesData(trace)
+
+	tests := []struct {
+		name         string
+		traces       [][]ptrace.Traces
+		expectedSent []*jptrace.TracesData
+		sendErr      error
+		getTraceErr  error
+		expectedErr  error
+	}{
+		{
+			name:   "single trace",
+			traces: [][]ptrace.Traces{{trace}},
+			expectedSent: []*jptrace.TracesData{
+				&td,
+			},
+		},
+		{
+			name:         "multiple traces",
+			traces:       [][]ptrace.Traces{{trace, trace}},
+			expectedSent: []*jptrace.TracesData{&td, &td},
+		},
+		{
+			name:         "multiple chunks",
+			traces:       [][]ptrace.Traces{{trace, trace}, {trace, trace}},
+			expectedSent: []*jptrace.TracesData{&td, &td, &td, &td},
+		},
+		{
+			name:        "storage error",
+			getTraceErr: assert.AnError,
+			expectedErr: assert.AnError,
+		},
+		{
+			name:        "send error",
+			traces:      [][]ptrace.Traces{{trace, trace}},
+			sendErr:     assert.AnError,
+			expectedErr: assert.AnError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reader.On("FindTraces", mock.Anything, query).
+				Return(iter.Seq2[[]ptrace.Traces, error](func(yield func([]ptrace.Traces, error) bool) {
+					if test.getTraceErr != nil {
+						yield(nil, test.getTraceErr)
+						return
+					}
+					for _, traces := range test.traces {
+						if !yield(traces, nil) {
+							return
+						}
+					}
+				})).Once()
+
+			server := NewHandler(reader)
+			stream := &testStream{
+				sendErr: test.sendErr,
+			}
+			err := server.FindTraces(&storage.FindTracesRequest{
+				Query: &storage.TraceQueryParameters{
+					ServiceName:   "service",
+					OperationName: "operation",
+				},
+			}, stream)
+			if test.expectedErr != nil {
+				require.ErrorIs(t, err, test.expectedErr)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, test.expectedSent, stream.sent)
+			}
+		})
+	}
+}
+
+func TestConvertKeyValueListToMap(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []*storage.KeyValue
+		expected pcommon.Map
+	}{
+		{
+			name:     "empty list",
+			input:    []*storage.KeyValue{},
+			expected: pcommon.NewMap(),
+		},
+		{
+			name:     "nil entry",
+			input:    []*storage.KeyValue{nil},
+			expected: pcommon.NewMap(),
+		},
+		{
+			name: "nil value",
+			input: []*storage.KeyValue{
+				{
+					Key:   "key1",
+					Value: nil,
+				},
+			},
+			expected: pcommon.NewMap(),
+		},
+		{
+			name: "primitive types",
+			input: []*storage.KeyValue{
+				{
+					Key: "key1",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_StringValue{StringValue: "value1"},
+					},
+				},
+				{
+					Key: "key2",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_IntValue{IntValue: 42},
+					},
+				},
+				{
+					Key: "key3",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_DoubleValue{DoubleValue: 3.14},
+					},
+				},
+				{
+					Key: "key4",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_BoolValue{BoolValue: true},
+					},
+				},
+				{
+					Key: "key5",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_BytesValue{BytesValue: []byte{1, 2}},
+					},
+				},
+			},
+			expected: func() pcommon.Map {
+				m := pcommon.NewMap()
+				m.PutStr("key1", "value1")
+				m.PutInt("key2", 42)
+				m.PutDouble("key3", 3.14)
+				m.PutBool("key4", true)
+				m.PutEmptyBytes("key5").FromRaw([]byte{1, 2})
+				return m
+			}(),
+		},
+		{
+			name: "nested map",
+			input: []*storage.KeyValue{
+				{
+					Key: "key1",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_KvlistValue{
+							KvlistValue: &storage.KeyValueList{
+								Values: []*storage.KeyValue{
+									{
+										Key: "nestedKey",
+										Value: &storage.AnyValue{
+											Value: &storage.AnyValue_StringValue{StringValue: "nestedValue"},
+										},
+									},
+									{
+										Key:   "nilValueKey",
+										Value: nil, // should be skipped
+									},
+								},
+							},
+						},
+					},
+				},
+				nil, // should be skipped
+			},
+			expected: func() pcommon.Map {
+				m := pcommon.NewMap()
+				nested := m.PutEmptyMap("key1")
+				nested.PutStr("nestedKey", "nestedValue")
+				return m
+			}(),
+		},
+		{
+			name: "array",
+			input: []*storage.KeyValue{
+				{
+					Key: "key1",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_ArrayValue{
+							ArrayValue: &storage.ArrayValue{
+								Values: []*storage.AnyValue{
+									{
+										Value: &storage.AnyValue_StringValue{StringValue: "value1"},
+									},
+									{
+										Value: &storage.AnyValue_IntValue{IntValue: 42},
+									},
+									{
+										Value: &storage.AnyValue_DoubleValue{DoubleValue: 3.14},
+									},
+									{
+										Value: &storage.AnyValue_BoolValue{BoolValue: true},
+									},
+									{
+										Value: &storage.AnyValue_BytesValue{BytesValue: []byte{1, 2}},
+									},
+									{
+										Value: &storage.AnyValue_KvlistValue{
+											KvlistValue: &storage.KeyValueList{
+												Values: []*storage.KeyValue{
+													{
+														Key: "nestedKey",
+														Value: &storage.AnyValue{
+															Value: &storage.AnyValue_StringValue{StringValue: "nestedValue"},
+														},
+													},
+													{
+														Key:   "nilValueKey",
+														Value: nil, // should be skipped
+													},
+													nil, // should be skipped
+												},
+											},
+										},
+									},
+									nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: func() pcommon.Map {
+				m := pcommon.NewMap()
+				slice := m.PutEmptySlice("key1")
+				slice.AppendEmpty().SetStr("value1")
+				slice.AppendEmpty().SetInt(42)
+				slice.AppendEmpty().SetDouble(3.14)
+				slice.AppendEmpty().SetBool(true)
+				slice.AppendEmpty().SetEmptyBytes().FromRaw([]byte{1, 2})
+				nested := slice.AppendEmpty().SetEmptyMap()
+				nested.PutStr("nestedKey", "nestedValue")
+				slice.AppendEmpty() // for the nil entry
+				return m
+			}(),
+		},
+		{
+			name: "nested array",
+			input: []*storage.KeyValue{
+				{
+					Key: "key1",
+					Value: &storage.AnyValue{
+						Value: &storage.AnyValue_ArrayValue{
+							ArrayValue: &storage.ArrayValue{
+								Values: []*storage.AnyValue{
+									{
+										Value: &storage.AnyValue_ArrayValue{
+											ArrayValue: &storage.ArrayValue{
+												Values: []*storage.AnyValue{
+													{
+														Value: &storage.AnyValue_StringValue{StringValue: "inner1"},
+													},
+													nil,
+												},
+											},
+										},
+									},
+									nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: func() pcommon.Map {
+				m := pcommon.NewMap()
+				slice := m.PutEmptySlice("key1")
+				nestedSlice := slice.AppendEmpty().SetEmptySlice()
+				nestedSlice.AppendEmpty().SetStr("inner1")
+				nestedSlice.AppendEmpty() // for the nil entry
+				slice.AppendEmpty()       // for the nil entry
+				return m
+			}(),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := convertKeyValueListToMap(test.input)
+			assert.Equal(t, test.expected, result)
 		})
 	}
 }
