79d9ef8dbf4bfefedf8f2deafd070b106a7bf340
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-14 18:43:06 -0400

    Add Unit Test
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/v1adapter/depreader_test.go
+++ internal/storage/v2/v1adapter/depreader_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jaegertracing/jaeger-idl/model/v1"
 	dependencystoremocks "github.com/jaegertracing/jaeger/internal/storage/v1/api/dependencystore/mocks"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore"
+	depstoremocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
@@ -24,6 +25,24 @@ func TestDependencyReader_GetDependencies(t *testing.T) {
 	mr.On("GetDependencies", mock.Anything, end, time.Minute).Return(expectedDeps, nil)
 	dr := NewDependencyReader(mr)
 	deps, err := dr.GetDependencies(context.Background(), query)
+	require.NoError(t, err)
+	require.Equal(t, expectedDeps, deps)
+}
+
+func TestDowngradedDependencyReader_GetDependencies(t *testing.T) {
+	end := time.Now()
+	start := end.Add(-1 * time.Minute)
+	query := depstore.QueryParameters{
+		StartTime: start,
+		EndTime:   end,
+	}
+	expectedDeps := []model.DependencyLink{{Parent: "parent", Child: "child", CallCount: 12}}
+	mr := new(depstoremocks.Reader)
+	mr.On("GetDependencies", mock.Anything, query).Return(expectedDeps, nil)
+	dr := &DowngradedDependencyReader{
+		reader: mr,
+	}
+	deps, err := dr.GetDependencies(context.Background(), end, time.Minute)
 	require.NoError(t, err)
 	require.Equal(t, expectedDeps, deps)
 }
