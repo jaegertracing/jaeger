82e597bf9f92f135a0524a07d3f04b25d448da94
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-14 21:53:44 -0400

    Add Missing Code Coverage
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/v1adapter/depreader_test.go
+++ internal/storage/v2/v1adapter/depreader_test.go
@@ -17,6 +17,24 @@ import (
 	depstoremocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore/mocks"
 )
 
+func TestGetV1DependencyReader(t *testing.T) {
+	t.Run("wrapped v1 reader", func(t *testing.T) {
+		reader := new(dependencystoremocks.Reader)
+		traceReader := &DependencyReader{
+			reader: reader,
+		}
+		v1Reader := GetV1DependencyReader(traceReader)
+		require.Equal(t, reader, v1Reader)
+	})
+
+	t.Run("native v2 reader", func(t *testing.T) {
+		reader := new(depstoremocks.Reader)
+		v1Reader := GetV1DependencyReader(reader)
+		require.IsType(t, &DowngradedDependencyReader{}, v1Reader)
+		require.Equal(t, reader, v1Reader.(*DowngradedDependencyReader).reader)
+	})
+}
+
 func TestDependencyReader_GetDependencies(t *testing.T) {
 	end := time.Now()
 	start := end.Add(-1 * time.Minute)
