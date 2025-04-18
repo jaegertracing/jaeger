1759b0a233f16f788b71de35c0e59d7172f5bd3f
Manik2708 <mehtamanik96@gmail.com>
2025-04-18 22:47:38 +0530

    cleanup
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

--- internal/storage/v2/memory/tenant.go
+++ internal/storage/v2/memory/tenant.go
@@ -44,34 +44,34 @@ func (t *Tenant) storeService(serviceName string) {
 func (t *Tenant) storeOperation(serviceName string, operation tracestore.Operation) {
 	t.Lock()
 	defer t.Unlock()
-	if _, ok := t.operations[serviceName]; !ok {
-		t.operations[serviceName] = make(map[tracestore.Operation]struct{})
+	if _, ok := t.operations[serviceName]; ok {
 		t.operations[serviceName][operation] = struct{}{}
-	} else {
-		t.operations[serviceName][operation] = struct{}{}
+		return
 	}
+	t.operations[serviceName] = make(map[tracestore.Operation]struct{})
+	t.operations[serviceName][operation] = struct{}{}
 }
 
 func (t *Tenant) storeTraces(traceId pcommon.TraceID, resourceSpanSlice ptrace.ResourceSpansSlice) {
 	t.Lock()
 	defer t.Unlock()
-	if foundTraces, ok := t.traces[traceId]; !ok {
-		traces := ptrace.NewTraces()
-		resourceSpanSlice.MoveAndAppendTo(traces.ResourceSpans())
-		t.traces[traceId] = traces
-		// if we have a limit, let's cleanup the oldest traces
-		if t.config.MaxTraces > 0 {
-			// we only have to deal with this slice if we have a limit
-			t.evict = (t.evict + 1) % t.config.MaxTraces
-			// do we have an item already on this position? if so, we are overriding it,
-			// and we need to remove from the map
-			if !t.ids[t.evict].IsEmpty() {
-				delete(t.traces, t.ids[t.evict])
-			}
-			// update the ring with the trace id
-			t.ids[t.evict] = traceId
-		}
-	} else {
+	if foundTraces, ok := t.traces[traceId]; ok {
 		resourceSpanSlice.MoveAndAppendTo(foundTraces.ResourceSpans())
+		return
+	}
+	traces := ptrace.NewTraces()
+	resourceSpanSlice.MoveAndAppendTo(traces.ResourceSpans())
+	t.traces[traceId] = traces
+	// if we have a limit, let's cleanup the oldest traces
+	if t.config.MaxTraces > 0 {
+		// we only have to deal with this slice if we have a limit
+		t.evict = (t.evict + 1) % t.config.MaxTraces
+		// do we have an item already on this position? if so, we are overriding it,
+		// and we need to remove from the map
+		if !t.ids[t.evict].IsEmpty() {
+			delete(t.traces, t.ids[t.evict])
+		}
+		// update the ring with the trace id
+		t.ids[t.evict] = traceId
 	}
 }
