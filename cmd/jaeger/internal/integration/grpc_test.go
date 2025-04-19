e8977112f339deebec09354f0689c121d564e5b3
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-19 11:24:37 -0400

    Remove GRPC Storage Integration Struct
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/jaeger/internal/integration/grpc_test.go
+++ cmd/jaeger/internal/integration/grpc_test.go
@@ -7,30 +7,7 @@ import (
 	"testing"
 
 	"github.com/jaegertracing/jaeger/internal/storage/integration"
-	"github.com/jaegertracing/jaeger/ports"
 )
-
-type GRPCStorageIntegration struct {
-	E2EStorageIntegration
-
-	remoteStorage        *integration.RemoteMemoryStorage
-	archiveRemoteStorage *integration.RemoteMemoryStorage
-}
-
-func (s *GRPCStorageIntegration) initializeRemoteStorages(t *testing.T) {
-	s.remoteStorage = integration.StartNewRemoteMemoryStorage(t, ports.RemoteStorageGRPC)
-	s.archiveRemoteStorage = integration.StartNewRemoteMemoryStorage(t, ports.RemoteStorageGRPC+1)
-}
-
-func (s *GRPCStorageIntegration) closeRemoteStorages(t *testing.T) {
-	s.remoteStorage.Close(t)
-	s.archiveRemoteStorage.Close(t)
-}
-
-func (s *GRPCStorageIntegration) cleanUp(t *testing.T) {
-	s.closeRemoteStorages(t)
-	s.initializeRemoteStorages(t)
-}
 
 func TestGRPCStorage(t *testing.T) {
 	integration.SkipUnlessEnv(t, "grpc")
