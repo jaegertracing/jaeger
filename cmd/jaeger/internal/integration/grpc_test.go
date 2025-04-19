3f22ca925ab1b783f78857d92fe608d0c3947b78
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-04-19 17:03:09 -0400

    Add Storage Cleaner For Memory Storage
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/jaeger/internal/integration/grpc_test.go
+++ cmd/jaeger/internal/integration/grpc_test.go
@@ -13,10 +13,12 @@ func TestGRPCStorage(t *testing.T) {
 	integration.SkipUnlessEnv(t, "grpc")
 
 	remoteBackend := &E2EStorageIntegration{
-		ConfigFile:         "../../config-remote-storage-backend.yaml",
-		SkipStorageCleaner: true,
-		HealthCheckPort:    12133,
-		MetricsPort:        8887,
+		ConfigFile:      "../../config-remote-storage-backend.yaml",
+		HealthCheckPort: 12133,
+		MetricsPort:     8887,
+		StorageIntegration: integration.StorageIntegration{
+			CleanUp: purge,
+		},
 	}
 	remoteBackend.e2eInitialize(t, "memory")
 	t.Log("Remote backend initialized")
@@ -24,6 +26,10 @@ func TestGRPCStorage(t *testing.T) {
 	collector := &E2EStorageIntegration{
 		ConfigFile:         "../../config-remote-storage.yaml",
 		SkipStorageCleaner: true,
+		EnvVarOverrides: map[string]string{
+			"JAEGER_QUERY_GRPC_ENDPOINT": "localhost:0",
+			"JAEGER_QUERY_HTTP_ENDPOINT": "localhost:0",
+		},
 	}
 	collector.e2eInitialize(t, "grpc")
 	t.Log("Collector initialized")
