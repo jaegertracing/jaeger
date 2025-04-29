2a3fcf37f85fa4d35560193feef2ec8daf049b08
avinash <avinashx36@gmail.com>
2025-04-29 20:45:35 +0530

    added otel kafkaexporter
    Signed-off-by: avinash <avinashx36@gmail.com>

--- internal/storage/v1/kafka/writer.go
+++ internal/storage/v1/kafka/writer.go
@@ -5,13 +5,12 @@ package kafka
 
 import (
 	"context"
-	"errors"
 
-	"github.com/Shopify/sarama"
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger-idl/model/v1"
 	"github.com/jaegertracing/jaeger/internal/metrics"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/v1adapter"
 )
 
@@ -23,14 +22,14 @@ type spanWriterMetrics struct {
 // SpanWriter writes spans to kafka. Implements spanstore.Writer
 type SpanWriter struct {
 	metrics    spanWriterMetrics
-	producer   sarama.AsyncProducer
+	producer   tracestore.Writer
 	marshaller Marshaller
 	topic      string
 }
 
 // NewSpanWriter initiates and returns a new kafka spanwriter
 func NewSpanWriter(
-	producer sarama.AsyncProducer,
+	producer tracestore.Writer,
 	marshaller Marshaller,
 	topic string,
 	factory metrics.Factory,
@@ -41,20 +40,6 @@ func NewSpanWriter(
 		SpansWrittenFailure: factory.Counter(metrics.Options{Name: "kafka_spans_written", Tags: map[string]string{"status": "failure"}}),
 	}
 
-	go func() {
-		for range producer.Successes() {
-			writeMetrics.SpansWrittenSuccess.Inc(1)
-		}
-	}()
-	go func() {
-		for e := range producer.Errors() {
-			if e != nil && e.Err != nil {
-				logger.Error(e.Err.Error())
-			}
-			writeMetrics.SpansWrittenFailure.Inc(1)
-		}
-	}()
-
 	return &SpanWriter{
 		producer:   producer,
 		marshaller: marshaller,
@@ -64,35 +49,22 @@ func NewSpanWriter(
 }
 
 // WriteSpan writes the span to kafka.
-func (w *SpanWriter) WriteSpan(_ context.Context, span *model.Span) error {
+func (w *SpanWriter) WriteSpan(ctx context.Context, span *model.Span) error {
 	batch := &model.Batch{
 		Spans: []*model.Span{span},
 	}
 	traces := v1adapter.V1BatchesToTraces([]*model.Batch{batch})
 
-	batches := v1adapter.V1BatchesFromTraces(traces)
-	if len(batches) == 0 || len(batches[0].Spans) == 0 {
-		w.metrics.SpansWrittenFailure.Inc(1)
-		return errors.New("span conversion resulted in empty batch")
-	}
-
-	spanBytes, err := w.marshaller.Marshal(batches[0].Spans[0])
+	// Write using tracestore.Writer interface
+	err := w.producer.WriteTraces(ctx, traces)
 	if err != nil {
 		w.metrics.SpansWrittenFailure.Inc(1)
 		return err
-	}
-
-	// The AsyncProducer accepts messages on a channel and produces them asynchronously
-	// in the background as efficiently as possible
-	w.producer.Input() <- &sarama.ProducerMessage{
-		Topic: w.topic,
-		Key:   sarama.StringEncoder(span.TraceID.String()),
-		Value: sarama.ByteEncoder(spanBytes),
 	}
 	return nil
 }
 
 // Close closes SpanWriter by closing producer
 func (w *SpanWriter) Close() error {
-	return w.producer.Close()
+	return nil
 }
