988cbcf40068c0e9ab13133a5fbffc13bd785ea4
avinash <avinashx36@gmail.com>
2025-05-02 08:04:11 +0000

    kafkaexporter
    Signed-off-by: avinash <avinashx36@gmail.com>

--- internal/storage/v1/kafka/writer.go
+++ internal/storage/v1/kafka/writer.go
@@ -5,12 +5,16 @@ package kafka
 
 import (
 	"context"
+	"errors"
 
+	kafkaexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/kafkaexporter"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger-idl/model/v1"
 	"github.com/jaegertracing/jaeger/internal/metrics"
-	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/v1adapter"
 )
 
@@ -21,50 +25,73 @@ type spanWriterMetrics struct {
 
 // SpanWriter writes spans to kafka. Implements spanstore.Writer
 type SpanWriter struct {
-	metrics    spanWriterMetrics
-	producer   tracestore.Writer
-	marshaller Marshaller
-	topic      string
+	metrics  spanWriterMetrics
+	exporter component.Component
+	pushFunc func(ctx context.Context, td ptrace.Traces) error
 }
 
 // NewSpanWriter initiates and returns a new kafka spanwriter
 func NewSpanWriter(
-	producer tracestore.Writer,
-	marshaller Marshaller,
 	topic string,
 	factory metrics.Factory,
+	brokers []string,
 	logger *zap.Logger,
-) *SpanWriter {
+) (*SpanWriter, error) {
 	writeMetrics := spanWriterMetrics{
 		SpansWrittenSuccess: factory.Counter(metrics.Options{Name: "kafka_spans_written", Tags: map[string]string{"status": "success"}}),
 		SpansWrittenFailure: factory.Counter(metrics.Options{Name: "kafka_spans_written", Tags: map[string]string{"status": "failure"}}),
 	}
 
-	return &SpanWriter{
-		producer:   producer,
-		marshaller: marshaller,
-		topic:      topic,
-		metrics:    writeMetrics,
+	exportercfg := kafkaexporter.NewFactory().CreateDefaultConfig().(*kafkaexporter.Config)
+	exportercfg.Topic = topic
+	exportercfg.Brokers = brokers
+
+	settings := exporter.Settings{
+		ID: component.MustNewID("kafka"),
+		TelemetrySettings: component.TelemetrySettings{
+			Logger:         logger,
+			TracerProvider: nil,
+			MeterProvider:  nil,
+		},
+		BuildInfo: component.NewDefaultBuildInfo(),
 	}
+
+	exp, err := kafkaexporter.NewFactory().CreateTraces(context.Background(), settings, exportercfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SpanWriter{
+		metrics:  writeMetrics,
+		exporter: exp,
+		pushFunc: exp.ConsumeTraces,
+	}, nil
 }
 
 // WriteSpan writes the span to kafka.
 func (w *SpanWriter) WriteSpan(ctx context.Context, span *model.Span) error {
-	batch := &model.Batch{
-		Spans: []*model.Span{span},
-	}
+	batch := &model.Batch{Spans: []*model.Span{span}}
 	traces := v1adapter.V1BatchesToTraces([]*model.Batch{batch})
+	if traces.SpanCount() == 0 {
+		w.metrics.SpansWrittenFailure.Inc(1)
+		return errors.New("trace data conversion resulted in empty trace")
+	}
 
-	// Write using tracestore.Writer interface
-	err := w.producer.WriteTraces(ctx, traces)
+	err := w.pushFunc(ctx, traces)
 	if err != nil {
 		w.metrics.SpansWrittenFailure.Inc(1)
 		return err
 	}
+	w.metrics.SpansWrittenSuccess.Inc(1)
+	// The AsyncProducer accepts messages on a channel and produces them asynchronously
+	// in the background as efficiently as possible
 	return nil
 }
 
 // Close closes SpanWriter by closing producer
 func (w *SpanWriter) Close() error {
+	if w.exporter != nil {
+		return w.exporter.Shutdown(context.Background())
+	}
 	return nil
 }
