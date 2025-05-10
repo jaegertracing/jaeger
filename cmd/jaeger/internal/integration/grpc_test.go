5bb9bd80b3d8f160565e25c7fefca8aad1627b71
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-05-10 18:13:20 -0400

    Add CUSTOM_STORAGE Env Var
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/jaeger/internal/integration/grpc_test.go
+++ cmd/jaeger/internal/integration/grpc_test.go
@@ -4,6 +4,7 @@
 package integration
 
 import (
+	"os"
 	"testing"
 
 	"github.com/jaegertracing/jaeger/internal/storage/integration"
@@ -12,16 +13,20 @@ import (
 func TestGRPCStorage(t *testing.T) {
 	integration.SkipUnlessEnv(t, "grpc")
 
-	remoteBackend := &E2EStorageIntegration{
-		ConfigFile:      "../../config-remote-storage-backend.yaml",
-		HealthCheckPort: 12133,
-		MetricsPort:     8887,
-		EnvVarOverrides: map[string]string{
-			"REMOTE_STORAGE_BACKEND_GRPC_ENDPOINT": "0.0.0.0:4316",
-		},
+	if os.Getenv("CUSTOM_STORAGE") != "true" {
+		remoteBackend := &E2EStorageIntegration{
+			ConfigFile:      "../../config-remote-storage-backend.yaml",
+			HealthCheckPort: 12133,
+			MetricsPort:     8887,
+			EnvVarOverrides: map[string]string{
+				"REMOTE_STORAGE_BACKEND_GRPC_ENDPOINT": "0.0.0.0:4316",
+			},
+		}
+		remoteBackend.e2eInitialize(t, "memory")
+		t.Log("Remote backend initialized")
+	} else {
+		t.Log("Skipping remote backend initialization; CUSTOM_STORAGE is enabled")
 	}
-	remoteBackend.e2eInitialize(t, "memory")
-	t.Log("Remote backend initialized")
 
 	collector := &E2EStorageIntegration{
 		ConfigFile:         "../../config-remote-storage.yaml",
