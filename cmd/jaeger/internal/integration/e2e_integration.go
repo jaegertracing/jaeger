3ba45f0fae36761aeb7b4088064f1578719aee6d
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-05-11 22:45:31 -0400

    [refactor] Allow storage cleaner to be overridden via environment variable  (#7114)
    ## Which problem is this PR solving?
    - Towards #7055 
    
    ## Description of the changes
    - Allows the storage cleaner endpoint to be overridden via the
    `PURGER_ENDPOINT` environment variables
    - Update the README to add instructions on setting the environment
    variable
    
    ## How was this change tested?
    - CI
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- cmd/jaeger/internal/integration/e2e_integration.go
+++ cmd/jaeger/internal/integration/e2e_integration.go
@@ -215,6 +215,9 @@ func createStorageCleanerConfig(t *testing.T, configFile string, storage string)
 
 func purge(t *testing.T) {
 	addr := fmt.Sprintf("http://0.0.0.0:%s%s", storagecleaner.Port, storagecleaner.URL)
+	if purgerAddr, ok := os.LookupEnv("PURGER_ENDPOINT"); ok {
+		addr = purgerAddr
+	}
 	t.Logf("Purging storage via %s", addr)
 	r, err := http.NewRequestWithContext(context.Background(), http.MethodPost, addr, nil)
 	require.NoError(t, err)
