48896f0e2b72e4dd1f2a9ddedde0238934925be1
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-22 16:27:01 +0300

    feat: add the implemntation for negative duration sanitizer
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ cmd/collector/app/sanitizer/negative_duration_santizer_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2025 The Jaeger Authors.
+
+package sanitizer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jaegertracing/jaeger-idl/model/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNegativeDurationSanitizer(t *testing.T) {
+	sanitizer := NewNegativeDurationSanitizer()
+
+	tests := []struct {
+		name     string
+		input    *model.Span
+		expected time.Duration
+	}{
+		{
+			name: "Negative duration",
+			input: &model.Span{
+				Duration: -42,
+			},
+			expected: minDuration,
+		},
+		{
+			name: "Zero duration",
+			input: &model.Span{
+				Duration: 0,
+			},
+			expected: minDuration,
+		},
+		{
+			name: "Positive duration",
+			input: &model.Span{
+				Duration: 1000,
+			},
+			expected: 1000,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := sanitizer(test.input)
+			assert.Equal(t, test.expected, actual.Duration)
+		})
+	}
+}
