c8f2a4edaaf33f9ad748e63629d07b4e99634e10
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-22 16:27:01 +0300

    feat: add the implemntation for negative duration sanitizer
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- cmd/collector/app/sanitizer/negative_duration_sanitizer.go
+++ cmd/collector/app/sanitizer/negative_duration_sanitizer.go
@@ -3,7 +3,13 @@
 package sanitizer
 
 import (
+	"time"
+
 	"github.com/jaegertracing/jaeger-idl/model/v1"
+)
+
+const (
+	minDuration = time.Duration(1)
 )
 
 // negativeDurationSanitizer sanitizes all negative durations to a default value.
@@ -17,5 +23,8 @@ func NewNegativeDurationSanitizer() SanitizeSpan {
 
 // Sanitize sanitizes the spans with negative durations.
 func (s *negativeDurationSanitizer) Sanitize(span *model.Span) *model.Span {
+	if span.Duration < minDuration {
+		span.Duration = minDuration
+	}
 	return span
 }
