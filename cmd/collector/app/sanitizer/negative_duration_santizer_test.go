e1a53985aaaa9b1e4e92db91d3c7445f5f204362
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-23 10:20:54 +0300

    chore: fmt
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- cmd/collector/app/sanitizer/negative_duration_santizer_test.go
+++ cmd/collector/app/sanitizer/negative_duration_santizer_test.go
@@ -6,8 +6,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/jaegertracing/jaeger-idl/model/v1"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/jaegertracing/jaeger-idl/model/v1"
 )
 
 func TestNegativeDurationSanitizer(t *testing.T) {
