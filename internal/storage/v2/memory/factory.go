5c2e77b763c0edcf804dba6061a5f3a51c143175
Manik2708 <mehtamanik96@gmail.com>
2025-05-25 01:20:08 +0530

    [v2] Implement factory for memory backend
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

---
+++ internal/storage/v2/memory/factory.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package memory
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/jaegertracing/jaeger/internal/distributedlock"
+	"github.com/jaegertracing/jaeger/internal/storage/v1"
+	"github.com/jaegertracing/jaeger/internal/storage/v1/api/samplingstore"
+	v1 "github.com/jaegertracing/jaeger/internal/storage/v1/memory"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+)
+
+var (
+	_ tracestore.Factory           = (*Factory)(nil)
+	_ storage.SamplingStoreFactory = (*Factory)(nil)
+	_ storage.Purger               = (*Factory)(nil)
+)
+
+type Factory struct {
+	logger *zap.Logger
+	store  *Store
+}
+
+func NewFactory(cfg v1.Configuration, logger *zap.Logger) (*Factory, error) {
+	store, err := NewStore(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &Factory{
+		store:  store,
+		logger: logger,
+	}, nil
+}
+
+func (f *Factory) CreateTraceReader() (tracestore.Reader, error) {
+	return f.store, nil
+}
+
+func (f *Factory) CreateTraceWriter() (tracestore.Writer, error) {
+	return f.store, nil
+}
+
+func (f *Factory) CreateDependencyReader() (depstore.Reader, error) {
+	return f.store, nil
+}
+
+func (*Factory) CreateSamplingStore(buckets int) (samplingstore.Store, error) {
+	return v1.NewSamplingStore(buckets), nil
+}
+
+func (*Factory) CreateLock() (distributedlock.Lock, error) {
+	return &v1.Lock{}, nil
+}
+
+func (f *Factory) Purge(_ context.Context) error {
+	return f.store.Purge()
+}
---
+++ internal/storage/v2/memory/factory_test.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package memory
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+
+	v1 "github.com/jaegertracing/jaeger/internal/storage/v1/memory"
+)
+
+func TestNewFactory(t *testing.T) {
+	f, err := NewFactory(v1.Configuration{
+		MaxTraces: 10,
+	}, zaptest.NewLogger(t))
+	require.NoError(t, err)
+	_, err = f.CreateTraceWriter()
+	require.NoError(t, err)
+	_, err = f.CreateTraceReader()
+	require.NoError(t, err)
+	_, err = f.CreateDependencyReader()
+	require.NoError(t, err)
+	_, err = f.CreateSamplingStore(5)
+	require.NoError(t, err)
+	_, err = f.CreateLock()
+	require.NoError(t, err)
+	require.NoError(t, f.Purge(context.Background()))
+}
+
+func TestNewFactoryErr(t *testing.T) {
+	f, err := NewFactory(v1.Configuration{}, zaptest.NewLogger(t))
+	require.ErrorContains(t, err, "max traces must be greater than zero")
+	assert.Nil(t, f)
+}
