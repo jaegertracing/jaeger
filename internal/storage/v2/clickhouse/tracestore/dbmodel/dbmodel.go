664dcd6e51813b794231d166144af69be197aa2a
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-06-01 06:38:21 -0400

    Add Helpers For Parsing Nested Types
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/dbmodel/dbmodel.go
+++ internal/storage/v2/clickhouse/tracestore/dbmodel/dbmodel.go
@@ -8,6 +8,7 @@ import "time"
 // Span represents a single row in the ClickHouse `spans` table.
 type Span struct {
 	// --- Span ---
+	// TODO: add attributes
 	ID            string        `ch:"id"`
 	TraceID       string        `ch:"trace_id"`
 	TraceState    string        `ch:"trace_state"`
@@ -16,10 +17,20 @@ type Span struct {
 	Kind          string        `ch:"kind"`
 	StartTime     time.Time     `ch:"start_time"`
 	Duration      time.Duration `ch:"duration"`
-	Events        []Event
-	Links         []Link
-	StatusCode    string `ch:"status_code"`
-	StatusMessage string `ch:"status_message"`
+	StatusCode    string        `ch:"status_code"`
+	StatusMessage string        `ch:"status_message"`
+
+	// --- Nested Types ---
+	// The fields below correspond to ClickHouse Nested columns, which act
+	// like a table inside a cell. The clickhouse-go driver does not support
+	// automatic decoding of Nested types via ScanStruct into these slice
+	// structs directly. Therefore, the raw data for these fields must be
+	// scanned into intermediate types (e.g., []map[string]any), and then
+	// manually decoded into the concrete Go structs defined here.
+	// For this reason, these fields do NOT have `ch` tags themselves.
+	// (Ref: https://github.com/ClickHouse/clickhouse-go/blob/main/examples/clickhouse_api/nested.go)
+	Events []Event
+	Links  []Link
 
 	// --- Resource ---
 	// TODO: add attributes
@@ -38,10 +49,51 @@ type Link struct {
 	TraceState string
 }
 
+func getLinksFromRaw(raw []map[string]any) []Link {
+	links := make([]Link, 0, len(raw))
+	for _, m := range raw {
+		links = append(links, getLinkFromRaw(m))
+	}
+	return links
+}
+
+func getLinkFromRaw(m map[string]any) Link {
+	var link Link
+	if traceID, ok := m["trace_id"].(string); ok {
+		link.TraceID = traceID
+	}
+	if spanID, ok := m["span_id"].(string); ok {
+		link.SpanID = spanID
+	}
+	if traceState, ok := m["trace_state"].(string); ok {
+		link.TraceState = traceState
+	}
+	return link
+}
+
 type Event struct {
 	// TODO: add attributes
 	Name      string
 	Timestamp time.Time
+}
+
+func getEventsFromRaw(raw []map[string]any) []Event {
+	events := make([]Event, 0, len(raw))
+	for _, m := range raw {
+		events = append(events, getEventFromRaw(m))
+	}
+	return events
+}
+
+func getEventFromRaw(m map[string]any) Event {
+	var event Event
+	if name, ok := m["name"].(string); ok {
+		event.Name = name
+	}
+	if ts, ok := m["timestamp"].(time.Time); ok {
+		event.Timestamp = ts
+	}
+	return event
 }
 
 type Service struct {
