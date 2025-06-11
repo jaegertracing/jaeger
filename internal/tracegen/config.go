bdb4b661310cdc858ffd2155024891ff97309c65
cocobond <A1750859115@outlook.com>
2025-06-11 11:14:24 +0800

    [tracegen] Add tracegen static generation (#7202) --- Signed-off-by: Xu YuSong<a1750859115@outlook.com>

--- internal/tracegen/config.go
+++ internal/tracegen/config.go
@@ -4,32 +4,38 @@
 package tracegen
 
 import (
+	"context"
+	"encoding/binary"
 	"errors"
 	"flag"
+	"math/rand"
 	"sync"
 	"sync/atomic"
 	"time"
 
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 // Config describes the test scenario.
 type Config struct {
-	Workers       int
-	Services      int
-	Traces        int
-	ChildSpans    int
-	Attributes    int
-	AttrKeys      int
-	AttrValues    int
-	Marshal       bool
-	Debug         bool
-	Firehose      bool
-	Pause         time.Duration
-	Duration      time.Duration
-	Service       string
-	TraceExporter string
+	Workers         int
+	Services        int
+	Traces          int
+	ChildSpans      int
+	Attributes      int
+	AttrKeys        int
+	AttrValues      int
+	Marshal         bool
+	Debug           bool
+	Firehose        bool
+	Pause           time.Duration
+	Duration        time.Duration
+	Service         string
+	TraceExporter   string
+	Static          bool
+	StaticTimeStart string
 }
 
 // Flags registers config flags.
@@ -47,6 +53,8 @@ func (c *Config) Flags(fs *flag.FlagSet) {
 	fs.StringVar(&c.Service, "service", "tracegen", "Service name prefix to use")
 	fs.IntVar(&c.Services, "services", 1, "Number of unique suffixes to add to service name when generating traces, e.g. tracegen-01 (but only one service per trace)")
 	fs.StringVar(&c.TraceExporter, "trace-exporter", "otlp-http", "Trace exporter (otlp/otlp-http|otlp-grpc|stdout). Exporters can be additionally configured via environment variables, see https://github.com/jaegertracing/jaeger/blob/main/cmd/tracegen/README.md")
+	fs.BoolVar(&c.Static, "static", false, "tmp: support generated static data to suit some Trace Bench, default: false")
+	fs.StringVar(&c.StaticTimeStart, "static-timestart", "2025-01-01T00:00:00.000000000+0000", "tmp: support generated static data to suit some Trace Bench. default&eg.: 2025-01-01T00:00:00.000000+00:00")
 }
 
 // Run executes the test scenario.
@@ -59,23 +67,108 @@ func Run(c *Config, tracers []trace.Tracer, logger *zap.Logger) error {
 
 	wg := sync.WaitGroup{}
 	var running uint32 = 1
-	for i := 0; i < c.Workers; i++ {
-		wg.Add(1)
-		w := worker{
-			id:      i,
-			tracers: tracers,
-			Config:  *c,
-			running: &running,
-			wg:      &wg,
-			logger:  logger.With(zap.Int("worker", i)),
+	if !c.Static {
+		for i := 0; i < c.Workers; i++ {
+			wg.Add(1)
+			w := worker{
+				id:      i,
+				tracers: tracers,
+				Config:  *c,
+				running: &running,
+				wg:      &wg,
+				logger:  logger.With(zap.Int("worker", i)),
+			}
+			go w.simulateTraces()
 		}
-
-		go w.simulateTraces()
+	} else {
+		for i := 0; i < c.Workers; i++ {
+			wg.Add(1)
+			w := worker{
+				id:      i,
+				tracers: tracers,
+				Config:  *c,
+				running: &running,
+				wg:      &wg,
+				logger:  logger.With(zap.Int("worker", i)),
+			}
+			// 使用标准 layout 解析
+			layout := "2006-01-02T15:04:05.000000000+0000"
+			t, err := time.Parse(layout, c.StaticTimeStart)
+			if err != nil {
+				panic("Static time start is not valid: " + err.Error() + "  check this flag format, default&eg.: 2025-01-01T00:00:00.000000+00:00")
+			}
+			// mark
+			startTime := t.Add(time.Microsecond)
+			go w.simulateStaticTraces(startTime)
+		}
 	}
+
 	if c.Duration > 0 {
 		time.Sleep(c.Duration)
 		atomic.StoreUint32(&running, 0)
 	}
 	wg.Wait()
 	return nil
+}
+
+// PseudoRandomIDGenerator For static data generation
+type PseudoRandomIDGenerator struct {
+	mu  sync.Mutex
+	rng *rand.Rand
+}
+
+// NewPseudoRandomIDGenerator  The Nth TracerProvider has a pseudo seed of N. / 应该改成可指定
+func NewPseudoRandomIDGenerator(seed int) sdktrace.IDGenerator {
+	src := rand.NewSource(int64(seed))
+	return &PseudoRandomIDGenerator{
+		rng: rand.New(src),
+	}
+}
+
+func (g *PseudoRandomIDGenerator) NewIDs(ctx context.Context) (trace.TraceID, trace.SpanID) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	tid := trace.TraceID{}
+	sid := trace.SpanID{}
+
+	for {
+		binary.NativeEndian.PutUint64(tid[:8], g.rng.Uint64())
+		binary.NativeEndian.PutUint64(tid[8:], g.rng.Uint64())
+		if tid.IsValid() {
+			break
+		}
+	}
+	if !tid.IsValid() {
+		tid[0] = 1
+	}
+
+	for {
+		binary.NativeEndian.PutUint64(sid[:], g.rng.Uint64())
+		if sid.IsValid() {
+			break
+		}
+	}
+	if !sid.IsValid() {
+		sid[0] = 1
+	}
+
+	return tid, sid
+}
+
+func (g *PseudoRandomIDGenerator) NewSpanID(ctx context.Context, traceID trace.TraceID) trace.SpanID {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	sid := trace.SpanID{}
+	for {
+		binary.NativeEndian.PutUint64(sid[:], g.rng.Uint64())
+		if sid.IsValid() {
+			break
+		}
+	}
+	if !sid.IsValid() {
+		sid[0] = 1
+	}
+	return sid
 }
