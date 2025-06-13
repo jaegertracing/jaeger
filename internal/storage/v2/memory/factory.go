159f231f235d5135f1f810b11ba0fe50e359195f
Manik2708 <mehtamanik96@gmail.com>
2025-06-13 23:48:19 +0530

    metrics
    Signed-off-by: Manik2708 <mehtamanik96@gmail.com>

--- internal/storage/v2/memory/factory.go
+++ internal/storage/v2/memory/factory.go
@@ -7,11 +7,14 @@ import (
 	"context"
 
 	"github.com/jaegertracing/jaeger/internal/distributedlock"
+	"github.com/jaegertracing/jaeger/internal/metrics"
 	"github.com/jaegertracing/jaeger/internal/storage/v1"
 	"github.com/jaegertracing/jaeger/internal/storage/v1/api/samplingstore"
 	v1 "github.com/jaegertracing/jaeger/internal/storage/v1/memory"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore/tracestoremetrics"
+	"github.com/jaegertracing/jaeger/internal/telemetry"
 )
 
 var (
@@ -21,21 +24,23 @@ var (
 )
 
 type Factory struct {
-	store *Store
+	store          *Store
+	metricsFactory metrics.Factory
 }
 
-func NewFactory(cfg v1.Configuration) (*Factory, error) {
+func NewFactory(cfg v1.Configuration, settings telemetry.Settings) (*Factory, error) {
 	store, err := NewStore(cfg)
 	if err != nil {
 		return nil, err
 	}
 	return &Factory{
-		store: store,
+		store:          store,
+		metricsFactory: settings.Metrics,
 	}, nil
 }
 
 func (f *Factory) CreateTraceReader() (tracestore.Reader, error) {
-	return f.store, nil
+	return tracestoremetrics.NewReaderDecorator(f.store, f.metricsFactory), nil
 }
 
 func (f *Factory) CreateTraceWriter() (tracestore.Writer, error) {
