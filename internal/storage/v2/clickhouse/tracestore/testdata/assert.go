15b118b1f737c3dc77b71c54887a6af350bcd184
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-06-19 09:42:02 -0400

    Add Test Fixture For Multiple Spans
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/testdata/assert.go
+++ internal/storage/v2/clickhouse/tracestore/testdata/assert.go
@@ -29,8 +29,8 @@ func RequireSpanEqual(t *testing.T, expected SpanRow, actual ptrace.Span) {
 	require.Equal(t, expected.Name, actual.Name())
 	require.Equal(t, expected.Kind, actual.Kind().String())
 	require.Equal(t, expected.StartTime.UnixNano(), actual.StartTimestamp().AsTime().UnixNano())
-	require.Equal(t, expected.StatusCode, actual.Status().Code().String(), "status code mismatch")
-	require.Equal(t, expected.StatusMessage, actual.Status().Message(), "status message mismatch")
+	require.Equal(t, expected.StatusCode, actual.Status().Code().String())
+	require.Equal(t, expected.StatusMessage, actual.Status().Message())
 	require.Equal(t, time.Duration(expected.RawDuration), actual.EndTimestamp().AsTime().Sub(actual.StartTimestamp().AsTime()))
 
 	require.Equal(t, actual.Events().Len(), len(expected.EventNames))
@@ -44,5 +44,25 @@ func RequireSpanEqual(t *testing.T, expected SpanRow, actual ptrace.Span) {
 		require.Equal(t, expected.LinkTraceIDs[i], l.TraceID().String())
 		require.Equal(t, expected.LinkSpanIDs[i], l.SpanID().String())
 		require.Equal(t, expected.LinkTraceStates[i], l.TraceState().AsRaw())
+	}
+}
+
+func RequireTracesEqual(t *testing.T, expected []SpanRow, actual []ptrace.Traces) {
+	t.Helper()
+
+	require.Len(t, actual, len(expected))
+
+	for i, e := range expected {
+		resources := actual[i].ResourceSpans()
+		require.Equal(t, 1, resources.Len())
+
+		scopes := resources.At(0).ScopeSpans()
+		require.Equal(t, 1, scopes.Len())
+		RequireScopeEqual(t, e, scopes.At(0).Scope())
+
+		spans := scopes.At(0).Spans()
+		require.Equal(t, 1, spans.Len())
+
+		RequireSpanEqual(t, e, spans.At(0))
 	}
 }
