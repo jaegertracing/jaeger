b90ee7db6dfedcf29af36e348926dd7e5bf501c0
Parship Chowdhury <i.am.parship@gmail.com>
2025-07-22 06:33:33 +0000

    all related files and imports removed
    Signed-off-by: Parship Chowdhury <i.am.parship@gmail.com>

--- cmd/all-in-one/setupcontext/setupcontext.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019 The Jaeger Authors.
-// SPDX-License-Identifier: Apache-2.0
-
-package setupcontext
-
-// SetAllInOne sets the internal flag to all in one on.
-func SetAllInOne() {
-	// This function is kept for compatibility but no longer stores state
-}
