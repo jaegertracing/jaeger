44388c7669a73df40a1a1e5dd9772b2268ad5780
Sujal Shah <sujalshah28092004@gmail.com>
2025-07-30 11:26:18 +0530

    deep_dependencies: Add `/deep-dependencies` endpoint
    This commit introduce new endpint with fake
    deep dependencies data.
    
    Resolves #6606.
    
    Signed-off-by: Sujal Shah <sujalshah28092004@gmail.com>

--- cmd/query/app/http_handler.go
+++ cmd/query/app/http_handler.go
@@ -22,6 +22,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/jaegertracing/jaeger-idl/model/v1"
+	deepdependencies "github.com/jaegertracing/jaeger/cmd/query/app/deepDependencies"
 	"github.com/jaegertracing/jaeger/cmd/query/app/qualitymetrics"
 	"github.com/jaegertracing/jaeger/cmd/query/app/querysvc"
 	"github.com/jaegertracing/jaeger/internal/jtracer"
@@ -118,6 +119,7 @@ func (aH *APIHandler) RegisterRoutes(router *mux.Router) {
 	aH.handleFunc(router, aH.getOperationsLegacy, "/services/{%s}/operations", serviceParam).Methods(http.MethodGet)
 	aH.handleFunc(router, aH.transformOTLP, "/transform").Methods(http.MethodPost)
 	aH.handleFunc(router, aH.dependencies, "/dependencies").Methods(http.MethodGet)
+	aH.handleFunc(router, aH.deepDependencies, "/deep-dependencies").Methods(http.MethodGet)
 	aH.handleFunc(router, aH.latencies, "/metrics/latencies").Methods(http.MethodGet)
 	aH.handleFunc(router, aH.calls, "/metrics/calls").Methods(http.MethodGet)
 	aH.handleFunc(router, aH.errors, "/metrics/errors").Methods(http.MethodGet)
@@ -308,6 +310,12 @@ func (aH *APIHandler) dependencies(w http.ResponseWriter, r *http.Request) {
 		Data: aH.deduplicateDependencies(filteredDependencies),
 	}
 	aH.writeJSON(w, r, &structuredRes)
+}
+
+func (aH *APIHandler) deepDependencies(w http.ResponseWriter, r *http.Request) {
+	focalService := r.URL.Query().Get("service")
+	data := deepdependencies.GetData(focalService)
+	aH.writeJSON(w, r, &data)
 }
 
 func (aH *APIHandler) latencies(w http.ResponseWriter, r *http.Request) {
