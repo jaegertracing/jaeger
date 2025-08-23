5d966bfee57930c598b6a7fe8efa287ab5019c72
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2025-08-23 10:51:28 +0530

    test
    Signed-off-by: chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>

--- scripts/build/build-hotrod-image.sh
+++ scripts/build/build-hotrod-image.sh
@@ -133,8 +133,8 @@ if [[ "${runtime}" == "k8s" ]]; then
 
   echo '::group:: run on Kubernetes'
   echo '::group:: Loading images into Kind cluster'
-  docker tag localhost:5000/jaegertracing/all-in-one:latest jaegertracing/all-in-one:latest
-  docker tag localhost:5000/jaegertracing/example-hotrod:latest jaegertracing/example-hotrod:latest
+  docker tag localhost:5000/jaegertracing/all-in-one:${GITHUB_SHA} jaegertracing/all-in-one:latest
+  docker tag localhost:5000/jaegertracing/example-hotrod:${GITHUB_SHA} jaegertracing/example-hotrod:latest
   kind load docker-image jaegertracing/all-in-one:latest --name kind
   kind load docker-image jaegertracing/example-hotrod:latest --name kind
   
