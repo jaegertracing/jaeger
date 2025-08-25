03761a8b522851f50cadedffb722b16010ff4c46
anmol7344 <anmol7344@gmail.com>
2025-08-25 19:49:32 +0000

    initial commit
    Signed-off-by: anmol7344 <anmol7344@gmail.com>

--- cmd/jaeger/internal/extension/expvar/extension.go
+++ cmd/jaeger/internal/extension/expvar/extension.go
@@ -63,11 +63,15 @@ func (c *expvarExtension) Start(ctx context.Context, host component.Host) error 
 }
 
 func (c *expvarExtension) Shutdown(ctx context.Context) error {
-	if c.server != nil {
-		if err := c.server.Shutdown(ctx); err != nil {
+	if c.server == nil {
+		return  nil
+	}
+
+	err := c.server.Shutdown(ctx)
+	c.shutdownWG.Wait()
+
+	if err != nil {
 			return fmt.Errorf("error shutting down expvar server: %w", err)
-		}
-		c.shutdownWG.Wait()
 	}
 	return nil
 }
