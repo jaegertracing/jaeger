b7f49f75e8a0390dedd9955f4441541af3ac96d8
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-06 17:47:19 -0400

    Add Attributes To Span Table
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/schema/schema.sql
+++ internal/storage/v2/clickhouse/schema/schema.sql
@@ -10,6 +10,12 @@ CREATE TABLE IF NOT EXISTS spans (
     status_message String,
     duration Int64,
 
+    bool_attrs Nested (key String, value Bool),
+    double_attrs Nested (key String, value Float64),
+    int_attrs Nested (key String, value Int64),
+    str_attrs Nested (key String, value String),
+    bytes_attrs Nested (key String, value String)
+
     events Nested (
         name String,
         timestamp DateTime64(9)
