bb628658427d0e6a1ae7557c13504d888420ee32
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-13 22:57:18 -0400

    Fix
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/schema/schema.sql
+++ internal/storage/v2/clickhouse/schema/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS spans (
         double_attributes Nested (key String, value Float64),
         int_attributes Nested (key String, value Int64),
         str_attributes Nested (key String, value String),
-        complex_attributes Nested (key String, value String),
+        complex_attributes Nested (key String, value String)
     ),
 
     
