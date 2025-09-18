9c2f55de351aa4b97345fc019f667915b103c8e6
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-18 08:14:38 -0400

    Run Formatter
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/spanrow.go
+++ internal/storage/v2/clickhouse/tracestore/spanrow.go
@@ -1,9 +1,13 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
 package tracestore
 
 import (
 	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+
 	"github.com/jaegertracing/jaeger/internal/storage/v2/clickhouse/tracestore/dbmodel"
 )
 
