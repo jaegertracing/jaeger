2aa70376f2872d1913f1193766ed644b7fdd2f99
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-18 20:38:50 -0400

    Add Basic SQL Script For Inserting Spans
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ internal/storage/v2/clickhouse/sql/spans_insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO spans (
+    id, 
+    trace_id,
+    trace_state, 
+    parent_span_id,
+    name,
+    kind,
+    start_time,
+    status_code,
+    status_message,
+    duration,
+) VALUES (
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+)
