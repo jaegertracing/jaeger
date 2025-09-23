413dcab94a64e2e034abc8b0647cf4cf770241f3
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-23 18:54:16 -0400

    Implement Factory With Minimal Config
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ internal/storage/v2/clickhouse/tracestore/factory.go
@@ -0,0 +1,58 @@
+package tracestore
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+	"github.com/jaegertracing/jaeger/internal/telemetry"
+)
+
+var (
+	_ io.Closer          = (*Factory)(nil)
+	_ tracestore.Factory = (*Factory)(nil)
+)
+
+type Factory struct {
+	config Config
+	telset telemetry.Settings
+	conn   driver.Conn
+}
+
+func NewFactory(cfg Config, telset telemetry.Settings) (*Factory, error) {
+	f := &Factory{
+		config: cfg,
+		telset: telset,
+	}
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: f.config.Addresses,
+		Auth: clickhouse.Auth{
+			Database: f.config.Auth.Database,
+			Username: f.config.Auth.Username,
+			Password: f.config.Auth.Password,
+		},
+		DialTimeout: f.config.DialTimeout,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create ClickHouse connection: %w", err)
+	}
+	f.conn = conn
+	return f, nil
+}
+
+func (f *Factory) CreateTraceReader() (tracestore.Reader, error) {
+	return NewReader(f.conn), nil
+}
+
+func (f *Factory) CreateTraceWriter() (tracestore.Writer, error) {
+	return NewWriter(f.conn), nil
+}
+
+func (f *Factory) Close() error {
+	if err := f.conn.Close(); err != nil {
+		return fmt.Errorf("failed to close ClickHouse connection: %w", err)
+	}
+	return nil
+}
