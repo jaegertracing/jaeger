326e27479df56a73b8869d5e91f95afd89fcaf15
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-23 18:40:21 -0400

    Add Config
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

---
+++ internal/storage/v2/clickhouse/tracestore/config.go
@@ -0,0 +1,19 @@
+package tracestore
+
+type Config struct {
+	// Addresses contains a list of ClickHouse server addresses to connect to.
+	Addresses []string `mapstructure:"addresses"`
+	// Auth contains the authentication configuration to connect to ClickHouse.
+	Auth AuthConfig `mapstructure:"auth"`
+	// DialTimeout is the timeout for establishing a connection to ClickHouse in seconds.
+	DialTimeout int `mapstructure:"dial_timeout"`
+}
+
+type AuthConfig struct {
+	// Database is the ClickHouse database to connect to.
+	Database string `mapstructure:"database"`
+	// Username is the username to connect to ClickHouse.
+	Username string `mapstructure:"username"`
+	// Password is the password to connect to ClickHouse.
+	Password string `mapstructure:"password"`
+}
