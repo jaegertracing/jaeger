94b53131edf1ad69b65ade4b4dfce2615c8db7a5
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-24 20:33:26 -0400

    Expose Protocol In Config
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/config.go
+++ internal/storage/v2/clickhouse/tracestore/config.go
@@ -5,11 +5,16 @@ package tracestore
 
 import (
 	"time"
+
+	"github.com/asaskevich/govalidator"
 )
 
 type Config struct {
+	// Protocol is the protocol to use to connect to ClickHouse.
+	// Supported values are "native" and "http". Default is "native".
+	Protocol string `mapstructure:"protocol" valid:"in(native,http),optional"`
 	// Addresses contains a list of ClickHouse server addresses to connect to.
-	Addresses []string `mapstructure:"addresses"`
+	Addresses []string `mapstructure:"addresses" valid:"required"`
 	// Auth contains the authentication configuration to connect to ClickHouse.
 	Auth AuthConfig `mapstructure:"auth"`
 	// DialTimeout is the timeout for establishing a connection to ClickHouse.
@@ -24,4 +29,9 @@ type AuthConfig struct {
 	Username string `mapstructure:"username"`
 	// Password is the password to connect to ClickHouse.
 	Password string `mapstructure:"password"`
+}
+
+func (cfg *Config) Validate() error {
+	_, err := govalidator.ValidateStruct(cfg)
+	return err
 }
