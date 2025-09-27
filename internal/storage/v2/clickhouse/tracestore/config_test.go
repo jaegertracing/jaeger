eef2fdcea2d82eb801cb57b0c23874413974a73e
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-09-27 18:32:42 -0400

    [clickhouse] Implement factory with minimal configuration (#7518)
    ## Which problem is this PR solving?
    - Towards #7137
    
    ## Description of the changes
    - This PR adds a factory for ClickHouse storage with very minimal
    configuration. In the follow-up PRs, we'll need to do the following:
      - Auto-create schema
      - Expose more customizations for ClickHouse through the configuration
      - Call into the factory from the storage extension
      - Add E2E tests
    
    ## How was this change tested?
    - Added unit tests
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [x] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>
    Signed-off-by: SoumyaRaikwar <somuraik@gmail.com>

---
+++ internal/storage/v2/clickhouse/tracestore/config_test.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package tracestore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		cfg     Config
+		wantErr bool
+	}{
+		{
+			name: "valid config with native protocol",
+			cfg: Config{
+				Protocol:  "native",
+				Addresses: []string{"localhost:9000"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid config with http protocol",
+			cfg: Config{
+				Protocol:  "http",
+				Addresses: []string{"localhost:8123"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid config with empty protocol",
+			cfg: Config{
+				Addresses: []string{"localhost:9000"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid config with multiple addresses",
+			cfg: Config{
+				Protocol:  "native",
+				Addresses: []string{"localhost:9000", "localhost:9001"},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid config with unsupported protocol",
+			cfg: Config{
+				Protocol:  "grpc",
+				Addresses: []string{"localhost:9000"},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid config with empty addresses",
+			cfg: Config{
+				Protocol:  "native",
+				Addresses: []string{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid config with nil addresses",
+			cfg: Config{
+				Protocol: "native",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.cfg.Validate()
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
