19f57ee9a8669b0d391b119c5a3022f598e15563
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-28 15:00:39 -0400

    Fix Factory
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/factory.go
+++ internal/storage/v2/clickhouse/factory.go
@@ -61,8 +61,20 @@ func NewFactory(ctx context.Context, cfg Configuration, telset telemetry.Setting
 		)
 	}
 	if f.config.CreateSchema {
-		if err = conn.Exec(ctx, sql.CreateSchema); err != nil {
-			return nil, fmt.Errorf("failed to create ClickHouse schema: %w", err)
+		if err = conn.Exec(ctx, sql.CreateSpansTable); err != nil {
+			return nil, fmt.Errorf("failed to create spans table: %w", err)
+		}
+		if err = conn.Exec(ctx, sql.CreateServicesTable); err != nil {
+			return nil, fmt.Errorf("failed to create services table: %w", err)
+		}
+		if err = conn.Exec(ctx, sql.CreateServicesMaterializedView); err != nil {
+			return nil, fmt.Errorf("failed to create services materialized view: %w", err)
+		}
+		if err = conn.Exec(ctx, sql.CreateOperationsTable); err != nil {
+			return nil, fmt.Errorf("failed to create operations table: %w", err)
+		}
+		if err = conn.Exec(ctx, sql.CreateOperationsMaterializedView); err != nil {
+			return nil, fmt.Errorf("failed to create operations materialized view: %w", err)
 		}
 	}
 	f.conn = conn
