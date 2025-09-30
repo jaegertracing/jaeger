c71ecbf26dbcb6e0b53ce4aac672c04b48c19301
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-09-30 18:29:45 -0400

    Move ClickHouse Test Server To Separate Package
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/factory_test.go
+++ internal/storage/v2/clickhouse/factory_test.go
@@ -6,62 +6,18 @@ package clickhouse
 import (
 	"context"
 	"errors"
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
-	"github.com/ClickHouse/ch-go/proto"
 	"github.com/ClickHouse/clickhouse-go/v2"
-	chproto "github.com/ClickHouse/clickhouse-go/v2/lib/proto"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/basicauthextension"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/config/configoptional"
 
 	"github.com/jaegertracing/jaeger/internal/storage/v2/clickhouse/sql"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/clickhouse/test"
 	"github.com/jaegertracing/jaeger/internal/telemetry"
 )
-
-var (
-	pingQuery      = "SELECT 1"
-	handshakeQuery = "SELECT displayName(), version(), revision(), timezone()"
-)
-
-type mockFailureConfig map[string]error
-
-func newMockClickHouseServer(failures mockFailureConfig) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, _ := io.ReadAll(r.Body)
-		query := string(body)
-
-		block := chproto.NewBlock()
-
-		if err, shouldFail := failures[query]; shouldFail {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		switch query {
-		case pingQuery:
-			block.AddColumn("1", "UInt8")
-			block.Append(uint8(1))
-		case handshakeQuery:
-			block.AddColumn("displayName()", "String")
-			block.AddColumn("version()", "String")
-			block.AddColumn("revision()", "UInt32")
-			block.AddColumn("timezone()", "String")
-			block.Append("mock-server", "23.3.1", chproto.DBMS_MIN_REVISION_WITH_CUSTOM_SERIALIZATION, "UTC")
-		default:
-		}
-
-		var buf proto.Buffer
-		block.Encode(&buf, clickhouse.ClientTCPProtocolVersion)
-
-		w.Header().Set("Content-Type", "application/octet-stream")
-		w.Write(buf.Buf)
-	}))
-}
 
 func TestFactory(t *testing.T) {
 	tests := []struct {
@@ -80,7 +36,7 @@ func TestFactory(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			srv := newMockClickHouseServer(mockFailureConfig{})
+			srv := test.NewServer(test.FailureConfig{})
 			defer srv.Close()
 
 			cfg := Configuration{
@@ -124,47 +80,47 @@ func TestFactory(t *testing.T) {
 func TestNewFactory_Errors(t *testing.T) {
 	tests := []struct {
 		name          string
-		failureConfig mockFailureConfig
+		failureConfig test.FailureConfig
 		expectedError string
 	}{
 		{
 			name: "ping error",
-			failureConfig: mockFailureConfig{
-				pingQuery: errors.New("ping error"),
+			failureConfig: test.FailureConfig{
+				test.PingQuery: errors.New("ping error"),
 			},
 			expectedError: "failed to ping ClickHouse",
 		},
 		{
 			name: "spans table creation error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.CreateSpansTable: errors.New("spans table creation error"),
 			},
 			expectedError: "failed to create spans table",
 		},
 		{
 			name: "services table creation error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.CreateServicesTable: errors.New("services table creation error"),
 			},
 			expectedError: "failed to create services table",
 		},
 		{
 			name: "services materialized view creation error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.CreateServicesMaterializedView: errors.New("services materialized view creation error"),
 			},
 			expectedError: "failed to create services materialized view",
 		},
 		{
 			name: "operations table creation error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.CreateOperationsTable: errors.New("operations table creation error"),
 			},
 			expectedError: "failed to create operations table",
 		},
 		{
 			name: "operations materialized view creation error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.CreateOperationsMaterializedView: errors.New("operations materialized view creation error"),
 			},
 			expectedError: "failed to create operations materialized view",
@@ -173,7 +129,7 @@ func TestNewFactory_Errors(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			srv := newMockClickHouseServer(tt.failureConfig)
+			srv := test.NewServer(tt.failureConfig)
 			defer srv.Close()
 
 			cfg := Configuration{
@@ -195,26 +151,26 @@ func TestNewFactory_Errors(t *testing.T) {
 func TestPurge(t *testing.T) {
 	tests := []struct {
 		name          string
-		failureConfig mockFailureConfig
+		failureConfig test.FailureConfig
 		expectedError string
 	}{
 		{
 			name: "truncate spans table error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.TruncateSpans: errors.New("truncate spans table error"),
 			},
 			expectedError: "failed to purge spans",
 		},
 		{
 			name: "truncate services table error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.TruncateServices: errors.New("truncate services table error"),
 			},
 			expectedError: "failed to purge services",
 		},
 		{
 			name: "truncate operations table error",
-			failureConfig: mockFailureConfig{
+			failureConfig: test.FailureConfig{
 				sql.TruncateOperations: errors.New("truncate operations table error"),
 			},
 			expectedError: "failed to purge operations",
@@ -223,7 +179,7 @@ func TestPurge(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			srv := newMockClickHouseServer(tt.failureConfig)
+			srv := test.NewServer(tt.failureConfig)
 			defer srv.Close()
 
 			cfg := Configuration{
---
+++ internal/storage/v2/clickhouse/test/server.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2025 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/ClickHouse/ch-go/proto"
+	"github.com/ClickHouse/clickhouse-go/v2"
+	chproto "github.com/ClickHouse/clickhouse-go/v2/lib/proto"
+)
+
+var (
+	PingQuery      = "SELECT 1"
+	HandshakeQuery = "SELECT displayName(), version(), revision(), timezone()"
+)
+
+type FailureConfig map[string]error
+
+// NewServer creates a new HTTP test server that simulates a ClickHouse server.
+// It should only be used in tests.
+func NewServer(failures FailureConfig) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := io.ReadAll(r.Body)
+		query := string(body)
+
+		block := chproto.NewBlock()
+
+		if err, shouldFail := failures[query]; shouldFail {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		switch query {
+		case PingQuery:
+			block.AddColumn("1", "UInt8")
+			block.Append(uint8(1))
+		case HandshakeQuery:
+			block.AddColumn("displayName()", "String")
+			block.AddColumn("version()", "String")
+			block.AddColumn("revision()", "UInt32")
+			block.AddColumn("timezone()", "String")
+			block.Append("mock-server", "23.3.1", chproto.DBMS_MIN_REVISION_WITH_CUSTOM_SERIALIZATION, "UTC")
+		default:
+		}
+
+		var buf proto.Buffer
+		block.Encode(&buf, clickhouse.ClientTCPProtocolVersion)
+
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(buf.Buf)
+	}))
+}
