e563c130d2b2a53c78444d782ca033ef7aaa872b
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2025-10-26 13:50:43 -0400

    [refactor][clickhouse] Add round-trip tests for ClickHouse's `dbmodel` package (#7622)
    Signed-off-by: SoumyaRaikwar <somuraik@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/dbmodel/from_test.go
+++ internal/storage/v2/clickhouse/tracestore/dbmodel/from_test.go
@@ -4,8 +4,8 @@
 package dbmodel
 
 import (
-	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -14,95 +14,19 @@ import (
 	"github.com/jaegertracing/jaeger/internal/jptrace"
 )
 
-func TestFromDBModel_Fixtures(t *testing.T) {
-	dbTrace := jsonToDBModel(t, "./testdata/dbmodel.json")
-	expected := jsonToPtrace(t, "./testdata/ptrace.json")
-	actual := FromRow(dbTrace)
+func TestFromRow(t *testing.T) {
+	now := time.Now().UTC()
+	duration := 2 * time.Second
 
-	require.Equal(t, expected.ResourceSpans().Len(), actual.ResourceSpans().Len(), "ResourceSpans count mismatch")
-	if actual.ResourceSpans().Len() == 0 {
-		t.Fatal("Actual trace contains no ResourceSpans")
-	}
-	expectedResourceSpans := expected.ResourceSpans().At(0)
-	actualResourceSpans := actual.ResourceSpans().At(0)
+	spanRow := createTestSpanRow(now, duration)
 
-	require.Equal(t, expectedResourceSpans.ScopeSpans().Len(), actualResourceSpans.ScopeSpans().Len(), "ScopeSpans count mismatch")
-	if actualResourceSpans.ScopeSpans().Len() == 0 {
-		t.Fatal("Actual ResourceSpans contains no ScopeSpans")
-	}
-	exceptedScopeSpans := expectedResourceSpans.ScopeSpans().At(0)
-	actualScopeSpans := actualResourceSpans.ScopeSpans().At(0)
+	expected := createTestTrace(now, duration)
 
-	require.Equal(t, exceptedScopeSpans.Spans().Len(), actualScopeSpans.Spans().Len(), "Spans count mismatch")
-	if actualScopeSpans.Spans().Len() == 0 {
-		t.Fatal("Actual ScopeSpans contains no Spans")
-	}
-	exceptedSpan := exceptedScopeSpans.Spans().At(0)
-	actualSpan := actualScopeSpans.Spans().At(0)
-
-	t.Run("Resource", func(*testing.T) {
-		// TODO: add resource attributes comparison
-	})
-
-	t.Run("Scope", func(t *testing.T) {
-		actualScope := actualScopeSpans.Scope()
-		expectedScope := exceptedScopeSpans.Scope()
-		require.Equal(t, expectedScope.Name(), actualScope.Name(), "Scope attributes mismatch")
-		require.Equal(t, expectedScope.Version(), actualScope.Version(), "Scope attributes mismatch")
-	})
-
-	t.Run("Span", func(t *testing.T) {
-		require.Equal(t, exceptedSpan.StartTimestamp(), actualSpan.StartTimestamp(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.TraceID(), actualSpan.TraceID(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.SpanID(), actualSpan.SpanID(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.ParentSpanID(), actualSpan.ParentSpanID(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.TraceState(), actualSpan.TraceState(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.Name(), actualSpan.Name(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.Kind(), actualSpan.Kind(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.EndTimestamp(), actualSpan.EndTimestamp(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.Status().Code(), actualSpan.Status().Code(), "Span attributes mismatch")
-		require.Equal(t, exceptedSpan.Status().Message(), actualSpan.Status().Message(), "Span attributes mismatch")
-		exceptedSpan.Attributes().Range(func(k string, v pcommon.Value) bool {
-			actualValue, ok := actualSpan.Attributes().Get(k)
-			require.True(t, ok, "Missing attribute %s", k)
-			require.Equal(t, v, actualValue, "Attribute %s mismatch", k)
-			return true
-		})
-	})
-
-	t.Run("Events", func(t *testing.T) {
-		exceptedEvents := exceptedSpan.Events()
-		actualEvents := actualSpan.Events()
-		require.Equal(t, exceptedEvents.Len(), actualEvents.Len(), "Events count mismatch")
-		for i := 0; i < exceptedEvents.Len(); i++ {
-			exceptedEvent := exceptedEvents.At(i)
-			actualEvent := actualEvents.At(i)
-			require.Equal(t, exceptedEvent.Name(), actualEvent.Name(), "Event attributes mismatch")
-			require.Equal(t, exceptedEvent.Timestamp(), actualEvent.Timestamp(), "Event attributes mismatch")
-			exceptedEvent.Attributes().Range(func(k string, v pcommon.Value) bool {
-				actualValue, ok := actualEvent.Attributes().Get(k)
-				require.True(t, ok, "Missing attribute %s", k)
-				require.Equal(t, v, actualValue, "Attribute %s mismatch", k)
-				return true
-			})
-		}
-	})
-
-	t.Run("Links", func(t *testing.T) {
-		exceptedLinks := exceptedSpan.Links()
-		actualLinks := actualSpan.Links()
-		require.Equal(t, exceptedLinks.Len(), actualLinks.Len(), "Links count mismatch")
-		for i := 0; i < exceptedLinks.Len(); i++ {
-			exceptedLink := exceptedLinks.At(i)
-			actualLink := actualLinks.At(i)
-			require.Equal(t, exceptedLink.TraceID(), actualLink.TraceID(), "Link attributes mismatch")
-			require.Equal(t, exceptedLink.SpanID(), actualLink.SpanID(), "Link attributes mismatch")
-			require.Equal(t, exceptedLink.TraceState(), actualLink.TraceState(), "Link attributes mismatch")
-		}
-	})
+	row := FromRow(spanRow)
+	require.Equal(t, expected, row)
 }
 
-func TestFromDBModel_DecodeID(t *testing.T) {
+func TestFromRow_DecodeID(t *testing.T) {
 	tests := []struct {
 		name string
 		arg  *SpanRow
@@ -184,18 +108,4 @@ func TestPutAttributes_Warnings(t *testing.T) {
 		require.Len(t, warnings, 1)
 		require.Contains(t, warnings[0], "failed to decode bytes attribute \"@bytes@bytes-key\"")
 	})
-}
-
-func jsonToDBModel(t *testing.T, filename string) (m *SpanRow) {
-	traceBytes := readJSONBytes(t, filename)
-	err := json.Unmarshal(traceBytes, &m)
-	require.NoError(t, err, "Failed to read file %s", filename)
-	return m
-}
-
-func jsonToPtrace(t *testing.T, filename string) (trace ptrace.Traces) {
-	unMarshaler := ptrace.JSONUnmarshaler{}
-	trace, err := unMarshaler.UnmarshalTraces(readJSONBytes(t, filename))
-	require.NoError(t, err, "Failed to unmarshal trace with %s", filename)
-	return trace
 }
