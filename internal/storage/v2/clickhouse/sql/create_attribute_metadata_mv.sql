02c16765da3d15dc6b4a30da30b285d8c1bad3fe
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-12-30 13:50:23 -0500

    Address Feedback
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/sql/create_attribute_metadata_mv.sql
+++ internal/storage/v2/clickhouse/sql/create_attribute_metadata_mv.sql
@@ -1,64 +1,39 @@
 CREATE MATERIALIZED VIEW IF NOT EXISTS attribute_metadata_mv TO attribute_metadata AS
 SELECT
-    attribute_key,
-    type
-FROM
-    (
-        SELECT
-            arrayJoin(arrayConcat(bool_attributes.key, resource_bool_attributes.key)) AS attribute_key,
-            'bool' AS type
-        FROM
-            spans
-        WHERE
-            length(bool_attributes.key) > 0 OR length(resource_bool_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(arrayConcat(double_attributes.key, resource_double_attributes.key)) AS attribute_key,
-            'double' AS type
-        FROM
-            spans
-        WHERE
-            length(double_attributes.key) > 0 OR length(resource_double_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(arrayConcat(int_attributes.key, resource_int_attributes.key)) AS attribute_key,
-            'int' AS type
-        FROM
-            spans
-        WHERE
-            length(int_attributes.key) > 0 OR length(resource_int_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(arrayConcat(str_attributes.key, resource_str_attributes.key)) AS attribute_key,
-            'str' AS type
-        FROM
-            spans
-        WHERE
-            length(str_attributes.key) > 0 OR length(resource_str_attributes.key) > 0
-        UNION ALL
-        SELECT
-            CASE
-                WHEN startsWith(key, '@bytes@') THEN substring(key, 8)
-                WHEN startsWith(key, '@map@') THEN substring(key, 6)
-                WHEN startsWith(key, '@slice@') THEN substring(key, 8)
-                ELSE key
-            END AS attribute_key,
-            CASE
-                WHEN startsWith(key, '@bytes@') THEN 'bytes'
-                WHEN startsWith(key, '@map@') THEN 'map'
-                WHEN startsWith(key, '@slice@') THEN 'slice'
-                ELSE ''
-            END AS type
-        FROM
-            (
-                SELECT
-                    arrayJoin(arrayConcat(complex_attributes.key, resource_complex_attributes.key)) AS key
-                FROM
-                    spans
-                WHERE
-                    length(complex_attributes.key) > 0 OR length(resource_complex_attributes.key) > 0
-            )
-    )
-GROUP BY
-    attribute_key,
-    type;
+    tp.1 AS attribute_key,
+    tp.2 AS type,
+    tp.3 AS level
+FROM (
+    SELECT
+        arrayJoin(arrayConcat(
+            arrayMap(k -> (k, 'bool',   'span'),     bool_attributes.key),
+            arrayMap(k -> (k, 'bool',   'resource'), resource_bool_attributes.key),
+            arrayMap(k -> (k, 'bool',   'scope'),    scope_bool_attributes.key),
+            arrayMap(k -> (k, 'double', 'span'),     double_attributes.key),
+            arrayMap(k -> (k, 'double', 'resource'), resource_double_attributes.key),
+            arrayMap(k -> (k, 'double', 'scope'),    scope_double_attributes.key),
+            arrayMap(k -> (k, 'int',    'span'),     int_attributes.key),
+            arrayMap(k -> (k, 'int',    'resource'), resource_int_attributes.key),
+            arrayMap(k -> (k, 'int',    'scope'),    scope_int_attributes.key),
+            arrayMap(k -> (k, 'str',    'span'),     str_attributes.key),
+            arrayMap(k -> (k, 'str',    'resource'), resource_str_attributes.key),
+            arrayMap(k -> (k, 'str',    'scope'),    scope_str_attributes.key),
+            arrayMap(k -> (
+                if(startsWith(k, '@bytes@'), substring(k, 8), if(startsWith(k, '@map@'), substring(k, 6), if(startsWith(k, '@slice@'), substring(k, 8), k))),
+                if(startsWith(k, '@bytes@'), 'bytes', if(startsWith(k, '@map@'), 'map', if(startsWith(k, '@slice@'), 'slice', ''))),
+                'span'
+            ), complex_attributes.key),
+            arrayMap(k -> (
+                if(startsWith(k, '@bytes@'), substring(k, 8), if(startsWith(k, '@map@'), substring(k, 6), if(startsWith(k, '@slice@'), substring(k, 8), k))),
+                if(startsWith(k, '@bytes@'), 'bytes', if(startsWith(k, '@map@'), 'map', if(startsWith(k, '@slice@'), 'slice', ''))),
+                'resource'
+            ), resource_complex_attributes.key),
+            arrayMap(k -> (
+                if(startsWith(k, '@bytes@'), substring(k, 8), if(startsWith(k, '@map@'), substring(k, 6), if(startsWith(k, '@slice@'), substring(k, 8), k))),
+                if(startsWith(k, '@bytes@'), 'bytes', if(startsWith(k, '@map@'), 'map', if(startsWith(k, '@slice@'), 'slice', ''))),
+                'scope'
+            ), scope_complex_attributes.key)
+        )) AS tp
+    FROM spans
+)
+GROUP BY attribute_key, type, level;
--- internal/storage/v2/clickhouse/sql/create_attribute_metadata_table.sql
+++ internal/storage/v2/clickhouse/sql/create_attribute_metadata_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE
     IF NOT EXISTS attribute_metadata (
         attribute_key String,
-        type String  -- 'bool', 'double', 'int', 'string', 'bytes', 'map', 'slice'
+        type String,  -- 'bool', 'double', 'int', 'str', 'bytes', 'map', 'slice'
+        level String  -- 'resource', 'scope', 'span'
     ) ENGINE = ReplacingMergeTree
-ORDER BY (attribute_key, type)
+ORDER BY (attribute_key, type, level)
