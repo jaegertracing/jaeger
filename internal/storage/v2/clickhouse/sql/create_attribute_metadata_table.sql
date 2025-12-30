01626eb12514f5f90741e65f10890585829600b0
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-12-30 10:38:03 -0500

    Use ReplacingMergeTree
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/sql/create_attribute_metadata_table.sql
+++ internal/storage/v2/clickhouse/sql/create_attribute_metadata_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE
     IF NOT EXISTS attribute_metadata (
         attribute_key String,
         type String  -- 'bool', 'double', 'int', 'string', 'bytes', 'map', 'slice'
-    ) ENGINE = MergeTree
-ORDER BY (attribute_key)
+    ) ENGINE = ReplacingMergeTree
+ORDER BY (attribute_key, type)
