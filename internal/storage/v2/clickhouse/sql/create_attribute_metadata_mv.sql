cba0a978bf7fbf1472c546358cab2406af013cb8
Mahad Zaryab <mahadzaryab1@gmail.com>
2025-12-30 10:38:23 -0500

    Remove Number of Unions
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/sql/create_attribute_metadata_mv.sql
+++ internal/storage/v2/clickhouse/sql/create_attribute_metadata_mv.sql
@@ -5,36 +5,36 @@ SELECT
 FROM
     (
         SELECT
-            arrayJoin(bool_attributes.key) AS attribute_key,
+            arrayJoin(arrayConcat(bool_attributes.key, resource_bool_attributes.key)) AS attribute_key,
             'bool' AS type
         FROM
             spans
         WHERE
-            length(bool_attributes.key) > 0
+            length(bool_attributes.key) > 0 OR length(resource_bool_attributes.key) > 0
         UNION ALL
         SELECT
-            arrayJoin(double_attributes.key) AS attribute_key,
+            arrayJoin(arrayConcat(double_attributes.key, resource_double_attributes.key)) AS attribute_key,
             'double' AS type
         FROM
             spans
         WHERE
-            length(double_attributes.key) > 0
+            length(double_attributes.key) > 0 OR length(resource_double_attributes.key) > 0
         UNION ALL
         SELECT
-            arrayJoin(int_attributes.key) AS attribute_key,
+            arrayJoin(arrayConcat(int_attributes.key, resource_int_attributes.key)) AS attribute_key,
             'int' AS type
         FROM
             spans
         WHERE
-            length(int_attributes.key) > 0
+            length(int_attributes.key) > 0 OR length(resource_int_attributes.key) > 0
         UNION ALL
         SELECT
-            arrayJoin(str_attributes.key) AS attribute_key,
+            arrayJoin(arrayConcat(str_attributes.key, resource_str_attributes.key)) AS attribute_key,
             'str' AS type
         FROM
             spans
         WHERE
-            length(str_attributes.key) > 0
+            length(str_attributes.key) > 0 OR length(resource_str_attributes.key) > 0
         UNION ALL
         SELECT
             CASE
@@ -52,66 +52,11 @@ FROM
         FROM
             (
                 SELECT
-                    arrayJoin(complex_attributes.key) AS key
+                    arrayJoin(arrayConcat(complex_attributes.key, resource_complex_attributes.key)) AS key
                 FROM
                     spans
                 WHERE
-                    length(complex_attributes.key) > 0
-            )
-        UNION ALL
-        SELECT
-            arrayJoin(resource_bool_attributes.key) AS attribute_key,
-            'bool' AS type
-        FROM
-            spans
-        WHERE
-            length(resource_bool_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(resource_double_attributes.key) AS attribute_key,
-            'double' AS type
-        FROM
-            spans
-        WHERE
-            length(resource_double_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(resource_int_attributes.key) AS attribute_key,
-            'int' AS type
-        FROM
-            spans
-        WHERE
-            length(resource_int_attributes.key) > 0
-        UNION ALL
-        SELECT
-            arrayJoin(resource_str_attributes.key) AS attribute_key,
-            'str' AS type
-        FROM
-            spans
-        WHERE
-            length(resource_str_attributes.key) > 0
-        UNION ALL
-        SELECT
-            CASE
-                WHEN startsWith(key, '@bytes@') THEN substring(key, 8)
-                WHEN startsWith(key, '@map@') THEN substring(key, 6)
-                WHEN startsWith(key, '@slice@') THEN substring(key, 8)
-                ELSE key
-            END AS attribute_key,
-            CASE
-                WHEN startsWith(key, '@bytes@') THEN 'bytes'
-                WHEN startsWith(key, '@map@') THEN 'map'
-                WHEN startsWith(key, '@slice@') THEN 'slice'
-                ELSE ''
-            END AS type
-        FROM
-            (
-                SELECT
-                    arrayJoin(resource_complex_attributes.key) AS key
-                FROM
-                    spans
-                WHERE
-                    length(resource_complex_attributes.key) > 0
+                    length(complex_attributes.key) > 0 OR length(resource_complex_attributes.key) > 0
             )
     )
 GROUP BY
