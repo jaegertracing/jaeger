9329045061c41e88e17973b99cb18b0739c18607
spline2hg <181270613+spline2hg@users.noreply.github.com>
2026-01-19 08:20:19 +0530

    Migrate server_test.go to use v2 storage mocks (#7892)
    ## Which problem is this PR solving?
    - Part of #7853 
    
    ## Description of the changes
    - This PR refactors
    `cmd/jaeger/internal/extension/jaegerquery/internal/server_test.go` to
    use v2 storage mocks.
    
    ## How was this change tested?
    - `make test`
    
    ## Checklist
    - [x] I have read
    https://github.com/jaegertracing/jaeger/blob/master/CONTRIBUTING_GUIDELINES.md
    - [x] I have signed all commits
    - [ ] I have added unit tests for the new functionality
    - [x] I have run lint and test steps successfully
      - for `jaeger`: `make lint test`
      - for `jaeger-ui`: `npm run lint` and `npm run test`
    
    ---------
    
    Signed-off-by: spline2hg <181270613+spline2hg@users.noreply.github.com>

--- cmd/jaeger/internal/extension/jaegerquery/internal/server_test.go
+++ cmd/jaeger/internal/extension/jaegerquery/internal/server_test.go
@@ -4,15 +4,14 @@
 package app
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"iter"
 	"net"
 	"net/http"
 	"testing"
 	"time"
 
-	"github.com/gogo/protobuf/jsonpb"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -21,6 +20,7 @@ import (
 	"go.opentelemetry.io/collector/config/confignet"
 	"go.opentelemetry.io/collector/config/configoptional"
 	"go.opentelemetry.io/collector/config/configtls"
+	"go.opentelemetry.io/collector/pdata/ptrace"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	traceapi "go.opentelemetry.io/otel/trace"
@@ -36,9 +36,9 @@ import (
 	"github.com/jaegertracing/jaeger-idl/proto-gen/api_v2"
 	"github.com/jaegertracing/jaeger/cmd/jaeger/internal/extension/jaegerquery/querysvc"
 	"github.com/jaegertracing/jaeger/internal/grpctest"
-	"github.com/jaegertracing/jaeger/internal/storage/v1/api/spanstore"
-	spanstoremocks "github.com/jaegertracing/jaeger/internal/storage/v1/api/spanstore/mocks"
 	depsmocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/depstore/mocks"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+	tracestoremocks "github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore/mocks"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/v1adapter"
 	"github.com/jaegertracing/jaeger/internal/telemetry"
 	"github.com/jaegertracing/jaeger/internal/tenancy"
@@ -315,24 +315,40 @@ var testCases = []struct {
 
 type fakeQueryService struct {
 	qs               *querysvc.QueryService
-	spanReader       *spanstoremocks.Reader
+	traceReader      *tracestoremocks.Reader
 	dependencyReader *depsmocks.Reader
 	expectedServices []string
 }
 
 func makeQuerySvc() *fakeQueryService {
-	spanReader := &spanstoremocks.Reader{}
-	traceReader := v1adapter.NewTraceReader(spanReader)
+	traceReader := &tracestoremocks.Reader{}
 	dependencyReader := &depsmocks.Reader{}
 	expectedServices := []string{"test"}
-	spanReader.On("GetServices", mock.Anything).Return(expectedServices, nil)
+	traceReader.On("GetServices", mock.Anything).Return(expectedServices, nil)
 	qs := querysvc.NewQueryService(traceReader, dependencyReader, querysvc.QueryServiceOptions{})
 	return &fakeQueryService{
 		qs:               qs,
-		spanReader:       spanReader,
+		traceReader:      traceReader,
 		dependencyReader: dependencyReader,
 		expectedServices: expectedServices,
 	}
+}
+
+func makeMockPTrace() ptrace.Traces {
+	trace := ptrace.NewTraces()
+	resources := trace.ResourceSpans().AppendEmpty()
+	resources.Resource().Attributes().PutStr("service.name", "service")
+	scopes := resources.ScopeSpans().AppendEmpty()
+
+	span1 := scopes.Spans().AppendEmpty()
+	span1.SetTraceID(v1adapter.FromV1TraceID(model.NewTraceID(0, 0x123456abc)))
+	span1.SetSpanID(v1adapter.FromV1SpanID(model.NewSpanID(1)))
+
+	span2 := scopes.Spans().AppendEmpty()
+	span2.SetTraceID(v1adapter.FromV1TraceID(model.NewTraceID(0, 0x123456abc)))
+	span2.SetSpanID(v1adapter.FromV1SpanID(model.NewSpanID(2)))
+
+	return trace
 }
 
 func optionalFromPtr[T any](ptr *T) configoptional.Optional[T] {
@@ -409,7 +425,10 @@ func TestServerHTTPTLS(t *testing.T) {
 						TLSClientConfig: clientTLSCfg,
 					},
 				}
-				querySvc.spanReader.On("FindTraces", mock.Anything, mock.Anything).Return([]*model.Trace{mockTrace}, nil).Once()
+				querySvc.traceReader.On("FindTraces", mock.Anything, mock.Anything).
+					Return(iter.Seq2[[]ptrace.Traces, error](func(yield func([]ptrace.Traces, error) bool) {
+						yield([]ptrace.Traces{makeMockPTrace()}, nil)
+					})).Once()
 				queryString := "/api/traces?service=service&start=0&end=0&operation=operation&limit=200&minDuration=20ms"
 				req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%s/%s", server.HTTPAddr(), queryString), http.NoBody)
 				require.NoError(t, err)
@@ -626,9 +645,8 @@ func TestServerGracefulExit(t *testing.T) {
 
 	logger := zap.New(zapCore)
 	telset := initTelSet(logger, nooptrace.NewTracerProvider())
-	spanReader := &spanstoremocks.Reader{}
-	spanReader.On("GetServices", mock.Anything).Return([]string{"test"}, nil)
-	traceReader := v1adapter.NewTraceReader(spanReader)
+	traceReader := &tracestoremocks.Reader{}
+	traceReader.On("GetServices", mock.Anything).Return([]string{"test"}, nil)
 	qs := querysvc.NewQueryService(traceReader, &depsmocks.Reader{}, querysvc.QueryServiceOptions{})
 	server, err := NewServer(context.Background(), qs, nil,
 		&QueryOptions{
@@ -737,7 +755,10 @@ func TestServerHTTPTenancy(t *testing.T) {
 	}
 	tenancyMgr := tenancy.NewManager(&serverOptions.Tenancy)
 	querySvc := makeQuerySvc()
-	querySvc.spanReader.On("FindTraces", mock.Anything, mock.Anything).Return([]*model.Trace{mockTrace}, nil).Once()
+	querySvc.traceReader.On("FindTraces", mock.Anything, mock.Anything).
+		Return(iter.Seq2[[]ptrace.Traces, error](func(yield func([]ptrace.Traces, error) bool) {
+			yield([]ptrace.Traces{makeMockPTrace()}, nil)
+		})).Once()
 	telset := initTelSet(zaptest.NewLogger(t), nooptrace.NewTracerProvider())
 	server, err := NewServer(context.Background(), querySvc.qs,
 		nil, serverOptions, tenancyMgr, telset)
@@ -782,18 +803,6 @@ func TestServerHTTPTenancy(t *testing.T) {
 }
 
 func TestServerHTTP_TracesRequest(t *testing.T) {
-	makeMockTrace := func(t *testing.T) *model.Trace {
-		out := new(bytes.Buffer)
-		err := new(jsonpb.Marshaler).Marshal(out, mockTrace)
-		require.NoError(t, err)
-		var trace model.Trace
-		require.NoError(t, jsonpb.Unmarshal(out, &trace))
-		trace.Spans[1].References = []model.SpanRef{
-			{TraceID: model.NewTraceID(0, 1), SpanID: model.NewSpanID(100)},
-		}
-		return &trace
-	}
-
 	tests := []struct {
 		name          string
 		httpEndpoint  string
@@ -841,8 +850,11 @@ func TestServerHTTP_TracesRequest(t *testing.T) {
 			})
 			tenancyMgr := tenancy.NewManager(&serverOptions.Tenancy)
 			querySvc := makeQuerySvc()
-			querySvc.spanReader.On("GetTrace", mock.AnythingOfType("*context.valueCtx"), spanstore.GetTraceParameters{TraceID: model.NewTraceID(0, 0x123456abc)}).
-				Return(makeMockTrace(t), nil).Once()
+			querySvc.traceReader.On("GetTraces", mock.Anything, mock.MatchedBy(func(params []tracestore.GetTraceParams) bool {
+				return len(params) == 1 && params[0].TraceID == v1adapter.FromV1TraceID(model.NewTraceID(0, 0x123456abc))
+			})).Return(iter.Seq2[[]ptrace.Traces, error](func(yield func([]ptrace.Traces, error) bool) {
+				yield([]ptrace.Traces{makeMockPTrace()}, nil)
+			})).Once()
 			telset := initTelSet(zaptest.NewLogger(t), tracerProvider)
 
 			server, err := NewServer(context.Background(), querySvc.qs,
