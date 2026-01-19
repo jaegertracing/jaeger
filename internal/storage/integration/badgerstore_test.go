776dfb39c7feb8e1808979ea9c49fa63e5c68dbe
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-01-19 12:32:16 +0200

    fix: integration test badger
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- internal/storage/integration/badgerstore_test.go
+++ internal/storage/integration/badgerstore_test.go
@@ -8,12 +8,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zaptest"
 
-	"github.com/jaegertracing/jaeger/internal/metrics"
 	v1badger "github.com/jaegertracing/jaeger/internal/storage/v1/badger"
 	"github.com/jaegertracing/jaeger/internal/storage/v2/badger"
+	"github.com/jaegertracing/jaeger/internal/telemetry"
 	"github.com/jaegertracing/jaeger/internal/testutils"
 )
 
@@ -26,7 +24,7 @@ func (s *BadgerIntegrationStorage) initialize(t *testing.T) {
 	cfg := v1badger.DefaultConfig()
 	cfg.Ephemeral = false
 	var err error
-	s.factory, err = badger.NewFactory(*cfg, metrics.NullFactory, zaptest.NewLogger(t, zaptest.WrapOptions(zap.AddCaller())))
+	s.factory, err = badger.NewFactory(*cfg, telemetry.NoopSettings())
 	require.NoError(t, err)
 	t.Cleanup(func() {
 		s.factory.Close()
