20c4bf139938114823f8f753a4e1ce88f822dcc9
Mahad Zaryab <mahadzaryab1@gmail.com>
2026-01-24 11:40:17 -0800

    Regenerate
    Signed-off-by: Mahad Zaryab <mahadzaryab1@gmail.com>

--- internal/storage/v2/clickhouse/tracestore/snapshots/TestFindTraces_WithFilters_2.sql
+++ internal/storage/v2/clickhouse/tracestore/snapshots/TestFindTraces_WithFilters_2.sql
@@ -86,32 +86,40 @@ WHERE s.trace_id IN (
 			AND s.start_time <= ?
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.bool_attributes.key, s.bool_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_bool_attributes.key, s.resource_bool_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_bool_attributes.key, s.resource_bool_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.double_attributes.key, s.double_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_double_attributes.key, s.resource_double_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_double_attributes.key, s.resource_double_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.int_attributes.key, s.int_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_int_attributes.key, s.resource_int_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_int_attributes.key, s.resource_int_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.complex_attributes.key, s.complex_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.complex_attributes.key, s.complex_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.complex_attributes.key, s.complex_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_complex_attributes.key, s.resource_complex_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.str_attributes.key, s.str_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.resource_str_attributes.key, s.resource_str_attributes.value)
-				OR arrayExists((key, value) -> key = ? AND value = ?, s.scope_str_attributes.key, s.scope_str_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.resource_str_attributes.key, s.resource_str_attributes.value)
+				OR 
+				arrayExists((key, value) -> key = ? AND value = ?, s.scope_str_attributes.key, s.scope_str_attributes.value)
 			)
 			AND (
 				arrayExists((key, value) -> key = ? AND value = ?, s.str_attributes.key, s.str_attributes.value)
