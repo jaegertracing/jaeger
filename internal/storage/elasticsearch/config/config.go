1a9fc9ebac3179c841f8b90fc78b6ee4922c4dce
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2026-02-25 17:29:32 -0500

    reduce ES default health-check timeout at startup in test
    Signed-off-by: Khiem Nguyen <ppkhiemnguyen@gmail.com>

--- internal/storage/elasticsearch/config/config.go
+++ internal/storage/elasticsearch/config/config.go
@@ -107,6 +107,8 @@ type Configuration struct {
 	Sniffing Sniffing               `mapstructure:"sniffing"`
 	// Disable the Elasticsearch health check
 	DisableHealthCheck bool `mapstructure:"disable_health_check"`
+	// Set the Elasticsearch health check timeout startup
+	HealthCheckTimeOutStartUp time.Duration `mapstruct:"health_check_timeout_startup"`
 	// SendGetBodyAs is the HTTP verb to use for requests that contain a body.
 	SendGetBodyAs string `mapstructure:"send_get_body_as"`
 	// QueryTimeout contains the timeout used for queries. A timeout of zero means no timeout.
@@ -580,6 +582,9 @@ func (c *Configuration) getESOptions(disableHealthCheck bool) []elastic.ClientOp
 	// Get base Elasticsearch options
 	options := []elastic.ClientOptionFunc{
 		elastic.SetURL(c.Servers...), elastic.SetSniff(c.Sniffing.Enabled), elastic.SetHealthcheck(!disableHealthCheck),
+	}
+	if c.HealthCheckTimeOutStartUp > 0 {
+		options = append(options, elastic.SetHealthcheckTimeoutStartup(c.HealthCheckTimeOutStartUp))
 	}
 	if c.Sniffing.UseHTTPS {
 		options = append(options, elastic.SetScheme("https"))
