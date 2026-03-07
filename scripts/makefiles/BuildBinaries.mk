8dba6bb1d38ad9fb027f221748070181fc155cc2
Yuri Shkuro <yurishkuro@users.noreply.github.com>
2026-03-07 15:07:17 -0500

    Apply suggestions from code review
    Signed-off-by: Yuri Shkuro <yurishkuro@users.noreply.github.com>

--- scripts/makefiles/BuildBinaries.mk
+++ scripts/makefiles/BuildBinaries.mk
@@ -150,9 +150,10 @@ _build-platform-binaries: \
 		build-esmapping-generator \
 		build-es-index-cleaner \
 		build-es-rollover
+# invoke make recursively such that DEBUG_BINARY=1 can take effect
 # skip debug builds if SKIP_DEBUG_BINARIES is set to 1 (e.g., during PRs to save CI time)
 ifneq ($(SKIP_DEBUG_BINARIES),1)
-_build-platform-binaries: _build-platform-binaries-debug
+	$(MAKE) _build-platform-binaries-debug GOOS=$(GOOS) GOARCH=$(GOARCH)
 endif
 
 # build binaries that support DEBUG release, for one specific platform GOOS/GOARCH
