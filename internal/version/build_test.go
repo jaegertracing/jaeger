3ac8c7585008a967e447994bce1ac227d16bf53b
chethanm99 <chethanm1399@gmail.com>
2026-03-13 16:35:31 +0530

    Add unit test for NewInfoMetrics function
    Signed-off-by: chethanm99 <chethanm1399@gmail.com>

--- internal/version/build_test.go
+++ internal/version/build_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGet(t *testing.T) {
@@ -32,4 +33,23 @@ func TestString(t *testing.T) {
 	}
 	expectedOutput := "git-commit=foobar, git-version=v1.2.3, build-date=2024-01-04"
 	assert.Equal(t, expectedOutput, test.String())
+}
+
+func TestNewInfoMetrics(t *testing.T) {
+	oldCommit := commitSHA
+	oldVersion := latestVersion
+	oldDate := date
+
+	t.Cleanup(func() {
+		commitSHA = oldCommit
+		latestVersion = oldVersion
+		date = oldDate
+	})
+
+	commitSHA = "foobar"
+	latestVersion = "v1.2.3"
+	date = "2024-01-04"
+
+	infoMetrics := NewInfoMetrics(nil)
+	require.NotNil(t, infoMetrics)
 }
