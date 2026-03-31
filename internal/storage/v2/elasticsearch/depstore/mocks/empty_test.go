7b0daea6484efbf748d2f686541a71a3ed57ee29
Makeepan Thanarasa <tmageepan@gmail.com>
2026-03-31 22:25:40 +0530

    tests: add empty_test.go to packages for coverage compliance
    Signed-off-by: Makeepan Thanarasa <tmageepan@gmail.com>

---
+++ internal/storage/v2/elasticsearch/depstore/mocks/empty_test.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package mocks
+
+// This file is added to satisfy the requirement of having at least one test file in each package.
---
+++ scripts/build/docker/debug/empty_test.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 The Jaeger Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package tools
+
+// This file is added to satisfy the requirement of having at least one test file in each package.
