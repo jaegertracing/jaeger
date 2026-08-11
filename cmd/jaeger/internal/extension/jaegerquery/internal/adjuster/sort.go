a8dcd8e3b9385a7e6eb11f7760b63e4cc8dd7eac
Ankita Salaria <255383093+Ankitavasudev@users.noreply.github.com>
2026-08-11 19:24:56 +0530

    fix: sort span events by timestamp instead of name
    Fixes #9212
    
    Signed-off-by: Ankita Salaria <ankitasalaria21@gmail.com>

--- cmd/jaeger/internal/extension/jaegerquery/internal/adjuster/sort.go
+++ cmd/jaeger/internal/extension/jaegerquery/internal/adjuster/sort.go
@@ -17,7 +17,7 @@ var _ Adjuster = (*SortAttributesAndEventsAdjuster)(nil)
 // - Resource attributes are sorted lexicographically by their keys.
 // - Scope attributes are sorted lexicographically by their keys.
 // - Span attributes are sorted lexicographically by their keys.
-// - Span events are sorted lexicographically by their names.
+// - Span events are sorted by timestamp.
 // - Attributes within each span event are sorted lexicographically by their keys.
 // - Attributes within each span link are sorted lexicographically by their keys.
 func SortCollections() SortAttributesAndEventsAdjuster {
@@ -79,7 +79,7 @@ func (SortAttributesAndEventsAdjuster) sortAttributes(attributes pcommon.Map) {
 
 func (s SortAttributesAndEventsAdjuster) sortEvents(events ptrace.SpanEventSlice) {
 	events.Sort(func(a, b ptrace.SpanEvent) bool {
-		return a.Name() < b.Name()
+		return a.TimeUnixNano() < b.TimeUnixNano()
 	})
 	for i := 0; i < events.Len(); i++ {
 		event := events.At(i)
