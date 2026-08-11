d242e1a4b1d77ac4add1a698902495e6d2f7ee74
Ankita Salaria <255383093+Ankitavasudev@users.noreply.github.com>
2026-08-11 17:25:42 +0530

    fix: sort span events by timestamp instead of name (#9212)
    Signed-off-by: Ankita Salaria <ankitasalaria21@gmail.com>

--- cmd/jaeger/internal/extension/jaegerquery/internal/adjuster/sort.go
+++ cmd/jaeger/internal/extension/jaegerquery/internal/adjuster/sort.go
@@ -1,4 +1,5 @@
 Signed-off-by: Ankita Salaria <ankitasalaria21@gmail.com>
+Signed-off-by: Ankita Salaria <ankitasalaria21@gmail.com>
 // Copyright (c) 2024 The Jaeger Authors.
 // SPDX-License-Identifier: Apache-2.0
 
