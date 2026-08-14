3fbd1af38740ca4a36dfa12f3f1c9a049a0ddaf2
Sarva Dubey <sarvadubey@gmail.com>
2026-08-14 06:07:13 +0530

    test(es): Cover native summary HTTP 5xx errors
    Signed-off-by: Sarva Dubey <sarvadubey@gmail.com>

--- cmd/jaeger/internal/integration/elasticsearch_test.go
+++ cmd/jaeger/internal/integration/elasticsearch_test.go
@@ -4,10 +4,19 @@
 package integration
 
 import (
+	"context"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+
+	"github.com/jaegertracing/jaeger/internal/jiter"
 	"github.com/jaegertracing/jaeger/internal/storage/integration"
 	"github.com/jaegertracing/jaeger/internal/storage/integration/capabilities"
+	"github.com/jaegertracing/jaeger/internal/storage/v2/api/tracestore"
+	"github.com/jaegertracing/jaeger/internal/telemetry/otelsemconv"
 )
 
 func TestElasticsearchStorage(t *testing.T) {
@@ -23,6 +32,74 @@ func TestElasticsearchStorage(t *testing.T) {
 	}
 	s.e2eInitialize(t, "elasticsearch")
 	s.RunSpanStoreTests(t)
+}
+
+func TestElasticsearchStorage_NativeTraceSummariesHTTP5xx(t *testing.T) {
+	integration.SkipUnlessEnv(t, integration.StorageElasticsearch)
+
+	s := &E2EStorageIntegration{
+		ConfigFile: "../../config-elasticsearch.yaml",
+		StorageIntegration: integration.StorageIntegration{
+			CleanUp:      purge,
+			Capabilities: capabilities.Elasticsearch(),
+		},
+	}
+	s.e2eInitialize(t, "elasticsearch")
+	purge(t)
+
+	const serviceName = "native-summary-http-5xx"
+	traceID := pcommon.TraceID{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	spanID := pcommon.SpanID{1, 2, 3, 4, 5, 6, 7, 8}
+	start := time.Now().UTC()
+
+	traces := ptrace.NewTraces()
+	resourceSpans := traces.ResourceSpans().AppendEmpty()
+	resourceSpans.Resource().Attributes().PutStr(otelsemconv.ServiceNameKey, serviceName)
+	span := resourceSpans.ScopeSpans().AppendEmpty().Spans().AppendEmpty()
+	span.SetTraceID(traceID)
+	span.SetSpanID(spanID)
+	span.SetKind(ptrace.SpanKindServer)
+	span.SetName("GET /failure")
+	span.SetStartTimestamp(pcommon.NewTimestampFromTime(start))
+	span.SetEndTimestamp(pcommon.NewTimestampFromTime(start.Add(time.Millisecond)))
+	span.Attributes().PutInt(otelsemconv.HTTPResponseStatusCodeKey, 500)
+
+	ctx := context.Background()
+	require.NoError(t, s.TraceWriter.WriteTraces(ctx, traces))
+
+	query := tracestore.TraceQueryParams{
+		ServiceName:  serviceName,
+		Attributes:   pcommon.NewMap(),
+		StartTimeMin: start.Add(-time.Minute),
+		StartTimeMax: start.Add(time.Minute),
+		SearchDepth:  10,
+	}
+
+	var summary *tracestore.TraceSummary
+	require.Eventually(t, func() bool {
+		batches, err := jiter.FlattenWithErrors(s.TraceReader.FindTraceSummaries(ctx, query))
+		if err != nil {
+			t.Logf("FindTraceSummaries failed: %v", err)
+			return false
+		}
+		for i := range batches {
+			if batches[i].TraceID == traceID {
+				candidate := batches[i]
+				summary = &candidate
+				break
+			}
+		}
+		if summary == nil {
+			return false
+		}
+		return summary.SpanCount == 1
+	}, 30*time.Second, time.Second)
+
+	require.NotNil(t, summary)
+	require.Equal(t, 1, summary.ErrorSpanCount)
+	require.Len(t, summary.Services, 1)
+	require.Equal(t, serviceName, summary.Services[0].Name)
+	require.Equal(t, 1, summary.Services[0].ErrorSpanCount)
 }
 
 func TestElasticsearchStorage_ManualRollover(t *testing.T) {
